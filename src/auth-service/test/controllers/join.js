e8416440ebcd1a5cfeab548a2bf22e94cfe8e212
baalmart <bbaalmart@gmail.com>
2019-10-14 13:39:59 +0300

    coverage and testing tools - using codecov+instabul for code coverage

---
+++ src/auth-service/test/controllers/join.js
@@ -0,0 +1,41 @@
+const assert = require('assert');
+const expect = require('chai').expect;
+const should = require('chai').should;
+
+
+//testing
+describe('basic string test', function () {
+    it('should return number of characters in a string', function () {
+        assert.equal("Hello".length, 5);
+    });
+
+    it('should return first character of the string', function () {
+        assert.equal("Hello".charAt(0), 'H');
+    });
+})
+
+
+// sign up a user
+describe("signup a user", () => {
+    //true case
+    it("", () => { });
+    //false case
+    it("", () => { });
+});
+
+//sign in a user
+describe("login a user", () => {
+    // true case
+    it("", () => { });
+    //false case
+    it("", () => { });
+});
+
+
+//get all the users
+describe("get all users", () => {
+    //true case
+    it("", () => { });
+    //false case
+    it("", () => { });
+});
