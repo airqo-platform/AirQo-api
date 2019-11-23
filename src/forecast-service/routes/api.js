4f4dac4e998714c505222a789740708de9133703
baalmart <bbaalmart@gmail.com>
2019-11-23 04:35:14 +0300

    the end point for getting the most recent entry for each channel

--- src/forecast-service/routes/api.js
+++ src/forecast-service/routes/api.js
@@ -6,6 +6,7 @@ middlewareConfig(router);
 
 router.post('/', forecastController.forecast);
 router.get('/channels', forecastController.storeChannels);
-router.get('/feeds/:id', forecastController.storeFeeds);
+router.get('/feeds/:ch_id', forecastController.storeFeeds);
+router.get('/feeds/recent/:ch_id', forecastController.getLastEntry);
 
 module.exports = router;
