f8c6217d8a0caf259ff511e68e4fa3d085bef8d7
baalmart <bbaalmart@gmail.com>
2020-01-09 18:08:42 +0300

    the using the http bridge to publish events

--- src/device-registry/controllers/http-bridge.js
+++ src/device-registry/controllers/http-bridge.js
@@ -1,16 +1,42 @@
 const http_bridge = require('../utils/http-bridge');
+const http_client = require('../config/http-client');
 
 const bridge = {
 
     publish: (req, res) => {
-
+        const deviceId = req.body.id;
+        const messageType = req.body.type
+        try {
+            http_bridge.publishAsync(1, deviceId, messageType);
+            res.status(200).send(`data sent for device with the ID: ${deviceId}`);
+        } catch (error) {
+            res.send(`data has not be sent to the device with ID: ${deviceId}`);
+        }
     },
-    updateConfigs: (req, res) => {
 
+    getConfigs: (req, res) => {
+        const version = req.body.version; // v1
+        const deviceId = req.body.id;
+        try {
+            const data = http_client.getConfig(version, deviceId);
+            res.status(200).send(data);
+        }
+        catch (error) {
+            res.status(500).send(error);
+        }
+    },
+
+    updateConfigs: (req, res) => {
+        const data = req.body.data;
+        const deviceId = req.body.id;
+        const data = http_client.updateConfig(data, deviceId);
+        res.send(data).status(200);
     },
 
     reviewConfigs: (req, res) => {
-
+        const deviceId = req.body.id;
+        data = http_client.reviewConfig(deviceId);
+        res.send(data).status(200);
     }
 
 }
--- src/device-registry/utils/http-bridge.js
+++ src/device-registry/utils/http-bridge.js
@@ -1,24 +1,13 @@
-const iot = require('@google-cloud/iot');
-const client = new iot.v1.DeviceManagerClient();
-const device_registry = 'projects/airqo-250220/locations/europe-west1/registries/device-registry';
-const uuidv1 = require('uuid/v1');
-const mqtt = require('mqtt');
-const projectId = 'airqo-250220';
-const region = `europe-west1`;
-const registryId = `device-registry`;
-const algorithm = `RS256`;
-const privateKeyFile = `./rsa_private.pem`;
-const mqttBridgeHostname = `mqtt.googleapis.com`;
-const mqttBridgePort = 8883;
-const messageType = `events`;
-const numMessages = 5;
+const privateKeyFile = require('../controllers/rsa_private.pem');
 const fs = require('fs');
 const jwt = require('jsonwebtoken');
-const tokenExpMins = 360;
-const httpBridgeAddress = `cloudiotdevice.googleapis.com`;
+const constants = require('../config/constants');
+const request = require('retry-request', { request: require('request') });
+
 
 // Create a Cloud IoT Core JWT for the given project ID, signed with the given
 // private key.
+
 // [START iot_http_jwt]
 const createJwt = (projectId, privateKeyFile, algorithm) => {
     // Create a JWT to authenticate this device. The device will be disconnected
@@ -27,134 +16,96 @@ const createJwt = (projectId, privateKeyFile, algorithm) => {
     const token = {
         iat: parseInt(Date.now() / 1000),
         exp: parseInt(Date.now() / 1000) + 20 * 60, // 20 minutes
-        aud: projectId,
+        aud: projectId
     };
     const privateKey = fs.readFileSync(privateKeyFile);
     return jwt.sign(token, privateKey, { algorithm: algorithm });
-};
+}
 // [END iot_http_jwt]
 
-
-// [START iot_http_variables]
-let iatTime = parseInt(Date.now() / 1000);
-
-const authToken = createJwt(
-    projectId,
-    privateKeyFile,
-    algorithm
-);
-
-const devicePath = `projects/${projectId}/locations/${region}/registries/${registryId}/devices/${deviceId}`;
-
-// The request path, set accordingly depending on the message type.
-const pathSuffix =
-    essageType === 'events' ? ':publishEvent' : ':setState';
-const urlBase = `https://${httpBridgeAddress}/v1/${devicePath}`;
-const url = `${urlBase}${pathSuffix}`;
-// [END iot_http_variables]
-
-
-const bridge = {
-    publishAsync: (authToken, messageCount, numMessages) => {
-        const payload = `${registryId}/${deviceId}-payload-${messageCount}`;
-        console.log('Publishing message:', payload);
-        const binaryData = Buffer.from(payload).toString('base64');
-        const postData =
-            messageType === 'events'
-                ? {
-                    binary_data: binaryData,
-                }
-                : {
-                    state: {
-                        binary_data: binaryData,
-                    },
-                };
-
-        const options = {
-            url: url,
-            headers: {
-                authorization: `Bearer ${authToken}`,
-                'content-type': 'application/json',
-                'cache-control': 'no-cache',
-            },
-            body: postData,
-            json: true,
-            method: 'POST',
-            retries: 5,
-            shouldRetryFn: function (incomingHttpMessage) {
-                return incomingHttpMessage.statusMessage !== 'OK';
-            },
-        };
-
-        // Send events for high-frequency updates, update state only occasionally.
-        const delayMs = messageType === 'events' ? 1000 : 2000;
-        console.log(JSON.stringify(request));
-        request(options, (error, response) => {
-            if (error) {
-                console.error('Received error: ', error);
-            } else if (response.body.error) {
-                console.error(`Received error: ${JSON.stringify(response.body.error)}`);
-            } else {
-                console.log('Message sent.');
-            }
-            if (messageCount < numMessages) {
-                // If we have published fewer than numMessage messages, publish payload
-                // messageCount + 1.
-                setTimeout(() => {
-                    const secsFromIssue = parseInt(Date.now() / 1000) - iatTime;
-                    if (secsFromIssue > tokenExpMins * 60) {
-                        iatTime = parseInt(Date.now() / 1000);
-                        console.log(`\tRefreshing token after ${secsFromIssue} seconds.`);
-                        authToken = createJwt(
-                            projectId,
-                            privateKeyFile,
-                            algorithm
-                        );
-                    }
-
-                    publishAsync(authToken, messageCount + 1, numMessages);
-                }, delayMs);
-            }
-        });
-    },
-    // [END iot_http_publish]
-
-    // [START iot_http_getconfig]
-    getConfig: (authToken, version) => {
-        console.log(`Getting config from URL: ${urlBase}`);
-
-        const options = {
-            url: `${urlBase}/config?local_version=${version}`,
-            headers: {
-                authorization: `Bearer ${authToken}`,
-                'content-type': 'application/json',
-                'cache-control': 'no-cache',
-            },
-            json: true,
-            retries: 5,
-            shouldRetryFn: function (incomingHttpMessage) {
-                console.log('Retry?');
-                return incomingHttpMessage.statusMessage !== 'OK';
-            },
-        };
-        console.log(JSON.stringify(request.RetryStrategies));
-        request(options, (error, response, body) => {
-            if (error) {
-                console.error('Received error: ', error);
-            } else if (response.body.error) {
-                console.error(`Received error: ${JSON.stringify(response.body.error)}`);
-            } else {
-                console.log('Received config', JSON.stringify(body));
-            }
-        });
-    },
-    // [END iot_http_getconfig]
-
-
-    updateConfigs: () => {
-
-    }
-
+const httpVariables = (messageType, deviceId) => {
+    const devicePath = `projects/${process.env.PROJECT_ID}/locations/
+        ${process.env.REGION}/registries/${process.env.REGISTRY_ID}/devices/${deviceId}`;
+    //request path depending on the message type
+    const pathSuffix = messageType === 'events' ? ':publishEvent' : ':setState';
+    const urlBase = `https://${constants.HTTP_BRIDGE_ADDRESS}/v1/${devicePath}`;
+    const url = `${urlBase}${pathSuffix}`;
+    return url;
 }
 
-module.exports = bridge;
+//publish numMessages message asynchronously, starting from message
+//messageCount. Telemetry events are published at a rate of 1 per second and states at a rate of 1 every 2 seconds.
+// [START iot_http_publish]
+const publishAsync = (messageCount, deviceId, messageType) => {
+    const url = httpVariables(messageType, deviceId);
+    let iatTime = parseInt(Date.now() / 1000);
+    const authToken = createJwt(
+        process.env.PROJECT_ID,
+        privateKeyFile,
+        constants.ALGORITHM
+    );
+    const payload = `${process.env.REGISTRY_ID}/${deviceId}-payload-${messageCount}`;
+    console.log('Publishing message:', payload);
+    const binaryData = Buffer.from(payload).toString('base64');
+    const postData =
+        messageType === 'events'
+            ? {
+                binary_data: binaryData,
+            }
+            : {
+                state: {
+                    binary_data: binaryData,
+                },
+            };
+
+    const options = {
+        url: url,
+        headers: {
+            authorization: `Bearer ${authToken}`,
+            'content-type': 'application/json',
+            'cache-control': 'no-cache',
+        },
+        body: postData,
+        json: true,
+        method: 'POST',
+        retries: 5,
+        shouldRetryFn: function (incomingHttpMessage) {
+            return incomingHttpMessage.statusMessage !== 'OK';
+        },
+    };
+
+    // Send events for high-frequency updates, update state only occasionally.
+    const delayMs = messageType === 'events' ? 1000 : 2000;
+    console.log(JSON.stringify(request));
+    request(options, (error, response) => {
+        if (error) {
+            console.error('Received error: ', error);
+        } else if (response.body.error) {
+            console.error(`Received error: ${JSON.stringify(response.body.error)}`);
+        } else {
+            console.log('Message sent.');
+        }
+        if (messageCount < constants.NUM_MESSAGES) {
+            // If we have published fewer than numMessage messages, publish payload
+            // messageCount + 1.
+            setTimeout(() => {
+                const secsFromIssue = parseInt(Date.now() / 1000) - iatTime;
+                if (secsFromIssue > constants.TOKEN_EXP_MINS * 60) {
+                    iatTime = parseInt(Date.now() / 1000);
+                    console.log(`\tRefreshing token after ${secsFromIssue} seconds.`);
+                    authToken = createJwt(
+                        process.env.PROJECT_ID,
+                        privateKeyFile,
+                        constants.ALGORITHM
+                    );
+                }
+
+                publishAsync(authToken, messageCount + 1, constants.NUM_MESSAGES);
+            }, delayMs);
+        }
+    });
+}
+// [END iot_http_publish]
+
+
+module.exports = { publishAsync: publishAsync, createJwt: createJwt };
