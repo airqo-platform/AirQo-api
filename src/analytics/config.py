0adabfc01badb4ae1a6b0daace633dfa3ab04151
Sserunjogi Richard <sserurich@gmail.com>
2020-03-11 15:52:53 +0300

    initial code commit

---
+++ src/analytics/config.py
@@ -0,0 +1,66 @@
+import logging
+from logging.handlers import TimedRotatingFileHandler
+import pathlib
+import os
+import sys
+
+PACKAGE_ROOT = pathlib.Path(__file__).resolve().parent.parent
+
+FORMATTER = logging.Formatter(
+    "%(asctime)s — %(name)s — %(levelname)s —"
+    "%(funcName)s:%(lineno)d — %(message)s")
+
+def get_console_handler():
+    console_handler = logging.StreamHandler(sys.stdout)
+    console_handler.setFormatter(FORMATTER)
+    return console_handler
+
+
+def get_file_handler():
+    file_handler = TimedRotatingFileHandler(
+        LOG_FILE, when='midnight')
+    file_handler.setFormatter(FORMATTER)
+    file_handler.setLevel(logging.WARNING)
+    return file_handler
+
+
+def get_logger(*, logger_name):
+    """Get logger with prepared handlers."""
+
+    logger = logging.getLogger(logger_name)
+
+    logger.setLevel(logging.DEBUG)
+
+    logger.addHandler(get_console_handler())
+    logger.addHandler(get_file_handler())
+    logger.propagate = False
+
+    return logger
+
+
+class Config:
+    DEBUG = False
+    TESTING = False
+    CSRF_ENABLED = True
+    SECRET_KEY = 'sample-code-local-environment'
+    #SERVER_PORT = 5000
+
+
+class ProductionConfig(Config):
+    DEBUG = False
+    #SERVER_PORT = os.environ.get('PORT', 5000)
+
+
+class DevelopmentConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+
+class TestingConfig(Config):
+    TESTING = True
+
+
+if __name__ == '__main__':
+    print('package root', PACKAGE_ROOT)
+
+
