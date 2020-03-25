e77f35d6325029dcb870435070a1090bf4139cf8
Martin <bbaalmart@gmail.com>
2020-03-25 10:38:53 +0300

    using mongo ATLAS in config and refactoring service name

--- src/data-mgt/config/constants.js
+++ src/data-mgt/config/constants.js
@@ -1,28 +1,29 @@
 const devConfig = {
-    MONGO_URL: 'mongodb://localhost/forecast-dev',
-    JWT_SECRET: 'thisisasecret'
+  MONGO_URL: "mongodb://localhost/data-mgt-dev",
+  JWT_SECRET: "thisisasecret"
 };
 const testConfig = {
-    MONGO_URL: 'mongodb://localhost/forecast-test',
-    JWT_SECRET: 'thisisasecret'
+  MONGO_URL: "mongodb://localhost/data-mgt-test",
+  JWT_SECRET: "thisisasecret"
 };
 const prodConfig = {
-    MONGO_URL: `mongodb://${process.env.MLAB_USERNAME}:${process.env.MLAB_PASSWORD}@ds245532.mlab.com:45532/airqo-forecast`,
-    JWT_SECRET: 'thisisasecret'
+  //   MONGO_URL: `mongodb://${process.env.MLAB_USERNAME}:${process.env.MLAB_PASSWORD}@ds245532.mlab.com:45532/airqo-forecast`,
+  MONGO_URL: `mongodb+srv://mato:${process.env.ATLAS_PASSWORD}@cluster0-z8365.gcp.mongodb.net/test?retryWrites=true&w=majority`,
+  JWT_SECRET: "thisisasecret"
 };
 const defaultConfig = {
-    PORT: process.env.PORT || 3000,
+  PORT: process.env.PORT || 3000
 };
 
 function envConfig(env) {
-    switch (env) {
-        case 'development':
-            return devConfig;
-        case 'test':
-            return testConfig;
-        default:
-            return prodConfig;
-    }
+  switch (env) {
+    case "development":
+      return devConfig;
+    case "test":
+      return testConfig;
+    default:
+      return prodConfig;
+  }
 }
 
-module.exports = { ...defaultConfig, ...envConfig(process.env.NODE_ENV), };
+module.exports = { ...defaultConfig, ...envConfig(process.env.NODE_ENV) };
