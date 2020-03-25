71a8bb91fc468c2f2a6b5fef0a918df4082baf19
Martin <bbaalmart@gmail.com>
2020-03-25 10:40:59 +0300

    CRUD endpoints for GPS coordinates

--- src/data-mgt/controllers/manager.js
+++ src/data-mgt/controllers/manager.js
@@ -1,80 +1,116 @@
-const HTTPStatus = require('http-status');
-const fetch = require('node-fetch');
-const request = require('request');
-const Channels = require('../models/Channel');
-const Feed = require('../models/Feed');
+const HTTPStatus = require("http-status");
+const fetch = require("node-fetch");
+const request = require("request");
+const Channels = require("../models/Channel");
+const Feed = require("../models/Feed");
+const DeviceGPS = require("../models/DeviceGPSValues");
 
 async function asyncForEach(array, callback) {
-    for (let index = 0; index < array.length; index++) {
-        await callback(array[index], index, array);
-    }
+  for (let index = 0; index < array.length; index++) {
+    await callback(array[index], index, array);
+  }
 }
 
 const data = {
-
-    prediction: (req, res) => {
-        let payload = req.body;
-        let forecast = {
-            "PM2.5": 132,
-            "PM10": 234,
-            "location": {
-                "longitude": payload.location.longitude,
-                "latitude": payload.location.latitude,
-            },
-            "type": payload.type,
-            "units": payload.units
-        }
-        res.json(forecast);
-    },
-
-    getLocationDetails: (req, res) => {
-        let lat = req.body.lat;
-        let lon = req.body.lon;
-    },
-
-    storeChannels: async (req, res) => {
-        const api_url_channels = `https://api.thingspeak.com/channels.json?api_key=${process.env.TS_API_KEY}`;
-        let fetch_response = await fetch(api_url_channels);
-        let json = await fetch_response.json();
-        res.status(200).send(json);
-    },
-    storeFeeds: async (req, res) => {
-        const api_url = `https://api.thingspeak.com/channels/${req.params.ch_id}/feeds.json`;
-        const fetch_response = await fetch(api_url);
-        const json = await fetch_response.json();
-        res.status(200).send(json);
-    },
-
-    getLastEntry: async (req, res) => {
-        try {
-            const api_url = `https://api.thingspeak.com/channels/${req.params.ch_id}/feeds.json`;
-            let fetch_response = await fetch(api_url);
-            let json = await fetch_response.json();
-            let entry = json.channel.last_entry_id;
-            let feed = await json.feeds.filter((obj) => {
-                return obj.entry_id === entry;
-            });
-            res.status(200).json(feed[0]);
-        }
-        catch (e) {
-            res.status(501).send(e.message);
-        }
-    },
-
-    weatherForecasts: async (req, res) => {
-
-        const lat = req.params.lat;
-        const lon = req.params.lon;
-
-
-
-        try {
-
-        }
-        catch (error) {
-
-        }
+  getLocationDetails: async (req, res) => {
+    try {
+      const cods = await DeviceGPS.find({ channel_id: req.params.id });
+      return res.status(HTTPStatus.OK).json(cods);
+    } catch (e) {
+      return res.status(HTTPStatus.BAD_REQUEST).json(e);
     }
-}
+  },
+
+  updateLocationDetails: async (req, res) => {
+    try {
+      let filter = { channel_id: req.params.id };
+      let update = {
+        latitude: req.params.latitude,
+        longitude: req.params.longitude
+      };
+      let feedback = await DeviceGPS.findOneAndUpdate(filter, update, {
+        new: true,
+        upsert: true,
+        rawResult: true
+      });
+
+      return res.status(HTTPStatus.OK).json();
+    } catch (e) {
+      return res.status(HTTPStatus.BAD_REQUEST).json(e);
+    }
+  },
+
+  insertLocationDetails: async (req, res) => {
+    try {
+      const cods = new DeviceGPS(req.body);
+      cods.save((error, savedData) => {
+        if (error) {
+          return res.status(HTTPStatus.INTERNAL_SERVER_ERROR).json(error);
+        } else {
+          return res.status(HTTPStatus.OK).json(savedData);
+        }
+      });
+    } catch (e) {
+      return res.status(HTTPStatus.BAD_REQUEST).json(e);
+    }
+  },
+
+  deleteLocationDetails: async (req, res) => {
+    try {
+      let filter = { channel_id: req.params.id };
+      DeviceGPS.deleteOne(filter, err => {
+        if (err) {
+          return handleError(err);
+        } else {
+          return res.status(HTTPStatus.OK);
+        }
+      });
+    } catch (e) {
+      return res.status(HTTPStatus.BAD_REQUEST).json(e);
+    }
+  },
+
+  storeChannels: async (req, res) => {
+    const api_url_channels = `https://api.thingspeak.com/channels.json?api_key=${process.env.TS_API_KEY}`;
+    let fetch_response = await fetch(api_url_channels);
+    let json = await fetch_response.json();
+    res.status(200).send(json);
+  },
+
+  storeFeeds: async (req, res) => {
+    const api_url = `https://api.thingspeak.com/channels/${req.params.ch_id}/feeds.json`;
+    const fetch_response = await fetch(api_url);
+    const json = await fetch_response.json();
+    res.status(200).send(json);
+  },
+
+  getLastEntry: async (req, res) => {
+    try {
+      const api_url = `https://api.thingspeak.com/channels/${req.params.ch_id}/feeds.json`;
+      let fetch_response = await fetch(api_url);
+      let json = await fetch_response.json();
+      let entry = json.channel.last_entry_id;
+      let feed = await json.feeds.filter(obj => {
+        return obj.entry_id === entry;
+      });
+
+      if (feed[0].field5 === 0.0 || feed[0].field6 === 0.0) {
+        //get the device's stored GPS values and return them
+      } else {
+        res.status(200).json(feed[0]);
+      }
+    } catch (e) {
+      res.status(501).send(e.message);
+    }
+  },
+
+  weatherForecasts: async (req, res) => {
+    const lat = req.params.lat;
+    const lon = req.params.lon;
+
+    try {
+    } catch (error) {}
+  }
+};
 
 module.exports = data;
--- src/data-mgt/routes/api.js
+++ src/data-mgt/routes/api.js
@@ -1,12 +1,17 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const manager = require('../controllers/manager');
-const middlewareConfig = require('../config/router.middleware');
+const manager = require("../controllers/manager");
+const middlewareConfig = require("../config/router.middleware");
 middlewareConfig(router);
 
-router.post('/', manager.prediction);
-router.get('/channels', manager.storeChannels);
-router.get('/feeds/:ch_id', manager.storeFeeds);
-router.get('/feeds/recent/:ch_id', manager.getLastEntry);
+router.get("/channels", manager.storeChannels);
+router.get("/feeds/:ch_id", manager.storeFeeds);
+router.get("/feeds/recent/:ch_id", manager.getLastEntry);
+
+//location of devices
+router.get("/channels/gps", manager.getLocationDetails);
+router.post("/channels/gps", manager.updateLocationDetails);
+router.put("/channels/gps", manager.insertLocationDetails);
+router.delete("/channels/gps", manager.deleteLocationDetails);
 
 module.exports = router;
