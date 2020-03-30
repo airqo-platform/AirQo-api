ad915c4e3d6c86f1d09d23c16415eb7e29e2f049
Martin <bbaalmart@gmail.com>
2020-03-30 08:09:15 +0300

    new controller functions for the use case

--- src/auth-service/controllers/join.js
+++ src/auth-service/controllers/join.js
@@ -1,7 +1,12 @@
 const User = require("../models/User");
+const Collaborator = require("../models/Collaborator");
 const HTTPStatus = require("http-status");
 const transport = require("../services/mailer");
 const constants = require("../config/constants");
+const privileges = require("../utils/privileges");
+const sendEmail = require("../services/mailer");
+const templates = require("../utils/email.templates");
+const msgs = require("../utils/email.msgs");
 
 const join = {
   listAll: async (req, res) => {
@@ -22,24 +27,40 @@ const join = {
     }
   },
 
+  findUserById: (req, res, next, id) => {
+    User.findById(id).exec((err, user) => {
+      if (err || !user) {
+        return res.status(400).json({
+          error: "No user found with these credentials!"
+        });
+      }
+      req.profile = user;
+      next();
+    });
+  },
+
   register: (req, res) => {
-    const {} = req.body;
+    const {
+      email,
+      firstName,
+      lastName,
+      userName,
+      password,
+      privilege
+    } = req.body;
 
-    const MAIL_OPTIONS = {
-      from: "welcome@airqo.net",
-      to: "receiver_email@service.com",
-      subject: "This is subject",
-      text: "This is email content"
-    };
+    console.log(process.env.ATLAS_URI);
 
-    console.log(process.env.MLAB_URI);
     try {
       const user = new User(req.body);
       user.save((error, savedData) => {
         if (error) {
           return res.status(500).json(error);
         } else {
-          return res.status(201).json(savedData);
+          //sending the confirmation email to the user
+          sendEmail(email, templates.confirm(savedData._id));
+          // return res.status(201).json(savedData);
+          return res.status(201).json({ msg: msgs.confirm });
         }
       });
     } catch (e) {
@@ -47,12 +68,120 @@ const join = {
     }
   },
 
-  login: (req, res, next) => {
+  //invoked when the user visits the confirmation url on the client
+  confirmEmail: async (req, res) => {
+    const { id } = req.params;
+    User.findById(id)
+      .then(user => {
+        //when the user does not exist in the DB
+        if (!user) {
+          res.json({ msg: msgs.couldNotFind });
+        }
+        // The user exists but has not been confirmed. So we confirm them...
+        else if (user && !user.emailConfirmed) {
+          User.findByIdAndUpdate(id, { confirmed: true })
+            .then(() => res.json({ msg: msgs.confirmed }))
+            .catch(err => console.log(err));
+        }
+        //when the user has already confirmed their email address
+        else {
+          res.json({ msg: msgs.alreadyConfirmed });
+        }
+      })
+      .catch(err => console.log(err));
+  },
+
+  loginUser: (req, res, next) => {
+    console.log("we have reached loginUser....");
     res.status(200).json(req.user.toAuthJSON());
     return next();
   },
 
-  addCollaborator: () => {}
+  loginCollaborator: (req, res, next) => {
+    res.status(200).json(req.colab.toAuthJSON());
+    return next();
+  },
+
+  deleteUser: (req, res, next) => {
+    let query = { _id: req.params.id };
+    User.findOneAndDelete(query, (error, response) => {
+      if (error) {
+        return res
+          .status(400)
+          .json({ error: errorHandler.getErrorMessage(err) });
+      } else {
+        res.status(200).send("user deleted");
+      }
+    });
+  },
+
+  updateUser: (req, res, next) => {
+    let query = { _id: req.params.id };
+    let updateDetails = req.body;
+    User.findOneAndUpdate(query, updateDetails, (error, response) => {
+      if (error) {
+      } else if (response) {
+      } else {
+      }
+    });
+  },
+
+  updateCollaborator: (req, res, next) => {
+    let query = { _id: req.params.id };
+    let updateDetails = req.body;
+    Collaborator.findOneAndUpdate(query, updateDetails, (error, response) => {
+      if (error) {
+        return res.status(HTTPStatus.BAD_GATEWAY).json(e);
+      } else if (response) {
+        return res.status(HTTPStatus.OK).json(response);
+      } else {
+        return res.status(HTTPStatus.BAD_REQUEST);
+      }
+    });
+  },
+  addCollaborator: async (req, res, next) => {
+    try {
+      //get the ID of the one requesting:
+      let id = req.params.id;
+      let colab = new Collaborator(req.body);
+      await colab.save((error, savedData) => {
+        if (error) {
+          return res.status(500).json(error);
+        } else {
+          return res.status(201).json(savedData);
+        }
+      });
+    } catch (e) {
+      return res.status(HTTPStatus.BAD_REQUEST).json(e);
+    }
+  },
+
+  deleteCollaborator: (req, res, next) => {
+    try {
+      let query = { _id: req.params.id };
+      Collaborator.findOneAndDelete(query, (error, response) => {
+        if (error) {
+          return res
+            .status(400)
+            .json({ error: errorHandler.getErrorMessage(err) });
+        } else {
+          res.status(200).send("user deleted");
+        }
+      });
+    } catch (e) {
+      return res.status(HTTPStatus.BAD_REQUEST).json(e);
+    }
+  },
+
+  logout: async (req, res) => {
+    try {
+      await req.logout();
+      req.session = null;
+      return res.status(HTTPStatus.OK).send("successfully logged out");
+    } catch (e) {
+      return res.status(HTTPStatus.BAD_GATEWAY).json(e);
+    }
+  }
 };
 
 module.exports = join;
