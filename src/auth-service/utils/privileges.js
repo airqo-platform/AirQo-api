92b0d7250eff5194e60d767665d2809264e11a0b
Martin <bbaalmart@gmail.com>
2020-03-30 08:07:14 +0300

    additional access control checks

--- src/auth-service/utils/privileges.js
+++ src/auth-service/utils/privileges.js
@@ -1,4 +1,5 @@
-const User = require("../model/User");
+const User = require("../models/User");
+const Colaborator = require("../models/Collaborator");
 
 const privileges = {
   //checking to see if the user is normal....ideally an admin should be able to do all that normal user can also do hence...
@@ -86,7 +87,7 @@ const privileges = {
     });
   },
 
-  IsNetworkAdmin: (req, res, next) => {
+  isNetworkAdmin: (req, res, next) => {
     let user_id = req.params.id;
     User.findById(user_id, (error, userDetails) => {
       if (error) {
@@ -107,7 +108,7 @@ const privileges = {
     });
   },
 
-  IsPrivate: (req, res, next) => {
+  isPrivate: (req, res, next) => {
     let user_id = req.params.id;
     User.findById(user_id, (error, userDetails) => {
       if (error) {
@@ -128,7 +129,7 @@ const privileges = {
     });
   },
 
-  IsUniversity: (req, res, next) => {
+  isUniversity: (req, res, next) => {
     let user_id = req.params.id;
     User.findById(user_id, (error, userDetails) => {
       if (error) {
@@ -149,7 +150,7 @@ const privileges = {
     });
   },
 
-  IsAirQoHost: (req, res, next) => {
+  isAirQoHost: (req, res, next) => {
     let user_id = req.params.id;
     User.findById(user_id, (error, userDetails) => {
       if (error) {
@@ -170,7 +171,7 @@ const privileges = {
     });
   },
 
-  IsMedia: (req, res, next) => {
+  isMedia: (req, res, next) => {
     let user_id = req.params.id;
     User.findById(user_id, (error, userDetails) => {
       if (error) {
@@ -191,7 +192,7 @@ const privileges = {
     });
   },
 
-  IsInstitution: (req, res, next) => {
+  isInstitution: (req, res, next) => {
     let user_id = req.params.id;
     User.findById(user_id, (error, userDetails) => {
       if (error) {
@@ -208,6 +209,65 @@ const privileges = {
           response.message = "unauthorized operation";
           res.status(401).send(response);
         }
+      }
+    });
+  },
+
+  isColabAdmin: (req, res, next) => {
+    let user_id = req.params.id;
+    Colaborator.findById(user_id, (error, userDetails) => {
+      if (error) {
+        let response = {};
+        response.success = false;
+        response.message = "internal server error";
+        res.status(500).send(response);
+      } else {
+        if (userDetails.admin === user_id) {
+          next();
+        } else {
+          let response = {};
+          response.success = false;
+          response.message = "unauthorized operation";
+          res.status(401).send(response);
+        }
+      }
+    });
+  },
+
+  isUser: (req, res, next) => {
+    let user_id = req.params.id;
+    User.findById(user_id, (error, userDetails) => {
+      if (error) {
+        let response = {};
+        response.success = false;
+        response.message = "internal server error";
+        res.status(500).send(response);
+      } else if (userDetails) {
+        next();
+      } else {
+        let response = {};
+        response.success = false;
+        response.message = "bad request?";
+        res.status(401).send(response);
+      }
+    });
+  },
+
+  isCollab: (req, res, next) => {
+    let user_id = req.params.id;
+    Colaborator.findById(user_id, (error, userDetails) => {
+      if (error) {
+        let response = {};
+        response.success = false;
+        response.message = "internal server error";
+        res.status(500).send(response);
+      } else if (userDetails) {
+        next();
+      } else {
+        let response = {};
+        response.success = false;
+        response.message = "bad request?";
+        res.status(401).send(response);
       }
     });
   }
