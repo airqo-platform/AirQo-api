f29e151ff1b4c2a5c3fd4fae5676f60b573fdbdf
Sserunjogi Richard <sserurich@gmail.com>
2020-03-31 16:51:12 +0300

    added cloud functions that continously update online db from clarity for hourly and daily measurements, endpoint and functionality for averages of the network for the last 28 days

--- src/analytics/controllers/controller.py
+++ src/analytics/controllers/controller.py
@@ -14,6 +14,18 @@ _logger = logging.getLogger(__name__)
 analytics_app = Blueprint('analytics_app', __name__)
 
 
+
+@analytics_app.route('/api/v1/device/codes', methods =['GET'])
+def get_device_codes():
+    devices_codes =  list(mongo.db.devices.find({},{"code": 1, "_id": 0}))
+    devices_codes_list=[]
+    for device_code in devices_codes[0:2]:        
+        last_time = mongo_helpers.get_last_time_from_device_hourly_measurements(device_code['code']) 
+        start_time = helpers.date_to_str(helpers.str_to_date(last_time) + timedelta(hours=1)) 
+        devices_codes_list.append({"code":device_code['code'],"start time":start_time, "last time": last_time})
+    return jsonify({'device codes':devices_codes_list }), 200
+
+
 @analytics_app.route('/api/v1/device/measurements/daily/save', methods =['GET'])
 def get_and_save_daily_measurements():
     devices_codes =  list(mongo.db.devices.find({},{"code": 1, "_id": 0}))
@@ -23,31 +35,34 @@ def get_and_save_daily_measurements():
         clarity_api.save_clarity_device_daily_measurements_v2(average, code['code'] )
     return jsonify({'response': 'all daily measurements saved'}), 200
 
-@analytics_app.route('/api/v1/device/codes', methods =['GET'])
-def get_device_codes():
+
+@analytics_app.route('/api/v1/device/measurements/daily/update', methods =['GET'])
+def update_device_daily_measurements():
     clarity_api = ClarityApi() 
     devices_codes =  list(mongo.db.devices.find({},{"code": 1, "_id": 0}))
-    devices_codes_list=[]
-    for device_code in devices_codes[0:2]:        
-        last_time = clarity_api.get_last_time_from_device_hourly_measurements(device_code['code']) 
-        start_time = helpers.date_to_str(helpers.str_to_date(last_time) + timedelta(hours=1)) 
-        devices_codes_list.append({"code":device_code['code'],"start time":start_time, "last time": last_time})
-    return jsonify({'device codes':devices_codes_list }), 200
+    average='day'
+    for device_code in devices_codes:
+        code= device_code['code']            
+        clarity_api.update_device_daily_measurements(code,average)
+    return jsonify({'response': 'all new hourly measurements saved'}), 200  
 
+@analytics_app.route('/api/v1/device/measurements/hourly/save', methods =['GET'])
+def get_and_save_hourly_measurements():
+    devices_codes =  list(mongo.db.devices.find({},{"code": 1, "_id": 0}))
+    clarity_api = ClarityApi()
+    average='hour'
+    for code in devices_codes:
+        clarity_api.save_clarity_device_hourly_measurements_v2(average, code['code'])
+    return jsonify({'response': 'all hourly measurements saved'}), 200
 
 @analytics_app.route('/api/v1/device/measurements/hourly/update', methods =['GET'])
 def update_device_hourly_measurements():
     clarity_api = ClarityApi() 
     devices_codes =  list(mongo.db.devices.find({},{"code": 1, "_id": 0}))
-    #end_time = clarity_api.date_to_str(datetime.now())        
     average='hour'
-    limit= 20000
-
-    for device_code in devices_codes[0:2]:
-        code= device_code['code']
-        last_time = clarity_api.get_last_time_from_device_hourly_measurements(code) 
-        start_time = helpers.date_to_str(helpers.str_to_date(last_time) + timedelta(hours=1))    
-        clarity_api.save_clarity_device_hourly_measurements(average,code,start_time, limit)
+    for device_code in devices_codes:
+        code= device_code['code']            
+        clarity_api.update_device_hourly_measurements(code,average)
     return jsonify({'response': 'all new hourly measurements saved'}), 200  
         
    
--- src/analytics/helpers/mongo_helpers.py
+++ src/analytics/helpers/mongo_helpers.py
@@ -4,7 +4,70 @@ from app import mongo
 from datetime import datetime
 
 
+def save_device_daily_historical_averages(data):
+    """
+    """
+    for i  in data:
+        mongo.db.device_daily_historical_averages.insert(i)
+
+def get_last_time_from_device_hourly_measurements(device_code):
+        """ Gets the time of the latest record inserted in hourly device measurements.
+
+        Args:
+            device_code: the code used to identify the device.
+
+        Returns:
+            time for the last record inserted.
+
+        """           
+        query = {'deviceCode': device_code}
+        last_record = list(mongo.db.device_hourly_measurements.find(query).sort([('time', -1)]).limit(1))
+        last_time = last_record[0]['time']
+        return last_time
+
+def get_last_time_from_device_daily_measurements(device_code):
+        """ Gets the time of the latest record inserted in daily device measurements.
+
+        Args:
+            device_code: the code used to identify the device.
+
+        Returns:
+            time for the last record inserted.
+
+        """           
+        query = {'deviceCode': device_code}
+        last_record = list(mongo.db.device_daily_measurements.find(query).sort([('time', -1)]).limit(1))
+        last_time = last_record[0]['time']
+        return last_time
+
+def update_hourly_measurements(data):
+    """
+     inserts new hourly measurements if they don't exist in db.
+    """
+    for i  in data:
+        key = {'_id': i['_id']}        
+        mongo.db.device_hourly_measurements.update(key,i, upsert=True)
+
+def update_daily_measurements(data):
+    """
+     inserts new daily measurements if they don't exist in db.
+    """
+    for i  in data:
+        key = {'_id': i['_id']}        
+        mongo.db.device_daily_measurements.update(key,i, upsert=True)
+
+def save_hourly_measurements(data):
+    """
+    Saves hourly measurements.
+    """
+    for i  in data:
+        mongo.db.device_hourly_measurements.insert(i)
+        
+
 def save_daily_measurements(data):
+    """
+     Inserts daily measurements into device_daily_measurements collection.
+    """
     for i  in data:
         mongo.db.device_daily_measurements.insert(i)
 
@@ -36,11 +99,11 @@ def get_filtered_data(device_code, start_date = None, end_date=None, frequency =
         if start_date == None:
             start = helpers.str_to_date_find('2019-06-01T00:00:00Z')
         else:
-            start = helpers.str_to_date_find(start_date)
+            start = helpers.str_to_date(start_date)
         if end_date == None:
             end = datetime.now()
         else:
-            end = helpers.str_to_date_find(end_date)
+            end = helpers.str_to_date(end_date)
 
         query = { 'deviceCode': device_code, 'time': {'$lte': end, '$gte': start} }
                                  
