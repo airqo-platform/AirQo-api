934952c9c3823e3dbdcc1ec409218e109009433a
Martin <bbaalmart@gmail.com>
2020-04-02 10:14:29 +0300

    crypto package installation, refactoring the mailer service  and static msgs

--- src/auth-service/config/constants.js
+++ src/auth-service/config/constants.js
@@ -1,17 +1,20 @@
 const devConfig = {
   MONGO_URI: "mongodb://localhost/airqo-auth-dev",
   JWT_SECRET: process.env.JWT_SECRET,
-  CLIENT_ORIGIN: "https://airqo.net/"
+  CLIENT_ORIGIN: "https://airqo.net/",
+  BCRYPT_SALT_ROUNDS: 12
 };
 const testConfig = {
   MONGO_URI: "mongodb://localhost/airqo-auth-test",
   JWT_SECRET: process.env.JWT_SECRET,
-  CLIENT_ORIGIN: "https://airqo.net/"
+  CLIENT_ORIGIN: "https://airqo.net/",
+  BCRYPT_SALT_ROUNDS: 12
 };
 const prodConfig = {
   MONGO_URI: process.env.ATLAS_URI,
   JWT_SECRET: process.env.JWT_SECRET,
-  CLIENT_ORIGIN: "https://airqo.net/"
+  CLIENT_ORIGIN: "https://airqo.net/",
+  BCRYPT_SALT_ROUNDS: 12
 };
 const defaultConfig = {
   PORT: process.env.PORT || 3000
--- src/auth-service/services/mailer.js
+++ src/auth-service/services/mailer.js
@@ -2,59 +2,13 @@ const nodemailer = require("nodemailer");
 const xoauth2 = require("xoauth2");
 const constants = require("../config/constants");
 
-const mailOptions = {
-  from: process.env.MAIL_USER,
-  to: "martin@airqo.net",
-  subject: "Join AirQo Analytics",
-  text: "Please review this new request"
-};
-
-const credentials = {
-  host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
-  auth: {
-    user: process.env.MAIL_USER,
-    pass: process.env.MAIL_PASS
-  }
-};
-
-const credentials_2 = {
+//create a nodemail transport
+const transporter = nodemailer.createTransport({
   service: "gmail",
-  host: "smtp.gmail.com",
-  secure: "true",
-  port: "465",
   auth: {
-    type: "OAuth2", //Authentication type
-    user: "your_email@service.com", //For example, xyz@gmail.com
-    clientId: "Your_ClientID",
-    clientSecret: "Client_Secret",
-    refreshToken: "Refresh_Token"
+    user: `${process.env.MAIL_USER}`,
+    pass: `${process.env.MAIL_PASS}`
   }
-};
-
-const transporter = nodemailer.createTransport(credentials);
-
-const sendEmail = async (to, content) => {
-  try {
-    const contacts = {
-      from: process.env.MAIL_USER,
-      to
-    };
-    const email = Object.assign({}, content, contacts);
-    await transporter.sendMail(email);
-  } catch (e) {
-    console.log(e);
-  }
-};
-
-transporter.sendMail(mailOptions, (e, r) => {
-  if (e) {
-    console.log(e);
-  } else {
-    console.log(r);
-  }
-  transporter.close();
 });
 
-module.exports = sendEmail;
+module.exports = transporter;
