5f5baba4ab9963304143d8fed38f584b849a7a08
Martin <bbaalmart@gmail.com>
2020-04-07 10:21:09 +0300

    correction of API endpoint for the password recovery email

--- src/auth-service/utils/email.msgs.js
+++ src/auth-service/utils/email.msgs.js
@@ -4,16 +4,16 @@ module.exports = {
   resend: "Confirmation email resent, maybe check your spam?",
   couldNotFind: "Could not find you!",
   alreadyConfirmed: "Your email was already confirmed",
-  recovery_email: token => {
+  recovery_email: (token) => {
     return (
       "You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n" +
       "Please click on the following link, or paste this into your browser to complete the process within one hour of receiving it:\n\n" +
-      `http://localhost:3000/reset/${token}\n\n` +
+      `http://localhost:3000/api/v1/users/reset/${token}\n\n` +
       "If you did not request this, please ignore this email and your password will remain unchanged.\n"
     );
   },
   join_request:
     "Your request to join the AirQo analytics platform has been received. We shall get back to you as soon as possible. \n\n" +
-    "Please click the following link to learn more about AirQo. \n\n" +
-    `https://airqo.net/`
+    "Please click the following link to learn more about AirQo. \n" +
+    `https://airqo.net/`,
 };
