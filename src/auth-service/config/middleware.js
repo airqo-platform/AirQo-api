093376db91d66a7cc3339c48da8b8d632a5886c2
Martin Bbaale <bbaalmart@gmail.com>
2020-04-11 16:56:24 +0300

    Bug in registration (#58)
    * establishing collections
    
    * email confirmation after registration
    
    * refactoring the DB configurations and addressing security concerns
    
    * update and refactor of the database configs, modification of entity schemas
    
    * installation of authentication and session packages
    
    * additional auth strategy for adding collaborators
    
    * additional access control checks
    
    * confirmation email
    
    * new controller functions for the use case
    
    * new routes that utilize the controller functions
    
    * successful registration emails
    
    * crypto package installation, refactoring the mailer service  and static msgs
    
    * new password reset fields and a preupdate hook
    
    * three new endpoints: updatePasswordViaEmail, reset password, and forgotPassword
    
    * installation of new package and more input validations
    
    * correction of API endpoint for the password recovery email
    
    * removing the validation checks for the user privileges
    
    * removal of privileges as mandatory field and addition of one new endpoint for password reset
    
    * adding firstName and lastName to the token signature

--- src/auth-service/config/middleware.js
+++ src/auth-service/config/middleware.js
@@ -1,31 +1,46 @@
-const morgan = require('morgan');
-const bodyParser = require('body-parser')
-const compression = require('compression')
-const helmet = require('helmet')
-const passport = require('passport');
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const compression = require("compression");
+const helmet = require("helmet");
+const passport = require("passport");
+const session = require("express-session");
+const MongoStore = require("connect-mongo")(session);
+const mongoose = require("mongoose");
 
-const {
-    isPrimitive
-} = require('util')
+const { isPrimitive } = require("util");
 
-const isDev = process.env.NODE_ENV === 'development';
-const isProd = process.env.NODE_ENV === 'production';
+const isDev = process.env.NODE_ENV === "development";
+const isProd = process.env.NODE_ENV === "production";
 
 //we export a middleware funtion which takes in the express application as the input
 
 module.exports = app => {
-    if (isProd) {
-        app.use(compression());
-        app.use(helmet());
-    }
-    app.use(passport.initialize());
-    app.use(bodyParser.json());
-    app.use(bodyParser.urlencoded({
-        extended: true
-    }));
+  if (isProd) {
+    app.use(compression());
+    app.use(helmet());
+  }
 
+  const options = { mongooseConnection: mongoose.connection };
 
-    if (isDev) {
-        app.use(morgan('dev'));
-    }
-}
+  app.use(
+    session({
+      secret: process.env.SESSION_SECRET,
+      store: new MongoStore(options),
+      resave: false, //been told that this thing clears the user after logout
+      saveUninitialized: false //same story here...
+    })
+  );
+
+  app.use(passport.initialize());
+  app.use(passport.session());
+  app.use(bodyParser.json());
+  app.use(
+    bodyParser.urlencoded({
+      extended: true
+    })
+  );
+
+  if (isDev) {
+    app.use(morgan("dev"));
+  }
+};
