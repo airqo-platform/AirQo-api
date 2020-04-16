e0992c301962ad7ca26bc37502def01bbb575a02
lilly-muyama <muyamalillian@gmail.com>
2020-04-16 15:58:36 +0300

    modified endpoint to use POST

--- src/ml-service/packages/sensor_location/locate/app.py
+++ src/ml-service/packages/sensor_location/locate/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_pymongo import PyMongo
 from modules import locate_model, locate_helper 
 #from flask_cors import CORS, crossorigin
@@ -35,21 +35,28 @@ def place_sensors():
 @app.route('/api/v1/map/parishes', methods = ['GET', 'POST'])
 #@crossorigin
 def place_sensors_map():
-    #json_data = locate_helper.get_data(request.data) #to be used when polygon is up and running
-    polygon = [[[ 32.506, 0.314], [32.577, 0.389], [32.609, 0.392], [32.641, 0.362], [32.582, 0.266], 
-                      [32.506, 0.314]]]
-    sensor_number = 20
-    if polygon==None:
-        return jsonify({'response': 'Please draw a polygon'}), 200
+    '''
+    Returns parishes recommended by the model given the geographical polygon
+    '''
+    if request.method == 'POST':
+        json_data = request.get_json()
+        if not json_data:
+           return {'message': 'No input data provided'}, 400
+        else:
+            sensor_number = json_data["sensor_number"]
+            polygon = json_data["polygon"]
+            #polygon = [[[ 32.506, 0.314], [32.577, 0.389], [32.609, 0.392], [32.641, 0.362], [32.582, 0.266], [32.506, 0.314]]]
+            if polygon==None:
+                return jsonify({'response': 'Please draw a polygon'}), 200
 
-    all_parishes = locate_model.get_parishes_map(polygon)
-    if len(all_parishes)<2:
-        return jsonify({'response': 'Invalid polygon'}), 200
-    else:
-        all_parishes_df = locate_helper.json_to_df(all_parishes)
-        all_parishes_df = locate_helper.process_data(all_parishes_df)
-        recommended_parishes = locate_helper.kmeans_algorithm(all_parishes_df, sensor_number)
-        return jsonify(recommended_parishes)
+            all_parishes = locate_model.get_parishes_map(polygon)
+            if len(all_parishes)<2:
+                return jsonify({'response': 'Invalid polygon'}), 200
+            else:
+                all_parishes_df = locate_helper.json_to_df(all_parishes)
+                all_parishes_df = locate_helper.process_data(all_parishes_df)
+                recommended_parishes = locate_helper.kmeans_algorithm(all_parishes_df, sensor_number)
+                return jsonify(recommended_parishes)
     
 if __name__ == "__main__":
    app.run()
