be7618adcbe384dd30a39873df39be34090d63c7
danielogen <danielogen@gmail.com>
2020-05-24 12:31:45 +0300

    added code for utilization of python-dotenv library

--- src/ml-service/packages/sensor_location/locate/config.py
+++ src/ml-service/packages/sensor_location/locate/config.py
@@ -3,6 +3,8 @@ from logging.handlers import TimedRotatingFileHandler
 import pathlib
 import os
 import sys
+from dotenv import load_dotenv
+
 
 class Config:
     DEBUG = False
@@ -12,19 +14,33 @@ class Config:
 
 
 class ProductionConfig(Config):
+    dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+    load_dotenv(dotenv_path)
     DEBUG = False
+    #SERVER_PORT = os.environ.get('PORT', 5000)
+    MONGO_URI = os.getenv("MONGO_URI")
 
 
 class DevelopmentConfig(Config):
+    dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+    load_dotenv(dotenv_path)
+
     DEVELOPMENT = True
     DEBUG = True
+    MONGO_URI = os.getenv("MONGO_URI")
 
 
 class TestingConfig(Config):
+    dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+    load_dotenv(dotenv_path)
+
     TESTING = True
+    MONGO_URI = os.getenv("MONGO_URI")
+
+
+app_config = {"development": DevelopmentConfig,
+              "testing": TestingConfig, "production": ProductionConfig}
 
 
 if __name__ == '__main__':
     print('package root', "Locate App")
-
-
