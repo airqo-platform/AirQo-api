d23a5799a1e4094a3b258dcea9bf9b3da98a2aaa
lilly-muyama <muyamalillian@gmail.com>
2020-06-09 15:07:34 +0300

    updated requirements.txt and yaml file

--- src/location-registry/controllers/controller.py
+++ src/location-registry/controllers/controller.py
@@ -29,7 +29,6 @@ def register_location():
             return {'message': 'No input data provided'}, 400
         else:
             loc_ref = json_data["locationReference"]
-            #print(loc_ref, file=sys.stderr)
             host_name = json_data["hostName"]
             mobility = json_data["mobility"]
             latitude = json_data["latitude"]
@@ -124,54 +123,8 @@ def update_location():
             road_status = json_data["roadStatus"]
             local_activities = json_data["localActivities"]
 
-            #loc_ref = json_data["locationReference"]
-            #host_name = json_data["hostName"]
-            #mobility = json_data["mobility"]
-            #latitude = json_data["latitude"]
-            #longitude = json_data["longitude"]
-            #internet = json_data["internet"]
-            #power = json_data["power"]
-            #height = json_data["height"]
-            #road_intensity = json_data["roadIntensity"]
-            #installation_type = json_data["installationType"]
-            #road_status = json_data["roadStatus"]
-            #local_activities = json_data["localActivities"]
-            #country = "Uganda"
-
-            #try:
-             #   region, district, county, subcounty, parish = helper.get_location_details(longitude, latitude)
-              #  location_name = helper.get_location_name(parish.capitalize(), district.capitalize())
-            #except:
-             #   region, district, county, subcounty, parish, location_name = None, None, None, None, None, None
-            #try:
-            #    altitude = helper.get_altitude(latitude,longitude)
-            #except:
-             #   altitude = None
-            #try:
-            #    landform_90 = helper.get_landform90(latitude, longitude)
-            #except:
-             #   landform_90 = None
-            #try:
-             #   landform_270, aspect  = helper.get_landform270(latitude, longitude)
-            #except:
-             #   landform_270, aspect = None, None
-            #try:
-             #   closest_road_type, closest_distance, closest_residential_distance= helper.distance_to_closest_road(latitude, longitude)
-            #except:
-             #   closest_road_type, closest_distance, closest_residential_distance = None, None, None
-            #try:
-             #   closest_motorway_distance = helper.distance_to_closest_motorway(latitude, longitude)
-            #except:
-             #   closest_motorway_distance = None
-            #try:
-             #   nearest_city_distance = helper.distance_to_nearest_city(latitude, longitude)
-            #except:
-             #   nearest_city_distance = None, None, None
             try:
                 helper.save_edited_location(loc_ref, power, internet, height, road_intensity, installation_type, road_status, local_activities)
-                #helper.save_edited_location(loc_ref, host_name, internet, power, height, road_intensity, installation_type, road_status, 
-                #local_activities, location_name, region, district, county, subcounty, parish, altitude, aspect, landform_90, 
-                #landform_270, closest_distance, closest_motorway_distance, closest_residential_distance, nearest_city_distance)
                 return {'message': 'Location has been successfully updated'}, 200
             except:
                 return {'message': 'An error occured. Please try again'}, 200
