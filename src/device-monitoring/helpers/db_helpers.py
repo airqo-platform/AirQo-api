2cd61e2b5684dea95466bf3d84245c9c0dc22571
lilly-muyama <muyamalillian@gmail.com>
2020-06-09 21:36:44 +0300

    merging device monitoring

---
+++ src/device-monitoring/helpers/db_helpers.py
@@ -0,0 +1,14 @@
+#from helpers import helpers
+from pymongo import MongoClient
+#from app import mongo, MONGO_URI
+from datetime import datetime, timedelta
+import os
+import sys
+
+MONGO_URI = os.getenv('MONGO_URI')
+
+
+def connect_mongo():
+    client = MongoClient(MONGO_URI)
+    db = client['airqo_devicemonitor']
+    return db
