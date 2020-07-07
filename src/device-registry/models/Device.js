e93de75c5541aa984221a247f6b7269623ce3ade
Martin <bbaalmart@gmail.com>
2020-07-07 17:57:17 +0300

    added staging db and other db + collection modifications in relation to the registration of device

--- src/device-registry/models/Device.js
+++ src/device-registry/models/Device.js
@@ -1,115 +1,127 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const ObjectId = mongoose.Schema.Types.ObjectId;
-const uniqueValidator = require('mongoose-unique-validator');
+const uniqueValidator = require("mongoose-unique-validator");
 
-const deviceSchema = new mongoose.Schema({
-    location: {
-        longitude: {
-            type: String,
-            required: [true, 'Longitude is required!'],
-            trim: true,
-        },
-        latitude: {
-            type: String,
-            required: [true, 'Latitude is required!'],
-            trim: true,
-        }
+const deviceSchema = new mongoose.Schema(
+  {
+    latitude: {
+      type: Number,
+      required: true,
+      required: [true, "Device latitude is required!"],
+    },
+    longitude: {
+      type: Number,
+      required: true,
+      required: [true, "Device longitude is required!"],
+    },
+    license: {
+      type: String,
+    },
+    writeKey: {
+      type: String,
+    },
+    readKey: {
+      type: String,
     },
     name: {
-        type: String,
-        required: [true, 'Device name is required!'],
-        trim: true,
-        unique: true
+      type: String,
+      required: [true, "Device name is required!"],
+      trim: true,
+      unique: true,
     },
     visibility: {
-        type: Boolean, require: [true, 'visibility is required'],
-        trim: true,
+      type: Boolean,
+      require: [true, "visibility is required"],
+      trim: true,
+    },
+    createdAt: {
+      type: Date,
+    },
+    elevation: {
+      type: Number,
     },
     owner: {
-        type: ObjectId,
-        require: [true, 'owner is required']
+      type: ObjectId,
+      require: [true, "owner is required"],
     },
     description: {
-        type: String,
-        required: [true, 'Description is required!'],
-        trim: true,
+      type: String,
+      required: [true, "Description is required!"],
+      trim: true,
     },
     mobile: {
-        type: Boolean, require: [true, 'mobility is required'],
-        trim: true,
+      type: Boolean,
+      trim: true,
+      default: false,
     },
     height: {
-        type: Number,
-        default: 0,
-        require: [true, 'height above ground is required']
+      type: Number,
+      default: 0,
     },
     distanceToRoad: {
-        type: Number,
-        default: 0,
-        require: [true, 'distance next to road is required']
+      type: Number,
+      default: 0,
     },
     mountType: {
-        motorbike: {
-            type: Boolean,
-            default: false,
-            trim: true,
-        },
-        wall: {
-            type: Boolean,
-            default: false,
-            trim: true,
-        },
-        pole: {
-            type: Boolean,
-            default: false,
-            trim: true,
-        },
+      type: String,
+      default: "pole",
+      trim: true,
     },
-    sensors: [{
-        type: ObjectId, ref: 'sensor'
-    }],
-}, {
-    timestamps: true
-});
+    ISP: {
+      type: String,
+    },
+    phoneNumber: {
+      type: Number,
+    },
+    device_manufacturer: {
+      type: String,
+    },
+    product_name: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
 deviceSchema.plugin(uniqueValidator, {
-    message: `{VALUE} already taken!`,
+  message: `{VALUE} already taken!`,
 });
 
 deviceSchema.methods = {
-    toJSON() {
-        return {
-            _id: this._id,
-            name: this.name,
-            location: this.location,
-            mountType: this.mountType,
-            createdAt: this.createdAt,
-            owner: this.owner,
-            height: this.height,
-            description: this.description,
-            mobile: this.mobile,
-            distanceToRoad: this.distanceToRoad,
-            sensors: this.sensors
-
-        }
-    }
-}
+  toJSON() {
+    return {
+      id: this.id,
+      name: this.name,
+      latitude: this.latitude,
+      longitude: this.longitude,
+      createdAt: this.createdAt,
+      owner: this.owner,
+      device_manufacturer: this.device_manufacturer,
+      product_name: this.product_name,
+      ISP: this.ISP,
+      phoneNumber: this.phoneNumber,
+      visibility: this.visibility,
+      description: this.description,
+    };
+  },
+};
 
 // I will add the check for the user after setting up the communications between services
 deviceSchema.statics = {
-    createDevice(args) {
-        return this.create({
-            ...args,
+  createDevice(args) {
+    return this.create({
+      ...args,
+    });
+  },
 
-        })
-    },
-
-    list({ skip = 0, limit = 5 } = {}) {
-        return this.find()
-            .sort({ createdAt: -1 })
-            .skip(skip)
-            .limit(limit)
-    }
+  list({ skip = 0, limit = 5 } = {}) {
+    return this.find()
+      .sort({ createdAt: -1 })
+      .skip(skip)
+      .limit(limit);
+  },
 };
 
 const device = mongoose.model("device", deviceSchema);
