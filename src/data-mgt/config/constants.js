63a2bedad07c489d52f5d5c9949ffd09d79cacef
Martin <bbaalmart@gmail.com>
2020-07-11 15:43:22 +0300

    database configs

--- src/data-mgt/config/constants.js
+++ src/data-mgt/config/constants.js
@@ -1,13 +1,21 @@
 const devConfig = {
-  MONGO_URL: "mongodb://localhost/data-mgt-dev",
+  MONGO_URL: "mongodb://localhost/",
+  DB_NAME: process.env.MONGO_DEV,
   JWT_SECRET: process.env.JWT_SECRET,
 };
 const testConfig = {
-  MONGO_URL: "mongodb://localhost/data-mgt-test",
+  MONGO_URL: "mongodb://localhost/",
+  DB_NAME: process.env.MONGO_TEST,
   JWT_SECRET: process.env.JWT_SECRET,
 };
 const prodConfig = {
   MONGO_URL: process.env.MONGO_GCE_URI,
+  DB_NAME: process.env.MONGO_PROD,
+  JWT_SECRET: process.env.JWT_SECRET,
+};
+const stageConfig = {
+  MONGO_URL: process.env.MONGO_GCE_URI,
+  DB_NAME: process.env.MONGO_STAGE,
   JWT_SECRET: process.env.JWT_SECRET,
 };
 const defaultConfig = {
@@ -18,8 +26,12 @@ function envConfig(env) {
   switch (env) {
     case "development":
       return devConfig;
-    case "test":
+    case "testing":
       return testConfig;
+    case "staging":
+      return stageConfig;
+    case "production":
+      return prodConfig;
     default:
       return prodConfig;
   }
