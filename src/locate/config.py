8df37519342dfd70f1edac47b207aba8955487f5
lilly-muyama <muyamalillian@gmail.com>
2020-07-15 18:53:16 +0300

    updating config.py

--- src/locate/config.py
+++ src/locate/config.py
@@ -5,17 +5,20 @@ import os
 import sys
 from dotenv import load_dotenv
 
+load_dotenv()
+
+SECRET_KEY= os.getenv('SECRET_KEY')
+
 class Config:
     DEBUG = False
     TESTING = False
     CSRF_ENABLED = True
-    SECRET_KEY = 'sample-code-local-environment'
+    SECRET_KEY = SECRET_KEY
 
 class ProductionConfig(Config):
     dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
     load_dotenv(dotenv_path)
     DEBUG = False
-    #SERVER_PORT = os.environ.get('PORT', 5000)
     MONGO_URI = os.getenv("MONGO_URI")
 
 class DevelopmentConfig(Config):
