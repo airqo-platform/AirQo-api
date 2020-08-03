759c68078a254680ec8beb0da777eec1e8c66426
Martin <bbaalmart@gmail.com>
2020-08-03 11:04:40 +0300

    modification of email verification field

--- src/auth-service/models/User.js
+++ src/auth-service/models/User.js
@@ -18,97 +18,99 @@ const ObjectId = mongoose.Schema.Types.ObjectId;
 const { tenantModel } = require("../config/multiTenant");
 
 function oneMonthFromNow() {
-    var d = new Date();
-    var targetMonth = d.getMonth() + 1;
-    d.setMonth(targetMonth);
-    if (d.getMonth() !== targetMonth % 12) {
-        d.setDate(0); // last day of previous month
-    }
-    return d;
+  var d = new Date();
+  var targetMonth = d.getMonth() + 1;
+  d.setMonth(targetMonth);
+  if (d.getMonth() !== targetMonth % 12) {
+    d.setDate(0); // last day of previous month
+  }
+  return d;
 }
 
 const UserSchema = new Schema({
-    email: {
-        type: String,
-        unique: true,
-        required: [true, "Email is required"],
-        trim: true,
-        validate: {
-            validator(email) {
-                return validator.isEmail(email);
-            },
-            message: "{VALUE} is not a valid email!",
-        },
+  email: {
+    type: String,
+    unique: true,
+    required: [true, "Email is required"],
+    trim: true,
+    validate: {
+      validator(email) {
+        return validator.isEmail(email);
+      },
+      message: "{VALUE} is not a valid email!",
     },
-    emailConfirmed: {
-        type: Boolean,
-        default: false,
+  },
+  isEmailVerified: {
+    type: Boolean,
+    default: false,
+  },
+  firstName: {
+    type: String,
+    required: [true, "FirstName is required!"],
+    trim: true,
+  },
+  lastName: {
+    type: String,
+    required: [true, "LastName is required"],
+    trim: true,
+  },
+  userName: {
+    type: String,
+    required: [true, "UserName is required!"],
+    trim: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: [true, "Password is required!"],
+    trim: true,
+    minlength: [6, "Password is required"],
+    validate: {
+      validator(password) {
+        return passwordReg.test(password);
+      },
+      message: "{VALUE} is not a valid password!",
     },
-    firstName: {
-        type: String,
-        required: [true, "FirstName is required!"],
-        trim: true,
-    },
-    lastName: {
-        type: String,
-        required: [true, "LastName is required"],
-        trim: true,
-    },
-    userName: {
-        type: String,
-        required: [true, "UserName is required!"],
-        trim: true,
-        unique: true,
-    },
-    password: {
-        type: String,
-        required: [true, "Password is required!"],
-        trim: true,
-        minlength: [6, "Password is required"],
-        validate: {
-            validator(password) {
-                return passwordReg.test(password);
-            },
-            message: "{VALUE} is not a valid password!",
-        },
-    },
-    interest: { type: String, default: "none" },
-    privilege: { type: String, default: "admin" },
-    accountStatus: { type: String, default: "false" },
-    hasAccess: { type: Boolean, default: false },
-    publisher: { type: Boolean, default: false },
-    duration: { type: Date, default: oneMonthFromNow },
-    bus_nature: { type: String, default: "none" },
-    org_department: { type: String, default: "none" },
-    uni_faculty: { type: String, default: "none" },
-    uni_course_yr: { type: String, default: 0 },
-    pref_locations: [{ type: ObjectId, ref: "loc" }],
-    country: { type: String, default: "Uganda" },
-    phoneNumber: { type: Number, default: 0, unique: true },
-    resetPasswordToken: { type: String },
-    resetPasswordExpires: { type: Date },
-    role: {
-        job_title: { type: String, default: "none" },
-        org_name: { type: String, default: "none" },
-    },
-    product: {
-        analytics: { type: Boolean, default: false },
-        locate: { type: Boolean, default: false },
-        admin: { type: Boolean, default: false },
-    },
-    notifications: {
-        email: { type: Boolean, default: false },
-        push: { type: Boolean, default: false },
-        text: { type: Boolean, default: false },
-        phone: { type: Boolean, default: false },
-    },
+  },
+  interest: { type: String, default: "none" },
+  privilege: { type: String, default: "admin" },
+  accountStatus: { type: String, default: "false" },
+  hasAccess: { type: Boolean, default: false },
+  publisher: { type: Boolean, default: false },
+  duration: { type: Date, default: oneMonthFromNow },
+  bus_nature: { type: String, default: "none" },
+  org_department: { type: String, default: "none" },
+  uni_faculty: { type: String, default: "none" },
+  uni_course_yr: { type: String, default: 0 },
+  pref_locations: [{ type: ObjectId, ref: "loc" }],
+  country: { type: String, default: "Uganda" },
+  phoneNumber: { type: Number, default: 0, unique: true },
+  resetPasswordToken: { type: String },
+  resetPasswordExpires: { type: Date },
+  emailVerificationToken: { type: String, required: true },
+  emailVerficationExpires: { type: Date },
+  role: {
+    job_title: { type: String, default: "none" },
+    org_name: { type: String, default: "none" },
+  },
+  product: {
+    analytics: { type: Boolean, default: false },
+    locate: { type: Boolean, default: false },
+    admin: { type: Boolean, default: false },
+  },
+  notifications: {
+    email: { type: Boolean, default: false },
+    push: { type: Boolean, default: false },
+    text: { type: Boolean, default: false },
+    phone: { type: Boolean, default: false },
+  },
 });
 
-UserSchema.pre("save", function(next) {
-    if (this.isModified("password")) {
-        this.password = this._hashPassword(this.password);
-    }
-    return next();
+UserSchema.pre("save", function (next) {
+  if (this.isModified("password")) {
+    this.password = this._hashPassword(this.password);
+  }
+  return next();
 });
 
 // UserSchema.pre("findOneAndUpdate", function(next) {
@@ -118,78 +120,79 @@ UserSchema.pre("save", function(next) {
 //     return next();
 // });
 
-UserSchema.pre("findOneAndUpdate", function() {
-    let that = this;
-    const update = that.getUpdate();
-    if (update.__v != null) {
-        delete update.__v;
+UserSchema.pre("findOneAndUpdate", function () {
+  let that = this;
+  const update = that.getUpdate();
+  if (update.__v != null) {
+    delete update.__v;
+  }
+  const keys = ["$set", "$setOnInsert"];
+  for (const key of keys) {
+    if (update[key] != null && update[key].__v != null) {
+      delete update[key].__v;
+      if (Object.keys(update[key]).length === 0) {
+        delete update[key];
+      }
     }
-    const keys = ["$set", "$setOnInsert"];
-    for (const key of keys) {
-        if (update[key] != null && update[key].__v != null) {
-            delete update[key].__v;
-            if (Object.keys(update[key]).length === 0) {
-                delete update[key];
-            }
-        }
-    }
-    update.$inc = update.$inc || {};
-    update.$inc.__v = 1;
+  }
+  update.$inc = update.$inc || {};
+  update.$inc.__v = 1;
 });
 
-UserSchema.pre("update", function(next) {
-    if (this.isModified("password")) {
-        this.password = this._hashPassword(this.password);
-    }
-    return next();
+UserSchema.pre("update", function (next) {
+  if (this.isModified("password")) {
+    this.password = this._hashPassword(this.password);
+  }
+  return next();
 });
 
 UserSchema.methods = {
-    _hashPassword(password) {
-        // bcrypt.hash(password, saltRounds).then(function (hash) {
-        //     return hash;
-        // })
-        return bcrypt.hashSync(password, saltRounds);
-    },
-    authenticateUser(password) {
-        // bcrypt.compare(password, this.password).then(function (res) {
-        //     return res;
-        // })
-        return bcrypt.compareSync(password, this.password);
-    },
-    createToken() {
-        return jwt.sign({
-                _id: this._id,
-                firstName: this.firstName,
-                lastName: this.lastName,
-                email: this.email,
-                pref_locations: this.pref_locations,
-                privilege: this.privilege,
-            },
-            constants.JWT_SECRET
-        );
-    },
-    toAuthJSON() {
-        return {
-            _id: this._id,
-            userName: this.userName,
-            token: `JWT ${this.createToken()}`,
-            email: this.email,
-        };
-    },
-    toJSON() {
-        return {
-            _id: this._id,
-            userName: this.userName,
-            email: this.email,
-            firstName: this.firstName,
-            lastName: this.lastName,
-            graph_defaults: this.graph_defaults,
-            pref_locations: this.pref_locations,
-            privilege: this.privilege,
-            phoneNumber: this.phoneNumber,
-        };
-    },
+  _hashPassword(password) {
+    // bcrypt.hash(password, saltRounds).then(function (hash) {
+    //     return hash;
+    // })
+    return bcrypt.hashSync(password, saltRounds);
+  },
+  authenticateUser(password) {
+    // bcrypt.compare(password, this.password).then(function (res) {
+    //     return res;
+    // })
+    return bcrypt.compareSync(password, this.password);
+  },
+  createToken() {
+    return jwt.sign(
+      {
+        _id: this._id,
+        firstName: this.firstName,
+        lastName: this.lastName,
+        email: this.email,
+        pref_locations: this.pref_locations,
+        privilege: this.privilege,
+      },
+      constants.JWT_SECRET
+    );
+  },
+  toAuthJSON() {
+    return {
+      _id: this._id,
+      userName: this.userName,
+      token: `JWT ${this.createToken()}`,
+      email: this.email,
+    };
+  },
+  toJSON() {
+    return {
+      _id: this._id,
+      userName: this.userName,
+      email: this.email,
+      firstName: this.firstName,
+      lastName: this.lastName,
+      graph_defaults: this.graph_defaults,
+      pref_locations: this.pref_locations,
+      privilege: this.privilege,
+      phoneNumber: this.phoneNumber,
+    };
+  },
 };
 
 const user = mongoose.model("user", UserSchema);
