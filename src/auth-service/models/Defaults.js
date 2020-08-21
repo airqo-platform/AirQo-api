0d7ac2876069740add30b855f72e1f995e030532
Martin <bbaalmart@gmail.com>
2020-08-21 03:55:46 +0300

    schema modifications for the Defaults collection

--- src/auth-service/models/Defaults.js
+++ src/auth-service/models/Defaults.js
@@ -4,71 +4,72 @@ var uniqueValidator = require("mongoose-unique-validator");
 const { tenantModel } = require("../config/multiTenant");
 
 const DefaultsSchema = new mongoose.Schema({
-    pollutant: {
-        type: String,
-        trim: true,
-        required: [true, "pollutant is required!"],
-        default: "none",
+  pollutant: {
+    type: String,
+    trim: true,
+    required: [true, "pollutant is required!"],
+  },
+  frequency: {
+    type: String,
+    required: [true, "frequency is required!"],
+  },
+  startDate: {
+    type: Date,
+    required: [true, "startDate is required!"],
+  },
+  endDate: {
+    type: Date,
+    required: [true, "endDate is required!"],
+  },
+  user: {
+    type: ObjectId,
+    required: [true, "user is required!"],
+    unique: false,
+  },
+  chartType: {
+    type: String,
+    required: [true, "chartTyoe is required!"],
+  },
+  chartTitle: {
+    type: String,
+    required: [true, "chartTitle is required!"],
+    unique: false,
+  },
+  locations: [
+    {
+      type: String,
     },
-    frequency: {
-        type: String,
-        default: "none",
-        required: [true, "frequency is required!"],
-    },
-    startDate: {
-        type: String,
-        default: "none",
-        required: [true, "startDate is required!"],
-    },
-    endDate: {
-        type: String,
-        default: "none",
-        required: [true, "endDate is required!"],
-    },
-    user: {
-        type: ObjectId,
-        required: [true, "user is required!"],
-        unique: false,
-    },
-    chartType: {
-        type: String,
-        default: "none",
-        required: [true, "chartTyoe is required!"],
-    },
-    chartTitle: {
-        type: String,
-        default: "none",
-        required: [true, "chartTitle is required!"],
-        unique: false,
-    },
+  ],
 });
 
 DefaultsSchema.plugin(uniqueValidator);
 
-DefaultsSchema.index({
+DefaultsSchema.index(
+  {
     chartTitle: 1,
     user: 1,
-}, {
+  },
+  {
     unique: true,
-});
+  }
+);
 
 DefaultsSchema.methods = {
-    toJSON() {
-        return {
-            _id: this._id,
-            pollutant: this.pollutant,
-            frequency: this.frequency,
-            startDate: this.startDate,
-            endDate: this.endDate,
-            user: this.user,
-            chartType: this.chartType,
-            chartTitle: this.chartTitle,
-        };
-    },
+  toJSON() {
+    return {
+      _id: this._id,
+      pollutant: this.pollutant,
+      frequency: this.frequency,
+      startDate: this.startDate,
+      endDate: this.endDate,
+      user: this.user,
+      chartType: this.chartType,
+      chartTitle: this.chartTitle,
+      locations: this.locations,
+    };
+  },
 };
 
 const defaults = mongoose.model("defaults", DefaultsSchema);
 
 module.exports = defaults;
-
-// module.exports = tenantModel("defaults", DefaultsSchema);
