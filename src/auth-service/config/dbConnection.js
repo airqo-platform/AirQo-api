b3d25e2a8207ef8793135065c9be717968b49774
Martin <bbaalmart@gmail.com>
2020-09-10 06:53:31 +0300

    updating production db configurations and url for password reset

--- src/auth-service/config/dbConnection.js
+++ src/auth-service/config/dbConnection.js
@@ -1,61 +1,21 @@
 const mongoose = require("mongoose");
-// const { Gstore, instances } = require("gstore-node");
-const config = require("./constants");
-// const { Datastore } = require("@google-cloud/datastore");
+const constants = require("./constants");
 
-// const gstore1 = new Gstore();
-// const gstore2 = new Gstore({ cache: true });
-// const gstore3 = new Gstore();
+mongoose.Promise = global.Promise;
 
-// const datastore1 = new Datastore({
-//   namespace: "default",
-//   projectId: "airqo-250220",
-// });
-// const datastore2 = new Datastore({
-//   namespace: "cache-on",
-//   projectId: "airqo-250220",
-// });ç
-// const datastore3 = new Datastore({
-//   namespace: "staging",
-//   projectId: "airqo-250220",
-// });
+try {
+  console.log("the value for MONGO URL is: " + constants.MONGO_URI);
+  mongoose.connect(
+    constants.MONGO_URI,
+    { dbName: constants.DB_NAME },
+    { useNewUrlParser: true }
+  );
+} catch (e) {
+  mongoose.createConnection(constants.MONGO_URI);
+}
 
-const URI = config.MONGO_URI;
-const db = mongoose.connection;
-
-const options = {
-  useCreateIndex: true,
-  useNewUrlParser: true,
-  useFindAndModify: false,
-};
-
-mongoose.connect(URI, options);
-// gstore1.connect(datastore1);
-// gstore2.connect(datastore2);
-// gstore3.connect(datastore3);
-
-//unique IDs to each instance
-// instances.set("default", gstore1);
-// instances.set("cache-on", gstore2);
-// instances.set("staging", gstore3);
-
-// When successfully connected
-db.on("connected", () => {
-  console.log("Established Mongoose Default Connection");
-});
-
-// When connection throws an error
-db.on("error", (err) => {
-  console.log("Mongoose Default Connection Error : " + err);
-});
-
-db.on("disconnected", () => {});
-
-// //when nodejs stops
-process.on("unhandledRejection", (reason, p) => {
-  console.log("Unhandled Rejection at: Promise", p, "reason:", reason);
-  db.close(() => {
-    console.log("mongoose is disconnected through the app");
-    process.exit(0);
+mongoose.connection
+  .once("open", () => console.log("MongoDB Running"))
+  .on("error", (e) => {
+    throw e;
   });
-});
