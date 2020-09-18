7826d43776c28041e54012d20ba0ed4c7e47ad6c
Martin <bbaalmart@gmail.com>
2020-09-18 03:09:53 +0300

    refactoring, generall app configurarations and installation of the express-session module.

--- src/device-registry/app.js
+++ src/device-registry/app.js
@@ -1,21 +1,25 @@
 var express = require("express");
 var path = require("path");
+var logger = require("morgan");
 const dotenv = require("dotenv");
+var bodyParser = require("body-parser");
 dotenv.config();
 var cookieParser = require("cookie-parser");
-const middlewareConfig = require("./config/app.middleware");
 var api = require("./routes/api");
 const { mongodb } = require("./config/database");
 mongodb;
 
 var app = express();
 
-middlewareConfig(app);
-
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger("dev"));
+app.use(bodyParser.json());
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+// app.use(bindCurrentNamespace);
 
 app.use("/api/v1/devices/", api);
 
