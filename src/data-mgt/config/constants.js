991ec7d6e50a25350442556de0677faf1d084c77
Martin <bbaalmart@gmail.com>
2020-09-22 11:09:31 +0300

    refactoring DB configurations

--- src/data-mgt/config/constants.js
+++ src/data-mgt/config/constants.js
@@ -1,14 +1,17 @@
 const devConfig = {
-  MONGO_URL: "mongodb://localhost/data-mgt-dev",
+  MONGO_URL: "mongodb://localhost/",
   JWT_SECRET: process.env.JWT_SECRET,
+  DB_NAME: process.env.MONGO_DEV,
 };
 const testConfig = {
-  MONGO_URL: "mongodb://localhost/data-mgt-test",
+  MONGO_URL: "mongodb://localhost/",
   JWT_SECRET: process.env.JWT_SECRET,
+  DB_NAME: process.env.MONGO_TEST,
 };
 const prodConfig = {
   MONGO_URL: process.env.MONGO_GCE_URI,
   JWT_SECRET: process.env.JWT_SECRET,
+  DB_NAME: process.env.MONGO_PROD,
 };
 const defaultConfig = {
   PORT: process.env.PORT || 3000,
