76b850c5d049164186201753dae980ae6901122f
Martin <bbaalmart@gmail.com>
2020-09-24 11:00:22 +0300

    error handling for duplicate userName in the system

--- src/auth-service/utils/register.js
+++ src/auth-service/utils/register.js
@@ -1,39 +1,46 @@
 const transporter = require("../services/mailer");
 
 function register(req, res, mailOptions, body, entity, tenant) {
-  console.log("the values coming in: ");
-  console.dir(body);
-  entity.findOne({ email: body.email }).then((user) => {
-    if (user) {
-      return res
-        .status(400)
-        .json({
-          success: false,
-          email: `Email already exists for this orgnanisation (${tenant})`,
-        });
-    } else {
-      //this is where I call the registration function
-      const user = new entity(body);
-      user.save((error, savedData) => {
-        if (error) {
-          return console.log(error);
+  try {
+    console.log("the values coming in: ");
+    console.dir(body);
+    entity
+      .findOne({ email: body.email, userName: body.userName })
+      .then((user) => {
+        if (user) {
+          return res.status(400).json({
+            success: false,
+            email: `email or userName already exist for this orgnanisation (${tenant})`,
+          });
         } else {
-          transporter.sendMail(mailOptions, (err, response) => {
-            if (err) {
-              console.error("there was an error: ", err);
+          //this is where I call the registration function
+          const user = new entity(body);
+          user.save((error, savedData) => {
+            if (error) {
+              return console.log(error);
             } else {
-              console.log("here is the res: ", response);
-              res.status(200).json({
-                savedData,
-                success: true,
-                message: "user added successfully",
+              transporter.sendMail(mailOptions, (err, response) => {
+                if (err) {
+                  console.error("there was an error: ", err);
+                } else {
+                  console.log("here is the res: ", response);
+                  res.status(200).json({
+                    savedData,
+                    success: true,
+                    message: "user added successfully",
+                  });
+                }
               });
             }
           });
         }
       });
-    }
-  });
+  } catch (e) {
+    res.status(500).json({
+      success: false,
+      message: e.message,
+    });
+  }
 }
 
 module.exports = register;
