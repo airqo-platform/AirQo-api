00458ca3b1d069351844208e4425f4e8e174c393
Martin <bbaalmart@gmail.com>
2020-09-26 13:18:02 +0300

    error handling for non existent endpoints

--- src/auth-service/app.js
+++ src/auth-service/app.js
@@ -38,9 +38,13 @@ app.use(function (err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get("env") === "development" ? err : {};
 
+  res
+    .status(err.status || 500)
+    .json({ success: false, message: "this endpoint does not exist" });
+
   // render the error page
-  res.status(err.status || 500);
-  res.json({ error: err });
+  // res.status(err.status || 500);
+  // res.json({ error: err });
 });
 
 module.exports = app;
--- src/device-registry/app.js
+++ src/device-registry/app.js
@@ -37,8 +37,11 @@ app.use(function(err, req, res, next) {
   res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
-  res.status(err.status || 500);
-  res.render("error");
+  // res.status(err.status || 500);
+  res
+    .status(err.status || 500)
+    .json({ success: false, message: "this endpoint does not exist" });
+  // res.render("error");
 });
 
 module.exports = app;
