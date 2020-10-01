fb7c3ef0de3197dcf019733f27b0900ed9a497d3
danielogen <danielogen@gmail.com>
2020-10-01 18:33:11 +0300

    update simple logic

--- src/device-monitoring/helpers/db_helpers.py
+++ src/device-monitoring/helpers/db_helpers.py
@@ -7,20 +7,20 @@ import sys
 from dotenv import load_dotenv
 load_dotenv()
 
-if os.getenv('FLASK_ENV') == 'development':
-    MONGO_URI = os.getenv('MONGO_DEV_URI')
-    DB_NAME = os.getenv('DB_NAME_DEV')
+if os.getenv('FLASK_ENV') == 'production':
+    MONGO_URI = os.getenv('MONGO_GCE_URI')
+    DB_NAME = os.getenv('DB_NAME_PROD')
 elif os.getenv('FLASK_ENV') == 'testing':
     MONGO_URI = os.getenv('MONGO_GCE_URI')
     DB_NAME = os.getenv('DB_NAME_STAGE')
 else:
-    MONGO_URI = os.getenv('MONGO_GCE_URI')
-    DB_NAME = os.getenv('DB_NAME_PROD')
+    MONGO_URI = os.getenv('MONGO_DEV_URI')
+    DB_NAME = os.getenv('DB_NAME_DEV')
 
 
 def connect_mongo(tenant):
     client = MongoClient(MONGO_URI)
-    if tenant == 'airqo':
+    if tenant.lower() == 'airqo':
         db_selected = DB_NAME
     else:
         db_selected = DB_NAME + '_' + tenant
