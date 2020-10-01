f37467e597653935198c69e1f754638e39be907a
danielogen <danielogen@gmail.com>
2020-10-01 17:19:53 +0300

    separation of environments

--- src/device-monitoring/helpers/db_helpers.py
+++ src/device-monitoring/helpers/db_helpers.py
@@ -1,20 +1,28 @@
-#from helpers import helpers
+
 from pymongo import MongoClient
-#from app import mongo, MONGO_URI
+import app
 from datetime import datetime, timedelta
 import os
 import sys
 from dotenv import load_dotenv
 load_dotenv()
 
-MONGO_URI = os.getenv('MONGO_URI')
+if os.getenv('FLASK_ENV') == 'development':
+    MONGO_URI = os.getenv('MONGO_DEV_URI')
+    DB_NAME = os.getenv('DB_NAME_DEV')
+elif os.getenv('FLASK_ENV') == 'testing':
+    MONGO_URI = os.getenv('MONGO_GCE_URI')
+    DB_NAME = os.getenv('DB_NAME_STAGE')
+else:
+    MONGO_URI = os.getenv('MONGO_GCE_URI')
+    DB_NAME = os.getenv('DB_NAME_PROD')
 
 
 def connect_mongo(tenant):
     client = MongoClient(MONGO_URI)
     if tenant == 'airqo':
-        db_selected = 'airqo_netmanager'
+        db_selected = DB_NAME
     else:
-        db_selected = 'airqo_netmanager_' + tenant
+        db_selected = DB_NAME + '_' + tenant
     db = client[db_selected]
     return db
