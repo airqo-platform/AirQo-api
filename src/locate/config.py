2efe2767df375587a00360068907a69f728a2df1
danielogen <danielogen@gmail.com>
2020-10-03 14:43:52 +0300

    refactor and update config file

--- src/locate/config.py
+++ src/locate/config.py
@@ -1,42 +1,43 @@
 import logging
-from logging.handlers import TimedRotatingFileHandler
 import pathlib
 import os
 import sys
 from dotenv import load_dotenv
 
-load_dotenv()
-
-SECRET_KEY= os.getenv('SECRET_KEY')
 
 class Config:
-    DEBUG = False
-    TESTING = False
-    CSRF_ENABLED = True
-    SECRET_KEY = SECRET_KEY
-
-class ProductionConfig(Config):
     dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
     load_dotenv(dotenv_path)
     DEBUG = False
-    MONGO_URI = os.getenv("MONGO_URI")
+    TESTING = False
+    CSRF_ENABLED = True
+    SECRET_KEY = os.getenv("SECRET_KEY")
+    DB_NAME = os.getenv("DB_NAME_PROD")
+    MONGO_URI = os.getenv("MONGO_GCE_URI")
+
+
+class ProductionConfig(Config):
+    pass
+
 
 class DevelopmentConfig(Config):
     dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
     load_dotenv(dotenv_path)
+
     DEVELOPMENT = True
     DEBUG = True
-    MONGO_URI = os.getenv("MONGO_URI")
+    MONGO_URI = os.getenv("MONGO_DEV_URI")
+    DB_NAME = os.getenv("DB_NAME_DEV")
+
 
 class TestingConfig(Config):
     dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
     load_dotenv(dotenv_path)
+
     TESTING = True
-    MONGO_URI = os.getenv("MONGO_URI")
-    
+    MONGO_URI = os.getenv("MONGO_GCE_URI")
+    DB_NAME = os.getenv("DB_NAME_STAGE")
+
+
 app_config = {"development": DevelopmentConfig,
               "testing": TestingConfig, "production": ProductionConfig}
-
-if __name__ == '__main__':
-    print('package root', "Locate App")
-    print(app_config)
