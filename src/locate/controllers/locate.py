161a49585e334f759de17f035ad4364f14b3a800
danielogen <danielogen@gmail.com>
2020-10-09 14:11:50 +0300

    more exceptions handling

--- src/locate/controllers/locate.py
+++ src/locate/controllers/locate.py
@@ -1,11 +1,13 @@
 from flask import Blueprint, request, jsonify
 from helpers import helper
-import sys, ast, json
+import sys, ast, json, logging
 from models.parishes import Parish
 from models.map import Map
 from helpers import db_helpers
 from routes import api
 from flask_caching import Cache
+
+_logger = logging.getLogger(__name__)
 
 locate_blueprint = Blueprint('locate_blueprint', __name__)
 cache = Cache(config={'CACHE_TYPE':'simple'})
@@ -13,8 +15,7 @@ cache = Cache(config={'CACHE_TYPE':'simple'})
 locate_map = Map()
 
 
-@locate_blueprint.route(api.route['parishes'], methods=['POST'])
-@cache.cached(timeout=300)
+@locate_blueprint.route(api.route['parishes'], methods=['DELETE', 'GET', 'PUT', 'PATCH', 'POST'])
 def place_sensors_map():
     '''
     Returns parishes recommended by the model given the polygon and must-have coordinates
@@ -50,49 +51,53 @@ def place_sensors_map():
                     return helper.recommend_locations(sensor_number, must_have_coordinates, geometry, tenant)
                 else:
                     return {'message': 'Coordinates must be in the form [[longitude, latitude]]'}, 200
+    else:
+        return jsonify({"message": "Invalid request method. Please refer to the API documentation", "success": False}), 400
 
 
-@locate_blueprint.route(api.route['save_map'], methods=['GET', 'POST'])
-@cache.cached(timeout=300)
+@locate_blueprint.route(api.route['save_map'], methods=['DELETE', 'GET', 'PUT', 'PATCH', 'POST'])
 def save_locate_map():
     '''
     Saves planning space
     '''
-    # make sure content type is of type 'json'
-    if request.content_type != 'application/json':
-        error = json.dumps(
-            {"message": "Invalid Content Type", "success": False})
-        return jsonify(error, 400)
+    if request.method == 'POST':
+        # make sure content type is of type 'json'
+        if request.content_type != 'application/json':
+            error = json.dumps(
+                {"message": "Invalid Content Type", "success": False})
+            return jsonify(error, 400)
 
-    # check that all fields are supplied
-    data = request.json
-    if not all([data.get('user_id'), data.get('space_name'), data.get('plan')]):
-        error = json.dumps(
-            {"message": "Missing field/s (user_id, space_name or plan)", "success": False})
-        return jsonify(error, 400)
+        # check that all fields are supplied
+        data = request.json
+        if not all([data.get('user_id'), data.get('space_name'), data.get('plan')]):
+            error = json.dumps(
+                {"message": "Missing field/s (user_id, space_name or plan)", "success": False})
+            return jsonify(error, 400)
 
-    # make user_id is of type string
-    if type(data.get('user_id')) is not str:
-        error = json.dumps(
-            {"message": "Invalid user_id. string required!", "success": False})
-        return jsonify(error, 400)
+        # make user_id is of type string
+        if type(data.get('user_id')) is not str:
+            error = json.dumps(
+                {"message": "Invalid user_id. string required!", "success": False})
+            return jsonify(error, 400)
 
-    # if all checks have passed, save planning space
-    user_id = data['user_id']
-    space_name = data['space_name']
-    plan = data['plan']
-    
-    tenant = request.args.get('tenant')
-    if not tenant:
-        return jsonify({"message": "please specify the organization name. Refer to the API documentation for details.", "success": False}), 400
+        # if all checks have passed, save planning space
+        user_id = data['user_id']
+        space_name = data['space_name']
+        plan = data['plan']
+        
+        tenant = request.args.get('tenant')
+        if not tenant:
+            return jsonify({"message": "please specify the organization name. Refer to the API documentation for details.", "success": False}), 400
 
-    locate_map.save_locate_map(tenant, user_id, space_name, plan)
-    return jsonify({"message": "Locate Planning Space Saved Successfully", "success": True}), 200
+        locate_map.save_locate_map(tenant, user_id, space_name, plan)
+        return jsonify({"message": "Locate Planning Space Saved Successfully", "success": True}), 200
+    else:
+        return jsonify({"message": "Invalid request method. Please refer to the API documentation", "success": False}), 400
 
 
-@locate_blueprint.route(api.route['get_map'])
-@cache.cached(timeout=300)
-def get_locate_map(user_id, methods=['GET']):
+
+@locate_blueprint.route(api.route['get_map'], methods=['DELETE', 'GET', 'PUT', 'PATCH', 'POST'])
+def get_locate_map(user_id):
     '''
     Get saved planning space for the user
     '''
@@ -110,48 +115,49 @@ def get_locate_map(user_id, methods=['GET']):
             return jsonify({"message": "no record available. please check the user_id or organization name.", "success": False}), 400
         return jsonify(response), 200
     else:
-        return jsonify({"message": "Invalid request method", "success": False}), 400
+        return jsonify({"message": "Invalid request method. please refer to the API documentation", "success": False}), 400
 
 
-@locate_blueprint.route(api.route['update_map'], methods=['GET', 'POST'])
-@cache.cached(timeout=300)
+@locate_blueprint.route(api.route['update_map'], methods=['DELETE', 'GET', 'PUT', 'PATCH', 'POST'])
 def update_locate_map(space_name):
     '''
     updates a previously saved planning space
     @param: space_name
     @return: message: <MESSAGE> , status: <BOOLEAN>
     '''
-
-    try:
-        # Get the value which needs to be updated
+    if request.method == 'PUT':
         try:
-            json_data = request.get_json()
-            update_plan = json_data.get('plan')
+            # Get the value which needs to be updated
+            try:
+                json_data = request.get_json()
+                update_plan = json_data.get('plan')
+            except:
+                # Bad request as the request body is not available
+                return jsonify({"message": "bad request! request body required.", "success": False}), 400
+            
+            # check for organization name
+            tenant = request.args.get('tenant')
+            if not tenant:
+                return jsonify({"message": "please specify the organization name. Refer to the API documentation for details.", "success": False}), 400
+            
+            # Updating the planning space
+            records_updated = locate_map.update_locate_map(tenant, space_name, update_plan)
+
+            # Check if resource is updated
+            if records_updated.modified_count > 0:
+                # Prepare the response as resource is updated successfully
+                return jsonify({"message": "planning space '" + space_name + "' updated successfully", "success": True}), 200
+            else:
+                # Bad request as the resource is not available to update
+                return jsonify({"message": "planning not updated. please make sure the plan name / request body is correct", "success": False}), 404
         except:
-            # Bad request as the request body is not available
-            return jsonify({"message": "bad request! request body required.", "success": False}), 400
-        
-        # check for organization name
-        tenant = request.args.get('tenant')
-        if not tenant:
-            return jsonify({"message": "please specify the organization name. Refer to the API documentation for details.", "success": False}), 400
-        
-        # Updating the planning space
-        records_updated = locate_map.update_locate_map(tenant, space_name, update_plan)
+            # Error while trying to update the resource
+            return jsonify({"message": "error occured while trying to update planning space", "success": False}), 500
+    else:
+       return jsonify({"message": "Invalid request method. Please refer to the API documentation", "success": False}), 400
+ 
 
-        # Check if resource is updated
-        if records_updated.modified_count > 0:
-            # Prepare the response as resource is updated successfully
-            return jsonify({"message": "planning space '" + space_name + "' updated successfully", "success": True}), 200
-        else:
-            # Bad request as the resource is not available to update
-            return jsonify({"message": "planning not updated. please make sure the plan name / request body is correct", "success": False}), 404
-    except:
-        # Error while trying to update the resource
-        return jsonify({"message": "error occured while trying to update planning space", "success": False}), 500
-
-@locate_blueprint.route(api.route['delete_map'], methods=['DELETE'])
-@cache.cached(timeout=300)
+@locate_blueprint.route(api.route['delete_map'], methods=['DELETE', 'GET', 'PUT', 'PATCH', 'POST'])
 def delete_locate_map(space_name):
     '''
     deletes a previously saved planning space
@@ -169,7 +175,7 @@ def delete_locate_map(space_name):
                     "message": "planning space deleted successfully", "success": True}
             else:
                 response = {
-                    "message": "planning space name not found. Please enter a correct planning space name", "Success": False}
+                    "message": "planning space name not found of " + tenant + " organization. Please enter a correct planning space or organization name", "Success": False}
             return jsonify(response), 200
         else:
             return jsonify({"message": "Bad request parameters!", "success": False}), 400
