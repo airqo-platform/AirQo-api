8dd3c481bf7be2cb545512724a18b9c31e45611c
Martin <bbaalmart@gmail.com>
2020-10-14 10:37:15 +0300

    chores: project setup

--- pipeline/cloud-functions/node/app.js
+++ pipeline/cloud-functions/node/app.js
@@ -1,31 +1,33 @@
-var createError = require("http-errors");
 var express = require("express");
 var path = require("path");
+var logger = require("morgan");
 const dotenv = require("dotenv");
+var bodyParser = require("body-parser");
 dotenv.config();
 var cookieParser = require("cookie-parser");
-
-var logger = require("morgan");
-
 var api = require("./routes/api");
+const { mongodb } = require("./config/database");
+mongodb;
 
 var app = express();
 
-// view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "jade");
-
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger("dev"));
+app.use(bodyParser.json());
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+// app.use(bindCurrentNamespace);
 
-app.use("/api/etl", api);
+app.use("/api/v1/pipeline", api);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-  next(createError(404));
+  var err = new Error("Not Found");
+  err.status = 404;
+  next(err);
 });
 
 // error handler
@@ -35,8 +37,13 @@ app.use(function (err, req, res, next) {
   res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
-  res.status(err.status || 500);
-  res.render("error");
+  // res.status(err.status || 500);
+  res.status(err.status || 500).json({
+    success: false,
+    message: "this endpoint does not exist",
+    error: err.message,
+  });
+  // res.render("error");
 });
 
 module.exports = app;
--- pipeline/cloud-functions/node/config/constants.js
+++ pipeline/cloud-functions/node/config/constants.js
@@ -1,36 +1,95 @@
-GET_CHANNELS_TS_URI = `https://api.thingspeak.com/channels.json?api_key=${process.env.TS_API_KEY}`;
-GET_RECENT_FEEDS = (channelID) => {
-  return `https://data-manager-dot-airqo-250220.uc.r.appspot.com/api/v1/data/feeds/recent/${channelID}`;
-};
-DEVICE_REGISTRY_STAGE_BASE_URI = `http://34.78.78.202:31002/api/v1/devices/`;
-DEVICE_REGISTRY_PROD_BASE_URI = `http://34.78.78.202:31002/api/v1/devices/`;
-SINGLE_INSERT_PARTIAL = (deviceName, ctype) => {
-  return `api/v1/devices/components/add/values?device=${deviceName}&component=${deviceName}_${ctype}&tenant=airqo`;
-};
-BULK_INSERT_PARTIAL = (base_url, deviceName, ctype) => {
-  return `${base_url}/components/add/values/bulk?device=${deviceName}&component=${deviceName}_${ctype}`;
-};
-const DEVICE_REGITRY_BASE_URL = "http://34.78.78.202:31002/api/v1/devices/";
-const MOST_RECENT_FEEDS_API_TS = (channel) => {
-  return `https://api.thingspeak.com/channels/${channel}/feeds.json`;
-};
-const MOST_RECENT_FEEDS_API = (channel) => {
-  return `https://data-manager-dot-airqo-250220.uc.r.appspot.com/api/v1/data/feeds/recent/${channel}`;
-};
-const SINGLE_INSERT_PARTIAL_API = (deviceName, ctype) => {
-  return `api/v1/devices/components/add/values?device=${deviceName}&component=${deviceName}_${ctype}&tenant=airqo`;
-};
-const SAMPLE_FILE =
-  "https://docs.google.com/spreadsheets/d/1ItQiF5LXhMLq4dRKRX6PqbM3cNMOt0SVtFU3_Zw0OmY/edit#gid=1433489674";
+const { logElement } = require("../utils/log");
 
-module.exports = {
-  MOST_RECENT_FEEDS_API,
-  SINGLE_INSERT_PARTIAL_API,
-  DEVICE_REGITRY_BASE_URL,
-  SAMPLE_FILE,
-  SINGLE_INSERT_PARTIAL,
-  DEVICE_REGISTRY_PROD_BASE_URI,
-  DEVICE_REGISTRY_STAGE_BASE_UR,
-  GET_RECENT_FEEDS,
-  GET_CHANNELS_TS_URI,
+const devConfig = {
+  MONGO_URI: `mongodb://localhost/`,
+  DB_NAME: process.env.MONGO_DEV,
 };
+const prodConfig = {
+  MONGO_URI: process.env.MONGO_GCE_URI,
+  DB_NAME: process.env.MONGO_PROD,
+};
+
+const stageConfig = {
+  MONGO_URI: process.env.MONGO_GCE_URI,
+  DB_NAME: process.env.MONGO_STAGE,
+};
+
+const defaultConfig = {
+  PORT: process.env.PORT || 3000,
+  CREATE_THING_URL: `https://api.thingspeak.com/channels.json?api_key=${process.env.TS_API_KEY}`,
+  DELETE_THING_URL: (device) => {
+    return `https://api.thingspeak.com/channels/${device}.json?api_key=${process.env.TS_API_KEY}`;
+  },
+  CLEAR_THING_URL: (device) => {
+    return `https://api.thingspeak.com/channels/${device}/feeds.json?api_key=${process.env.TS_API_KEY}`;
+  },
+  UPDATE_THING: (device) => {
+    return `https://api.thingspeak.com/channels/${device}.json?api_key=${process.env.TS_API_KEY}`;
+  },
+  ADD_VALUE: (field, value, apiKey) => {
+    `https://api.thingspeak.com/update.json?api_key=${apiKey}&${field}=${value}`;
+  },
+  ADD_VALUES: (device) => {
+    return `https://api.thingspeak.com/channels/${device}/bulk_update.json`;
+  },
+  JWT_SECRET: process.env.JWT_SECRET,
+  REGION: "europe-west1",
+  MQTT_BRIDGE_HOST_NAME: "mqtt.googleapis.com",
+  MQTT_BRIDGE_PORT: 8883,
+  NUM_MESSAGES: 5,
+  TOKEN_EXP_MINS: 360,
+  ALGORITHM: "RS256",
+  HTTP_BRIDGE_ADDRESS: "cloudiotdevice.googleapis.com",
+  MESSAGE_TYPE: "events",
+  MINIMUM_BACKOFF_TIME: 1,
+  MAXIMUM_BACKOFF_TIME: 32,
+  DEVICE_CREATION: {
+    field1: "Sensor1 PM2.5",
+    field2: "Sensor1 PM10",
+    field3: "Sensor2 PM2.5",
+    field4: "Sensor2 PM10",
+    field5: "Humidity",
+    field5: "Battery Voltage",
+    field6: "Temperature",
+    field7: "NO2",
+    field8: "SO3",
+  },
+  N_VALUES: 200,
+  GET_CHANNELS_TS_URI: `https://api.thingspeak.com/channels.json?api_key=${process.env.TS_API_KEY}`,
+  GET_RECENT_FEEDS: (channelID) => {
+    return `https://data-manager-dot-airqo-250220.uc.r.appspot.com/api/v1/data/feeds/recent/${channelID}`;
+  },
+  DEVICE_REGISTRY_STAGE_BASE_URI: `http://34.78.78.202:31002/api/v1/devices/`,
+  DEVICE_REGISTRY_PROD_BASE_URI: `http://34.78.78.202:31002/api/v1/devices/`,
+  SINGLE_INSERT_PARTIAL: (deviceName, ctype) => {
+    return `api/v1/devices/components/add/values?device=${deviceName}&component=${deviceName}_${ctype}&tenant=airqo`;
+  },
+  BULK_INSERT_PARTIAL: (base_url, deviceName, ctype) => {
+    return `${base_url}/components/add/values/bulk?device=${deviceName}&component=${deviceName}_${ctype}`;
+  },
+  DEVICE_REGITRY_BASE_URL: "http://34.78.78.202:31002/api/v1/devices/",
+  MOST_RECENT_FEEDS_API_TS: (channel) => {
+    return `https://api.thingspeak.com/channels/${channel}/feeds.json`;
+  },
+  MOST_RECENT_FEEDS_API: (channel) => {
+    return `https://data-manager-dot-airqo-250220.uc.r.appspot.com/api/v1/data/feeds/recent/${channel}`;
+  },
+  SINGLE_INSERT_PARTIAL_API: (deviceName, ctype) => {
+    return `api/v1/devices/components/add/values?device=${deviceName}&component=${deviceName}_${ctype}&tenant=airqo`;
+  },
+  SAMPLE_FILE:
+    "https://docs.google.com/spreadsheets/d/1ItQiF5LXhMLq4dRKRX6PqbM3cNMOt0SVtFU3_Zw0OmY/edit#gid=1433489674",
+};
+
+function envConfig(env) {
+  switch (env) {
+    case "development":
+      return devConfig;
+    case "staging":
+      return stageConfig;
+    default:
+      return prodConfig;
+  }
+}
+
+module.exports = { ...defaultConfig, ...envConfig(process.env.NODE_ENV) };
