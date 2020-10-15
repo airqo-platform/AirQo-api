42ed77e6b133c69c6485df0b9d0520edb1da2ce0
Martin <bbaalmart@gmail.com>
2020-10-15 18:06:15 +0300

    adding tenancy to our models

--- src/analytics/models/report.py
+++ src/analytics/models/report.py
@@ -1,6 +1,7 @@
-import app 
-from datetime import datetime,timedelta
+import app
+from datetime import datetime, timedelta
 from helpers import mongo_helpers, helpers
+
 
 class Report():
     """The class contains functionality for creating, retrieving report templates.
@@ -10,13 +11,15 @@ class Report():
         attr2 (:obj:`int`, optional): Description of `attr2`.
 
     """
-    
-    def __init__(self):
-        """ initialize """ 
+
+    def __init__(self, tenant):
+        """ initialize """
+        self.tenant = tenant
 
     # save default report template
-    def save_default_report_template(self, user_id, report_name, report_body):        
-        app.mongo.db.report_template.insert({
+    def save_default_report_template(self, user_id, report_name, report_body):
+        db = mongo_helpers.connect_mongo(self.tenant)
+        db.report_template.insert({
             "user_id": user_id,
             "report_date": datetime.now(),
             "report_type": "default",
@@ -24,45 +27,45 @@ class Report():
             "report_body": report_body
         })
 
-
     # get default report template
-    def get_default_report_template(self):        
-        documents = app.mongo.db.report_template.find({"report_type": "default"})
+    def get_default_report_template(self):
+        db = mongo_helpers.connect_mongo(self.tenant)
+        documents = db.report_template.find({"report_type": "default"})
         return documents
 
-
     # save monthly report
-    def save_monthly_report(self, user_id, report_name, report_body):    
-        app.mongo.db.report_template.insert({
+    def save_monthly_report(self, user_id, report_name, report_body):
+        db = mongo_helpers.connect_mongo(self.tenant)
+        db.report_template.insert({
             "user_id": user_id,
             "report_date": datetime.now(),
             "report_name": report_name,
             "report_body": report_body
         })
 
-
     # get monthly
-    def get_monthly_report(self, user_id):    
-        documents = app.mongo.db.report_template.find({"user_id": str(user_id)})
+    def get_monthly_report(self, user_id):
+        db = mongo_helpers.connect_mongo(self.tenant)
+        documents = db.report_template.find({"user_id": str(user_id)})
         return documents
 
-
     # update previously saved report
-    def update_default_report_template(self,report_body):    
-        response = app.mongo.db.report_template.update_one(
+    def update_default_report_template(self, report_body):
+        db = mongo_helpers.connect_mongo(self.tenant)
+        response = db.report_template.update_one(
             {'report_type': 'default'}, {'$set': {'report_body': report_body}})
         return response
 
-
     # update previously saved report
-    def update_monthly_report(self, report_name, report_body):    
-        response = app.mongo.db.report_template.update_one(
+    def update_monthly_report(self, report_name, report_body):
+        db = mongo_helpers.connect_mongo(self.tenant)
+        response = db.report_template.update_one(
             {"report_name": report_name}, {'$set': {'report_body': report_body}})
         return response
 
-
     # delete previously saved planning space
-    def delete_monthly_report(self, report_name):    
-        response = app.mongo.db.report_template.delete_one({'report_name': report_name})
-        return response
 
+    def delete_monthly_report(self, report_name):
+        db = mongo_helpers.connect_mongo(self.tenant)
+        response = db.report_template.delete_one({'report_name': report_name})
+        return response
