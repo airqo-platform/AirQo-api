4da59e322a7d78630524f678072c6267e0f8c9fd
Martin <bbaalmart@gmail.com>
2020-10-15 17:45:44 +0300

    environment separation

--- src/analytics/app.py
+++ src/analytics/app.py
@@ -6,7 +6,7 @@ from os.path import join, dirname
 from dotenv import load_dotenv
 from flask_pymongo import PyMongo
 from flask_cors import CORS
-
+load_dotenv()
 
 
 _logger = logging.getLogger(__name__)
@@ -14,26 +14,26 @@ _logger = logging.getLogger(__name__)
 mongo = PyMongo()
 MONGO_URI = ''
 
+
 def create_app(*, config_object) -> Flask:
     """Create a flask app instance."""
-    
+
     flask_app = Flask(__name__)
-    flask_app.config.from_object(app_config[config_object]) 
-    #flask_app.config["MONGO_URI"] =  
+    flask_app.config.from_object(app_config[config_object])
+    # flask_app.config["MONGO_URI"] =
     MONGO_URI = flask_app.config["MONGO_URI"]
-    #allow cross domain requests
+    # allow cross domain requests
     CORS(flask_app)
     flask_app.config['CORS_HEADERS'] = 'Content-Type'
-    #register the app with the db.
+    # register the app with the db.
     mongo.init_app(flask_app)
-    
+
     #import blueprints
     from controllers.monitoring_site import monitoring_site_bp
     from controllers.helpers import analytics_app
     from controllers.dashboard import dashboard_bp
-    from controllers.graph import graph_bp  
-    from controllers.report import report_bp 
-
+    from controllers.graph import graph_bp
+    from controllers.report import report_bp
 
     flask_app.register_blueprint(analytics_app)
     flask_app.register_blueprint(monitoring_site_bp)
@@ -41,9 +41,7 @@ def create_app(*, config_object) -> Flask:
     flask_app.register_blueprint(graph_bp)
     flask_app.register_blueprint(report_bp)
 
-
     return flask_app
-   
 
 
 application = create_app(config_object=os.getenv("FLASK_ENV"))
--- src/analytics/config.py
+++ src/analytics/config.py
@@ -4,6 +4,7 @@ import os
 import sys
 from dotenv import load_dotenv
 
+
 class Config:
     DEBUG = False
     TESTING = False
@@ -11,18 +12,17 @@ class Config:
     SECRET_KEY = os.getenv("SECRET_KEY")
     #SERVER_PORT = 5000
 
-    CLARITY_API_BASE_URL= os.getenv("CLARITY_API_BASE_URL")
-    CLARITY_API_KEY= os.getenv("CLARITY_API_KEY")
-
-    
+    CLARITY_API_BASE_URL = os.getenv("CLARITY_API_BASE_URL")
+    CLARITY_API_KEY = os.getenv("CLARITY_API_KEY")
 
 
 class ProductionConfig(Config):
-    dotenv_path = os.path.join(os.path.dirname(__file__), 'production.env')
+    dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
     load_dotenv(dotenv_path)
     DEBUG = False
     #SERVER_PORT = os.environ.get('PORT', 5000)
-    MONGO_URI = os.getenv("MONGO_URI") 
+    MONGO_URI = os.getenv("MONGO_GCE_URI")
+    DB_NAME = os.getenv("MONGO_PROD")
 
 
 class DevelopmentConfig(Config):
@@ -31,21 +31,22 @@ class DevelopmentConfig(Config):
 
     DEVELOPMENT = True
     DEBUG = True
-    MONGO_URI = os.getenv("MONGO_URI") 
+    MONGO_URI = os.getenv("MONGO_LOCAL_URI")
+    DB_NAME = os.getenv("MONGO_DEV")
 
 
 class TestingConfig(Config):
-    dotenv_path = os.path.join(os.path.dirname(__file__), 'testing.env')
+    dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
     load_dotenv(dotenv_path)
 
     TESTING = True
-    MONGO_URI = os.getenv("MONGO_URI") 
-    
+    MONGO_URI = os.getenv("MONGO_GCE_URI")
+    DB_NAME = os.getenv("MONGO_STAGE")
 
-app_config = {"development": DevelopmentConfig, "testing": TestingConfig, "production": ProductionConfig}
+
+app_config = {"development": DevelopmentConfig,
+              "testing": TestingConfig, "production": ProductionConfig}
 
 
 if __name__ == '__main__':
     print('package root', "Analytics App")
-
-
