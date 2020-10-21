c586deca7535a400f4a1e1fb310f1953755df848
Priscilla-AL <priscilla@airqo.net>
2020-10-21 10:42:15 +0300

    Update calibrate.py

--- src/calibrate/controllers/calibrate.py
+++ src/calibrate/controllers/calibrate.py
@@ -11,10 +11,11 @@ calibrate_bp = Blueprint('calibrate_bp', __name__)
 
 @calibrate_bp.route(api.route['calibrate'], methods=['POST', 'GET'])
 def calibrate_pm25_values():
-    #predicted_values = intercept +  slope * actual_june_bam_x_values
+    #predicted_values = intercept +  slope * raw_value
     if request.method == 'GET':
         raw_value = request.args.get('raw_value')
-        calibrated_value = 0.469 + 0.686 * float(raw_value)
+        #calibrated_value = 0.469 + 0.686 * float(raw_value)
+        calibrated_value = intercept + slope * float(raw_value)
         uncertainty_value = 1.2*float(raw_value)
         std_value = 0.5*float(raw_value)
         return jsonify({"message": "caliiiii", "calibrattion": calibrated_value, "uncertainty_value": calibrated_value, "std_value": calibrated_value})
