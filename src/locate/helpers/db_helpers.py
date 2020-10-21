d7eb64addd25dfc6a007f6f15e124504b1d45728
danielogen <danielogen@gmail.com>
2020-10-21 16:33:04 +0300

    refactor db_helper file

--- src/locate/helpers/db_helpers.py
+++ src/locate/helpers/db_helpers.py
@@ -1,24 +1,16 @@
 from pymongo import MongoClient
-import app
 from datetime import datetime, timedelta
 import os
 import sys
+from config import app_config
 from dotenv import load_dotenv
 load_dotenv()
 
-if os.getenv('FLASK_ENV') == 'production':
-    MONGO_URI = os.getenv('MONGO_GCE_URI')
-    DB_NAME = os.getenv('DB_NAME_PROD')
-elif os.getenv('FLASK_ENV') == 'testing':
-    MONGO_URI = os.getenv('MONGO_GCE_URI')
-    DB_NAME = os.getenv('DB_NAME_STAGE')
-else:
-    MONGO_URI = os.getenv('MONGO_DEV_URI')
-    DB_NAME = os.getenv('DB_NAME_DEV')
+
+app_configuration = app_config.get(os.getenv("FLASK_ENV"))
 
 
 def connect_mongo(tenant):
-    client = MongoClient(MONGO_URI)
-    db_selected = DB_NAME + '_' + tenant.lower()
-    db = client[db_selected]
+    client = MongoClient(app_configuration.MONGO_URI)
+    db = client[f'{app_configuration.DB_NAME}_{tenant.lower()}']
     return db
