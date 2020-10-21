f040bc613e044f33d1ef2fedafa4db696f47fbe8
Priscilla-AL <priscilla@airqo.net>
2020-10-21 21:08:36 +0300

    Update calibrate.py

--- src/calibrate/controllers/calibrate.py
+++ src/calibrate/controllers/calibrate.py
@@ -14,8 +14,8 @@ def calibrate_pm25_values():
     #predicted_values = intercept +  slope * raw_value
     if request.method == 'GET':
         raw_value = request.args.get('raw_value')
-        #calibrated_value = 0.469 + 0.686 * float(raw_value)
-        calibrated_value = intercept + slope * float(raw_value)
+        calibrated_value = 0.469 + 0.686 * float(raw_value)
+        #calibrated_value = intercept + slope * float(raw_value)
         uncertainty_value = 1.2*float(raw_value)
         std_value = 0.5*float(raw_value)
         return jsonify({"message": "caliiiii", "calibrattion": calibrated_value, "uncertainty_value": calibrated_value, "std_value": calibrated_value})
