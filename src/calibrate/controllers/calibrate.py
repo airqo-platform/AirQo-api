f3ccb2fd3683bf012d870c40cbb9552971ce5736
Priscilla-AL <priscilla@airqo.net>
2020-10-22 12:15:17 +0300

    Update calibrate.py

--- src/calibrate/controllers/calibrate.py
+++ src/calibrate/controllers/calibrate.py
@@ -5,6 +5,10 @@ import logging
 import app
 import json
 import uncertainties.unumpy as unp
+from jobs import regression
+
+
+# from jobs.regression import regression_bp
 
 calibrate_bp = Blueprint('calibrate_bp', __name__)
 
@@ -12,10 +16,13 @@ calibrate_bp = Blueprint('calibrate_bp', __name__)
 @calibrate_bp.route(api.route['calibrate'], methods=['POST', 'GET'])
 def calibrate_pm25_values():
     #predicted_values = intercept +  slope * raw_value
+
+    print(regression.intercept())
+   
     if request.method == 'GET':
         raw_value = request.args.get('raw_value')
         calibrated_value = 0.469 + 0.686 * float(raw_value)
-        #calibrated_value = intercept + slope * float(raw_value)
+        #calibrated_value = regression.intercept + regression.slope * float(raw_value) 
         uncertainty_value = 1.2*float(raw_value)
         std_value = 0.5*float(raw_value)
         return jsonify({"message": "caliiiii", "calibrattion": calibrated_value, "uncertainty_value": calibrated_value, "std_value": calibrated_value})
