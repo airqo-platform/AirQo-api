2d231b7caffa0369ab92cf74a10ac002bbc2c533
Martin <bbaalmart@gmail.com>
2020-10-23 14:41:41 +0300

    refactoring to reduce function length

---
+++ src/device-registry/utils/get-events.js
@@ -0,0 +1,123 @@
+const EventSchema = require("models/Event");
+const HTTPStatus = require("http-status");
+const { getModelByTenant } = require("utils/multitenancy");
+const isEmpty = require("is-empty");
+
+const responseDevice = async (res, filter, tenant) => {
+  try {
+    const events = await getModelByTenant(tenant, "event", EventSchema)
+      .find(filter)
+      .exec();
+    if (!isEmpty(events)) {
+      return res.status(HTTPStatus.OK).json({
+        success: true,
+        message: `successfully listed the Events for device ${device}`,
+        events,
+      });
+    } else if (isEmpty(events)) {
+      return res.status(HTTPStatus.BAD_GATEWAY).json({
+        success: false,
+        message: `unable to find the Events for device ${device}`,
+      });
+    }
+  } catch (e) {
+    return res.status(HTTPStatus.BAD_GATEWAY).json({
+      success: false,
+      message: `Server Error`,
+      error: e.message,
+    });
+  }
+};
+
+const responseComponent = async (res, filter, tenant) => {
+  try {
+    const events = await getModelByTenant(tenant, "event", EventSchema)
+      .find(filter)
+      .exec();
+    if (!isEmpty(events)) {
+      return res.status(HTTPStatus.OK).json({
+        success: true,
+        message: `successfully listed the Events for the component ${filter.componentName}`,
+        events,
+      });
+    } else if (isEmpty(events)) {
+      return res.status(HTTPStatus.BAD_GATEWAY).json({
+        success: false,
+        message: `unable to find the Events for the component ${filter.componentName}`,
+      });
+    }
+  } catch (e) {
+    return res.status(HTTPStatus.BAD_GATEWAY).json({
+      success: false,
+      message: `Server Error`,
+      error: e.message,
+    });
+  }
+};
+
+const responseDeviceAndComponent = async (res, filter, tenant) => {
+  try {
+    const event = await getModelByTenant(tenant, "event", EventSchema)
+      .find(filter)
+      .exec();
+    if (!isEmpty(event)) {
+      return res.status(HTTPStatus.OK).json({
+        success: true,
+        message: `successfully listed the Events for this device (${filter.deviceName}) component (${filter.componentName})`,
+        event,
+      });
+    } else if (isEmpty(event)) {
+      return res.status(HTTPStatus.BAD_GATEWAY).json({
+        success: false,
+        message: `unable to find that Component ${filter.componentName} for device ${filter.deviceName}`,
+      });
+    }
+  } catch (e) {
+    return res.status(HTTPStatus.BAD_GATEWAY).json({
+      success: false,
+      message: `Server Error`,
+      error: e.message,
+    });
+  }
+};
+
+const responseAll = async (req, res, tenant) => {
+  try {
+    const limit = parseInt(req.query.limit, 0);
+    const skip = parseInt(req.query.skip, 0);
+    const events = await getModelByTenant(tenant, "event", EventSchema).list({
+      limit,
+      skip,
+    });
+    if (!isEmpty(events)) {
+      return res.status(HTTPStatus.OK).json({
+        success: true,
+        message: "successfully listed all platform Events",
+        tip:
+          "use documented query parameters (device/comp) to filter your search results",
+        events,
+      });
+    } else if (isEmpty(events)) {
+      return res.status(HTTPStatus.BAD_GATEWAY).json({
+        success: false,
+        message: `unable to find all the platform Events`,
+      });
+    }
+  } catch (e) {
+    return res.status(HTTPStatus.BAD_GATEWAY).json({
+      success: false,
+      message: `Server Error`,
+      error: e.message,
+    });
+  }
+};
+
+const responseDateRanges = () => {};
+
+module.exports = {
+  responseAll,
+  responseDateRanges,
+  responseDevice,
+  responseDeviceAndComponent,
+  responseComponent,
+};
