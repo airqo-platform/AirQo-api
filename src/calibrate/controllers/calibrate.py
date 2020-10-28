a7b5cef1898762e6cfdd3ae40ba3fa8664cc44ed
Priscilla-AL <priscilla@airqo.net>
2020-10-28 09:58:03 +0300

    Update calibrate.py

--- src/calibrate/controllers/calibrate.py
+++ src/calibrate/controllers/calibrate.py
@@ -20,19 +20,11 @@ db = client['airqo_netmanager_staging']
 
 @calibrate_bp.route(api.route['calibrate'], methods=['POST', 'GET'])
 def calibrate_pm25_values():
-    #predicted_values = intercept +  slope * raw_value
-   
     if request.method == 'GET':
-        #raw_value = request.args.get('raw_value')
-        #calibrated_value = 0.469 + 0.686 * float(raw_value)   {"sensorID":{ '$eq': 'sensor_2' } },
-        # raw_value = list(db.sensors.find({'quantityKind':1}))
-    
-
         raw_values_dict = db.events.find({'values.raw': {'$exists': 1}},{'_id':0, 'values.raw': 1})
         raw_value_list = []
         for value in raw_values_dict:
             raw_value_list.append(value)
-                   
         raw_values = [pm['values'][0]['raw'] for pm in raw_value_list]    
   
         calibrated_value = regression.intercept + regression.slope * raw_values
