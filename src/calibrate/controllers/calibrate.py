f3416172d28cf16b18c61fb5887586a98f2974b4
Priscilla-AL <priscilla@airqo.net>
2020-10-28 21:34:09 +0300

    Update calibrate.py

--- src/calibrate/controllers/calibrate.py
+++ src/calibrate/controllers/calibrate.py
@@ -16,20 +16,21 @@ calibrate_bp = Blueprint('calibrate_bp', __name__)
 
 MONGO_URI = os.getenv("MONGO_URI")
 client = MongoClient(MONGO_URI)
-db = client['airqo_netmanager_staging']
+db = client['airqo_netmanager_staging_airqo']
 
 
 @calibrate_bp.route(api.route['calibrate'], methods=['POST', 'GET'])
 def calibrate_pm25_values():
     if request.method == 'GET':
-        raw_values_dict = db.events.find({'values.raw': {'$exists': 1}},{'_id':0, 'values.raw': 1})
-        raw_value_list = []
-        for value in raw_values_dict:
-            raw_value_list.append(value)
-        raw_values = [pm['values'][0]['raw'] for pm in raw_value_list]    
-  
-        calibrated_value = regression.intercept + regression.slope * raw_values
+        # raw_values_dict = db.events.find({'values.raw': {'$exists': 1}},{'_id':0, 'values.raw': 1})
+        # raw_value_list = []
+        # for value in raw_values_dict:
+        #     raw_value_list.append(value)
+        # raw_values = [pm['values'][0]['raw'] for pm in raw_value_list]  
+      
+        raw_values = request.args.get('raw_value')
+        calibrated_value = regression.intercept + regression.slope * float(raw_values)
         calibrated_value = calibrated_value.tolist()
         performance_eval = regression.performance
         
-        return jsonify({" raw_values": raw_values, "calibrated Values": calibrated_value, "Performance Evaluation": performance_eval})
+        return jsonify({"calibrated Value": calibrated_value, "Performance Evaluation": performance_eval})
