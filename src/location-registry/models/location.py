f3f9a76a4f9e8517fd492b76f7b9596cf35b50c0
danielogen <danielogen@gmail.com>
2020-10-28 11:01:08 +0300

    refactor the models to use db_helper function

--- src/location-registry/models/location.py
+++ src/location-registry/models/location.py
@@ -1,59 +1,48 @@
 import pandas as pd
 from pymongo import MongoClient
-from dotenv import load_dotenv
+from helpers import db_helpers
 import os
 import sys
-load_dotenv()
+import app
 
-if os.getenv('FLASK_ENV') == 'production':
-    MONGO_URI = os.getenv('PROD_MONGO_URI')
-    DB_NAME  = os.getenv('DB_NAME_PROD')
-elif os.getenv('FLASK_ENV') == 'testing':
-    MONGO_URI = os.getenv('PROD_MONGO_URI')
-    DB_NAME  = os.getenv('DB_NAME_STAGING')
-else:
-    MONGO_URI = os.getenv('DEV_MONGO_URI')
-    DB_NAME  = os.getenv('DB_NAME_PROD')
 
 class Location():
 
     def __init__(self):
         ''' 
         initialize 
-        '''    
+        '''
 
     def register_location(self, tenant_id, loc_ref, host_name, mobility=None, longitude=None, latitude=None, road_intensity=None, description=None,
-                          road_status=None, local_activities=None, loc_name=None, country=None, region=None, district=None, 
-                          county=None, subcounty=None, parish=None, altitude=None, aspect=None, landform_90=None, landform_270=None, 
-                          distance_from_nearest_road=None, distance_from_motorway=None, distance_from_residential=None, 
+                          road_status=None, local_activities=None, loc_name=None, country=None, region=None, district=None,
+                          county=None, subcounty=None, parish=None, altitude=None, aspect=None, landform_90=None, landform_270=None,
+                          distance_from_nearest_road=None, distance_from_motorway=None, distance_from_residential=None,
                           distance_from_city=None):
         '''
         Saves a new location into the database
         '''
         location_dict = {'loc_ref': loc_ref, 'host': host_name, 'mobility': mobility, 'latitude': latitude, 'longitude': longitude,
-                         'road_intensity': road_intensity, 'description': description, 'road_status': road_status, 
-                         'local_activities': local_activities, 'location_name': loc_name, 'country': country, 'region': region, 
+                         'road_intensity': road_intensity, 'description': description, 'road_status': road_status,
+                         'local_activities': local_activities, 'location_name': loc_name, 'country': country, 'region': region,
                          'district': district, 'county': county, 'subcounty': subcounty, 'parish': parish, 'altitude': altitude, 'aspect': aspect,
                          'landform_90': landform_90, 'landform_270': landform_270, 'distance_from_nearest_road': distance_from_nearest_road,
                          'distance_from_motorway': distance_from_motorway, 'distance_from_residential': distance_from_residential,
                          'distance_from_city': distance_from_city}
 
-        client = MongoClient(MONGO_URI)
-        db_name = DB_NAME+tenant_id
-        db = client[db_name]
+        db = db_helpers.connect_mongo(tenant_id)
         db.location_registry.insert_one(location_dict)
 
     def all_locations(self, tenant_id):
         '''
         Gets specific fields of all locations to be displayed
         '''
-        client = MongoClient(MONGO_URI)
-        db_name = f'{DB_NAME}_{tenant_id.lower()}'
+        client = MongoClient(db_helpers.app_configuration.MONGO_URI)
+        db_name = db_helpers.app_configuration.DB_NAME
         print(db_name, file=sys.stderr)
         dbnames = client.list_database_names()
         if db_name not in dbnames:
-            return {'message':'Organization does not exist', 'success':False}, 400
-        db = client[db_name]
+            return {'message': 'Organization does not exist', 'success': False}, 400
+        db = db_helpers.connect_mongo(tenant_id)
         query = {}
         projection = {'_id': 0, 'loc_ref': 1, 'location_name': 1, 'mobility': 1, 'latitude': 1, 'longitude': 1, 'country': 1, 'region': 1,
                       'district': 1, 'county': 1, 'subcounty': 1, 'parish': 1, 'description': 1}
@@ -64,18 +53,19 @@ class Location():
         '''
         Gets all the data in the database for a specific location
         '''
-        client = MongoClient(MONGO_URI)
-        db_name = f'{DB_NAME}_{tenant_id.lower()}'
+        client = MongoClient(db_helpers.app_configuration.MONGO_URI)
+        db_name = db_helpers.app_configuration.DB_NAME
+        print(db_name, file=sys.stderr)
         dbnames = client.list_database_names()
         if db_name not in dbnames:
-            return {'message':'Organization does not exist', 'success':False}, 400
+            return {'message': 'Organization does not exist', 'success': False}, 400
         else:
-            db = client[db_name]
+            db = db_helpers.connect_mongo(tenant_id)
             query = {'loc_ref': loc_ref}
             projection = {'_id': 0}
             records = list(db.location_registry.find(query, projection))
-            if len(records)==0:
-                return {'message':'Invalid location reference', 'success':False}, 400
+            if len(records) == 0:
+                return {'message': 'Invalid location reference', 'success': False}, 400
             else:
                 return records[0]
 
@@ -83,19 +73,20 @@ class Location():
         '''
         Gets all the data in the database for a specific location
         '''
-        client = MongoClient(MONGO_URI)
-        db_name = f'{DB_NAME}_{tenant_id.lower()}'
+        client = MongoClient(db_helpers.app_configuration.MONGO_URI)
+        db_name = db_helpers.app_configuration.DB_NAME
+        print(db_name, file=sys.stderr)
         dbnames = client.list_database_names()
         if db_name not in dbnames:
-            return {'message':'Organization does not exist', 'success':False}, 400
+            return {'message': 'Organization does not exist', 'success': False}, 400
         else:
-            db = client[db_name]
+            db = db_helpers.connect_mongo(tenant_id)
             query = {'loc_ref': loc_ref}
-            projection = {'_id': 0, 'loc_ref': 1, 'host': 1, 'mobility': 1, 'latitude': 1, 'longitude': 1, 'road_intensity': 1, 
-            'description': 1, 'road_status': 1, 'local_activities': 1}
+            projection = {'_id': 0, 'loc_ref': 1, 'host': 1, 'mobility': 1, 'latitude': 1, 'longitude': 1, 'road_intensity': 1,
+                          'description': 1, 'road_status': 1, 'local_activities': 1}
             records = list(db.location_registry.find(query, projection))
-            if len(records)==0:
-                return {'message':'Invalid location reference', 'success':False}, 400
+            if len(records) == 0:
+                return {'message': 'Invalid location reference', 'success': False}, 400
             else:
                 try:
                     revised_activities = []
@@ -106,34 +97,34 @@ class Location():
                     return records[0]
                 except:
                     return records[0]
-        
 
     def save_edited_location(self, tenant_id, loc_ref, road_intensity, description, road_status, local_activities):
         '''
         Saves updated location details to database
         '''
 
-        client = MongoClient(MONGO_URI)
-        db_name = f'{DB_NAME}_{tenant_id.lower()}'
-        db_names = client.list_database_names()
+        client = MongoClient(db_helpers.app_configuration.MONGO_URI)
+        db_name = db_helpers.app_configuration.DB_NAME
+        print(db_name, file=sys.stderr)
+        dbnames = client.list_database_names()
         if db_name not in db_names:
-            return {'message': 'Organization does not exist', 'success':False}, 400
+            return {'message': 'Organization does not exist', 'success': False}, 400
         else:
-            db = client[db_name]
+            db = db_helpers.connect_mongo(tenant_id)
             a = db.location_registry.find({'loc_ref': loc_ref}).count()
             print(a, file=sys.stderr)
             if db.location_registry.find({'loc_ref': loc_ref}).count() == 0:
-                return {'message': 'Location not found', 'success':False}, 400
+                return {'message': 'Location not found', 'success': False}, 400
             else:
                 db.location_registry.update_one(
                     {'loc_ref': loc_ref},
                     {'$set':
-                    {
-                        'road_intensity': road_intensity,
-                        'description': description,
-                        'road_status': road_status,
-                        'local_activities': local_activities
-                    }
-                 }
-            )
-            return {'message':'Location successfully updated', 'success':True}, 200
+                     {
+                         'road_intensity': road_intensity,
+                         'description': description,
+                         'road_status': road_status,
+                         'local_activities': local_activities
+                     }
+                     }
+                )
+            return {'message': 'Location successfully updated', 'success': True}, 200
