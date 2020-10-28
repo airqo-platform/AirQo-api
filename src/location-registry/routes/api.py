18e10a4d1eab148e980e8c2e61d8d2cf7b99d409
danielogen <danielogen@gmail.com>
2020-10-28 11:01:38 +0300

    add routes

---
+++ src/location-registry/routes/api.py
@@ -0,0 +1,11 @@
+base_url = '/api/v1'
+route = {
+    'root': '/',
+    'health_check': '/health',
+    'create_id': base_url + '/location_registry/create_id',
+    'register': base_url + '/location_registry/register',
+    'locations': base_url + '/location_registry/locations',
+    'location': base_url + '/location_registry/location',
+    'edit': base_url + '/location_registry/edit',
+    'update': base_url + '/location_registry/update',
+}
