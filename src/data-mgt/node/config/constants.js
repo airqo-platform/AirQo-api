2e323c89769df6b7859e2aa1f358a08cdd951e58
danielogen <danielogen@gmail.com>
2020-10-29 06:43:01 +0300

    add redis constants

--- src/data-mgt/node/config/constants.js
+++ src/data-mgt/node/config/constants.js
@@ -2,16 +2,22 @@ const devConfig = {
   MONGO_URI: "mongodb://localhost/",
   JWT_SECRET: process.env.JWT_SECRET,
   DB_NAME: process.env.MONGO_DEV,
+  REDIS_SERVER: process.env.REDIS_SERVER_DEV,
+  REDIS_PORT: process.env.REDIS_PORT,
 };
 const stageConfig = {
   MONGO_URI: process.env.MONGO_GCE_URI,
   JWT_SECRET: process.env.JWT_SECRET,
   DB_NAME: process.env.MONGO_STAGE,
+  REDIS_SERVER: process.env.REDIS_SERVER,
+  REDIS_PORT: process.env.REDIS_PORT,
 };
 const prodConfig = {
   MONGO_URI: process.env.MONGO_GCE_URI,
   JWT_SECRET: process.env.JWT_SECRET,
   DB_NAME: process.env.MONGO_PROD,
+  REDIS_SERVER: process.env.REDIS_SERVER,
+  REDIS_PORT: process.env.REDIS_PORT,
 };
 const defaultConfig = {
   PORT: process.env.PORT || 3000,
