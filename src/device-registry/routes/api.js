5c10c86c42e077d605d250ab002f4abf42f6c612
Martin <bbaalmart@gmail.com>
2020-11-12 08:42:29 +0300

    controllers and utils for more CRUD endpoints of activity logs

--- src/device-registry/routes/api.js
+++ src/device-registry/routes/api.js
@@ -13,7 +13,49 @@ const imageController = require("../controllers/process-image");
 
 middlewareConfig(router);
 
+/******************* Devices using ThingSpeak  *************************************/
 router.get("/", deviceController.listAll);
+router.post("/ts", deviceController.createThing);
+router.delete("/ts/delete", deviceController.deleteThing);
+router.delete("/ts/clear", deviceController.clearThing);
+router.put("/ts/update", deviceController.updateThingSettings);
+router.get("/by/location", deviceController.listAllByLocation);
+//location activities
+router.post("/ts/activity", deviceController.doActivity);
+router.get("/ts/activity", deviceController.getActivities);
+router.put("/ts/activity/update", deviceController.updateActivity);
+router.delete("/ts/activity/delete", deviceController.deleteActivity);
+router.post(
+  "/upload-images",
+  imageUpload.array("image"),
+  imageController.uploadMany
+);
+
+/******************* components or sensors **************************/
+router.get("/list/components/", componentController.listAll);
+router.post("/add/components/", componentController.addComponent);
+router.delete("/delete/components/", componentController.deleteComponent);
+router.put("/update/components/", componentController.updateComponent);
+router.post("/add/components/types", componentController.createType);
+router.get("/list/components/types", componentController.getTypes);
+
+/******************* Events *****************************************/
+router.post("/events/add", componentController.addValues);
+router.post("/events/add/bulk", componentController.addBulk);
+router.get("/events", componentController.getValues);
+router.post("/events/push/one", componentController.writeToThing);
+router.post("/events/push", componentController.writeToThingJSON);
+
+/************************** using GCP tools  *********************************/
+//configuration of devices
+// router.get('/mqtt/config/gcp', mqttBridge.reviewConfigs);
+// router.get('/http/config/gcp', httpBridge.reviewConfigs);
+// router.put('/mqtt/config/gcp', mqttBridge.updateConfigs);
+// router.put('/http/config/gcp', httpBridge.updateConfigs);
+
+//publish telemetry
+// router.push('/mqtt/publish/gcp', mqttBridge.publish);
+// router.push('/http/publish/gcp', httpBridge.publish);
 // router.get("/gcp", deviceController.listAllGcp);
 
 // router.post(
@@ -36,45 +78,5 @@ router.get("/", deviceController.listAll);
 //   deviceController.updateDevice
 // );
 // router.put("/:name/gcp", deviceController.updateDeviceGcp);
-
-/******************* creation of Things   *****************/
-router.post("/ts", deviceController.createThing);
-router.delete("/ts/delete", deviceController.deleteThing);
-router.delete("/ts/clear", deviceController.clearThing);
-router.put("/ts/update", deviceController.updateThingSettings);
-router.post("/ts/activity", deviceController.doActivity);
-router.get("/by/location", deviceController.listAllByLocation);
-
-/********************** photo uploads ***************************/
-router.post(
-  "/upload-images",
-  imageUpload.array("image"),
-  imageController.uploadMany
-);
-
-/******************* components ************************* */
-router.get("/list/components/", componentController.listAll);
-router.post("/add/components/", componentController.addComponent);
-router.delete("/delete/components/", componentController.deleteComponent);
-router.put("/update/components/", componentController.updateComponent);
-router.post("/add/components/types", componentController.createType);
-router.get("/list/components/types", componentController.getTypes);
-
-/******************* device measurements ******************************/
-router.post("/events/add", componentController.addValues);
-router.post("/events/add/bulk", componentController.addBulk);
-router.get("/events", componentController.getValues);
-router.post("/events/push/one", componentController.writeToThing);
-router.post("/events/push", componentController.writeToThingJSON);
-
-//configuration of devices
-// router.get('/mqtt/config/gcp', mqttBridge.reviewConfigs);
-// router.get('/http/config/gcp', httpBridge.reviewConfigs);
-// router.put('/mqtt/config/gcp', mqttBridge.updateConfigs);
-// router.put('/http/config/gcp', httpBridge.updateConfigs);
-
-//publish telemetry
-// router.push('/mqtt/publish/gcp', mqttBridge.publish);
-// router.push('/http/publish/gcp', httpBridge.publish);
 
 module.exports = router;
