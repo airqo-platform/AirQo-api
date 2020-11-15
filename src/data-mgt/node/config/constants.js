48d4e9deec94a494353b2b687d78ddd2847c4345
Martin <bbaalmart@gmail.com>
2020-11-15 10:29:56 +0300

    url for getting the hourly measurement of a channel

--- src/data-mgt/node/config/constants.js
+++ src/data-mgt/node/config/constants.js
@@ -41,7 +41,7 @@ const defaultConfig = {
     return `https://api.thingspeak.com/channels/${channel}/feeds.json`;
   },
   GENERATE_LAST_HOURLY_ENTRY: (channel) => {
-    return `https://api.thingspeak.com/channels/${channel}/feeds.json?days=1&sum=60`;
+    return `https://api.thingspeak.com/channels/${channel}/feeds.json?days=1&average=60`;
   },
   GET_FEEDS: (channel) => {
     return `https://api.thingspeak.com/channels/${channel}/feeds.json`;
