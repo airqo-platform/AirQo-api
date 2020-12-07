250c752db2ccd3c7f91cd5c8d58f622f4c4d8810
Martin <bbaalmart@gmail.com>
2020-12-07 11:46:36 +0300

    adding the terraform set up files

---
+++ infra/setup/pipeline-k8s-worker1/main.tf
@@ -0,0 +1,46 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "3.5.0"
+    }
+  }
+}
+
+provider "google" {
+
+  credentials = file("../AirQo-72961f2e56df.json")
+
+  project = "airqo-250220"
+  region  = "us-central1"
+  zone    = "us-central1-c"
+}
+
+// A single Compute Engine instance
+resource "google_compute_instance" "default" {
+  name         = "pipeline-k8s-worker1"
+  machine_type = "e2-small"
+  zone         = "us-central1-c"
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-os-cloud/ubuntu-1804-lts"
+    }
+  }
+
+  network_interface {
+    network = "pipeline-k8s-cluster"
+
+    access_config {
+      // Include this section to give the VM an external ip address
+    }
+  }
+
+  metadata = {
+    ssh-keys = "balmart:${file("~/.ssh/id_rsa.pub")}"
+  }
+}
+
+output "ip" {
+  value = google_compute_instance.default.network_interface.0.access_config.0.nat_ip
+}
