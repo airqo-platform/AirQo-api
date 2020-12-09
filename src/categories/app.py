d823f3de3452ab673a8c56ccdbe3eb6929f9dd67
Martin <bbaalmart@gmail.com>
2020-12-09 10:23:46 +0300

    introducing categories microservice

---
+++ src/categories/app.py
@@ -0,0 +1,39 @@
+from flask import Flask
+import logging
+import os
+from flask_cors import CORS
+from flask_pymongo import PyMongo
+from config import app_config
+from dotenv import load_dotenv
+# import blueprints
+from controllers.health import health_bp
+from controllers.category import category_bp
+load_dotenv()
+
+
+_logger = logging.getLogger(__name__)
+
+# db initialization
+mongo = PyMongo()
+
+
+def create_app(environment):
+    # create a flask app instance
+    app = Flask(__name__)
+    app.config.from_object(app_config[environment])
+    mongo.init_app(app)
+
+    # Allow cross-brower resource sharing
+    CORS(app)
+
+    # register blueprints
+    app.register_blueprint(health_bp)
+    app.register_blueprint(exceedance_bp)
+
+    return app
+
+
+application = create_app(os.getenv("FLASK_ENV"))
+
+if __name__ == '__main__':
+    application.run(debug=True)
---
+++ src/categories/routes/api.py
@@ -0,0 +1,6 @@
+base_url = '/api/v1/exceedance'
+route = {
+    'root': '/',
+    'health_check': '/health',
+    'exceedances': base_url + '/values',
+}
