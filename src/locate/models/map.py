890f3cf614dc132c0e3ea881fba34e01bf8d74b6
danielogen <danielogen@gmail.com>
2020-12-10 16:38:27 +0300

    function to check if planning space name exist

--- src/locate/models/map.py
+++ src/locate/models/map.py
@@ -35,6 +35,14 @@ class Map():
         documents = db.locate_map.find({"user_id": str(user_id)})
         return documents
 
+    def plan_space_exits(self, tenant, user_id, space_name):
+        '''
+        check if planning space name already exits for a given user. Avoid duplicates
+        '''
+        db = db_helpers.connect_mongo(tenant)
+        documents = db.locate_map.find({"$and": [{"user_id":str(user_id)}, {"space_name":space_name}]})
+        return documents.count()
+
 
     def update_locate_map(self, tenant, space_name, updated_plan):
         '''
