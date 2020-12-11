65031c2c16f9776d39128b897c0db3d77ef4ca5e
Martin <bbaalmart@gmail.com>
2020-12-11 13:19:19 +0300

    refactoring controllers for categories

--- src/categories/controllers/health.py
+++ src/categories/controllers/health.py
@@ -14,17 +14,17 @@ load_dotenv()
 
 
 _logger = logging.getLogger(__name__)
-monitor_bp = Blueprint('monitor_bp', __name__)
+health_bp = Blueprint('health', __name__)
 
 
-@monitor_bp.route(api.route['root'], methods=['GET'])
+@health_bp.route(api.route['root'], methods=['GET'])
 def root():
     if request.method == 'GET':
         _logger.info('root endpoint OK')
         return jsonify({"message": "ok", "status": True}), 200
 
 
-@monitor_bp.route(api.route['health'], methods=['GET'])
+@health_bp.route(api.route['health'], methods=['GET'])
 def health():
     if request.method == 'GET':
         _logger.info('health status OK')
