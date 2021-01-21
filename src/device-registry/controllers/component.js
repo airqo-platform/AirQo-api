6264e5c4c416c7b43825a5242895b52c5bdcfe52
Martin <bbaalmart@gmail.com>
2021-01-21 09:53:45 +0300

    refactor the components controller - transfer some logic to helper function

--- src/device-registry/controllers/component.js
+++ src/device-registry/controllers/component.js
@@ -29,124 +29,14 @@ const {
   callbackErrors,
 } = require("../utils/errors");
 
-const getApiKeys = async (deviceName, tenant) => {
-  logText("...................................");
-  logText("getting api keys...");
-  const deviceDetails = await getModelByTenant(
-    tenant.toLowerCase(),
-    "component",
-    ComponentSchema
-  )
-    .find({
-      name: deviceName,
-    })
-    .exec();
-  logElement("the write key", deviceDetails.writeKey);
-  logElement("the read key", deviceDetails.readKey);
-  const writeKey = deviceDetails.writeKey;
-  const readKey = deviceDetails.readKey;
-  return { writeKey, readKey };
-};
-
-const getArrayLength = async (array, model, event) => {};
-
-const generateDateFormat = async (ISODate) => {
-  date = new Date(ISODate);
-  year = date.getFullYear();
-  month = date.getMonth() + 1;
-  dt = date.getDate();
-
-  if (dt < 10) {
-    dt = "0" + dt;
-  }
-  if (month < 10) {
-    month = "0" + month;
-  }
-
-  return `${year}-${month}-${dt}`;
-};
-
-const doesDeviceExist = async (deviceName, tenant) => {
-  try {
-    logText(".......................................");
-    logText("doesDeviceExist?...");
-    const device = await getModelByTenant(
-      tenant.toLowerCase(),
-      "device",
-      DeviceSchema
-    )
-      .find({
-        name: deviceName,
-      })
-      .exec();
-    logElement("device element", device);
-    logObject("device Object", device);
-    logElement("does device exist?", !isEmpty(device));
-    if (!isEmpty(device)) {
-      return true;
-    } else if (isEmpty(device)) {
-      return false;
-    }
-  } catch (e) {
-    logElement("unable to check device existence in system", e);
-    return false;
-  }
-};
-
-const doesComponentExist = async (componentName, deviceName, tenant) => {
-  try {
-    logText(".......................................");
-    logText("doesComponentExist?...");
-    const component = await getModelByTenant(
-      tenant.toLowerCase(),
-      "component",
-      ComponentSchema
-    )
-      .find({
-        name: componentName,
-        deviceID: deviceName,
-      })
-      .exec();
-    logElement("component element", component);
-    logObject("component Object", component);
-    logElement("does component exist?", !isEmpty(component));
-    if (!isEmpty(component)) {
-      return true;
-    } else if (isEmpty(ComponentDetails)) {
-      return false;
-    }
-  } catch (e) {
-    logElement("unable to check Component existence in system", e);
-    return false;
-  }
-};
-
-const doesComponentTypeExist = async (name, tenant) => {
-  try {
-    logText(".......................................");
-    logText("doesComponentExist?...");
-    const componentType = await getModelByTenant(
-      tenant.toLowerCase(),
-      "componentType",
-      ComponentTypeSchema
-    )
-      .find({
-        name: name,
-      })
-      .exec();
-    logElement("component type element", componentType);
-    logObject("component type Object", componentType);
-    logElement("does component type exist?", !isEmpty(componentType));
-    if (!isEmpty(componentType)) {
-      return true;
-    } else if (isEmpty(componentType)) {
-      return false;
-    }
-  } catch (e) {
-    logElement("unable to check component type existence in system", e);
-    return false;
-  }
-};
+const {
+  getApiKeys,
+  getArrayLength,
+  generateDateFormat,
+  doesDeviceExist,
+  doesComponentExist,
+  doesComponentTypeExist,
+} = require("../utils/componentControllerHelpers");
 
 const Component = {
   listAll: async (req, res) => {
