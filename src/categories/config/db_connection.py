64d25e25f222c3f3febca8ec50b474828e529790
danielogen <danielogen@gmail.com>
2021-02-07 19:40:53 +0300

    modify constants and db connection configs

--- src/categories/config/db_connection.py
+++ src/categories/config/db_connection.py
@@ -1,17 +1,21 @@
 
+from flask_pymongo import PyMongo
 from pymongo import MongoClient
 from datetime import datetime, timedelta
 import os
 import sys
-from config import app_config
+from config import constants
 from dotenv import load_dotenv
 load_dotenv()
 
 
-app_configuration = app_config.get(os.getenv("FLASK_ENV"))
+app_configuration = constants.app_config.get(os.getenv("FLASK_ENV"))
 
 
 def connect_mongo(tenant):
+    print("MONGO URI", app_configuration.MONGO_URI)
+    print("DB_NAME", app_configuration.DB_NAME)
+
     client = MongoClient(app_configuration.MONGO_URI)
     db = client[f'{app_configuration.DB_NAME}_{tenant.lower()}']
     return db
