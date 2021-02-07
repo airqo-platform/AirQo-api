4b670335ab503b8a782c3c1f421b88f90e1c4bc6
danielogen <danielogen@gmail.com>
2021-02-07 19:40:01 +0300

    update app entrypoint

--- src/categories/app.py
+++ src/categories/app.py
@@ -1,39 +1,16 @@
+import argparse
 from flask import Flask
 import logging
 import os
-from flask_cors import CORS
-from flask_pymongo import PyMongo
-from config import constants
+from controllers import category
 from dotenv import load_dotenv
-# import blueprints
-from controllers.health import health_bp
-from controllers.category import category_bp
 load_dotenv()
 
 
-_logger = logging.getLogger(__name__)
-
-# db initialization
-mongo = PyMongo()
-
-
-def create_app(environment):
-    # create a flask app instance
-    app = Flask(__name__)
-    app.config.from_object(constants.app_config[environment])
-    mongo.init_app(app)
-
-    # Allow cross-brower resource sharing
-    CORS(app)
-
-    # register blueprints
-    app.register_blueprint(health_bp)
-    app.register_blueprint(category_bp)
-
-    return app
-
-
-application = create_app(os.getenv("FLASK_ENV"))
-
 if __name__ == '__main__':
-    application.run(debug=True)
+    parser = argparse.ArgumentParser(description='save the category count.')
+    parser.add_argument('--tenant',
+                        default="kcca",
+                        help='the tenant key is the organisation name')
+    args = parser.parse_args()
+    category.get_pm25categorycount_for_locations(args.tenant)
