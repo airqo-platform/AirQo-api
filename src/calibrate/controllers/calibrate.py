0ccfced65adcadedf7bafe14d043b99231da7f9e
Priscilla-AL <priscilla@airqo.net>
2021-02-08 11:59:32 +0300

    Combine calibration functions (one API)

--- src/calibrate/controllers/calibrate.py
+++ src/calibrate/controllers/calibrate.py
@@ -20,24 +20,24 @@ client = MongoClient(MONGO_URI)
 db = client['airqo_netmanager_staging_airqo']
 col = db['calibration_ratios']
 
-@calibrate_bp.route(api.route['calibrate'], methods=['POST', 'GET'])
+# @calibrate_bp.route(api.route['calibrate'], methods=['POST', 'GET'])
 
-def calibrate_pm25_values():
-    if request.method == 'GET':
-        raw_values = request.args.get('raw_value')
-        temp = request.args.get('temp')
-        humidity = request.args.get('humidity')
+# def calibrate_pm25_values():
+#     if request.method == 'GET':
+#         raw_values = request.args.get('raw_value')
+#         temp = request.args.get('temp')
+#         humidity = request.args.get('humidity')
 
-        rgModel = rg.Regression()
-        hourly_combined_dataset = rgModel.hourly_combined_dataset
+#         rgModel = rg.Regression()
+#         hourly_combined_dataset = rgModel.hourly_combined_dataset
 
-        calibrated_value_lr = rgModel.simple_lr(raw_values, hourly_combined_dataset)
-        calibrated_value_lr = calibrated_value_lr.tolist()
+#         calibrated_value_lr = rgModel.simple_lr(raw_values, hourly_combined_dataset)
+#         calibrated_value_lr = calibrated_value_lr.tolist()
 
-        calibrated_value_mlr = rgModel.multivariate_lr(raw_values, temp, humidity, hourly_combined_dataset)
-        calibrated_value_mlr = calibrated_value_mlr.tolist()
+#         calibrated_value_mlr = rgModel.multivariate_lr(raw_values, temp, humidity, hourly_combined_dataset)
+#         calibrated_value_mlr = calibrated_value_mlr.tolist()
 
-        return jsonify({"calibrated Value1": calibrated_value_lr, "calibrated Value2": calibrated_value_mlr})
+#         return jsonify({"calibrated Value1": calibrated_value_lr, "calibrated Value2": calibrated_value_mlr})
 
 @calibrate_bp.route(api.route['ratios'], methods=['POST', 'GET'])
 def save_ratios():
@@ -57,6 +57,7 @@ def save_ratios():
 
 @calibrate_bp.route(api.route['mobilecalibrate'], methods=['POST', 'GET'])
 def calibrate():
+
     if request.method == 'POST':
         data = request.get_json()
         datetime = data.get('datetime')
@@ -66,12 +67,16 @@ def calibrate():
         
         if (not datetime or not raw_values):
             return jsonify({"message": "Please specify the datetime and raw_values in the body. Refer to the API documentation for details.", "success": False}), 400
-        
+    
+        rgModel = rg.Regression()
+        hourly_combined_dataset = rgModel.hourly_combined_dataset
+
         calibrateModel = cb.Calibrate()
         response = []
         for raw_value in raw_values:
             value = calibrateModel.calibrate_sensor_raw_data(datetime, raw_value.get('sensor_id'), raw_value.get('raw_value'))
-            response.append({'Sensor_id':raw_value.get('sensor_id'),'calibrated_value': value})
+            value_lr = rgModel.simple_lr(raw_value.get('raw_value'), hourly_combined_dataset)
+            response.append({'Sensor_id':raw_value.get('sensor_id'),'calibrated_value': value, 'calibrated_value_LR': value_lr})
         return jsonify(response), 200
 
     
