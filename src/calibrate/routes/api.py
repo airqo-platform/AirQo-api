b7ac892a7dd44f6ee7fdb4f97e6deb4cce39437e
Priscilla-AL <priscilla@airqo.net>
2021-02-08 12:34:39 +0300

    update URL

--- src/calibrate/routes/api.py
+++ src/calibrate/routes/api.py
@@ -3,6 +3,5 @@ base_url = '/api/v1'
 route = {
     'root': '/',
     'calibrate': f'{base_url}/calibrate',
-    'mobilecalibrate': f'{base_url}/calibrate/mobile',
     'ratios': f'{base_url}/calibrate/ratios',
 }
