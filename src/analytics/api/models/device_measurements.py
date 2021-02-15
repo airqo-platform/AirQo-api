77371d8573bc2cb4c5f1a56dbb2b15106b4c96b9
ja-odur <odurjoseph8@gmail.com>
2021-02-15 10:58:32 +0300

    add deviceDailyAverage model

--- src/analytics/api/models/device_measurements.py
+++ src/analytics/api/models/device_measurements.py
@@ -1,6 +1,26 @@
 from datetime import datetime
 import pandas as pd
 from api.models.base.base_model import BasePyMongoModel
+from api.utils.dates import date_to_str, str_to_date
+
+
+class DeviceDailyHistoricalAverages(BasePyMongoModel):
+    def __init__(self, tenant):
+        super().__init__(tenant, collection_name='device_daily_historical_averages')
+
+    def get_last_28_days_measurements(self):
+        """
+        Gets all the daily measurements for the device for the past 28 days from current day.
+
+        Returns:
+            A list of the daily measurements for the past 28 days.
+        """
+        created_at = str_to_date(date_to_str(datetime.now().date()))
+        query = {
+            '$match': {'created_at': {'$gte': created_at}}
+        }
+        projection = {'$project': {'_id': 0}}
+        return list(self.collection.aggregate([query, projection]))
 
 
 class DeviceHourlyMeasurement(BasePyMongoModel):
