a04ffeffed20c0cc3b6824ad7b02af21d47c3c45
ja-odur <odurjoseph8@gmail.com>
2021-02-15 12:41:43 +0300

    add pm category function

--- src/analytics/api/utils/pollutants/__init__.py
+++ src/analytics/api/utils/pollutants/__init__.py
@@ -1,1 +1,1 @@
-from .pm_25 import set_pm25_category_background
+from .pm_25 import PM_25_CATEGORY, categorise_pm25_values, set_pm25_category_background
--- src/analytics/api/utils/pollutants/pm_25.py
+++ src/analytics/api/utils/pollutants/pm_25.py
@@ -9,6 +9,16 @@ PM_25_COLOR_MAPPER = {
 
 }
 
+PM_25_CATEGORY = {
+    'Good': [0, 12],
+    'Moderate': [12, 35.4],
+    'UHFSG': [35.4, 55.4],
+    'Unhealthy': [55.4, 150.4],
+    'VeryUnhealthy': [150.4, 250.4],
+    'Hazardous': [250.4, 500.4],
+    'All': [0, 2000],
+}
+
 
 def set_pm25_category_background(pm25_value):
     keys = sorted(PM_25_COLOR_MAPPER.keys(), reverse=True)
@@ -16,3 +26,23 @@ def set_pm25_category_background(pm25_value):
     for key in keys:
         if pm25_value > key:
             return PM_25_COLOR_MAPPER[key]
+
+
+def categorise_pm25_values(records, category):
+    categorised = []
+
+    if category == 'All':
+        return records
+
+    try:
+        min_value, max_value = PM_25_CATEGORY.get(category, [])
+    except ValueError:
+        return []
+
+    for record in records:
+        if min_value < record['Last_Hour_PM25_Value'] <= max_value:
+            categorised.append(record)
+
+    return categorised
+
+
