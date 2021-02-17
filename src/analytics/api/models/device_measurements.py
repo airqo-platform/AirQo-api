61525c9dd17b75569fd4ab9fee06962809932eda
ja-odur <odurjoseph8@gmail.com>
2021-02-17 17:17:11 +0300

    add model util to fetch all pollutant values

--- src/analytics/api/models/device_measurements.py
+++ src/analytics/api/models/device_measurements.py
@@ -158,7 +158,7 @@ class DeviceHourlyMeasurement(BasePyMongoModel):
             start_date (datetime): the datetime from which observations to be returned should start(lower boundary).
             end_date (datetime): the datetime from which observations to be returned should end(upper boundary).
             frequency (str): the frequency of the observataions i.e. hourly, daily, monthly.
-            pollutant (str): the pollutant whose observatations are to be returned i.e. PM 2.5, PM 10, NO2.
+            pollutant (str/list): the pollutant whose observatations are to be returned i.e. PM 2.5, PM 10, NO2.
         Returns:
             A list of the data(pollutant values & their corresponding time) for the specified pollutant from the device
              with the specified code observed between
@@ -243,3 +243,56 @@ class DeviceHourlyMeasurement(BasePyMongoModel):
         #     return self.resample_timeseries_data(results, 'M', 'time', 2)
 
         return filtered.exec(projections=projection['$project'])
+
+    def get_all_pollutant_values(self, device_codes, start_date=None, end_date=None, frequency='daily'):
+        """
+        Gets all the data for the all pollutants from the specified device codes between the specified start_date and
+        end_date for the specified time frequency.
+        Args:
+            device_codes: list  codes used to identify devices
+            start_date: the datetime from which observations to be returned should start(lower boundary).
+            end_date: the datetime from which observations to be returned should end(upper boundary)
+            frequency: the frequency of the observations i.e. hourly, daily, monthly.
+
+        Returns: A list of the data(pollutant values & their corresponding time) for all pollutants from all devices
+                 with the specified code observed between time period.
+        """
+
+        start = start_date and datetime.strptime(start_date, '%Y-%m-%dT%H:%M:%S.%fZ') or \
+                datetime.strptime('2019-06-01T00:00:00Z', '%Y-%m-%dT%H:%M:%SZ')
+
+        end = end_date and datetime.strptime(end_date, '%Y-%m-%dT%H:%M:%S.%fZ') or datetime.now()
+
+        time_format = '%Y-%m-%dT%H:%M:%S%z'
+
+        if frequency == 'daily':
+            time_format = '%Y-%m-%d'
+        elif frequency == 'hourly':
+            time_format = '%Y-%m-%d %H:%M'
+
+        projection = {
+            '_id': 0,
+            'deviceCode': 1,
+            'time': {
+                '$dateToString': {
+                    'format': time_format,
+                    'date': '$time',
+                    'timezone': 'Africa/Kampala'
+                }
+            },
+            'pm25_value': {
+                '$round': ['$characteristics.pm2_5ConcMass.value', 2]
+            },
+            'pm10_value': {
+                '$round': ['$characteristics.pm10ConcMass.value', 2]
+            },
+            'no2_value': {
+                '$round': ['$characteristics.no2Conc.value', 2]
+            }
+        }
+
+        return (
+            self.filter_by(time={'$lte': end, '$gte': start})
+                .in_filter_by(deviceCode=device_codes)
+                .exec(projection)
+        )
