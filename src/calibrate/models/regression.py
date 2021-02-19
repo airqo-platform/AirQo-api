fa575950ffcd1ae6d62babc43e1389e0cf5b6142
Priscilla-AL <priscilla@airqo.net>
2021-02-19 19:58:09 +0300

    Add error metrics

--- src/calibrate/models/regression.py
+++ src/calibrate/models/regression.py
@@ -13,14 +13,19 @@ class Regression():
         self.hourly_combined_dataset = rg.combine_datasets(lowcost_hourly_mean, bam_hourly_mean)
 
     def simple_lr(self, raw_value, hourly_combined_dataset):
-        slope, intercept = rg.simple_linear_regression(hourly_combined_dataset)
+        slope, intercept, MAE, RMSE, r2_score = rg.simple_linear_regression(hourly_combined_dataset)
         calibrated_value_lr = intercept + slope * float(raw_value)
-               
+        print('MAE:', MAE)   
+        print('RMSE:', RMSE)   
+        print('R2:', r2_score)  
         return calibrated_value_lr
 
     def multivariate_lr(self, raw_values, temp, humidity, hourly_combined_dataset):
-        slope, intercept = rg.mlr(hourly_combined_dataset)
-        calibrated_value_mlr = float(raw_values) * slope[0] + float(temp) * slope[1] + float(humidity) * slope[2] + intercept      
+        slope, intercept, MAE2, RMSE2, r2_score2 = rg.mlr(hourly_combined_dataset)
+        calibrated_value_mlr = float(raw_values) * slope[0] + float(temp) * slope[1] + float(humidity) * slope[2] + intercept    
+        print('MAE2:', MAE2)   
+        print('RMSE2:', RMSE2) 
+        print('R2_2:', r2_score2) 
         return calibrated_value_mlr
 
 if __name__ == "__main__":
