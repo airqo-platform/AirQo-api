f41c74e8555b5da018cffa89bca30baa2a9c3d19
Martin <bbaalmart@gmail.com>
2021-02-23 08:35:27 +0300

    substitute forEach for for...of to introduce async and also add appropriate response for partial successes

--- src/device-registry/utils/insertMeasurements.js
+++ src/device-registry/utils/insertMeasurements.js
@@ -3,17 +3,14 @@ const { getModelByTenant } = require("./multitenancy");
 const { logObject, logText, logElement } = require("./log");
 const EventSchema = require("../models/Event");
 const HTTPStatus = require("http-status");
-const {
-  axiosError,
-  tryCatchErrors,
-  missingQueryParams,
-  callbackErrors,
-  unclearError,
-} = require("./errors");
 
-const insert = (res, tenant, transformedMeasurements) => {
-  const errors = [];
-  transformedMeasurements.forEach(async (measurement) => {
+const insert = async (res, tenant, transformedMeasurements) => {
+  let nAdded = 0;
+  let eventsAdded = [];
+  let eventsRejected = [];
+  let errors = [];
+
+  for (const measurement of transformedMeasurements) {
     try {
       const eventBody = {
         "values.time": { $ne: measurement.time },
@@ -34,22 +31,35 @@ const insert = (res, tenant, transformedMeasurements) => {
         upsert: true,
       });
       if (addedEvents) {
-        logText("the events have successfully been added");
-        return res.status(HTTPStatus.OK).json({
-          success: true,
-          message: "successfully added the event(s)",
-          nAdded: addedEvents.nModified,
-        });
+        nAdded += 1;
+        eventsAdded.push(measurement);
       } else if (!addedEvents) {
-        logText("unable to add the events ");
+        eventsRejected.push(measurement);
+        errors.push("unable to add the events ");
       } else {
-        logText("just unable to add events");
+        eventsRejected.push(measurement);
+        errors.push("unable to add the events ");
       }
     } catch (e) {
-      logElement("there is an error: ", e.message);
-      tryCatchErrors(res, e);
+      eventsRejected.push(measurement);
+      errors.push(e.message);
     }
-  });
+  }
+  if (errors.length > 0) {
+    return res.status(HTTPStatus.BAD_GATEWAY).json({
+      success: false,
+      message: "finished the operation with some errors",
+      errors: errors,
+      valuesRejected: eventsRejected,
+      valuesAdded: eventsAdded,
+    });
+  } else {
+    return res.status(HTTPStatus.OK).json({
+      success: true,
+      message: "successfully added all the events",
+      valuesAdded: eventsAdded,
+    });
+  }
 };
 
 module.exports = insert;
