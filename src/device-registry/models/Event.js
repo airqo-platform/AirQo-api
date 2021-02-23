0a6ccbe83ecc739f6683d08ec5bbc348c6ecfe5e
Martin <bbaalmart@gmail.com>
2021-02-23 06:11:02 +0300

    move measurements into a new Schema

--- src/device-registry/models/Event.js
+++ src/device-registry/models/Event.js
@@ -1,6 +1,139 @@
 const { Schema, model } = require("mongoose");
 const uniqueValidator = require("mongoose-unique-validator");
 
+const measurementsSchema = new Schema({
+  time: {
+    type: Date,
+    required: [true, "the timestamp is required"],
+    unique: true,
+  },
+  frequency: {
+    type: String,
+    required: [true, "the frequency is required"],
+  },
+  device: {
+    type: String,
+    required: [true, "The device name is required"],
+    trim: true,
+  },
+  channelID: {
+    type: Number,
+    trim: true,
+  },
+  pm1: {
+    value: {
+      type: Number,
+    },
+    calibratedValue: { type: Number },
+    uncertaintyValue: { type: Number },
+    standardDeviationValue: { type: Number },
+  },
+  pm2_5: {
+    value: {
+      type: Number,
+      required: [true, "the raw value is required"],
+    },
+    calibratedValue: { type: Number },
+    uncertaintyValue: { type: Number },
+    standardDeviationValue: { type: Number },
+  },
+  s2_pm2_5: {
+    value: {
+      type: Number,
+      required: [true, "the raw value is required"],
+    },
+    calibratedValue: { type: Number },
+    uncertaintyValue: { type: Number },
+    standardDeviationValue: { type: Number },
+  },
+  pm10: {
+    value: {
+      type: Number,
+      required: [true, "the raw value is required"],
+    },
+    calibratedValue: { type: Number },
+    uncertaintyValue: { type: Number },
+    standardDeviationValue: { type: Number },
+  },
+  s2_pm10: {
+    value: {
+      type: Number,
+      required: [true, "the raw value is required"],
+    },
+    calibratedValue: { type: Number },
+    uncertaintyValue: { type: Number },
+    standardDeviationValue: { type: Number },
+  },
+  no2: {
+    value: {
+      type: Number,
+    },
+    calibratedValue: { type: Number },
+    uncertaintyValue: { type: Number },
+    standardDeviationValue: { type: Number },
+  },
+  battery: {
+    value: {
+      type: Number,
+    },
+  },
+  location: {
+    latitude: {
+      value: {
+        type: Number,
+      },
+    },
+    longitude: {
+      value: {
+        type: Number,
+      },
+    },
+  },
+  altitude: {
+    value: {
+      type: Number,
+    },
+  },
+  speed: {
+    value: {
+      type: Number,
+    },
+  },
+  satellites: {
+    value: {
+      type: Number,
+    },
+  },
+  hdop: {
+    value: {
+      type: Number,
+    },
+  },
+  internalTemperature: {
+    value: {
+      type: Number,
+    },
+  },
+  internalHumidity: {
+    value: {
+      type: Number,
+    },
+  },
+  externalTemperature: {
+    value: {
+      type: Number,
+    },
+  },
+  ExternalHumidity: {
+    value: {
+      type: Number,
+    },
+  },
+  ExternalPressure: {
+    value: { type: Number },
+  },
+});
+
 const eventSchema = new Schema(
   {
     day: {
@@ -11,147 +144,14 @@ const eventSchema = new Schema(
     nValues: {
       type: Number,
     },
-    values: [
-      {
-        time: {
-          type: Date,
-          required: [true, "the timestamp is required"],
-          unique: true,
-        },
-        frequency: {
-          type: String,
-          required: [true, "the frequency is required"],
-        },
-        device: {
-          type: String,
-          required: [true, "The device name is required"],
-          trim: true,
-        },
-        channelID: {
-          type: Number,
-          trim: true,
-        },
-        pm1: {
-          value: {
-            type: Number,
-          },
-          calibratedValue: { type: Number },
-          uncertaintyValue: { type: Number },
-          standardDeviationValue: { type: Number },
-        },
-        pm2_5: {
-          value: {
-            type: Number,
-            required: [true, "the raw value is required"],
-          },
-          calibratedValue: { type: Number },
-          uncertaintyValue: { type: Number },
-          standardDeviationValue: { type: Number },
-        },
-        s2_pm2_5: {
-          value: {
-            type: Number,
-            required: [true, "the raw value is required"],
-          },
-          calibratedValue: { type: Number },
-          uncertaintyValue: { type: Number },
-          standardDeviationValue: { type: Number },
-        },
-        pm10: {
-          value: {
-            type: Number,
-            required: [true, "the raw value is required"],
-          },
-          calibratedValue: { type: Number },
-          uncertaintyValue: { type: Number },
-          standardDeviationValue: { type: Number },
-        },
-        s2_pm10: {
-          value: {
-            type: Number,
-            required: [true, "the raw value is required"],
-          },
-          calibratedValue: { type: Number },
-          uncertaintyValue: { type: Number },
-          standardDeviationValue: { type: Number },
-        },
-        no2: {
-          value: {
-            type: Number,
-          },
-          calibratedValue: { type: Number },
-          uncertaintyValue: { type: Number },
-          standardDeviationValue: { type: Number },
-        },
-        battery: {
-          value: {
-            type: Number,
-          },
-        },
-        location: {
-          latitude: {
-            value: {
-              type: Number,
-            },
-          },
-          longitude: {
-            value: {
-              type: Number,
-            },
-          },
-        },
-        altitude: {
-          value: {
-            type: Number,
-          },
-        },
-        speed: {
-          value: {
-            type: Number,
-          },
-        },
-        satellites: {
-          value: {
-            type: Number,
-          },
-        },
-        hdop: {
-          value: {
-            type: Number,
-          },
-        },
-        internalTemperature: {
-          value: {
-            type: Number,
-          },
-        },
-        internalHumidity: {
-          value: {
-            type: Number,
-          },
-        },
-        externalTemperature: {
-          value: {
-            type: Number,
-          },
-        },
-        ExternalHumidity: {
-          value: {
-            type: Number,
-          },
-        },
-        ExternalPressure: {
-          value: { type: Number },
-        },
-      },
-    ],
+    values: [{ type: measurementsSchema, unique: true }],
   },
   {
     timestamps: true,
   }
 );
 
-eventSchema.index({ device: 1 });
+// eventSchema.index({ device: 1 });
 
 eventSchema.pre("save", function() {
   const err = new Error("something went wrong");
