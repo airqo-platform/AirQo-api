b20a75e8ee1a3443585f48af54dd8e750dee3cfc
danielogen <danielogen@gmail.com>
2021-03-05 16:01:14 +0300

    refactor app.py

--- src/location-registry/app.py
+++ src/location-registry/app.py
@@ -1,26 +1,48 @@
+from config import constants
+from pathlib import Path
 from flask import Flask
 import logging
 import os
 from dotenv import load_dotenv
 from flask_cors import CORS
 from flask_pymongo import PyMongo
-from controllers.controller import location_blueprint, cache
-from controllers.helpers import monitor_bp
-from config import app_config
+from controllers.controller import cache
+
 load_dotenv()
 
+BASE_DIR = Path(__file__).resolve().parent.parent
+print("BASE_DIR", BASE_DIR)
+
+
 _logger = logging.getLogger(__name__)
-app = Flask(__name__)
-app.config.from_object(app_config[os.getenv("FLASK_ENV").strip()])
 
-CORS(app)
-cache.init_app(app)
-mongo = PyMongo(app)
-
-# register blueprints
-app.register_blueprint(location_blueprint)
-app.register_blueprint(monitor_bp)
+# db initialization
+mongo = PyMongo()
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
+def create_app(environment):
+    # create a flask app instance
+    app = Flask(__name__)
+
+    app.config.from_object(constants.app_config[environment])
+    cache.init_app(app)
+    mongo.init_app(app)
+
+    # Allow cross-brower resource sharing
+    CORS(app)
+
+    # import blueprints
+    from controllers.controller import location_blueprint
+    from controllers.helpers import monitor_bp
+
+    # register blueprints
+    app.register_blueprint(location_blueprint)
+    app.register_blueprint(monitor_bp)
+
+    return app
+
+
+application = create_app(os.getenv("FLASK_ENV"))
+
+if __name__ == '__main__':
+    application.run(debug=True)
