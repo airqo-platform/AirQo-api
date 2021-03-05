a452d0cededadcabfa305d8f6272c6c57a5ef0c2
danielogen <danielogen@gmail.com>
2021-03-05 16:01:03 +0300

    update routes

--- src/location-registry/routes/api.py
+++ src/location-registry/routes/api.py
@@ -1,11 +1,11 @@
 base_url = '/api/v1'
 route = {
-    'root': '/',
-    'health_check': '/health',
-    'create_id': base_url + '/location_registry/create_id',
-    'register': base_url + '/location_registry/register',
-    'locations': base_url + '/location_registry/locations',
-    'location': base_url + '/location_registry/location',
-    'edit': base_url + '/location_registry/edit',
-    'update': base_url + '/location_registry/update',
+    'root': f'{base_url}/location_registry',
+    'health_check': f'{base_url}/location_registry/health',
+    'create_id': f'{base_url}/location_registry/create_id',
+    'register': f'{base_url}/location_registry/register',
+    'locations': f'{base_url}/location_registry/locations',
+    'location': f'{base_url}/location_registry/location',
+    'edit': f'{base_url}/location_registry/edit',
+    'update': f'{base_url}/location_registry/update',
 }
