44146e834aa649d840629f6d1e3827600ca38b16
Noah Nsimbe <nsimbenoah@gmail.com>
2021-03-08 12:06:45 +0300

    dependency fix

--- src/data-mgt/python/jobs/kcca_device_measurements.py
+++ src/data-mgt/python/jobs/kcca_device_measurements.py
@@ -1,4 +1,3 @@
-from datetime import datetime, timedelta
 import os
 import pandas as pd
 import requests
--- src/data-mgt/python/jobs/main.py
+++ src/data-mgt/python/jobs/main.py
@@ -10,6 +10,7 @@ Each function must have the same name as the cloud function
 def get_airqo_device_measurements(event, context):
     airqo_device_measurements.process_airqo_device_data()
 
+
 def get_kcca_device_measurements(event, context):
     kcca_device_measurements.process_kcca_device_data()
 
