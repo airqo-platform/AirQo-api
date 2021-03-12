0586ca3c5afd0ce373903ba85655dfd08b9723bf
Martin <bbaalmart@gmail.com>
2021-03-13 00:11:37 +0300

    move uploads function from config to util

--- src/device-registry/config/cloudinary.js
+++ src/device-registry/config/cloudinary.js
@@ -1,7 +1,8 @@
 const cloudinary = require("cloudinary");
-const dotenv = require("dotenv");
+// const dotenv = require("dotenv");
 
-dotenv.config();
+// dotenv.config();
+console.log("cloud name", process.env.CLOUD_NAME);
 
 cloudinary.config({
   cloud_name: process.env.CLOUD_NAME,
@@ -9,20 +10,4 @@ cloudinary.config({
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-exports.uploads = (file, folder) => {
-  return new Promise((resolve) => {
-    cloudinary.uploader.upload(
-      file,
-      (result) => {
-        resolve({
-          url: result.url,
-          id: result.public_id,
-        });
-      },
-      {
-        resource_type: "auto",
-        folder: folder,
-      }
-    );
-  });
-};
+module.exports = cloudinary;
