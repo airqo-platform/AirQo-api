03230f2decc63f507a4136b69eafeacbabcece1a
lilly-muyama <muyamalillian@gmail.com>
2021-03-25 21:29:41 +0300

    more test functions

--- src/predict/tests/test_utils.py
+++ src/predict/tests/test_utils.py
@@ -6,34 +6,13 @@ from helpers.utils import get_hourly_met_forecasts, get_location_hourly_weather_
 from helpers.utils import checkKey, get_closest_channel, convert_local_string_date_to_tz_aware_datetime, string_to_hourly_datetime
 from helpers.utils import get_gp_predictions, str_to_date
 
-#def test_get_hourly_met_forecasts():
-#    print('test_get_hourly_met_forecasts')
 
-#def test_get_location_hourly_weather_forecasts(latitude:float, longitude:float):
-#    print('test_get_location_hourly_weather_forecasts')
+def test_good_get_closest_channel():
+    assert get_closest_channel(-0.2, 29.9)== 672528
 
-#def test_load_json_data(full_file_path):
-#    print('test_load_json_data')
+def test_good_get_gp_predictions():
+    assert len(get_gp_predictions()) == 10000
 
-#def test_save_json_data(file_name, data_to_save):
-#    print('test_save_json_data')
-
-#def test_checkKey(dict, key):
-#    print('test_checkKey')
-
-#def test_get_closest_channel(latitude, longitude) -> int:
-#    print('test_get_closest_channel')
-
-#def test_convert_local_string_date_to_tz_aware_datetime(local_date_string):
-#    print('test_convert_local_string_date_to_tz_aware_datetime')
-
-#def test_string_to_hourly_datetime(my_list):
-#    print('test_string_to_hourly_datetime')
-
-#def test_get_gp_predictions():
-#    print('test_get_gp_predictions')
-
-def test_good_date():
-    print('test_str_to_date')
+def test_good_str_to_date():
     assert str_to_date('2020-02-01 00:00:00') == datetime(2020, 2, 1, 0, 0, 0)
 
