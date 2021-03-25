5bd3b98dd16984fb9e4385c43cd5dfd86db2538d
Noah Nsimbe <nsimbenoah@gmail.com>
2021-03-26 01:08:24 +0300

    pm2.5 calibrated value

--- src/data-mgt/python/batch-jobs/airqo/Readme.md
+++ src/data-mgt/python/batch-jobs/airqo/Readme.md
@@ -1,9 +1,8 @@
-Add [Add the Google Credentials file](https://airqo.slack.com/archives/GTHGHCB4G/p1616442599002000) to this directory add rename it to `bigquery.json`
-
-Build the image
-`docker build -t airqo-batch .`
-
-Create and start a container
-`docker run --rm --env DEVICE_REGISTRY_BASE_URL=<device registry base url> airqo-batch`
-
-If you are testing with the device registry microservice running locally, include `--network="host"` i.e. `docker run --network="host" --env DEVICE_REGISTRY_BASE_URL=http://localhost:3000/api/v1/ --rm airqo-batch`
+> Add the [Google Credentials file](https://airqo.slack.com/archives/GTHGHCB4G/p1616442599002000) to this directory and rename it to `bigquery.json`
+>
+> Build the image
+> `docker build -t airqo-batch .`
+>
+> Create and start a container
+>
+>`docker run --network="host" --env-file env.airqo.batch --rm airqo-batch`
--- src/data-mgt/python/batch-jobs/airqo/batch_fetch_airqo_data.py
+++ src/data-mgt/python/batch-jobs/airqo/batch_fetch_airqo_data.py
@@ -1,7 +1,6 @@
 import json
 import math
 import os
-from threading import Thread
 
 import requests
 import luigi
@@ -10,12 +9,33 @@ from google.cloud import bigquery
 import traceback
 
 DEVICE_REGISTRY_BASE_URL = os.getenv("DEVICE_REGISTRY_BASE_URL")
+CALIBRATE_URL = os.getenv("CALIBRATE_URL")
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "bigquery.json"
 
-"""
-Luigi task to fetch all airqo devices
-"""
+
+class DataConstants:
+    DEVICE = "device"
+    CHANNEL_ID = "channelID"
+    LOCATION = "location"
+    FREQUENCY = "frequency"
+    TIME = "time"
+    PM2_5 = "pm2_5"
+    PM10 = "pm10"
+    S2_PM2_5 = "s2_pm2_5"
+    S2_PM10 = "s2_pm10"
+    BATTERY = "battery"
+    ALTITUDE = "altitude"
+    SPEED = "speed"
+    SATELLITES = "satellites"
+    HDOP = "hdop"
+    INTERNAL_TEMP = "internalTemperature"
+    INTERNAL_HUM = "internalHumidity"
+
+
 class GetAirqoDevices(luigi.Task):
+    """
+    Luigi task to fetch all airqo devices
+    """
 
     def run(self):
 
@@ -32,10 +52,10 @@ class GetAirqoDevices(luigi.Task):
         return luigi.LocalTarget("data/devices.json")
 
 
-"""
-formatting all values to float else null
-"""
 def check_float(string):
+    """
+    formatting all values to float else null
+    """
 
     try:
         value = float(string)
@@ -44,27 +64,6 @@ def check_float(string):
         return value
     except Exception:
         return None
-
-
-def events_collection_insertion(data, tenant):
-
-    try:
-
-        device = data.pop("device")
-
-        json_data = json.dumps([data])
-
-        headers = {'Content-Type': 'application/json'}
-        url = DEVICE_REGISTRY_BASE_URL + "devices/events/add?device=" + device + "&tenant=" + tenant
-
-        results = requests.post(url, json_data, headers=headers, verify=False)
-
-        print(results.json())
-
-    except Exception:
-        print("================ Error Occurred ==================")
-        traceback.print_exc()
-        print("================ Error End ==================")
 
 
 class GetDeviceMeasurements(luigi.Task):
@@ -83,16 +82,13 @@ class GetDeviceMeasurements(luigi.Task):
 
         details = []
 
-        # create a to hold all threads
-        threads = []
-
         for device_index, device_row in devices.iterrows():
 
             query = """
             SELECT created_at as time, pm2_5, pm10 , s2_pm2_5,
              s2_pm10, temperature as internalTemperature, humidity as internalHumidity, voltage as battery, altitude,
              no_sats as satellites, hdope as hdop, wind as speed FROM airqo-250220.thingspeak.clean_feeds_pms WHERE
-             channel_id={0} ORDER BY created_at ASC
+             channel_id={0} ORDER BY created_at
                """.format(int(device_row["channelID"]))
 
             dataframe = (
@@ -103,39 +99,126 @@ class GetDeviceMeasurements(luigi.Task):
 
                 data = dict({
 
-                    "device": device_row['name'],
-                    "channelID": device_row["channelID"],
-
-                    "location": {"latitude": {"value": check_float(device_row["latitude"])},
-                                 "longitude": {"value": check_float(device_row["longitude"])}},
-
-                    "frequency": "minute",
-                    "time": pd.Timestamp(row["time"]).isoformat(),
-                    "pm2_5": {"value": check_float(row["pm2_5"])},
-                    "pm10": {"value": check_float(row["pm10"])},
-                    "s2_pm2_5": {"value": check_float(row["s2_pm2_5"])},
-                    "s2_pm10": {"value": check_float(row["s2_pm10"])},
-                    "battery": {"value": check_float(row["battery"])},
-                    "altitude": {"value": check_float(row["altitude"])},
-                    "speed": {"value": check_float(row["speed"])},
-                    "satellites": {"value": check_float(row["satellites"])},
-                    "hdop": {"value": check_float(row["hdop"])},
-                    "internalTemperature": {"value": check_float(row["internalTemperature"])},
-                    "internalHumidity": {"value": check_float(row["internalHumidity"])},
+                    DataConstants.DEVICE: device_row['name'],
+                    DataConstants.CHANNEL_ID: device_row["channelID"],
+                    DataConstants.LOCATION: {"latitude":
+                                                 {"value": check_float(device_row["latitude"])},
+                                             "longitude":
+                                                 {"value": check_float(device_row["longitude"])}
+                                             },
+                    DataConstants.FREQUENCY: "minute",
+                    DataConstants.TIME: pd.Timestamp(row["time"]).isoformat(),
+                    DataConstants.PM2_5: {"value": check_float(row["pm2_5"])},
+                    DataConstants.PM10: {"value": check_float(row["pm10"])},
+                    DataConstants.S2_PM2_5: {"value": check_float(row["s2_pm2_5"])},
+                    DataConstants.S2_PM10: {"value": check_float(row["s2_pm10"])},
+                    DataConstants.BATTERY: {"value": check_float(row["battery"])},
+                    DataConstants.ALTITUDE: {"value": check_float(row["altitude"])},
+                    DataConstants.SPEED: {"value": check_float(row["speed"])},
+                    DataConstants.SATELLITES: {"value": check_float(row["satellites"])},
+                    DataConstants.HDOP: {"value": check_float(row["hdop"])},
+                    DataConstants.INTERNAL_TEMP: {"value": check_float(row["internalTemperature"])},
+                    DataConstants.INTERNAL_HUM: {"value": check_float(row["internalHumidity"])},
                 })
 
                 details.append(data)
 
-                thread = Thread(target=events_collection_insertion, args=(data, "airqo"))
-                threads.append(thread)
-                thread.start()
-
-        # wait for all threads to terminate before ending the function
-        for thread in threads:
-            thread.join()
-
         with self.output().open('w') as f:
             json.dump(list(details), f)
+
+
+def get_calibrated_value(channel_id, time, value):
+
+    data = {
+        "datetime": time,
+        "raw_values": [
+            {
+                "raw_value": value,
+                "sensor_id": channel_id
+            }
+        ]
+    }
+
+    post_request = requests.post(url=CALIBRATE_URL, json=data)
+    response = post_request.json()
+
+    calibrated_value = None
+
+    for result in response:
+        if "calibrated_value" in result:
+            calibrated_value = result["calibrated_value"]
+            break
+
+    return calibrated_value
+
+
+class AddCalibratedValues(luigi.Task):
+
+    def requires(self):
+        return GetDeviceMeasurements()
+
+    def output(self):
+        return luigi.LocalTarget("data/device_measurements_with_calibrated_values.json")
+
+    def run(self):
+
+        device_measurements = pd.read_json('data/device_measurements.json')
+
+        calibrated_data = []
+
+        for index, row in device_measurements.iterrows():
+
+            data = row.to_dict()
+
+            calibrated_value = get_calibrated_value(row[DataConstants.CHANNEL_ID],
+                                                    row[DataConstants.TIME],
+                                                    row[DataConstants.PM2_5]["value"])
+
+            data[DataConstants.PM2_5]["calibratedValue"] = calibrated_value
+
+            calibrated_data.append(data)
+
+        with self.output().open('w') as f:
+            json.dump(list(calibrated_data), f)
+
+
+def events_collection_insertion(data, tenant):
+
+    try:
+
+        device = data.pop(DataConstants.DEVICE)
+
+        json_data = json.dumps([data])
+
+        headers = {'Content-Type': 'application/json'}
+        url = DEVICE_REGISTRY_BASE_URL + "devices/events/add?device=" + device + "&tenant=" + tenant
+
+        results = requests.post(url, json_data, headers=headers, verify=False)
+
+        print(results.json())
+
+    except Exception:
+        print("================ Error Occurred ==================")
+        traceback.print_exc()
+        print("================ Error End =======================")
+
+
+class AddValuesToEventsCollection(luigi.Task):
+
+    def requires(self):
+        return AddCalibratedValues()
+
+    def output(self):
+        return luigi.LocalTarget("data/output.json")
+
+    def run(self):
+        device_measurements = pd.read_json('data/device_measurements_with_calibrated_values.json')
+
+        for index, row in device_measurements.iterrows():
+            events_collection_insertion(row.to_dict(), "airqo")
+
+        with self.output().open('w') as f:
+            json.dump(list([]), f)
 
 
 if __name__ == '__main__':
