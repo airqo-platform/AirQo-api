8c0288e1fdb6ba8dfa0cf42c96512fab47381791
danielogen <danielogen@gmail.com>
2021-03-26 09:21:11 +0300

    [PLAT-430] update production ip block

--- infra/terraform/gcp/production/network.tf
+++ infra/terraform/gcp/production/network.tf
@@ -7,5 +7,5 @@ resource "google_compute_subnetwork" "airqo_production_network_subnetwork" {
   name          = "${var.network}-subnetwork-${var.subnetwork-region}"
   region        = var.subnetwork-region
   network       = google_compute_network.airqo_production_network.self_link
-  ip_cidr_range = "10.242.0.0/16"
+  ip_cidr_range = "10.240.0.0/16"
 }
