27c75d21162ebf34fc453f193eab5b4f27d0eedd
danielogen <danielogen@gmail.com>
2021-03-28 14:56:26 +0300

    [PLAT-430] add configs for lb, db, and devops machines

--- infra/terraform/gcp/staging/main.tf
+++ infra/terraform/gcp/staging/main.tf
@@ -5,7 +5,7 @@ resource "google_compute_instance" "airqo-controller-instance" {
   ## provisioned
   #count        = 2
   #name         = "${var.instance-name}-${count.index}"
-  name         = var.controller-node-instance-name
+  name         = var.instance-name["controller"]
   machine_type = var.vm_type["1point7gig"]
 
   zone = var.region
@@ -27,7 +27,7 @@ resource "google_compute_instance" "airqo-controller-instance" {
   }
 
   metadata = {
-    hostname = "airqo-staging-controller.airqo.net"
+    hostname = "${var.instance-name["controller"]}.airqo.net"
   }
 
   network_interface {
@@ -40,14 +40,16 @@ resource "google_compute_instance" "airqo-controller-instance" {
   }
 }
 
-resource "google_compute_instance" "airqo-staging-worker-instance" {
+# add loadbalancer VM
+resource "google_compute_instance" "airqo-loadbalancer-instance" {
 
   ## for a setup having multiple instances of the same type, you can do
   ## the following, there would be 2 instances of the same configuration
   ## provisioned
-  count        = 1
-  name         = "${var.worker-nodes-instance-name}-${count.index}"
-  machine_type = var.vm_type["7point5gig"]
+  #count        = 2
+  #name         = "${var.instance-name}-${count.index}"
+  name         = var.instance-name["loadbalancer"]
+  machine_type = var.vm_type["1point7gig"]
 
   zone = var.region
 
@@ -68,7 +70,134 @@ resource "google_compute_instance" "airqo-staging-worker-instance" {
   }
 
   metadata = {
-    hostname = "airqo-staging-worker-${count.index}.airqo.net"
+    hostname = "${var.instance-name["loadbalancer"]}.airqo.net"
+  }
+
+  network_interface {
+    subnetwork = google_compute_subnetwork.airqo_staging_network_subnetwork.name
+    network_ip = "10.241.0.3"
+
+    access_config {
+      // Ephemeral IP
+    }
+  }
+}
+
+# add devops VM
+resource "google_compute_instance" "airqo-devops-instance" {
+
+  ## for a setup having multiple instances of the same type, you can do
+  ## the following, there would be 2 instances of the same configuration
+  ## provisioned
+  #count        = 2
+  #name         = "${var.instance-name}-${count.index}"
+  name         = var.instance-name["devops"]
+  machine_type = var.vm_type["1point7gig"]
+
+  zone = var.region
+
+  tags = [
+    "${var.network}-firewall-ssh",
+    "${var.network}-firewall-http",
+    "${var.network}-firewall-https",
+    "${var.network}-firewall-icmp",
+    "${var.network}-firewall-tcps",
+    "${var.network}-firewall-secure-forward",
+  ]
+
+  boot_disk {
+    initialize_params {
+      image = var.os["ubuntu-1804"]
+      size  = var.disk_size["tiny"]
+    }
+  }
+
+  metadata = {
+    hostname = "${var.instance-name["devops"]}.airqo.net"
+  }
+
+  network_interface {
+    subnetwork = google_compute_subnetwork.airqo_staging_network_subnetwork.name
+    network_ip = "10.241.0.4"
+
+    access_config {
+      // Ephemeral IP
+    }
+  }
+}
+
+# add devops VM
+resource "google_compute_instance" "airqo-db-instance" {
+
+  ## for a setup having multiple instances of the same type, you can do
+  ## the following, there would be 2 instances of the same configuration
+  ## provisioned
+  #count        = 2
+  #name         = "${var.instance-name}-${count.index}"
+  name         = var.instance-name["db"]
+  machine_type = var.vm_type["1point7gig"]
+
+  zone = var.region
+
+  tags = [
+    "${var.network}-firewall-ssh",
+    "${var.network}-firewall-http",
+    "${var.network}-firewall-https",
+    "${var.network}-firewall-icmp",
+    "${var.network}-firewall-tcps",
+    "${var.network}-firewall-secure-forward",
+  ]
+
+  boot_disk {
+    initialize_params {
+      image = var.os["ubuntu-1804"]
+      size  = var.disk_size["tiny"]
+    }
+  }
+
+  metadata = {
+    hostname = "${var.instance-name["db"]}.airqo.net"
+  }
+
+  network_interface {
+    subnetwork = google_compute_subnetwork.airqo_staging_network_subnetwork.name
+    network_ip = "10.241.0.5"
+
+    access_config {
+      // Ephemeral IP
+    }
+  }
+}
+
+resource "google_compute_instance" "airqo-staging-worker-instance" {
+
+  ## for a setup having multiple instances of the same type, you can do
+  ## the following, there would be 2 instances of the same configuration
+  ## provisioned
+  count        = 1
+  name         = "${var.instance-name["worker"]}-${count.index}"
+  machine_type = var.vm_type["1point7gig"]
+
+  zone = var.region
+
+  tags = [
+    "${var.network}-firewall-ssh",
+    "${var.network}-firewall-http",
+    "${var.network}-firewall-https",
+    "${var.network}-firewall-icmp",
+    "${var.network}-firewall-tcps",
+    "${var.network}-firewall-secure-forward",
+  ]
+
+  boot_disk {
+    initialize_params {
+      image = var.os["ubuntu-1804"]
+      size  = var.disk_size["tiny"]
+    }
+  }
+
+  metadata = {
+    hostname = "${var.instance-name["worker"]}-${count.index}.airqo.net"
   }
 
   network_interface {
