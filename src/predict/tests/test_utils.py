6d19fbf46147e949454c3941dfa3edaef33163e4
lilly-muyama <muyamalillian@gmail.com>
2021-03-30 16:22:25 +0300

    more test functions forr utils.py and validation.py

--- src/predict/tests/test_utils.py
+++ src/predict/tests/test_utils.py
@@ -1,7 +1,8 @@
 import pytest
 import sys
+import os
 from datetime import datetime
-sys.path.append('../')
+sys.path.append('./')
 from helpers.utils import get_hourly_met_forecasts, get_location_hourly_weather_forecasts, load_json_data, save_json_data
 from helpers.utils import checkKey, get_closest_channel, convert_local_string_date_to_tz_aware_datetime, string_to_hourly_datetime
 from helpers.utils import get_gp_predictions, str_to_date
@@ -10,9 +11,25 @@ from helpers.utils import get_gp_predictions, str_to_date
 def test_good_get_closest_channel():
     assert get_closest_channel(-0.2, 29.9)== 672528
 
+def test_raises_exception_on_non_numerical_args():
+    with pytest.raises(TypeError):
+        get_closest_channel('cinder', 'rella')
+
+def test_raises_exception_on_no_of_args():
+    with pytest.raises(TypeError):
+        get_closest_channel(32.1)
+
 def test_good_get_gp_predictions():
     assert len(get_gp_predictions()) == 10000
 
 def test_good_str_to_date():
     assert str_to_date('2020-02-01 00:00:00') == datetime(2020, 2, 1, 0, 0, 0)
+
+def test_raises_exception_on_non_string_args():
+    with pytest.raises(TypeError):
+        str_to_date(12)
+
+def test_raises_exception_on_incorrect_string_args():
+    with pytest.raises(ValueError):
+        str_to_date('Cinderella')
 
--- src/predict/tests/test_validation.py
+++ src/predict/tests/test_validation.py
@@ -1,12 +1,33 @@
-from marshmallow import Schema, fields, ValidationError,validate
-import json
-import typing as t
+import sys
+import pytest
+sys.path.append('../')
+from helpers.validation import validate_inputs, validate_inputs_for_next_24hour_predictions
 
 
-def test_filter_error_rows(errors: dict,validated_input: t.List[dict]) -> t.List[dict]:
+def test_filter_error_rows():
+    pass
 
 
-def test_validate_inputs(input_data)
+def test_validate_inputs():
+    test_data = {
+        'selected_datetime':'2020-01-24 00:00',
+        'latitude':'0.3284992',
+        'longitude':'32.5396499'
+        }
+    assert len(validate_inputs(data))==2 and validate_inputs(data)[1] == None
 
 
-def test_validate_inputs_for_next_24hour_predictions(input_data)
+def test_validate_inputs_for_next_24hour_predictions():
+    pass
+
+if __name__=='__main__':
+    data = {
+        'selected_datetime':'2020-01-24 00:00',
+        'latitude':'0.3284992',
+        'longitude':'32.5396499'
+        }
+    validate_results=validate_inputs(data)
+    if len(validate_inputs(data))==2 and validate_inputs(data)[1] == None: 
+        print('Yes')
+    else:
+        print('No')
