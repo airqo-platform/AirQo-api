a9b1c18d212d8feaeb2d10429a37a077db877f90
Priscilla-AL <priscilla@airqo.net>
2021-04-17 18:24:51 +0300

    Update regression.py

--- src/calibrate/jobs/regression.py
+++ src/calibrate/jobs/regression.py
@@ -16,18 +16,22 @@ client = bigquery.Client.from_service_account_json("jobs/airqo-250220-5149c2aac8
 def get_lowcost_data():
     sql = """
     SELECT 
-        created_at, SUM(pm2_5 + s2_pm2_5)/2 as lowcost_pm2_5,SUM(pm10 + s2_pm10)/2 as lowcost_pm10
+        created_at, SUM(pm2_5 + s2_pm2_5)/2 as pm2_5,SUM(pm10 + s2_pm10)/2 as pm10
     FROM 
         `airqo-250220.thingspeak.clean_feeds_pms`
     WHERE 
         channel_id = 967600
+    AND 
+        created_at >= '2020-07-15 00:00:00'
+    AND 
+        created_at <= '2021-03-23 23:59:59'
     GROUP BY 
         created_at
     ORDER BY 
         created_at
         """
     lowcost_data = client.query(sql).to_dataframe()
-    lowcost_data = lowcost_data[(lowcost_data['lowcost_pm2_5'] > 0)&(lowcost_data['lowcost_pm2_5'] <= 500.4)]
+    lowcost_data = lowcost_data[(lowcost_data['pm2_5'] > 0)&(lowcost_data['pm2_5'] <= 500.4)]
                                        
     lowcost_data["TimeStamp"] = pd.to_datetime(lowcost_data["created_at"])
     lowcost_data["TimeStamp"] = lowcost_data["TimeStamp"]+datetime.timedelta(hours=3)
@@ -49,6 +53,10 @@ def get_bam_data():
         `airqo-250220.thingspeak.airqo_bam_data`
     WHERE 
         channel_id = -24516
+    AND 
+        Time >= '2020-07-15 00:00:00'
+    AND 
+        Time <= '2021-03-23 23:59:59'
     GROUP BY 
         Time,ConcHR_ug_m3, AT_C, RH
     ORDER BY 
@@ -73,7 +81,7 @@ def combine_datasets(lowcost_hourly_mean, bam_hourly_mean):
     bam_hourly_mean["Time"] = bam_hourly_timestamp
 
     hourly_combined_dataset = pd.merge(lowcost_hourly_mean, bam_hourly_mean, on='Time')
-    hourly_combined_dataset = hourly_combined_dataset[hourly_combined_dataset['lowcost_pm2_5'].notna()]
+    hourly_combined_dataset = hourly_combined_dataset[hourly_combined_dataset['pm2_5'].notna()]
     hourly_combined_dataset = hourly_combined_dataset[hourly_combined_dataset['bam_pm'].notna()]
 
     # extract month feature
@@ -84,7 +92,7 @@ def combine_datasets(lowcost_hourly_mean, bam_hourly_mean):
 
 def simple_linear_regression(hourly_combined_dataset):
 
-    X_muk = hourly_combined_dataset['lowcost_pm2_5'].values
+    X_muk = hourly_combined_dataset['pm2_5'].values
     X_muk = X_muk.reshape((-1, 1))
     y_muk = hourly_combined_dataset['bam_pm'].values
 
@@ -106,7 +114,7 @@ def simple_linear_regression(hourly_combined_dataset):
 
 
 def mlr(hourly_combined_dataset):
-    X_MLR_muk = hourly_combined_dataset[['lowcost_pm2_5','temperature','humidity','lowcost_pm10']].values
+    X_MLR_muk = hourly_combined_dataset[['pm2_5','temperature','humidity']].values
     y_MLR_muk = hourly_combined_dataset['bam_pm'].values    
 
     X_train_MLR_muk, X_test_MLR_muk, y_train_MLR_muk, y_test_MLR_muk = train_test_split(X_MLR_muk, y_MLR_muk, test_size=0.2, random_state=0)
@@ -125,13 +133,13 @@ def mlr(hourly_combined_dataset):
     return slope, intercept, MAE2, RMSE2, r2_score2
 
 def random_forest(hourly_combined_dataset):
-    X_MLR_muk = hourly_combined_dataset[['lowcost_pm2_5','temperature','humidity','lowcost_pm10','month','hour']].values
-    y_MLR_muk = hourly_combined_dataset['bam_pm'].values    
+    X_rf_muk = hourly_combined_dataset[['pm2_5','temperature','humidity','pm10','month','hour']].values
+    y_rf_muk = hourly_combined_dataset['bam_pm'].values    
 
-    X_train_MLR_muk, X_test_MLR_muk, y_train_MLR_muk, y_test_MLR_muk = train_test_split(X_MLR_muk, y_MLR_muk, test_size=0.2, random_state=0)
+    X_train_rf_muk, X_test_rf_muk, y_train_rf_muk, y_test_rf_muk = train_test_split(X_rf_muk, y_rf_muk, test_size=0.2, random_state=0)
     rf_regressor = RandomForestRegressor() 
     # Fitting the model 
-    rf_reg = rf_regressor.fit(X_train_MLR_muk, y_train_MLR_muk) 
+    rf_reg = rf_regressor.fit(X_train_rf_muk, y_train_rf_muk) 
     '''RandomForestRegressor(bootstrap=True, ccp_alpha=0.0, criterion='mse', 
                     max_depth=None, max_features='auto', max_leaf_nodes=None, 
                     max_samples=None, min_impurity_decrease=0.0, 
@@ -140,11 +148,11 @@ def random_forest(hourly_combined_dataset):
                     n_estimators=100, n_jobs=None, oob_score=False, 
                     random_state=None, verbose=0, warm_start=False)'''
 
-    y_pred_mlr_muk = rf_regressor.predict(X_test_MLR_muk)
-    MAE2 = metrics.mean_absolute_error(y_test_MLR_muk, y_pred_mlr_muk)   
-    RMSE2 =  np.sqrt(metrics.mean_squared_error(y_test_MLR_muk, y_pred_mlr_muk))
-    r2_score = metrics.r2_score(y_test_MLR_muk, y_pred_mlr_muk)
+    y_pred_rf_muk = rf_regressor.predict(X_test_rf_muk)
+    MAE3 = metrics.mean_absolute_error(y_test_rf_muk, y_pred_rf_muk)   
+    RMSE3 =  np.sqrt(metrics.mean_squared_error(y_test_rf_muk, y_pred_rf_muk))
+    r2_score3 = metrics.r2_score(y_test_rf_muk, y_pred_rf_muk)
  
-    return rf_reg, MAE2, RMSE2, r2_score2
+    return rf_reg, MAE3, RMSE3, r2_score3
 
     
