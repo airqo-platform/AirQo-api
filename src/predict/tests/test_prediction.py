946d6e9cb78796617e3f1b439ba7c21a32f14160
lilly-muyama <muyamalillian@gmail.com>
2021-04-19 13:07:50 +0300

    adding decorators

--- src/predict/tests/test_prediction.py
+++ src/predict/tests/test_prediction.py
@@ -4,10 +4,34 @@ import sys
 import requests
 sys.path.append('./')
 
+sample_response = {
+  "formatted_results": {
+    "predictions": [
+      {
+        "lower_ci": 66.08,
+        "prediction_time": "Thu, 23 Jan 2020 22:00:00 GMT",
+        "prediction_value": 110.74,
+        "upper_ci": 155.4
+      },
+      
+      {
+        "lower_ci": 55.46,
+        "prediction_time": "Fri, 24 Jan 2020 11:00:00 GMT",
+        "prediction_value": 65.12,
+        "upper_ci": 74.77
+      },
+      
+     
+    ]
+  }
+}
+
+@patch('requests.get')
 def test_get_next_24hr_predictions():
     response = requests.get('http://127.0.0.1:5000/api/v2/predict/730016/1597336460')
     assert response.status_code== 201
 
+@patch('request.post')
 def test_predict_avgs():
     json_data = {
         "selected_datetime":"2020-01-24 00:00",
@@ -17,11 +41,12 @@ def test_predict_avgs():
     response = requests.post('http://127.0.0.1:5000/api/v1/predict', data=json_data)
     assert response.status_code== 200
 
+@patch('request.post')
 def test_bad_predict_avgs():
     response = requests.post('http://127.0.0.1:5000/api/v1/predict')
     assert response.status_code== 400
 
-
+@patch('request.get')
 def test_predictions_for_heatmap():
     response = requests.get('http://127.0.0.1:5000/api/v1/predict/heatmap')
     assert response.status_code== 200
