b809cbe60f05eb45f7c4df5d319e1b4b90ce1366
lilly-muyama <muyamalillian@gmail.com>
2021-04-19 13:20:58 +0300

    test_predictions_for_heatmap

--- src/predict/tests/test_prediction.py
+++ src/predict/tests/test_prediction.py
@@ -49,7 +49,8 @@ def test_bad_predict_avgs(mock_post):
     response = requests.post('http://127.0.0.1:5000/api/v1/predict')
     assert response.status_code== 400
 
-@patch('request.get')
+@patch('requests.get')
 def test_predictions_for_heatmap(mock_get):
+    mock_get.return_value.status_code = 200
     response = requests.get('http://127.0.0.1:5000/api/v1/predict/heatmap')
     assert response.status_code== 200
