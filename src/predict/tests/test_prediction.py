ea6ba2048d49fbcf97cea4b1ea5f982e594e8a8e
lilly-muyama <muyamalillian@gmail.com>
2021-04-19 13:16:50 +0300

    test_predict_avgs

--- src/predict/tests/test_prediction.py
+++ src/predict/tests/test_prediction.py
@@ -32,7 +32,7 @@ def test_get_next_24hr_predictions(mock_get):
     response = requests.get('http://127.0.0.1:5000/api/v2/predict/730016/1597336460')
     assert response.status_code== 201
 
-@patch('request.post')
+@patch('requests.post')
 def test_predict_avgs(mock_post):
     mock_post.return_value.status_code = 500
     json_data = {
