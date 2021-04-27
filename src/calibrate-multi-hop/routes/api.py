7b4d9ce3a17cf8f44d5061d67392f26f342643d7
Priscilla-AL <priscilla@airqo.net>
2021-04-27 18:31:56 +0300

    Update api.py

--- src/calibrate-multi-hop/routes/api.py
+++ src/calibrate-multi-hop/routes/api.py
@@ -2,6 +2,6 @@ base_url = '/api/v1'
 
 route = {
     'root': '/',
-    'calibrate': f'{base_url}/calibrate',
+    'calibrate': f'{base_url}/calibrate/multihop',
     'ratios': f'{base_url}/calibrate/ratios',
 }
