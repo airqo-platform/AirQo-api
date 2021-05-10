6fb649937b025258a572cefd332dedfd573e211c
noah Nsimbe <nsimbenoah@gmail.com>
2021-05-10 09:48:07 +0300

    change requests

--- src/device-registry/utils/nearest-device.js
+++ src/device-registry/utils/nearest-device.js
@@ -1,4 +1,4 @@
-const { logElement } = require("../utils/log");
+const { logElement } = require("./log");
 
 const nearestDevices = {
 
@@ -15,6 +15,7 @@ const nearestDevices = {
 
                     if (distance < radius) {
                         logElement("Found a node ", distance);
+                        device['distance'] = radius;
                         nearest_devices.push(device);
                     }
                 }
@@ -29,31 +30,31 @@ const nearestDevices = {
 
     calculateDistance: ( latitude1, longitude1, latitude2, longitude2) => {
         
-        // distance between latitudes and longitudes
-        var latitudeDisatnce = nearestDevices.degreesToRadians(latitude2 - latitude1);
-        var longitudeDisatnce = nearestDevices.degreesToRadians(longitude2 - longitude1);
+        // getting distance between latitudes and longitudes
+        const latitudeDisatnce = nearestDevices.degreesToRadians(latitude2 - latitude1);
+        const longitudeDisatnce = nearestDevices.degreesToRadians(longitude2 - longitude1);
   
-        // convert to radians
+        // converting degrees to radians
         latitude1 = nearestDevices.degreesToRadians(latitude1);
         latitude2 = nearestDevices.degreesToRadians(latitude2);
   
         // Applying Haversine formula
-        haversine = Math.pow(
+        const haversine = Math.pow(
           Math.sin(latitudeDisatnce / 2), 2) + 
           Math.pow(Math.sin(longitudeDisatnce / 2), 2) * 
           Math.cos(latitude1) * 
           Math.cos(latitude2);
   
         // Earth's radius in kilometers
-        var radius = 6371;
+        const radius = 6371;
   
-        var c = 2 *Math.asin(Math.sqrt(haversine));
+        const c = 2 * Math.asin(Math.sqrt(haversine));
         return radius * c;
     },
 
     degreesToRadians: ( degrees ) => {
         
-        var pi = Math.PI;
+        const pi = Math.PI;
         return degrees * (pi/180);
     },
 }
