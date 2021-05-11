621f8ac74faaa0d3fa70ff0d173cb7b98ad5012a
ja-odur <odurjoseph8@gmail.com>
2021-05-11 16:03:00 +0300

    [PLAT-538] add request query param validator

---
+++ src/device-registry/utils/validators/requestQuery.js
@@ -0,0 +1,23 @@
+const HTTPStatus = require("http-status");
+const _ = require("underscore");
+
+const validateRequestQuery = (paramsArr) => (req, res, next) =>  {
+    const error = {};
+    paramsArr.map(param => {
+        if (!req.query[param]) {
+            error[param] = "This is a required request query parameter";
+        }
+    })
+    if(_.isEmpty(error)) {
+        return next();
+    }
+    res.status(HTTPStatus.BAD_REQUEST).json({
+        success: false,
+        message: "some request query parameter(s) are required",
+        error,
+    });
+}
+
+module.exports = {
+  validateRequestQuery
+};
