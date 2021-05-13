3c2f22055845e1eedadc38fd7a7e340a3dd18bf7
Martin <bbaalmart@gmail.com>
2021-05-13 06:35:58 +0300

    resolve a runtime error for device already exists during device creation

--- src/device-registry/controllers/create-device.js
+++ src/device-registry/controllers/create-device.js
@@ -49,10 +49,11 @@ const device = {
           ...tsBody,
           ...constants.DEVICE_CREATION,
         };
-        const deviceDetails = await getDetail(tenant, device);
-        const doesDeviceExist = !isEmpty(deviceDetails);
+        const deviceDetails = await getDetail(tenant, name);
+        logObject("deviceDetails", deviceDetails);
+        const doesDeviceExist = isEmpty(deviceDetails);
         logElement("isDevicePresent ?", doesDeviceExist);
-        if (!doesDeviceExist) {
+        if (doesDeviceExist) {
           logText("adding device on TS...");
           let channel;
           if (tenant.toLowerCase() === "airqo") {
