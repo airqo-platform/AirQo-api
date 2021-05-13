6375e8a3824104d2bff395acc5f1ed0ffd146249
Martin <bbaalmart@gmail.com>
2021-05-13 18:22:11 +0300

    update validation middleware to check for email

--- src/auth-service/utils/validations.login.js
+++ src/auth-service/utils/validations.login.js
@@ -3,15 +3,13 @@ const isEmpty = require("is-empty");
 module.exports = function validateLoginInput(data) {
   let errors = {};
   // Convert empty fields to an empty string so we can use validator functions
-  data.userName = !isEmpty(data.userName) ? data.userName : "";
+  data.email = !isEmpty(data.email) ? data.email : "";
   data.password = !isEmpty(data.password) ? data.password : "";
-  // Email checks
 
-  // userName checks
-  if (Validator.isEmpty(data.userName)) {
-    errors.userName = "userName field is required";
+  // email checks
+  if (Validator.isEmpty(data.email)) {
+    errors.email = "email field is required";
   }
-
   // Password checks
   if (Validator.isEmpty(data.password)) {
     errors.password = "password field is required";
