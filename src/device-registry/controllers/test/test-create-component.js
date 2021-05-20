1ca942cc21aff73a6cb231939aa413fd22cb8231
Martin <bbaalmart@gmail.com>
2021-05-20 08:42:42 +0300

    introduce renamed or moved and  new unit test files

---
+++ src/device-registry/controllers/test/test-create-component.js
@@ -0,0 +1,48 @@
+"use-strict";
+const componentController = require("../create-component");
+
+let chai = require("chai"),
+  expect = chai.expect;
+chai.should();
+
+describe("list all components", function() {
+  beforeEach(function() {});
+  afterEach(function() {});
+  it("should list all components", function() {});
+});
+
+describe("add a component", function() {
+  beforeEach(function() {});
+  afterEach(function() {});
+  it("should add a component", function() {});
+});
+
+describe("delete a component", function() {
+  beforeEach(function() {});
+  afterEach(function() {});
+  it("should delete a component", function() {});
+});
+
+describe("update a component", function() {
+  beforeEach(function() {});
+  afterEach(function() {});
+  it("should update a component", function() {});
+});
+
+describe("create a component type", function() {
+  beforeEach(function() {});
+  afterEach(function() {});
+  it("should create a component type", function() {});
+});
+
+describe("list all component types", function() {
+  beforeEach(function() {});
+  afterEach(function() {});
+  it("should list all component types", function() {});
+});
+
+describe("calibrate a component", function() {
+  beforeEach(function() {});
+  afterEach(function() {});
+  it("should calibrate a component", function() {});
+});
---
+++ src/device-registry/utils/test/test-update-event.js
@@ -0,0 +1,10 @@
+"use-strict";
+let chai = require("chai"),
+  expect = chai.expect;
+chai.should();
+
+describe("transform events", function() {
+  beforeEach(function() {});
+  afterEach(function() {});
+  it("should return the transformed events", function() {});
+});
