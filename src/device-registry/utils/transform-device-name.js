322cc086e1380cb5ea9863a1779626ddcbe826dc
Martin <bbaalmart@gmail.com>
2021-05-20 08:18:37 +0300

    renamed and or deleted files during refactoring

--- src/device-registry/utils/transform-device-name.js
+++
@@ -1,9 +0,0 @@
-const transformDeviceName = (name) => {
-  let firstElement = name.split(" ").slice(0, 1);
-  let removedOnlySpaces = firstElement[0].replace(/\s+/g, "_").toLowerCase();
-  let removeHyphens = removedOnlySpaces.replace(/\-+/g, "_").toLowerCase();
-  let enforcedNamingConvention = removeHyphens.replace(/airqo/, "aq");
-  return enforcedNamingConvention;
-};
-
-module.exports = transformDeviceName;
