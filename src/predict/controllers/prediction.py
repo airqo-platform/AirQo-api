f675e20603e7293bf9ff1b47a21ebf4c5dc73d93
danielogen <danielogen@gmail.com>
2021-05-21 11:34:45 +0300

    fix redis caching

--- src/predict/controllers/prediction.py
+++ src/predict/controllers/prediction.py
@@ -15,8 +15,12 @@ from flask_cors import CORS
 from dotenv import load_dotenv
 load_dotenv()
 
-cache = Cache(config={'CACHE_TYPE': 'RedisCache', 'CACHE_REDIS_HOST': os.getenv(
-    'REDIS_SERVER'), 'CACHE_REDIS_PORT': os.getenv('REDIS_PORT')})
+cache = Cache(config={
+    'CACHE_TYPE': 'redis',
+    'CACHE_REDIS_HOST': os.getenv('REDIS_SERVER'),
+    'CACHE_REDIS_PORT': os.getenv('REDIS_PORT'),
+    'CACHE_REDIS_URL': f"redis://{os.getenv('REDIS_SERVER')}:{os.getenv('REDIS_PORT')}",
+})
 
 _logger = logging.getLogger(__name__)
 
@@ -214,7 +218,6 @@ def predict_channel_next_24_hours():
 
 @ml_app.route(api.route['predict_for_heatmap'], methods=['GET'])
 @cache.cached(timeout=3600)
-
 def predictions_for_heatmap():
     '''
     makes predictions for a specified location at a given time.
