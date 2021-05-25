9c560441c1663ef637111327fb43b48e4ba7bd77
ja-odur <odurjoseph8@gmail.com>
2021-05-25 16:54:50 +0300

    [PLAT-569] remove unused code

--- src/device-status/app.py
+++ src/device-status/app.py
@@ -1,12 +1,5 @@
 import argparse
-from flask import Flask
-from config import db_connection
 from controllers import status
-import logging
-import os
-from flask_cors import CORS
-from flask_pymongo import PyMongo
-from config import constants, db_connection
 from dotenv import load_dotenv
 load_dotenv()
 
--- src/device-status/models/device.py
+++ src/device-status/models/device.py
@@ -1,35 +1,32 @@
-from config import db_connection
+from config.db_connection import connect_mongo
 
 
 class Device:
-    """The class contains functionality for retrieving device status .
-    Attributes:
-        attr1 (str): Description of `attr1`.
-        attr2 (:obj:`int`, optional): Description of `attr2`.
-    """
 
-    # get device status infromation
     def get_devices(self, tenant):
-        db = db_connection.connect_mongo(tenant)
+        db = connect_mongo(tenant)
         documents = db.devices.find(
             {})
         return documents
-# {"locationID": {'$ne': ''}}, {'_id': 0}
 
-    # get devices status infromation
     def get_device_power(self, tenant):
-        db = db_connection.connect_mongo(tenant)
-        documents = db.devices.find({'$and': [{'locationID': {'$ne': ""}}, {'status': {'$ne': "Retired"}}, {'power': {'$ne': ""}}]}, {
-                                    'power': 1, 'name': 1, 'locationID': 1})
-        return documents
+        db = connect_mongo(tenant)
+        return db.devices.find(
+            {
+                '$and': [
+                    {'locationID': {'$ne': ""}},
+                    {'status': {'$ne': "Retired"}},
+                    {'power': {'$ne': ""}}
+                ]
+            },
+            {
+                'power': 1, 'name': 1, 'locationID': 1
+            }
+        )
 
-        # get device status infromation
     def get_device_status(self, tenant):
-        db = db_connection.connect_mongo(tenant)
-        documents = db.devices.find(
-            {'isActive': {'$eq': True}}, {'name': 1, 'location_id': 1, 'nextMaintenance': 1, 'channelID': 1})
-        return documents
-
-
-if __name__ == "__main__":
-    dx = Device()
+        db = connect_mongo(tenant)
+        return db.devices.find(
+            {'isActive': {'$eq': True}},
+            {'name': 1, 'location_id': 1, 'nextMaintenance': 1, 'channelID': 1}
+        )
