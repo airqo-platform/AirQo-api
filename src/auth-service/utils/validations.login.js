5d70927131fc679fde8b800d2af3237f8cd73b8b
Martin <bbaalmart@gmail.com>
2021-05-26 12:21:47 +0300

    update the validation to check for userName

--- src/auth-service/utils/validations.login.js
+++ src/auth-service/utils/validations.login.js
@@ -2,15 +2,13 @@ const Validator = require("validator");
 const isEmpty = require("is-empty");
 module.exports = function validateLoginInput(data) {
   let errors = {};
-  // Convert empty fields to an empty string so we can use validator functions
-  data.email = !isEmpty(data.email) ? data.email : "";
+  data.userName = !isEmpty(data.userName) ? data.userName : "";
   data.password = !isEmpty(data.password) ? data.password : "";
 
-  // email checks
-  if (Validator.isEmpty(data.email)) {
-    errors.email = "email field is required";
+  if (Validator.isEmpty(data.userName)) {
+    errors.userName = "the userName field is required";
   }
-  // Password checks
+
   if (Validator.isEmpty(data.password)) {
     errors.password = "password field is required";
   }
