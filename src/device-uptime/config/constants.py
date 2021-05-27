a33b7791d0b2ecaff75ff85e4136095546743a22
ja-odur <odurjoseph8@gmail.com>
2021-05-27 14:00:58 +0300

    [PLAT-569] refactor config files

--- src/device-uptime/config/constants.py
+++ src/device-uptime/config/constants.py
@@ -1,35 +1,30 @@
-import logging
-import pathlib
 import os
-import sys
 from dotenv import load_dotenv
 from pathlib import Path
 
-
 BASE_DIR = Path(__file__).resolve().parent.parent
-print("BASE_DIR", BASE_DIR)
+dotenv_path = os.path.join(BASE_DIR, '.env')
+load_dotenv(dotenv_path)
 
 
 class Config:
-    dotenv_path = os.path.join(BASE_DIR, '.env')
-    print("dotenv", dotenv_path)
-    load_dotenv(dotenv_path)
     DEBUG = False
     TESTING = False
     CSRF_ENABLED = True
     SECRET_KEY = os.getenv("SECRET_KEY")
     DB_NAME = os.getenv("DB_NAME_PROD")
     MONGO_URI = os.getenv('MONGO_GCE_URI')
+    BASE_API_URL = "https://staging-platform.airqo.net/api/v1"
+    DAILY_EVENTS_URL = f"{BASE_API_URL}/devices/events"
 
 
 class ProductionConfig(Config):
     DEVELOPMENT = False
+    BASE_API_URL = "https://platform.airqo.net/api/v1"
+    DAILY_EVENTS_URL = f"{BASE_API_URL}/devices/events"
 
 
 class DevelopmentConfig(Config):
-    dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
-    load_dotenv(dotenv_path)
-
     DEVELOPMENT = True
     DEBUG = True
     MONGO_URI = os.getenv("MONGO_DEV_URI")
@@ -37,15 +32,19 @@ class DevelopmentConfig(Config):
 
 
 class TestingConfig(Config):
-    dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
-    load_dotenv(dotenv_path)
-
     TESTING = True
     MONGO_URI = os.getenv('MONGO_GCE_URI')
     DB_NAME = os.getenv("DB_NAME_STAGE")
 
 
-app_config = {"development": DevelopmentConfig,
-              "testing": TestingConfig,
-              "production": ProductionConfig,
-              "staging": TestingConfig}
+app_config = {
+    "development": DevelopmentConfig,
+    "testing": TestingConfig,
+    "production": ProductionConfig,
+    "staging": TestingConfig
+}
+
+environment = os.getenv("ENV")
+print("ENVIRONMENT", environment or 'production')
+
+configuration = app_config.get(environment, ProductionConfig)
--- src/device-uptime/config/db_connection.py
+++ src/device-uptime/config/db_connection.py
@@ -1,21 +1,8 @@
-
-from flask_pymongo import PyMongo
 from pymongo import MongoClient
-from datetime import datetime, timedelta
-import os
-import sys
-from config import constants
-from dotenv import load_dotenv
-load_dotenv()
-
-
-app_configuration = constants.app_config.get(os.getenv("FLASK_ENV"))
+from config.constants import configuration
 
 
 def connect_mongo(tenant):
-    print("MONGO URI", app_configuration.MONGO_URI)
-    print("DB_NAME", app_configuration.DB_NAME)
-
-    client = MongoClient(app_configuration.MONGO_URI)
-    db = client[f'{app_configuration.DB_NAME}_{tenant.lower()}']
+    client = MongoClient(configuration.MONGO_URI)
+    db = client[f'{configuration.DB_NAME}_{tenant.lower()}']
     return db
