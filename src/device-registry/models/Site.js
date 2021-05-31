a849f83129563d89f1c4e334707910f60405a89d
Martin <bbaalmart@gmail.com>
2021-05-31 12:45:12 +0300

    add more fields and also create model before exporting the module

--- src/device-registry/models/Site.js
+++ src/device-registry/models/Site.js
@@ -1,10 +1,24 @@
 const mongoose = require("mongoose");
 const ObjectId = mongoose.Schema.Types.ObjectId;
 const uniqueValidator = require("mongoose-unique-validator");
+const { getModelByTenant } = require("../utils/multitenancy");
 
 const siteSchema = new mongoose.Schema(
   {
     name: {
+      type: String,
+      trim: true,
+    },
+    generated_name: {
+      type: String,
+      trim: true,
+    },
+    formatted_name: {
+      type: String,
+      trim: true,
+      unique: true,
+    },
+    description: {
       type: String,
       trim: true,
     },
@@ -14,10 +28,27 @@ const siteSchema = new mongoose.Schema(
     longitude: {
       type: Number,
     },
+    activities: [
+      {
+        type: String,
+      },
+    ],
     altitude: {
       type: Number,
     },
-    roadDistance: {
+    distance_to_nearest_road: {
+      type: String,
+      trim: true,
+    },
+    distance_to_nearest_motorway: {
+      type: String,
+      trim: true,
+    },
+    distance_to_nearest_city: {
+      type: String,
+      trim: true,
+    },
+    distance_to_nearest_residential_area: {
       type: String,
       trim: true,
     },
@@ -25,36 +56,58 @@ const siteSchema = new mongoose.Schema(
       type: String,
       trim: true,
     },
-    landUse: {
+    land_use: {
       type: String,
       trim: true,
     },
-    roadIntensity: {
+    road_intensity: {
       type: Number,
     },
-    roadStatus: {
-      type: String,
-    },
-    siteActivities: {
+    road_status: {
       type: String,
     },
     aspect: {
       type: String,
     },
-    landform90: {
+    landform_90: {
       type: Number,
     },
-    landform270: {
+    landform_270: {
       type: Number,
     },
-    greeness: {
+    greenness: {
       type: Number,
     },
-    trafficFactor: {
+    traffic_factor: {
       type: Number,
     },
-    locationID: {
-      type: ObjectId,
+    parish: {
+      type: String,
+      trim: true,
+    },
+    village: {
+      type: String,
+      trim: true,
+    },
+    district: {
+      type: String,
+      trim: true,
+    },
+    region: {
+      type: String,
+      trim: true,
+    },
+    county: {
+      type: String,
+      trim: true,
+    },
+    sub_county: {
+      type: String,
+      trim: true,
+    },
+    country: {
+      type: String,
+      trim: true,
     },
   },
   {
@@ -71,37 +124,57 @@ siteSchema.methods = {
     return {
       _id: this._id,
       name: this.name,
-      createdAt: this.createdAt,
-      description: this.description,
-      county: this.county,
-      subcounty: this.subcounty,
-      parish: this.parish,
-      road_intensity: this.road_intensity,
-      distance_from_motorway: this.distance_from_motorway,
-      distance_from_residential: this.distance_from_residential,
-      distance_from_city: this.distance_from_city,
-      distance_from_nearest_road: this.distance_from_nearest_road,
-      host: this.host,
+      generated_name: this.generated_name,
+      formatted_name: this.formatted_name,
+      site_id: this.site_id,
       latitude: this.latitude,
       longitude: this.longitude,
+      createdAt: this.createdAt,
+      description: this.description,
+      site_activities: this.site_activities,
+      county: this.county,
+      sub_county: this.sub_county,
+      parish: this.parish,
+      village: this.village,
+      region: this.region,
+      district: this.district,
+      road_intensity: this.road_intensity,
+      distance_to_nearest_motor_way: this.distance_to_nearest_motor_way,
+      distance_to_nearest_residential_area: this
+        .distance_to_nearest_residential_area,
+      distance_to_nearest_city: this.distance_to_nearest_city,
+      distance_to_nearest_road: this.distance_to_nearest_road,
     };
   },
-};
-
-// I will add the check for the user after setting up the communications between services
-siteSchema.statics = {
-  createLocation(args) {
+  create(args) {
     return this.create({
       ...args,
     });
   },
+};
 
-  list({ skip = 0, limit = 5 } = {}) {
-    return this.find()
+siteSchema.statics = {
+  list({ skip = 0, limit = 5, filter = {} } = {}) {
+    return this.find(filter)
       .sort({ createdAt: -1 })
       .skip(skip)
       .limit(limit);
   },
+  delete({ filter = {}, options = {} } = {}) {
+    let opts = options;
+    opts[justOne] = true;
+    return this.remove(filter, opts);
+  },
+  update({ filter = {}, options = {} } = {}) {
+    let opts = options;
+    return this.update(filter, opts);
+  },
 };
 
-module.exports = siteSchema;
+siteSchema.methods = {};
+
+const siteModel = async (tenant) => {
+  return await getModelByTenant(tenant.toLowerCase(), "site", siteSchema);
+};
+
+module.exports = siteModel;
