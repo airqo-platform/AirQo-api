e2578d9e1216b3e2ac04c097cfed32212e9df8b0
Martin <bbaalmart@gmail.com>
2021-06-15 09:53:25 +0300

    all controller logic for requesting access

--- src/auth-service/controllers/request.js
+++ src/auth-service/controllers/request.js
@@ -2,12 +2,12 @@ const CandidateSchema = require("../models/Candidate");
 const UserSchema = require("../models/User");
 const HTTPStatus = require("http-status");
 const msgs = require("../utils/email.msgs");
-const validateCandidateInput = require("../utils/validations.candidate");
 const register = require("../utils/register");
 const { getModelByTenant } = require("../utils/multitenancy");
 const constants = require("../config/constants");
-const validateRegisterInput = require("../utils/validations.register");
-const generator = require("generate-password");
+const requestUtil = require("../utils/request");
+const generateFilter = require("../utils/generate-filter");
+const validations = require("../utils/validations");
 
 const CandidateModel = (tenant) => {
   return getModelByTenant(tenant, "candidate", CandidateSchema);
@@ -17,53 +17,157 @@ const UserModel = (tenant) => {
   return getModelByTenant(tenant, "user", UserSchema);
 };
 
-const { tryCatchErrors, missingQueryParams } = require("../utils/errors");
+const { tryCatchErrors, missingQueryParams } = require("utils/errors");
+const { logObject } = require("utils/log");
+const { logText, logElement } = require("../utils/log");
+const isEmpty = require("is-empty");
+const { request } = require("../app");
 
 const candidate = {
-  registerCandidate: (req, res) => {
-    const { tenant } = req.query;
-    const { errors, isValid } = validateCandidateInput(req.body);
-    if (!isValid) {
-      return res
-        .status(400)
-        .json({ success: false, errors, message: "validation error" });
-    }
-
-    const mailOptions = {
-      from: constants.EMAIL,
-      to: `${req.body.email}`,
-      subject: "AirQo Platform JOIN request",
-      text: msgs.joinRequest,
-    };
-
-    register(
-      req,
-      res,
-      mailOptions,
-      req.body,
-      CandidateModel(tenant.toLowerCase())
-    );
-  },
-
-  getAllCandidates: async (req, res) => {
+  create: async (req, res) => {
     try {
       const { tenant } = req.query;
-      const users = await CandidateModel(tenant.toLowerCase()).find();
-      return res.status(HTTPStatus.OK).json({
-        success: true,
-        message: "Candidates fetched successfully",
-        users,
+      const {
+        firstName,
+        lastName,
+        email,
+        organization,
+        jobTitle,
+        website,
+        description,
+        category,
+      } = req.body;
+      const { errors, isValid } = validations.candidate(req.body);
+      if (!isValid) {
+        return res
+          .status(HTTPStatus.BAD_REQUEST)
+          .json({ success: false, errors, message: "validation error" });
+      }
+      const mailOptions = {
+        from: constants.EMAIL,
+        to: `${req.body.email}`,
+        subject: "AirQo Platform JOIN request",
+        text: msgs.joinRequest,
+      };
+
+      let responseFromUserCreation = await requestUtil.create(
+        tenant.toLowerCase(),
+        firstName,
+        lastName,
+        email,
+        organization,
+        jobTitle,
+        website,
+        description,
+        category,
+        mailOptions
+      );
+
+      if (responseFromUserCreation.success == true) {
+        return res.status(HTTPStatus.OK).json({
+          success: true,
+          message: responseFromUserCreation.message,
+          candidate: responseFromUserCreation.data,
+        });
+      } else if (responseFromUserCreation.success == false) {
+        if (responseFromUserCreation.error) {
+          return res.status(HTTPStatus.BAD_GATEWAY).json({
+            success: false,
+            message: responseFromUserCreation.message,
+            error: responseFromUserCreation.error,
+          });
+        } else {
+          return res.status(HTTPStatus.BAD_REQUEST).json({
+            success: false,
+            message: responseFromUserCreation.message,
+          });
+        }
+      }
+    } catch (error) {
+      return res.status(HTTPStatus.BAD_GATEWAY).json({
+        success: false,
+        message: "controller server error",
+        error: error.message,
       });
-    } catch (e) {
-      return res
-        .status(HTTPStatus.BAD_REQUEST)
-        .json({ success: false, message: "Some Error" });
     }
   },
-  confirmCandidate: (req, res) => {
+
+  list: async (req, res) => {
+    try {
+      const { tenant } = req.query;
+      if (isEmpty(tenant)) {
+        missingQueryParams(req, res);
+      }
+      const limit = parseInt(req.query.limit, 0);
+      const skip = parseInt(req.query.skip, 0);
+      let responseFromFilter = generateFilter.candidates(req);
+      logObject("responseFromFilter", responseFromFilter);
+      if (responseFromFilter.success == true) {
+        let filter = responseFromFilter.data;
+        const responseFromListCandidate = await requestUtil.list({
+          tenant,
+          filter,
+          limit,
+          skip,
+        });
+        logObject("responseFromListCandidate", responseFromListCandidate);
+        if (responseFromListCandidate.success == true) {
+          return res.status(HTTPStatus.OK).json({
+            success: true,
+            message: responseFromListCandidate.message,
+            candidates: responseFromListCandidate.data,
+          });
+        } else if (responseFromListCandidate.success == false) {
+          if (responseFromListCandidate.error) {
+            return res.status(HTTPStatus.BAD_GATEWAY).json({
+              success: false,
+              message: responseFromListCandidate.message,
+              error: responseFromListCandidate.error,
+            });
+          } else {
+            return res.status(HTTPStatus.BAD_REQUEST).json({
+              success: false,
+              message: responseFromListCandidate.message,
+            });
+          }
+        }
+      } else if (responseFromFilter.success == false) {
+        if (responseFromFilter.error) {
+          if (responseFromFilter.error) {
+            return res.status(HTTPStatus.BAD_GATEWAY).json({
+              success: false,
+              message: responseFromFilter.message,
+              error: responseFromFilter.error,
+            });
+          } else {
+            return res.status(HTTPStatus.BAD_REQUEST).json({
+              success: false,
+              message: responseFromFilter.message,
+            });
+          }
+        }
+      }
+    } catch (e) {
+      return res.status(HTTPStatus.BAD_GATEWAY).json({
+        success: false,
+        message: "controller server error",
+        error: e.message,
+      });
+    }
+  },
+  confirm: async (req, res) => {
     console.log("inside the confirm candidate");
     try {
-      const { errors, isValid } = validateRegisterInput(req.body);
+      const {
+        firstName,
+        lastName,
+        email,
+        organization,
+        jobTitle,
+        website,
+        category,
+      } = req.body;
+      const { errors, isValid } = validations.candidate(req.body);
       if (!isValid) {
         return res
           .status(HTTPStatus.BAD_REQUEST)
@@ -74,44 +178,187 @@ const candidate = {
       if (!tenant) {
         missingQueryParams(req, res);
       }
-      const { firstName, lastName, email } = req.body;
+      let responseFromFilter = generateFilter.candidates(req);
+      logObject("responseFromFilter", responseFromFilter);
 
-      let password = generator.generate({
-        length: 10,
-        numbers: true,
-        uppercase: true,
-        lowercase: true,
-      });
+      if (responseFromFilter.success == true) {
+        let filter = responseFromFilter.data;
+        logObject("the filter in controller", filter);
+        let responseFromConfirmCandidate = await requestUtil.confirm(
+          tenant,
+          firstName,
+          lastName,
+          email,
+          organization,
+          jobTitle,
+          website,
+          category,
+          filter
+        );
 
-      let mailOptions = {};
-      if (tenant.toLowerCase() == "kcca") {
-        mailOptions = {
-          from: constants.EMAIL,
-          to: `${req.body.email}`,
-          subject: "Welcome to the AirQo KCCA Platform",
-          text: `${msgs.welcome_kcca(firstName, lastName, password, email)}`,
-        };
-      } else {
-        mailOptions = {
-          from: constants.EMAIL,
-          to: `${req.body.email}`,
-          subject: "Welcome to the AirQo Platform",
-          text: `${msgs.welcome_general(firstName, lastName, password, email)}`,
-        };
+        logObject("responseFromConfirmCandidate", responseFromConfirmCandidate);
+        if (responseFromConfirmCandidate.success == true) {
+          return res.status(HTTPStatus.OK).json({
+            success: true,
+            message: responseFromConfirmCandidate.message,
+            user: responseFromConfirmCandidate.data,
+          });
+        } else if (responseFromConfirmCandidate.success == false) {
+          if (responseFromConfirmCandidate.error) {
+            res.status(HTTPStatus.BAD_GATEWAY).json({
+              success: false,
+              message: responseFromConfirmCandidate.message,
+              error: responseFromConfirmCandidate.error,
+            });
+          } else {
+            res.status(HTTPStatus.BAD_REQUEST).json({
+              success: false,
+              message: responseFromConfirmCandidate.message,
+            });
+          }
+        }
+      } else if (responseFromFilter.success == false) {
+        if (responseFromFilter.error) {
+          if (responseFromFilter.error) {
+            return res.status(HTTPStatus.BAD_GATEWAY).json({
+              success: false,
+              message: responseFromFilter.message,
+              error: responseFromFilter.error,
+            });
+          } else {
+            return res.status(HTTPStatus.BAD_REQUEST).json({
+              success: false,
+              message: responseFromFilter.message,
+            });
+          }
+        }
       }
-      register(
-        req,
-        res,
-        mailOptions,
-        req.body,
-        UserModel(tenant.toLowerCase()),
-        tenant
-      );
     } catch (e) {
       return res.status(HTTPStatus.BAD_GATEWAY).json({
         success: false,
-        message: "unable to register user",
+        message: "contoller server error",
         error: e.message,
+      });
+    }
+  },
+  delete: async (req, res) => {
+    try {
+      const { tenant } = req.query;
+      if (!tenant) {
+        missingQueryParams(req, res);
+      }
+      const responseFromFilter = generateFilter.candidates(req);
+
+      if (responseFromFilter.success == true) {
+        let responseFromDeleteCandidate = await requestUtil.delete(
+          tenant,
+          responseFromFilter.data
+        );
+
+        if (responseFromDeleteCandidate.success == true) {
+          res.status(HTTPStatus.OK).json({
+            success: true,
+            message: responseFromDeleteCandidate.message,
+            candidate: responseFromDeleteCandidate.data,
+          });
+        } else if (responseFromDeleteCandidate.success == false) {
+          if (responseFromDeleteCandidate.error) {
+            res.status(HTTPStatus.BAD_GATEWAY).json({
+              success: false,
+              message: responseFromDeleteCandidate.message,
+              candidate: responseFromDeleteCandidate.data,
+              error: responseFromDeleteCandidate.error,
+            });
+          } else {
+            res.status(HTTPStatus.BAD_REQUEST).json({
+              success: false,
+              message: responseFromDeleteCandidate.message,
+              candidate: responseFromDeleteCandidate.data,
+            });
+          }
+        }
+      } else if (responseFromFilter.success == false) {
+        if (responseFromFilter.error) {
+          return res.status(HTTPStatus.BAD_GATEWAY).json({
+            success: false,
+            message: responseFromFilter.message,
+            error: responseFromFilter.error,
+          });
+        } else {
+          return res.status(HTTPStatus.BAD_REQUEST).json({
+            success: false,
+            message: responseFromFilter.message,
+          });
+        }
+      }
+    } catch (error) {
+      return res.status(HTTPStatus.BAD_GATEWAY).json({
+        success: false,
+        message: "controller server error",
+        error: error.message,
+      });
+    }
+  },
+  update: async (req, res) => {
+    try {
+      const { tenant } = req.query;
+      if (!tenant) {
+        missingQueryParams(req, res);
+      }
+      const responseFromFilter = generateFilter.candidates(req);
+      logObject("responseFromFilter", responseFromFilter);
+
+      if (responseFromFilter.success == true) {
+        let filter = responseFromFilter.data;
+        let requestBody = req.body;
+        delete requestBody._id;
+        let responseFromUpdateCandidate = await requestUtil.update(
+          tenant,
+          filter,
+          requestBody
+        );
+        logObject("responseFromUpdateCandidate", responseFromUpdateCandidate);
+        if (responseFromUpdateCandidate.success == true) {
+          res.status(HTTPStatus.OK).json({
+            success: true,
+            message: responseFromUpdateCandidate.message,
+            candidate: responseFromUpdateCandidate.data,
+          });
+        } else if (responseFromUpdateCandidate.success == false) {
+          if (responseFromUpdateCandidate.error) {
+            res.status(HTTPStatus.BAD_GATEWAY).json({
+              success: false,
+              message: responseFromUpdateCandidate.message,
+              candidate: responseFromUpdateCandidate.data,
+              error: responseFromUpdateCandidate.error,
+            });
+          } else {
+            res.status(HTTPStatus.BAD_REQUEST).json({
+              success: false,
+              message: responseFromUpdateCandidate.message,
+              candidate: responseFromUpdateCandidate.data,
+            });
+          }
+        }
+      } else if (responseFromFilter.success == false) {
+        if (responseFromFilter.error) {
+          return res.status(HTTPStatus.BAD_GATEWAY).json({
+            success: false,
+            message: responseFromFilter.message,
+            error: responseFromFilter.error,
+          });
+        } else {
+          return res.status(HTTPStatus.BAD_REQUEST).json({
+            success: false,
+            message: responseFromFilter.message,
+          });
+        }
+      }
+    } catch (error) {
+      return res.status(HTTPStatus.BAD_GATEWAY).json({
+        success: false,
+        message: "controller server error",
+        error: error.message,
       });
     }
   },
