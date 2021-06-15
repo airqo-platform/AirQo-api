5056521960adc470c0ca8f4fbe0fa0da0ddcb6a2
Martin <bbaalmart@gmail.com>
2021-06-15 10:00:20 +0300

    a new filter for DB queries to be used in the controller logic

--- src/auth-service/utils/generate-filter.js
+++ src/auth-service/utils/generate-filter.js
@@ -1,22 +1,124 @@
-const { logElement } = require("./log");
+const { logElement, logObject } = require("./log");
 
 const filter = {
-  users: () => {
+  users: (req) => {
     try {
+      let { privilege, id, username, active } = req.query;
+      let { email, resetPasswordToken } = req.body;
+      let filter = {};
+      if (email) {
+        filter["email"] = email;
+      }
+      if (resetPasswordToken) {
+        filter["resetPasswordToken"] = resetPasswordToken;
+        filter["resetPasswordExpires"] = {
+          $gt: Date.now(),
+        };
+      }
+      if (privilege) {
+        filter["privilege"] = privilege;
+      }
+      if (id) {
+        filter["_id"] = id;
+      }
+      if (active) {
+        if (active == "yes") {
+          filter["isActive"] = true;
+        } else if (active == "no") {
+          filter["isActive"] = false;
+        }
+      }
+      if (username) {
+        filter["userName"] = username;
+      }
+      return {
+        success: true,
+        message: "successfully created the filter",
+        data: filter,
+      };
     } catch (e) {
-      logElement("server error", e.message);
+      return {
+        success: false,
+        message: "filter util server error",
+        error: e.message,
+      };
     }
   },
-  candidates: () => {
+  candidates: (req) => {
     try {
+      let { category, id } = req.query;
+      let { email } = req.body;
+      let filter = {};
+      if (email) {
+        filter["email"] = email;
+      }
+      if (category) {
+        filter["category"] = category;
+      }
+      if (id) {
+        filter["_id"] = id;
+      }
+      return {
+        success: true,
+        message: "successfully created the filter",
+        data: filter,
+      };
     } catch (e) {
-      logElement("server error", e.message);
+      return {
+        success: false,
+        message: "filter util server error",
+        error: e.message,
+      };
     }
   },
-  defaults: () => {
+  defaults: (req) => {
     try {
+      let { chart, user, id } = req.query;
+      let filter = {};
+      if (chart) {
+        filter["chartTitle"] = chart;
+      }
+      if (user) {
+        filter["user"] = user;
+      }
+      if (id) {
+        filter["_id"] = id;
+      }
+      return {
+        success: true,
+        message: "successfully created the filter",
+        data: filter,
+      };
     } catch (e) {
-      logElement("server error", e.message);
+      return {
+        success: false,
+        message: "filter util server error",
+        error: e.message,
+      };
+    }
+  },
+
+  defaults: (req) => {
+    try {
+      let { user, chartTitle } = req.query;
+      let filter = {};
+      if (user) {
+        filter["user"] = user;
+      }
+      if (chartTitle) {
+        filter["chartTitle"] = chartTitle;
+      }
+      return {
+        success: true,
+        message: "successfully created the filter",
+        data: filter,
+      };
+    } catch (e) {
+      return {
+        success: false,
+        message: "filter util server error",
+        error: e.message,
+      };
     }
   },
 };
