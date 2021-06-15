e0748fa5140373703d257b389f483f0ec73ddb39
Martin <bbaalmart@gmail.com>
2021-06-15 09:52:34 +0300

    db configuration to resolve connection timeouts and sudden server stops

--- src/auth-service/config/dbConnection.js
+++ src/auth-service/config/dbConnection.js
@@ -1,5 +1,11 @@
 const mongoose = require("mongoose");
+mongoose.set("useNewUrlParser", true);
 mongoose.set("useFindAndModify", false);
+mongoose.set("useCreateIndex", true);
+mongoose.set("debug", false);
+mongoose.set("serverSelectionTimeoutMS", 3600000);
+mongoose.set("connectTimeoutMS", 1200000);
+mongoose.set("socketTimeoutMS", 600000);
 const constants = require("./constants");
 const { logElement, logText, logObject } = require("../utils/log");
 const URI = constants.MONGO_URI;
@@ -13,33 +19,48 @@ const options = {
   autoIndex: true,
   poolSize: 10,
   bufferMaxEntries: 0,
-  connectTimeoutMS: 10000,
-  socketTimeoutMS: 30000,
+  connectTimeoutMS: 1200000,
+  socketTimeoutMS: 600000,
+  serverSelectionTimeoutMS: 3600000,
   dbName: constants.DB_NAME,
 };
 
 const connect = () => mongoose.createConnection(URI, options);
 
 const connectToMongoDB = () => {
-  const db = connect();
-  db.on("open", () => {
-    logText(`mongoose connection opened on: ${URI}`);
-  });
+  try {
+    const db = connect();
+    db.on("open", () => {
+      logText(`mongoose connection opened on: ${URI}`);
+    });
 
-  db.on("error", (err) => {
-    logElement("Mongoose connection error" + err);
-    process.exit(0);
-  });
+    db.on("error", (err) => {
+      logElement("Mongoose connection error", err);
+      // process.exit(0);
+    });
 
-  process.on("unlimitedRejection", (reason, p) => {
-    console.log("Unhandled Rejection at: Promise", p, "reason:", reason);
-    process.exit(0);
-    // db.close(() => {
-    //   logText("mongoose is disconnected through the app");
-    //   process.exit(0);
-    // });
-  });
-  return db;
+    db.on("disconnection", (err) => {
+      logElement("mongoose disconnected", err);
+    });
+
+    process.on("unlimitedRejection", (reason, p) => {
+      console.log("Unhandled Rejection at: Promise", p, "reason:", reason);
+      // process.exit(0);
+      // db.close(() => {
+      //   logText("mongoose is disconnected through the app");
+      //   process.exit(0);
+      // });
+    });
+
+    process.on("uncaughtException", (err) => {
+      console.error("There was an uncaught error", err);
+      // process.exit(1);
+    });
+
+    return db;
+  } catch (error) {
+    logElement("dbConnnection server error", error.message);
+  }
 };
 
 const mongodb = connectToMongoDB();
