48e81dfca8f33525b5c5af7c0b13c169fb9b3ca2
Martin <bbaalmart@gmail.com>
2021-06-25 11:34:05 +0300

    just styling changes

--- src/exceedances/job/views/exceedance.py
+++ src/exceedances/job/views/exceedance.py
@@ -29,12 +29,31 @@ db = client['airqo_analytics']
 """
 we need to get these codes from the DB and not have them static here
 """
-code_locations_dict = {'Luzira': 'A9WLJS5F', 'Kawala': 'AR2RHV97', 'Nakulabye': 'AMBD741S', 'Namirembe': 'ARBL6HVZ',
-                       'Kiswa': 'APYZC5J7', 'Civic Centre': 'A95X5F9T', 'Ggaba': 'AKSLS0FP', 'Kazo Angola': 'AW66FF7V', 'Kisugu': 'APZ80RTM',
-                       'Kisenyi II': 'AY2J2Q7Z', 'Kyebando': 'ALS2LCWY',  'Mutundwe': 'AZB4PFV4', 'Bwaise II': 'ANQ16PZJ',
-                       'Naguru I': 'AGPZ1C42', 'Nakawa': 'ADPYGHD0', 'Kamwokya II': 'AJCK5L86', 'Wandegeya': 'A743BPWK', 'Nsambya Central': 'AX9RGBN0',
-                       'Komamboga': 'AB6051M4', 'Kisenyi II': 'AT6NJNZR', 'Nakasero II': 'ARSWTW30',  'Kibuye I': 'A0WN66FH',
-                       'Kyanja': 'ACMVQ44H'}
+code_locations_dict = {
+    'Luzira': 'A9WLJS5F',
+    'Kawala': 'AR2RHV97',
+    'Nakulabye': 'AMBD741S',
+    'Namirembe': 'ARBL6HVZ',
+    'Kiswa': 'APYZC5J7',
+    'Civic Centre': 'A95X5F9T',
+    'Ggaba': 'AKSLS0FP',
+    'Kazo Angola': 'AW66FF7V',
+    'Kisugu': 'APZ80RTM',
+    'Kisenyi II': 'AY2J2Q7Z',
+    'Kyebando': 'ALS2LCWY',
+    'Mutundwe': 'AZB4PFV4',
+    'Bwaise II': 'ANQ16PZJ',
+    'Naguru I': 'AGPZ1C42',
+    'Nakawa': 'ADPYGHD0',
+    'Kamwokya II': 'AJCK5L86',
+    'Wandegeya': 'A743BPWK',
+    'Nsambya Central': 'AX9RGBN0',
+    'Komamboga': 'AB6051M4',
+    'Kisenyi II': 'AT6NJNZR',
+    'Nakasero II': 'ARSWTW30',
+    'Kibuye I': 'A0WN66FH',
+    'Kyanja': 'ACMVQ44H'
+}
 
 
 def resample_timeseries_data(data, frequency, datetime_field, decimal_places):
