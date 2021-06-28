73ab376ae9459e2c774de0e02b51898cccbbcf97
ja-odur <odurjoseph8@gmail.com>
2021-06-29 02:05:41 +0300

    add sites model

--- src/analytics/api/models/__init__.py
+++ src/analytics/api/models/__init__.py
@@ -4,3 +4,4 @@ from .device_measurements import DeviceHourlyMeasurementModel, DeviceDailyHistor
 from .device_exceedances import DeviceDailyExceedancesModel
 from .collection import Collection
 from .report_template import ReportTemplateModel
+from .site import SiteModel
