077e65fb47abad81d4e21c059f2704f146049bb3
ja-odur <odurjoseph8@gmail.com>
2021-06-30 21:09:16 +0300

    compute and store exceedance

--- src/exceedances/utils/exceedances.py
+++ src/exceedances/utils/exceedances.py
@@ -1,20 +1,8 @@
-from datetime import datetime, timedelta
-import pandas as pd
-import requests
-import requests
-import urllib3
-from dataclasses import dataclass
-from config import configuration
-import numpy as np
-from models import Event, Site, Exceedance
-
-# disable tls/ssl warnings
-urllib3.disable_warnings()
-
-
-@dataclass
-class Exceedances:
-    standard: str
+from datetime import datetime
+from collections import defaultdict
+from dateutil.tz import UTC
+from dateutil.relativedelta import relativedelta
+from models import Event, Exceedance
 
 
 class CalculateExceedances:
@@ -30,7 +18,7 @@ class CalculateExceedances:
             'Unhealthy': [55.4, 150.4],
             'VeryUnhealthy': [150.4, 250.4],
             'Hazardous': [250.4, 500.4],
-            'All': [0, 2000],
+            # 'All': [0, 2000],
         }
         self.PM_10_AQI_LIMIT = {
             'Good': [0, 54],
@@ -39,7 +27,7 @@ class CalculateExceedances:
             'Unhealthy': [254, 354],
             'VeryUnhealthy': [354, 424],
             'Hazardous': [424, 604],
-            'All': [0, 2000],
+            # 'All': [0, 2000],
         }
         self.NO2_AQI_LIMIT = {
             'Good': [0, 53],
@@ -48,8 +36,51 @@ class CalculateExceedances:
             'Unhealthy': [360, 649],
             'VeryUnhealthy': [649, 1249],
             'Hazardous': [1249, 2049],
-            'All': [0, 2049],
+            # 'All': [0, 2049],
         }
+
+    def calculate_WHO_exceedance(self, events):
+        exceednace_pm2_5, exceedance_pm10, exceedance_no2 = 0, 0, 0
+
+        for event in events:
+            exceednace_pm2_5 += event.get("pm2_5") > self.WHO_LIMIT.get("pm2_5") and 1 or 0
+            exceedance_pm10 += event.get("pm10") > self.WHO_LIMIT.get("pm10") and 1 or 0
+            exceedance_no2 += event.get("no2") > self.WHO_LIMIT.get("no2") and 1 or 0
+
+        return {
+            "pm2_5": exceednace_pm2_5,
+            "pm10": exceedance_pm10,
+            "no2": exceedance_no2,
+            "total": len(events)
+        }
+
+    def calculate_aqi_exceedance(self, events):
+        # TODO include defaults of zero
+        pm2_5, pm10, no2 = {}, {}, {}
+
+        for event in events:
+            for key, [min_value, max_value] in self.PM_25_AQI_LIMIT:
+                if min_value < event.get("pm2_5") <= max_value:
+                    pm2_5[key] = pm2_5.get(key, 0) + 1
+                    break
+
+            for key, [min_value, max_value] in self.PM_10_AQI_LIMIT:
+                if min_value < event.get("pm10") <= max_value:
+                    pm10[key] = pm10.get(key, 0) + 1
+                    break
+
+            for key, [min_value, max_value] in self.NO2_AQI_LIMIT:
+                if min_value < event.get("no2") <= max_value:
+                    no2[key] = no2.get(key, 0) + 1
+                    break
+
+            return {
+                "pm2_5": pm2_5,
+                "pm10": pm10,
+                "no2": no2,
+                "total": len(events)
+            }
+
 
     def calculate_exceedances(self):
         """
@@ -57,205 +88,40 @@ class CalculateExceedances:
         Using a loop, calculate each Site's exceedance accordingly
         return the exceedance list for all sites accordingly
         """
-        self.get_events()
-        aqi = self.get_AQ1()
-        who = self.get_WHO()
+        created_at = datetime.utcnow()
+        created_at = created_at.replace(tzinfo=UTC)
+        grouped_events = self.events.values()
+        exceedances = []
 
-        # aqi= {
-        #          "pm2_5":{pm2_5},
-        #          "pm10":{p}m10},
-        #          "no2": {no2}
-        #         }
+        for site_events in grouped_events:
+            WHO = self.calculate_WHO_exceedance(site_events)
+            aqi = self.calculate_aqi_exceedance(site_events)
 
-        return [
-            {
-                "site_id": "",
-                "time": "",
-                "who": {
-                    "pm2_5": "",
-                    "pm10": "",
-                    "no2": ""
-                },
-                "aqi": {
-                    "pm2_5": {
-                        "unhealthy": "",
-                        "v_healthy": "",
-                        "hazardous": "",
-                        "UHFSG": ""
-                    },
-                    "pm10": {
-                        "unhealthy": "",
-                        "v_healthy": "",
-                        "hazardous": "",
-                        "UHFSG": ""
-                    },
-                    "no2": {
-                        "unhealthy": "",
-                        "v_healthy": "",
-                        "hazardous": "",
-                        "UHFSG": ""
-                    }
-                },
-            },
-            {},
-            {}]
+            exceedances.append({
+                "site_id": site_events[0]["site_id"],
+                "time": created_at.isoformat(),
+                "who": WHO,
+                "aqi": aqi,
+            })
+
+        return exceedances
 
     def save_exceedances(self, exceedances):
-        """
-        Get the exceedance array/list that needs to be stored
-        And then save it into the database accordingly
-        """
-
-    def calculate_AQ1_pm2_5(self, element):
-        """
-        UH4SG_pm2_5_count = 0
-        Unhealthy_pm2_5_count = 0
-        VeryUnhealthy_pm2_5_count = 0
-        Hazardous_pm2_5_count = 0,
-
-        UH4SG_pm10_count = 0
-        Unhealthy_pm10_count = 0
-        VeryUnhealthy_pm10_count = 0
-        Hazardous_pm10_count = 0,
-
-        UH4SG_no2_count = 0
-        Unhealthy_no2_count = 0
-        VeryUnhealthy_no2_count = 0
-        Hazardous_no2_count = 0,
-
-            if (self.PM_25_AQI_LIMIT.UHFSG.includes(element.value.pm2_5)){
-                UH4SG_pm2_5_count +=1
-            }
-
-            if (self.PM_25_AQI_LIMIT.Unhealthy.includes(element.value.pm2_5)){
-                 Unhealthy_pm2_5_count +=1
-            }
-
-             if (self.PM_25_AQI_LIMIT.Unhealthy.includes(element.value.pm2_5){
-                 Unhealthy_pm2_5_count +=1
-            }
-
-            if (self.PM_25_AQI_LIMIT.VeryUnhealthy.includes(element.value.pm2_5)){
-                VeryUnhealthy_pm2_5_count +=1
-            }
-
-            if (self.PM_25_AQI_LIMIT.Hazardous.includes(element.value.pm2_5)){
-                Hazardous_pm2_5_count +=1
-            }
-
-            return {
-                        UH4SG: UH4SG_pm2_5_count
-                        Unhealthy: Unhealthy_pm2_5_count
-                        VeryUnhealthy: VeryUnhealthy_pm2_5_count
-                        Hazardous: Hazardous_pm2_5_count,
-                   }
-        """
-
-    def calculate_AQI_pm10(self, measurements):
-        """
-        if (self.PM10_AQI_LIMIT.UHFSG.includes(element.value.pm10)){
-         UH4SG_pm10_count += 1
-        }
-
-        if (self.PM10_AQI_LIMIT.Unhealthy.includes(element.value.pm10)){
-             Unhealthy_pm10_count += 1
-        }
-
-         if (self.PM10_AQI_LIMIT.Unhealthy.includes(element.value.pm10){
-             Unhealthy_pm10_count += 1
-        }
-
-        if (self.PM10_AQI_LIMIT.VeryUnhealthy.includes(element.value.pm10)){
-            VeryUnhealthy_pm10_count += 1
-        }
-
-        if (self.PM10_AQI_LIMIT.Hazardous.includes(element.value.pm10)){
-            Hazardous_pm10_count += 1
-        }
-        return  {
-                        UH4SG: UH4SG_pm10_count = 0
-                        Unhealthy: Unhealthy_pm10_count = 0
-                        VeryUnhealthy: VeryUnhealthy_pm10_count = 0
-                        Hazardous: Hazardous_pm10_count = 0,
-                    }
-        """
-
-    def calculate_AQ1_no2(self, measurements):
-        """
-         if (self.NO2_AQI_LIMIT.UHFSG.includes(element.value.no2)){
-                UH4SG_no2_count +=1
-            }
-
-            if ((self.NO2_AQI_LIMIT.Unhealthy.includes(element.value.no2)){
-                 Unhealthy_no2_count +=1
-            }
-
-             if (self.NO2_AQI_LIMIT.Unhealthy.includes(element.value.no2){
-                 Unhealthy_no2_count +=1
-            }
-
-            if (self.NO2_AQI_LIMIT.VeryUnhealthy.includes(element.value.no2){
-                VeryUnhealthy_no2_count +=1
-            }
-
-            if (self.NO2_AQI_LIMIT.Hazardous.includes(element.value.no2)){
-                Hazardous_no2_count +=1
-            }
-
-            return {
-                         UH4SG: UH4SG_no2_count = 0
-                         Unhealthy: Unhealthy_no2_count = 0
-                         VeryUnhealthy: VeryUnhealthy_no2_count = 0
-                         Hazardous: Hazardous_no2_count = 0,
-                    }
-        """
-
-    def calculate_AQ1(self, measurements, pollutant):
-        """
-        return the exceedance for all pollutants using the
-        AQI metric
-
-        site_measurements = measurements
-
-        get the AQI counts for for the provided sites
-
-        if (pollutant == 2_5){
-        pm2_5= calculate_AQ1_pm2_5(measurements)
-        }
-
-        if (pollutant == 10){
-         pm10 = calculate_AQI_pm10(measurements)
-        }
-
-        if (pollutant == no2){
-         no2= calculate_AQI_no2(measurements)
-        }
-
-
-         return {
-                 "pm2_5":{pm2_5},
-                 "pm10":{p}m10},
-                 "no2": {no2}
-                }
-
-"""
-
-    def calculate_WHO(self):
-        """
-
-
-
-
-        """
+        created_at = datetime.utcnow()
+        created_at = created_at.replace(tzinfo=UTC)
+        exceedance_model = Exceedance(self.tenant)
+        record = {"day": created_at.isoformat(), "exceedances": exceedances}
+        return exceedance_model.save_exceedance(record)
 
     def get_events(self):
-        api_url = f'{configuration.DAILY_EVENTS_URL}?tenant={self.tenant}'
-        events = requests.get(api_url, verify=False)
-        if events.status_code != 200:
-            return {}
+        end_date = datetime.utcnow()
+        start_date = end_date - relativedelta(days=1)
 
-        return events.json()
+        event_model = Event(self.tenant)
+        modified_events = defaultdict(list)
+        events = event_model.get_events(start_date, end_date)
 
-    def get_sites(self):
-        site_model = Site(self.tenant)
-        sites = site_model.get_sites()
+        for event in events:
+            modified_events[event["site_id"]].append(events)
+
+        return modified_events
