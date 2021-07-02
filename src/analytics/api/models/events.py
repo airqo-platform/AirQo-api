494cb4d11cc49230929930f48e0cc37cac9e7d5e
ja-odur <odurjoseph8@gmail.com>
2021-07-02 05:42:29 +0300

    calculate frequency data on the fly

--- src/analytics/api/models/events.py
+++ src/analytics/api/models/events.py
@@ -23,33 +23,45 @@ class EventsModel(BasePyMongoModel):
         )
 
     def get_events(self, sites, start_date, end_date, frequency):
+        time_format_mapper = {
+            'raw': '%Y-%m-%dT%H:%M:%S%z',
+            'hourly': '%Y-%m-%d %H:00',
+            'daily': '%Y-%m-%d',
+            'monthly': '%Y-%m-01'
+        }
+
         return (
             self
                 .date_range("values.time", start_date=start_date, end_date=end_date)
-                .filter_by(**{"values.frequency": frequency})
+                .filter_by(**{"values.frequency": "raw"})
                 .unwind("values")
                 .replace_root("values")
+                .lookup("sites", local_field="site_id", foreign_field="_id", col_as="sites")
                 .project(
                     _id=0,
-                    time={"$toString": "$time"},
+                    time={
+                        "$dateToString": {
+                            'format': time_format_mapper.get(frequency) or time_format_mapper.get('hourly'),
+                            'date': '$time',
+                            'timezone': 'Africa/Kampala'
+                        }
+                    },
                     **{"pm2_5.value": 1},
                     **{"pm10.value": 1},
                     **{"no2.value": 1},
                     frequency=1,
-                    site_id={"$toString": "$site_id"}
+                    site_id={"$toString": "$site_id"},
+                    sites={"name": 1, "description": 1, "generated_name": 1}
                 )
                 .match_in(site_id=sites)
                 .group(
-                    _id="$site_id",
+                    _id={"site_id": "$site_id", "time": "$time"},
                     frequency={'$first': "$frequency"},
-                    site_reading={
-                        '$push': {
-                            'time': '$time',
-                            'pm2_5': '$pm2_5.value',
-                            'pm10': '$pm10.value',
-                            'no2': '$no2.value'
-                        }
-                    }
+                    pm2_5={"$avg": "$pm2_5.value"},
+                    pm10={"$avg": "$pm10.value"},
+                    no2={"$avg": "$no2.value"},
+                    sites={'$first': "$sites"}
                 )
+                .sort(**{"_id.time": 1})
                 .exec()
         )
