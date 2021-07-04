795dbe2633dbb10b0fb5f3ab195d9a1e70e6928d
noah Nsimbe <nsimbenoah@gmail.com>
2021-07-05 02:23:40 +0300

    schema registry

--- src/calibrate/controllers/kafka.py
+++ src/calibrate/controllers/kafka.py
@@ -2,10 +2,11 @@ import os
 
 from confluent_kafka.avro import AvroConsumer, AvroProducer
 from confluent_kafka.avro.serializer import SerializerError
+from models import regression as rg
 
 # Ref: https://github.com/confluentinc/confluent-kafka-python
 
-BOOTSTRAP_SERVERS = os.getenv("BOOTSTRAP_SERVERS", "127.0.0.1:9092")
+BOOTSTRAP_SERVERS = os.getenv("BOOTSTRAP_SERVERS", "34.123.249.54:31000")
 SCHEMA_REGISTRY_URL = os.getenv("SCHEMA_REGISTRY_URL", "http://127.0.0.1:8081")
 INPUT_TOPIC = os.getenv("INPUT_TOPIC")
 OUTPUT_TOPIC = os.getenv("OUTPUT_TOPIC")
@@ -40,12 +41,15 @@ def consume_measurements():
 
     consumer.subscribe([INPUT_TOPIC])
 
+    rgModel = rg.Regression()
+    hourly_combined_dataset = rgModel.hourly_combined_dataset
+
     while True:
         try:
             msg = consumer.poll(10)
 
         except SerializerError as e:
-            print("Message deserialization failed for {}: {}".format(msg, e))
+            print("Message deserialization failed: {}".format(e))
             break
 
         if msg is None:
@@ -55,8 +59,40 @@ def consume_measurements():
             print("AvroConsumer error: {}".format(msg.error()))
             continue
 
+        calibrated_measurements = []
+
         print(msg.value())
-        produce_measurements(msg.value(), "tenant")
+
+        try:
+
+            measurements = list(dict(msg.value()))
+            for measure in measurements:
+
+                try:
+                    measurement = dict(measure)
+
+                    pm25 = measurement.get('pm_2_5').get('value', None)
+                    pm10 = measurement.get('pm10').get('value', None)
+                    temperature = measurement.get('internalTemperature').get('value', None)
+                    humidity = measurement.get('internalHumidity').get('value', None)
+                    datetime = measurement.get('time', None)
+
+                    if pm25 and pm10 and temperature and humidity and datetime:
+                        calibrated_value = rgModel.random_forest(datetime, pm25, pm10, temperature,
+                                                                 humidity, hourly_combined_dataset)
+                        measurement["pm_2_5"]["calibratedValue"] = calibrated_value
+
+                    calibrated_measurements.append(measurement)
+
+                except Exception as ex:
+                    print(ex)
+                    continue
+
+            if calibrated_measurements:
+                produce_measurements(calibrated_measurements, "tenant")
+
+        except Exception as e:
+            print(e)
 
     consumer.close()
 
