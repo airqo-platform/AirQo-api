770d226b879231f19250642e8fa5cb2b459573e8
noah Nsimbe <nsimbenoah@gmail.com>
2021-07-05 00:33:16 +0300

    confluent kafka & avro

--- src/calibrate/Kafka.Dockerfile
+++ src/calibrate/Kafka.Dockerfile
@@ -3,6 +3,9 @@ FROM python:3.7-slim
 
 ARG BOOTSTRAP_SERVERS=127.0.0.1:9092
 ARG SCHEMA_REGISTRY=http://127.0.0.1:8081
+ARG INPUT_TOPIC=non-calibrated-measurements
+ARG OUTPUT_TOPIC=calibrated-measurements
+ARG CONSUMER_GROUP=calibrate-measurements-group
 
 # Copy the source code to app folder
 COPY . /app/
@@ -14,7 +17,10 @@ WORKDIR /app/
 RUN --mount=type=cache,target=/root/.cache/pip pip install -r requirements.txt
 
 ENV SCHEMA_REGISTRY_URL=$SCHEMA_REGISTRY \
-    BOOTSTRAP_SERVERS=$BOOTSTRAP_SERVERS
+    BOOTSTRAP_SERVERS=$BOOTSTRAP_SERVERS \
+    INPUT_TOPIC=$INPUT_TOPIC \
+    OUTPUT_TOPIC=$OUTPUT_TOPIC \
+    CONSUMER_GROUP=$CONSUMER_GROUP
 
 # Run the application
 CMD ["python", "controllers/kafka.py"]
--- src/calibrate/controllers/kafka.py
+++ src/calibrate/controllers/kafka.py
@@ -1,3 +1,65 @@
+import os
+
+from confluent_kafka.avro import AvroConsumer, AvroProducer
+from confluent_kafka.avro.serializer import SerializerError
+
+# Ref: https://github.com/confluentinc/confluent-kafka-python
+
+BOOTSTRAP_SERVERS = os.getenv("BOOTSTRAP_SERVERS", "127.0.0.1:9092")
+SCHEMA_REGISTRY_URL = os.getenv("SCHEMA_REGISTRY_URL", "http://127.0.0.1:8081")
+INPUT_TOPIC = os.getenv("INPUT_TOPIC")
+OUTPUT_TOPIC = os.getenv("OUTPUT_TOPIC")
+CONSUMER_GROUP = os.getenv("CONSUMER_GROUP")
+
+
+def delivery_report(err, msg):
+    """ Called once for each message produced to indicate delivery result.
+        Triggered by poll() or flush(). """
+    if err is not None:
+        print('Message delivery failed: {}'.format(err))
+    else:
+        print('Message delivered to {} [{}]'.format(msg.topic(), msg.partition()))
+
+
+def produce_measurements(measurements, key):
+
+    avr_producer = AvroProducer({
+        'bootstrap.servers': BOOTSTRAP_SERVERS,
+        'on_delivery': delivery_report,
+        'schema.registry.url': SCHEMA_REGISTRY_URL
+    })
+    avr_producer.produce(topic=OUTPUT_TOPIC, value=measurements, key=key)
+    avr_producer.flush()
+
+
+def consume_measurements():
+    consumer = AvroConsumer({
+        'bootstrap.servers': BOOTSTRAP_SERVERS,
+        'group.id': CONSUMER_GROUP,
+        'schema.registry.url': SCHEMA_REGISTRY_URL})
+
+    consumer.subscribe([INPUT_TOPIC])
+
+    while True:
+        try:
+            msg = consumer.poll(10)
+
+        except SerializerError as e:
+            print("Message deserialization failed for {}: {}".format(msg, e))
+            break
+
+        if msg is None:
+            continue
+
+        if msg.error():
+            print("AvroConsumer error: {}".format(msg.error()))
+            continue
+
+        print(msg.value())
+        produce_measurements(msg.value(), "tenant")
+
+    consumer.close()
+
 
 if __name__ == "__main__":
-    print("hello world")
+    consume_measurements()
