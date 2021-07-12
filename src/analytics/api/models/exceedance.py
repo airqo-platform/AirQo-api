36220f80780264ff6f724b435d2aa0b22289c1f3
ja-odur <odurjoseph8@gmail.com>
2021-07-12 22:36:59 +0300

    add custom pipeline stage functions

--- src/analytics/api/models/exceedance.py
+++ src/analytics/api/models/exceedance.py
@@ -5,46 +5,56 @@ class ExceedanceModel(BasePyMongoModel):
     def __init__(self, tenant):
         super().__init__(tenant, collection_name="exceedances")
 
+    def group_by_pollutant(self, pollutant, standard):
+        if str(standard).lower() == 'who':
+            return self.group(
+                _id="$site_id",
+                **{f"{pollutant}": {"$avg": f"$who.{pollutant}"}},
+            )
+
+        return self.group(
+            _id="$site_id",
+            Good={"$avg": f"$aqi.{pollutant}.Good"},
+            Moderate={"$avg": f"$aqi.{pollutant}.Moderate"},
+            UHFSG={"$avg": f"$aqi.{pollutant}.UHFSG"},
+            Unhealthy={"$avg": f"$aqi.{pollutant}.Unhealthy"},
+            VeryUnhealthy={"$avg": f"$aqi.{pollutant}.VeryUnhealthy"},
+            Hazardous={"$avg": f"$aqi.{pollutant}.Hazardous"},
+            total={"$avg": "$aqi.total"},
+        )
+
+    def add_field_by_pollutant(self, pollutant, standard):
+        if str(standard).lower() == 'who':
+            return self.add_fields(
+
+                who={
+                    "total": {"$round": "$total"},
+                    f"{pollutant}": {"$round": f"${pollutant}"},
+                },
+            )
+
+        return self.add_fields(
+            aqi={
+                "total": {"$round": "$total"},
+                f"{pollutant}": {
+                    "Good": {"$round": "$Good"},
+                    "Moderate": {"$round": "$Moderate"},
+                    "UHFSG": {"$round": "$UHFSG"},
+                    "Unhealthy": {"$round": "$Unhealthy"},
+                    "VeryUnhealthy": {"$round": "$VeryUnhealthy"},
+                    "Hazardous": {"$round": "$Hazardous"},
+                }
+            },
+        )
+
     def get_exceedances(self, start_date, end_date, pollutant, standard):
         return (
             self
                 .date_range("exceedances.time", start_date=start_date, end_date=end_date)
                 .unwind("exceedances")
                 .replace_root("exceedances")
-                .project(site_id={"$toString": "$site_id"}, who=1, aqi=1)
-                .group(
-                    _id="site_id",
-                    who={
-                        "pm2_5": {"$round": {"$avg": "$who.pm2_5"}},
-                        "pm10": {"$round": {"$avg": "$who.pm10"}},
-                        "no2": {"$round": {"$avg": "$who.no2"}},
-                    },
-                    aqi={
-                        "pm2_5": {
-                            "Good": {"$round": {"$avg": "$aqi.pm2_5.Good"}},
-                            "Moderate": {"$round": {"$avg": "$aqi.pm2_5.Moderate"}},
-                            "UNFSG": {"$round": {"$avg": "$aqi.pm2_5.UNFSG"}},
-                            "Unhealthy": {"$round": {"$avg": "$aqi.pm2_5.Unhealthy"}},
-                            "VeryUnhealthy": {"$round": {"$avg": "$aqi.pm2_5.VeryUnhealthy"}},
-                            "Hazardous": {"$round": {"$avg": "$aqi.pm2_5.Hazardous"}},
-                        },
-                        "pm10": {
-                            "Good": {"$round": {"$avg": "$aqi.pm10.Good"}},
-                            "Moderate": {"$round": {"$avg": "$aqi.pm10.Moderate"}},
-                            "UNFSG": {"$round": {"$avg": "$aqi.pm10.UNFSG"}},
-                            "Unhealthy": {"$round": {"$avg": "$aqi.pm10.Unhealthy"}},
-                            "VeryUnhealthy": {"$round": {"$avg": "$aqi.pm10.VeryUnhealthy"}},
-                            "Hazardous": {"$round": {"$avg": "$aqi.pm10.Hazardous"}},
-                        },
-                        "no2": {
-                            "Good": {"$round": {"$avg": "$aqi.no2.Good"}},
-                            "Moderate": {"$round": {"$avg": "$aqi.no2.Moderate"}},
-                            "UNFSG": {"$round": {"$avg": "$aqi.no2.UNFSG"}},
-                            "Unhealthy": {"$round": {"$avg": "$aqi.no2.Unhealthy"}},
-                            "VeryUnhealthy": {"$round": {"$avg": "$aqi.no2.VeryUnhealthy"}},
-                            "Hazardous": {"$round": {"$avg": "$aqi.no2.Hazardous"}},
-                        },
-                    }
-                )
+                .group_by_pollutant(pollutant, standard)
+                .add_field_by_pollutant(pollutant, standard)
+                .project(site_id={"$toString": "$_id"}, total=1, who=1, aqi=1)
                 .exec()
         )
