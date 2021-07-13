3551574686b3b548661c196a3a3547d25b431793
noah Nsimbe <nsimbenoah@gmail.com>
2021-07-13 11:33:33 +0300

    adding recent parameter

--- src/data-mgt/python/events-monitor/main.py
+++ src/data-mgt/python/events-monitor/main.py
@@ -19,7 +19,7 @@ def notify_slack(data):
 
 def run_checks():
 
-    api_url = f"{DEVICE_REGISTRY_BASE_URL}devices/events?tenant={TENANT}"
+    api_url = f"{DEVICE_REGISTRY_BASE_URL}devices/events?tenant={TENANT}&recent=yes"
 
     results = requests.get(api_url)
 
