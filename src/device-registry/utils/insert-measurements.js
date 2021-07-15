c4e4fab22d94a7d4fd6c3c0b99e10683ccd871ef
Martin <bbaalmart@gmail.com>
2021-07-15 06:18:39 +0300

    new response object returned after measurement insertion

--- src/device-registry/utils/insert-measurements.js
+++ src/device-registry/utils/insert-measurements.js
@@ -2,8 +2,6 @@ const constants = require("../config/constants");
 const { getModelByTenant } = require("./multitenancy");
 const { logObject, logText, logElement } = require("./log");
 const EventSchema = require("../models/Event");
-const HTTPStatus = require("http-status");
-const componentSchema = require("../models/Component");
 
 const insert = async (tenant, transformedMeasurements) => {
   let nAdded = 0;
@@ -11,12 +9,14 @@ const insert = async (tenant, transformedMeasurements) => {
   let eventsRejected = [];
   let errors = [];
 
+  logObject("the transformed measurements received", transformedMeasurements);
+
   for (const measurement of transformedMeasurements) {
     try {
-      console.log("the measurement: ", measurement);
+      logObject("the measurement in the insertion process", measurement);
       const eventBody = {
         day: measurement.day,
-        nValues: { $lt: constants.N_VALUES },
+        nValues: { $lt: `${constants.N_VALUES}` },
         $or: [
           { "values.time": { $ne: measurement.time } },
           { "values.device": { $ne: measurement.device } },
@@ -39,19 +39,62 @@ const insert = async (tenant, transformedMeasurements) => {
       ).updateOne(eventBody, options, {
         upsert: true,
       });
+      logObject("addedEvents", addedEvents);
       if (addedEvents) {
         nAdded += 1;
         eventsAdded.push(measurement);
       } else if (!addedEvents) {
         eventsRejected.push(measurement);
-        errors.push("unable to add the events ");
+        let errMsg = {
+          msg: "unable to add the events",
+          record: {
+            ...(measurement.device ? { device: measurement.device } : {}),
+            ...(measurement.frequency
+              ? { frequency: measurement.frequency }
+              : {}),
+            ...(measurement.time ? { time: measurement.time } : {}),
+            ...(measurement.device_id
+              ? { device_id: measurement.device_id }
+              : {}),
+            ...(measurement.site_id ? { site_id: measurement.site_id } : {}),
+          },
+        };
+        errors.push(errMsg);
       } else {
         eventsRejected.push(measurement);
-        errors.push("unable to add the events ");
+        let errMsg = {
+          msg: "unable to add the events",
+          record: {
+            ...(measurement.device ? { device: measurement.device } : {}),
+            ...(measurement.frequency
+              ? { frequency: measurement.frequency }
+              : {}),
+            ...(measurement.time ? { time: measurement.time } : {}),
+            ...(measurement.device_id
+              ? { device_id: measurement.device_id }
+              : {}),
+            ...(measurement.site_id ? { site_id: measurement.site_id } : {}),
+          },
+        };
+        errors.push(errMsg);
       }
     } catch (e) {
       eventsRejected.push(measurement);
-      errors.push(e.message);
+      let errMsg = {
+        msg: "duplicate record",
+        record: {
+          ...(measurement.device ? { device: measurement.device } : {}),
+          ...(measurement.frequency
+            ? { frequency: measurement.frequency }
+            : {}),
+          ...(measurement.time ? { time: measurement.time } : {}),
+          ...(measurement.device_id
+            ? { device_id: measurement.device_id }
+            : {}),
+          ...(measurement.site_id ? { site_id: measurement.site_id } : {}),
+        },
+      };
+      errors.push(errMsg);
     }
   }
 
@@ -60,14 +103,11 @@ const insert = async (tenant, transformedMeasurements) => {
       success: false,
       message: "finished the operation with some errors",
       errors: errors,
-      valuesRejected: eventsRejected,
-      valuesAdded: eventsAdded,
     };
   } else {
     return {
       success: true,
       message: "successfully added all the events",
-      valuesAdded: eventsAdded,
     };
   }
 };
