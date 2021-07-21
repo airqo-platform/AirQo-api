237f0f616e7ea81a12965e811e5f9ba00235dc69
lilly-muyama <muyamalillian@gmail.com>
2021-07-21 15:37:05 +0300

    adding cache

--- src/gp-model/controllers/heatmap.py
+++ src/gp-model/controllers/heatmap.py
@@ -1,6 +1,21 @@
-from flask import Blueprint, request, jsonify
+from flask import Blueprint, request
 from helpers.utils import get_gp_predictions
+from flask_caching import Cache
+from routes import api
+from flask_cors import CORS
+from dotenv import load_dotenv
+load_dotenv()
 
+cache = Cache(config={
+    'CACHE_TYPE': 'redis',
+    'CACHE_REDIS_HOST': os.getenv('REDIS_SERVER'),
+    'CACHE_REDIS_PORT': os.getenv('REDIS_PORT'),
+    'CACHE_REDIS_URL': f"redis://{os.getenv('REDIS_SERVER')}:{os.getenv('REDIS_PORT')}",
+})
+
+_logger = logging.getLogger(__name__)
+
+ml_app = Blueprint('ml_app', __name__)
 
 @ml_app.route(api.route['predict_for_heatmap'], methods=['GET'])
 @cache.cached(timeout=3600)
