b07bb43b608ce309ab7f41ce3467acd0d6bda316
ja-odur <odurjoseph8@gmail.com>
2021-07-22 23:35:43 +0300

    PLAT-678: add duration constants in config file

--- src/device-status/config.py
+++ src/device-status/config.py
@@ -8,6 +8,9 @@ load_dotenv(dotenv_path)
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+ONE_HOUR = 3600
+TWO_WEEKS = 14 * 24 * ONE_HOUR
+
 
 class Config:
     DEBUG = False
@@ -16,6 +19,8 @@ class Config:
     DB_NAME = os.getenv("DB_NAME_PROD")
     REGISTRY_MONGO_URI = os.getenv('REGISTRY_MONGO_GCE_URI')
     MONITORING_MONGO_URI = os.getenv('MONITORING_MONGO_GCE_URI')
+    MAX_ONLINE_ACCEPTABLE_DURATION = os.getenv('MAX_ONLINE_ACCEPTABLE_DURATION', ONE_HOUR)
+    DUE_FOR_MAINTENANCE_DURATION = os.getenv('DUE_FOR_MAINTENANCE_DURATION', TWO_WEEKS)
     SECRET_KEY = os.getenv("SECRET_KEY")
     BASE_API_URL = "https://staging-platform.airqo.net/api/v1/data"
     RECENT_FEEDS_URL = f"{BASE_API_URL}/feeds/transform/recent"
