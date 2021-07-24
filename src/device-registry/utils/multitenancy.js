83a5c4c6d6b4f453a1f14f1ab451cebad83f5683
Martin <bbaalmart@gmail.com>
2021-07-25 02:39:52 +0300

    remove index enforcer

--- src/device-registry/utils/multitenancy.js
+++ src/device-registry/utils/multitenancy.js
@@ -4,10 +4,6 @@ const { logElement, logText, logObject } = require("../utils/log");
 const log4js = require("log4js");
 const logger = log4js.getLogger("multitenancy");
 
-/****
- * creating a new mongoDB connection by switching tenant
- * using this to create a new connection based on tenant ID
- */
 const getTenantDB = (tenantId, modelName, schema) => {
   const dbName = `${constants.DB_NAME}_${tenantId}`;
   if (mongodb) {
@@ -20,23 +16,7 @@ const getTenantDB = (tenantId, modelName, schema) => {
 const getModelByTenant = (tenantId, modelName, schema) => {
   logElement("tenantId", tenantId);
   const tenantDb = getTenantDB(tenantId, modelName, schema);
-  const model = tenantDb.model(modelName);
-
-  model.ensureIndexes(function(err) {
-    logger.info("ENSURE INDEX");
-    if (err) {
-      logger.error(`ensureIndexes -- ${err}`);
-    }
-  });
-
-  model.on("index", function(err) {
-    logger.info("ON INDEX");
-    if (err) {
-      logger.error(`ensureIndexes -- ${err}`);
-    }
-  });
-
-  return model;
+  return tenantDb.model(modelName);
 };
 
 module.exports = { getModelByTenant };
