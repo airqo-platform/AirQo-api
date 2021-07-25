55b59c2a3b193d879e424ffb8fa11de978a51756
danielogen <danielogen@gmail.com>
2021-07-25 21:04:21 +0300

    update

--- src/calibrate-multi-hop/app.py
+++ src/calibrate-multi-hop/app.py
@@ -3,8 +3,7 @@ import os
 from flask import Flask
 from flask_cors import CORS
 from flask_pymongo import PyMongo
-
-from config import configuration
+from config.config import app_config
 from controllers.calibrate import calibrate_bp
 
 
@@ -13,7 +12,7 @@ mongo = PyMongo()
 
 def create_app(environment):
     app = Flask(__name__)
-    app.config.from_object(configuration[environment])
+    app.config.from_object(app_config[environment])
 
     mongo.init_app(app)
     CORS(app)
