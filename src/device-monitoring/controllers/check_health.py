458787cce386afe472e41c38737d0973e6304bca
ja-odur <odurjoseph8@gmail.com>
2021-07-26 22:57:36 +0300

    [PLAT-679] un-nest routes folder

--- src/device-monitoring/controllers/check_health.py
+++ src/device-monitoring/controllers/check_health.py
@@ -1,15 +1,13 @@
 from flask import Blueprint, request, jsonify
 import logging
-from routes import api
-from dotenv import load_dotenv
-load_dotenv()
-
+import routes
 
 _logger = logging.getLogger(__name__)
+
 health_check_bp = Blueprint('monitor_bp', __name__)
 
 
-@health_check_bp.route(api.route['health_check'], methods=['GET', 'POST'])
+@health_check_bp.route(routes.HEALTH, methods=['GET', 'POST'])
 def health():
     if request.method == 'GET':
         _logger.info('health status OK')
---
+++ src/device-monitoring/routes.py
@@ -0,0 +1,26 @@
+BASE_URL = '/api/v1/monitor'
+
+HEALTH = '/health'
+
+DEVICE_STATUS = f'{BASE_URL}/devices/status'
+
+LATEST_DEVICES_STATUS = f'{BASE_URL}/device/status/latest'
+
+NETWORK_UPTIME = f'{BASE_URL}/network/uptime'
+
+DEVICE_UPTIME = f'{BASE_URL}/device/uptime'
+
+ALL_DEVICE_UPTIME = f'{BASE_URL}/devices/uptime'
+
+
+# 'maintenance_logs': base_url + '/device/maintenance_logs',
+# 'device_name_maintenance_log': base_url + '/device/maintenance_logs/<device_name>',
+# 'device_power': base_url + '/device/power_type',
+# 'devices': base_url + '/devices',
+# 'latest_offline_devices': base_url + '/devices/offline',
+# 'best_performing_devices': base_url + '/network/devices/bestperforming',
+# 'worst_performing_devices': base_url + '/network/devices/worstperforming',
+
+# 'online_offline': base_url + '/devices/online_offline',
+# 'device_battery_voltage': base_url + '/device/batteryvoltage',
+# 'device_sensor_correlation': base_url + '/device/sensors/correlation',
