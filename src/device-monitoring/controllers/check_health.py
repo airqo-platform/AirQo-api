eb310d7f81ab520718a739cc1d39895e6c89146d
ja-odur <odurjoseph8@gmail.com>
2021-07-26 22:29:36 +0300

    [PLAT-679] simplify health endpoint

--- src/device-monitoring/controllers/check_health.py
+++ src/device-monitoring/controllers/check_health.py
@@ -1,30 +1,16 @@
-
 from flask import Blueprint, request, jsonify
 import logging
-import datetime as dt
-from config.db_connection import app_configuration
-from bson import json_util, ObjectId
-import json
-from datetime import datetime, timedelta
 from routes import api
-import os
 from dotenv import load_dotenv
 load_dotenv()
 
 
 _logger = logging.getLogger(__name__)
-monitor_bp = Blueprint('monitor_bp', __name__)
+health_check_bp = Blueprint('monitor_bp', __name__)
 
 
-@monitor_bp.route(api.route['root'], methods=['GET', 'POST'])
-def root():
-    if request.method == 'GET':
-        _logger.info('root endpoint OK')
-        return jsonify({"message": "ok", "status": True}), 200
-
-
-@monitor_bp.route(api.route['health_check'], methods=['GET', 'POST'])
+@health_check_bp.route(api.route['health_check'], methods=['GET', 'POST'])
 def health():
     if request.method == 'GET':
         _logger.info('health status OK')
-        return jsonify({"message": f'health check passed. DB_URL: {app_configuration.MONGO_URI}, DB_NAME: {app_configuration.DB_NAME}', "status": True}), 200
+        return jsonify({"message": 'App status - OK.', "success": True}), 200
