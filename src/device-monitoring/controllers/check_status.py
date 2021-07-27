c4378270a7af7a923a76de824eed49619231c625
ja-odur <odurjoseph8@gmail.com>
2021-07-28 00:17:33 +0300

    [PLAT-679] add device uptime endpoint

--- src/device-monitoring/controllers/check_status.py
+++ src/device-monitoring/controllers/check_status.py
@@ -2,8 +2,7 @@ from flask import Blueprint, request, jsonify
 import logging
 from helpers.convert_dates import validate_datetime
 from helpers.convert_object_ids import convert_model_ids
-from helpers.group_by import group_by
-from models import DeviceStatus, NetworkUptime
+from models import DeviceStatus, NetworkUptime, DeviceUptime
 import routes
 
 _logger = logging.getLogger(__name__)
@@ -77,73 +76,33 @@ def get_network_uptime():
     return jsonify(response), 200
 
 
-@device_status_bp.route(routes.DEVICE_UPTIME, methods=['GET', 'POST', 'PUT', 'DELETE', 'PATCH'])
+@device_status_bp.route(routes.DEVICE_UPTIME, methods=['GET'])
 def get_device_uptime():
-    '''
-    Get device uptime
-    '''
-    model = device_status.DeviceStatus()
-    if request.method == 'GET':
-        REQUIRED_ARGS = ['tenant', 'device_name', 'days']
-        errors = {}
-
-        for arg in REQUIRED_ARGS:
-            if not request.args.get(arg):
-                errors[arg] = f"'{arg}' is a required parameter"
-
-        tenant = request.args.get('tenant')
-        device_name = request.args.get('device_name')
-        try:
-            days = int(request.args.get('days'))
-            if days <= 0:
-                errors["days"] = f"'{request.args.get('days')}' must be greater than zero"
-        except (ValueError, TypeError):
-            errors["days"] = f"'{request.args.get('days')}' is not a valid integer"
-
-
-        if errors:
-            return jsonify(dict(
-                message="Please specify one of the following query parameters. "
-                        "Refer to the API documentation for details.",
-                errors=errors
-            )), 400
-
-        result = model.get_device_uptime(tenant, device_name, days)
-        result = convert_model_ids(result)
-
-        response = dict(message="device uptime query successful", data=result)
-        return jsonify(response), 200
-    else:
-        return jsonify({"message": "Invalid request method. Please refer to the API documentation", "success": False}), 400
-
-
-@device_status_bp.route(routes.ALL_DEVICE_UPTIME, methods=['GET'])
-def get_all_devices_uptime():
-    REQUIRED_ARGS = ['tenant', 'days']
     errors = {}
-
-    for arg in REQUIRED_ARGS:
-        if not request.args.get(arg):
-            errors[arg] = f"'{arg}' is a required parameter"
-
     tenant = request.args.get('tenant')
+    device_name = request.args.get('device_name')
 
     try:
-        days = int(request.args.get('days'))
-        if days <= 0:
-            errors["days"] = f"'{request.args.get('days')}' must be greater than zero"
-    except (ValueError, TypeError):
-        errors["days"] = f"'{request.args.get('days')}' is not a valid integer"
+        start_date = validate_datetime(request.args.get('startDate'))
+    except Exception:
+        errors['startDate'] = 'This field is required.' \
+                              'Please provide a valid ISO formatted datetime string (%Y-%m-%dT%H:%M:%S.%fZ)'
+
+    try:
+        end_date = validate_datetime(request.args.get('endDate'))
+    except Exception:
+        errors['endDate'] = 'This field is required.' \
+                              'Please provide a valid ISO formatted datetime string (%Y-%m-%dT%H:%M:%S.%fZ)'
 
     if errors:
-        return jsonify(dict(
-            message="Please specify one of the following query parameters. "
-                    "Refer to the API documentation for details.",
-            errors=errors
-        )), 400
-    model = device_status.DeviceStatus()
-    result = model.get_all_devices_uptime(tenant, days)
-    result = group_by('device_name', convert_model_ids(result))
+        return jsonify({
+            'message': 'Some errors occurred while processing this request',
+            'errors': errors
+        }), 400
+
+    model = DeviceUptime(tenant)
+    result = model.get_device_uptime(start_date, end_date, device_name)
 
     response = dict(message="device uptime query successful", data=result)
     return jsonify(response), 200
+
