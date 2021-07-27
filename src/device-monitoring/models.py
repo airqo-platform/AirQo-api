9c430fd898e888474c1b70caf2ee9b66b6895fb2
ja-odur <odurjoseph8@gmail.com>
2021-07-28 00:16:35 +0300

    [PLAT-679] add deviceUptime model

--- src/device-monitoring/models.py
+++ src/device-monitoring/models.py
@@ -1,34 +1,6 @@
 from config.db_connection import connect_mongo
 from pymongo import DESCENDING, ASCENDING
 
-# DEVICE_STATUS_PROJECT = {
-#     '_id': {'$toString': '$_id'},
-#     'count_due_maintenance': 1,
-#     'count_of_alternator_devices': 1,
-#     'count_of_mains': 1,
-#     'count_of_offline_devices': 1,
-#     'count_of_online_devices': 1,
-#     'count_of_solar_devices': 1,
-#     'count_overdue_maintenance': 1,
-#     'count_unspecified_maintenance': 1,
-#     'created_at': 1,
-#     'offline_devices': {
-#         '_id': {'$toString': '$_id'},
-#         'device_number': 1,
-#         'elapsed_time': 1,
-#         'isActive': 1,
-#         'isRetired': 1,
-#         'latitude': 1,
-#         'longitude': 1,
-#         'maintenance_status': 1,
-#         'mobility': 1,
-#         'name': 1,
-#         'nextMaintenance': 1,
-#         'powerType': 1,
-#         'site_id': {'$toString': '$site_id'},
-#     }
-# }
-
 
 class DeviceStatus:
     def __init__(self, tenant):
@@ -63,3 +35,49 @@ class NetworkUptime:
         db_filter = {'created_at': {'$gte': start_date, '$lt': end_date}}
 
         return list(self.collection.find(db_filter).sort('created_at', ASCENDING))
+
+
+class DeviceUptime:
+    def __init__(self, tenant):
+        self.tenant = tenant
+        self.db = self._connect()
+        self.collection = self.db['device_uptime']
+
+    def _connect(self):
+        return connect_mongo(self.tenant)
+
+    def get_device_uptime(self, start_date, end_date, device_name):
+
+        db_filter = {'created_at': {'$gte': start_date, '$lt': end_date}}
+
+        if device_name:
+            db_filter['device_name'] = device_name
+
+        return list(
+            self.collection.aggregate(
+                [
+                    {"$match": db_filter},
+                    {
+                        "$group": {
+                            "_id": '$device_name',
+                            # "uptime": {'$push': "$$ROOT"}
+                            "values": {'$push': {
+                                '_id': {'$toString': '$_id'},
+                                "battery_voltage": "$battery_voltage",
+                                "channel_id": "$channel_id",
+                                "created_at": { '$dateToString': {
+                                    'date':"$created_at",
+                                    'format': '%Y-%m-%dT%H:%M:%S%z',
+                                    'timezone': 'Africa/Kampala',
+                                }},
+                                "device_name": "$device_name",
+                                "downtime": "$downtime",
+                                "sensor_one_pm2_5": "$sensor_one_pm2_5",
+                                "sensor_two_pm2_5": "$sensor_two_pm2_5",
+                                "uptime": "$uptime"
+                            }}
+                        }
+                    }
+                ]
+            )
+        )
