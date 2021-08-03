ceccf2dd2fbc79d474fa7cab2aa385fc545178c5
Noah Nsimbe <nsimbenoah@gmail.com>
2021-08-03 16:14:26 +0300

    applying lombok to airqo connector

--- src/data-mgt/java/airqo-source-connector/src/main/java/airqo/models/AirQoDevicesResponse.java
+++ src/data-mgt/java/airqo-source-connector/src/main/java/airqo/models/AirQoDevicesResponse.java
@@ -1,31 +1,19 @@
 package airqo.models;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Getter
+@Setter
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class AirQoDevicesResponse {
 
     private boolean success = false;
     private List<AirqoDevice> devices = new ArrayList<>();
-
-    public boolean isSuccess() {
-        return success;
-    }
-
-    public void setSuccess(boolean success) {
-        this.success = success;
-    }
-
-    public List<AirqoDevice> getDevices() {
-        return devices;
-    }
-
-    public void setDevices(List<AirqoDevice> devices) {
-        this.devices = devices;
-    }
 
     @Override
     public String toString() {
--- src/data-mgt/java/airqo-source-connector/src/main/java/airqo/models/AirqoDevice.java
+++ src/data-mgt/java/airqo-source-connector/src/main/java/airqo/models/AirqoDevice.java
@@ -4,9 +4,13 @@ import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.io.Serializable;
 
+@Getter
+@Setter
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class AirqoDevice implements Serializable {
 
@@ -24,30 +28,6 @@ public class AirqoDevice implements Serializable {
     @Expose
     @JsonAlias({ "site_details", "siteDetails" })
     private SiteDetails site = new SiteDetails();
-
-    public SiteDetails getSite() {
-        return site;
-    }
-
-    public void setSite(SiteDetails site) {
-        this.site = site;
-    }
-
-    public int getDeviceNumber() {
-        return deviceNumber;
-    }
-
-    public void setDeviceNumber(int deviceNumber) {
-        this.deviceNumber = deviceNumber;
-    }
-
-    public String getDevice() {
-        return device;
-    }
-
-    public void setDevice(String device) {
-        this.device = device;
-    }
 
     @Override
     public String toString() {
