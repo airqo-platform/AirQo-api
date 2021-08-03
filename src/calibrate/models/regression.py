25ee1096b2e38ccb5b6429434c2f53c1f71a6912
Priscilla-AL <priscilla@airqo.net>
2021-08-03 15:51:54 +0300

    Update regression.py
    Clean file and update links

--- src/calibrate/models/regression.py
+++ src/calibrate/models/regression.py
@@ -1,4 +1,5 @@
 import numpy as np
+from jobs import get_data as gd
 from jobs import regression as rg
 import pandas as pd
 
@@ -9,25 +10,10 @@ class Regression():
     """
     def __init__(self):
         """ initialize """
-        lowcost_hourly_mean = rg.get_lowcost_data()
-        bam_hourly_mean = rg.get_bam_data()
-        self.hourly_combined_dataset = rg.combine_datasets(lowcost_hourly_mean, bam_hourly_mean)
+        lowcost_hourly_mean = gd.get_lowcost_data()
+        bam_hourly_mean = gd.get_bam_data()
+        self.hourly_combined_dataset = gd.combine_datasets(lowcost_hourly_mean, bam_hourly_mean)
 
-    def simple_lr(self, raw_value, hourly_combined_dataset):
-        slope, intercept, MAE, RMSE, r2_score = rg.simple_linear_regression(hourly_combined_dataset)
-        calibrated_value_lr = intercept + slope * float(raw_value)
-        print('MAE:', MAE)   
-        print('RMSE:', RMSE)   
-        print('R2:', r2_score)  
-        return calibrated_value_lr
-
-    def multivariate_lr(self, raw_value, temp, humidity, hourly_combined_dataset):
-        slope, intercept, MAE2, RMSE2, r2_score2 = rg.mlr(hourly_combined_dataset)
-        calibrated_value_mlr = float(raw_values) * slope[0] + float(temp) * slope[1] + float(humidity) * slope[2] + intercept    
-        print('MAE2:', MAE2)   
-        print('RMSE2:', RMSE2) 
-        print('R2_2:', r2_score2) 
-        return calibrated_value_mlr
 
     def random_forest(self, datetime, pm2_5, pm10, temperature,humidity,hourly_combined_dataset):
         rf_reg, MAE3, RMSE3, r2_score3 = rg.random_forest(hourly_combined_dataset)
