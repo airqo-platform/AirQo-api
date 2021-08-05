df680266392251de89af127434b6752be7118a56
Priscilla-AL <priscilla@airqo.net>
2021-08-05 20:01:39 +0300

    Update regression.py

--- src/calibrate/models/regression.py
+++ src/calibrate/models/regression.py
@@ -1,25 +1,24 @@
 import numpy as np
 import pandas as pd
 import pickle
-from jobs import get_data as gd
+# from jobs import get_data as gd
 from jobs import regression as rg
 
 class Regression():
     """
         The class contains functionality for computing device calibrated values .
     """
-    def __init__(self):
-        """ initialize """
-        lowcost_hourly_mean = gd.get_lowcost_data()
-        bam_hourly_mean = gd.get_bam_data()
-        self.hourly_combined_dataset = gd.combine_datasets(lowcost_hourly_mean, bam_hourly_mean)
+    # def __init__(self):
+    #     """ initialize """
+    #     lowcost_hourly_mean = gd.get_lowcost_data()
+    #     bam_hourly_mean = gd.get_bam_data()
+    #     self.hourly_combined_dataset = gd.combine_datasets(lowcost_hourly_mean, bam_hourly_mean)
 
-    def random_forest(self, hourly_combined_dataset, pm2_5,s2_pm2_5,pm10,s2_pm10,temperature,humidity, datetime):
-        rf_regressor = rg.random_forest(hourly_combined_dataset)
+    # def random_forest(self, hourly_combined_dataset, pm2_5,s2_pm2_5,pm10,s2_pm10,temperature,humidity, datetime):
+    #     rf_regressor = rg.random_forest(hourly_combined_dataset)
 
-        # save the model to disk
-        filename = 'jobs/rf_reg_model.sav'
-        pickle.dump(rf_regressor, open(filename, 'wb'))
+    def compute_calibrated_val(self,pm2_5,s2_pm2_5,pm10,s2_pm10,temperature,humidity, datetime):
+        rf_regressor = pickle.load(open('jobs/rf_reg_model.sav', 'rb'))
         
         # features from datetime and PM
         datetime = pd.to_datetime(datetime)
@@ -30,14 +29,13 @@ class Regression():
                                     index=['input'])
         input_variables["avg_pm2_5"] = input_variables[['pm2_5','s2_pm2_5']].mean(axis=1).round(2)
         input_variables["avg_pm10"] =  input_variables[['pm10','s2_pm10']].mean(axis=1).round(2)
-        input_variables["error_pm2_5"]=input_variables["pm10"]-input_variables["s2_pm10"]
         input_variables["error_pm10"]=np.abs(input_variables["pm10"]-input_variables["s2_pm10"])
         input_variables["error_pm2_5"]=np.abs(input_variables["pm2_5"]-input_variables["s2_pm2_5"])
         input_variables["pm2_5_pm10"]=input_variables["avg_pm2_5"]-input_variables["avg_pm10"]
         input_variables["pm2_5_pm10_mod"]=input_variables["pm2_5_pm10"]/input_variables["avg_pm10"]
         input_variables = input_variables.drop(['pm2_5','s2_pm2_5','pm10','s2_pm10'], axis=1)
         
-        #reorganise
+        #reorganise columns
         input_variables = input_variables[['avg_pm2_5','avg_pm10','temperature','humidity','hour','error_pm2_5','error_pm10','pm2_5_pm10', 'pm2_5_pm10_mod']]
 
         calibrated_value_rf =  rf_regressor.predict(input_variables)[0]          
