914eccaf2a247cb6df832619cf0643aecec171a7
Martin <bbaalmart@gmail.com>
2021-08-18 08:34:08 +0300

    the disburse money controller logic

--- src/incentives/controllers/incentivize.js
+++ src/incentives/controllers/incentivize.js
@@ -1,93 +1,80 @@
-const Transaction = require('../models/transaction');
-const validate = require('../utils/validateAccount');
-const fetch = require('node-fetch');
-const constants = require('../config/constants');
-const token = require('../utils/reimbursements-token');
+const Transaction = require("../models/transaction");
+const fetch = require("node-fetch");
+const constants = require("../config/constants");
+const token = require("../utils/reimbursements-token");
+const disbursements = require("../utils/disbursements");
+const { logElement, logObject, logText } = require("../utils/log");
 
 const incentivize = {
+  disburseMoney: (req, res) => {},
 
-    getStatus: (req, res) => {
-        res.status(200).send("we are getting the status");
-    },
+  getStatus: (req, res) => {
+    res.status(200).send("we are getting the status");
+  },
 
-    getBalance: async (req, res) => {
-        const options = {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': `Bearer ${token}`,
-                'Accept': 'application/json',
-                'Ocp-Apim-Subscription-Key': `${process.env.DISBURSEMENTS_MOMO_PRIMARY_KEY}`,
-                'X-Target-Environment': ''
-            }
-        };
-        try {
-            const response = await fetch(`${constants.MOMO_URL_DISBURSEMENTS}/v1_0/account/balance`,
-                options);
-            let json = await response.json();
-            res.status(200).send(json);
-        }
-        catch (e) {
-            res.status(500).send(e.message);
-        }
-
-    },
-
-    callback: (req, res) => {
-        res.status(200).send('callback things in the moment');
-    },
-
-    data: (req, res) => {
-        const deviceId = req.params.id;
-        const option = req.params.option;
-        //check the existence of the devices
-        //assumption is that this device was registered with a phone number.
-        //get the device' phone number and purchase data for it accordingly using the payment option selected.
-        //assumption is that we are using the merchant account for the device.
-        //send a success message about this purchase accordingly.
-    },
-
-    momo: async (req, res) => {
-        let deviceId = req.body.id;
-        let phone = req.body.phone;
-        let option = req.body.option;
-        let amount = req.body.amount;
-        let reference = req.body.ref
-
-        //check validity of the device being added.
-        //obtain the phone number of the device that was registered with that device
-        //validate if the phone number is active and able to receive funds.
-        //then send mobile money to that number accordingly.
-        const body = {
-            "amount": amount,
-            "currency": "UGX",
-            "externalId": "",
-            "payee": {
-                "partyIdType": "MSISDN",
-                "partyId": phone
-            },
-            "payerMessage": "AirQo funds transfer",
-            "payeeNote": ""
-        };
-        const options = {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': `Bearer ${token}`,
-                'Accept': 'application/json',
-                'Ocp-Apim-Subscription-Key': `${process.env.DISBURSEMENTS_MOMO_PRIMARY_KEY}`,
-                'X-Target-Environment': 'sandbox',
-                'X-Reference-id': `${reference}`,
-            },
-            body: body
-        };
-
-        fetch(`${constants.MOMO_URL_DISBURSEMENTS}/v1_0/transfer`,
-            options)
-            .then(res => res.json)
-            .then(json => res.status(200).send(json))
-            .catch(error => res.status(500).send(error.message))
+  getBalance: async (req, res) => {
+    const options = {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+        Accept: "application/json",
+        "Ocp-Apim-Subscription-Key": `${process.env.DISBURSEMENTS_MOMO_PRIMARY_KEY}`,
+        "X-Target-Environment": "",
+      },
+    };
+    try {
+      const response = await fetch(
+        `${constants.MOMO_URL_DISBURSEMENTS}/v1_0/account/balance`,
+        options
+      );
+      let json = await response.json();
+      res.status(200).send(json);
+    } catch (e) {
+      res.status(500).send(e.message);
     }
-}
+  },
+
+  callback: (req, res) => {
+    res.status(200).send("callback things in the moment");
+  },
+
+  data: (req, res) => {
+    const deviceId = req.params.id;
+    const option = req.params.option;
+    //check the existence of the devices
+    //assumption is that this device was registered with a phone number.
+    //get the device' phone number and purchase data for it accordingly using the payment option selected.
+    //assumption is that we are using the merchant account for the device.
+    //send a success message about this purchase accordingly.
+  },
+
+  disburse: async (req, res) => {
+    let deviceId = req.body.id;
+    let phone = req.body.phone;
+    let option = req.body.option;
+    let amount = req.body.amount;
+    let reference = req.body.ref;
+
+    const responseFromDisburse = await disbursements();
+
+    logObject("responseFromDisburse", responseFromDisburse);
+
+    if (responseFromDisburse.success === true) {
+      res.status(200).json({
+        transaction,
+        accountBalance,
+      });
+    }
+
+    if (responseFromDisburse.success === false) {
+      res.status(500).json({
+        message: "unable to disburse",
+      });
+    }
+    logObject("the transaction in the controller", transaction);
+    logElement("the account balance in the controller", accountBalance);
+  },
+};
 
 module.exports = incentivize;
