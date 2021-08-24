b04ec5b741d9c03fdb7d95ea2e6fbaf138593a7a
Martin <bbaalmart@gmail.com>
2021-08-24 08:50:11 +0300

    make the encryptions shorter

--- src/device-registry/models/Device.js
+++ src/device-registry/models/Device.js
@@ -4,9 +4,8 @@ const uniqueValidator = require("mongoose-unique-validator");
 const tranformDeviceName = require("../utils/transform-device-name");
 const { logObject, logElement, logText } = require("../utils/log");
 const { monthsInfront } = require("../utils/date");
-const Cryptr = require("cryptr");
 const constants = require("../config/constants");
-const cryptr = new Cryptr(`${constants.KEY_ENCRYPTION_KEY}`);
+const cryptoJS = require("crypto-js");
 const isEmpty = require("is-empty");
 const jsonify = require("../utils/jsonify");
 const log4js = require("log4js");
@@ -181,18 +180,16 @@ const deviceSchema = new mongoose.Schema(
 );
 
 deviceSchema.plugin(uniqueValidator, {
-  message: `{VALUE} already taken!`,
+  message: `{VALUE} must be unique!`,
 });
 
 deviceSchema.pre("save", function(next) {
   if (this.isModified("name")) {
-    // this.name = this._transformDeviceName(this.name);
     if (this.writeKey && this.readKey) {
       this.writeKey = this._encryptKey(this.writeKey);
       this.readKey = this._encryptKey(this.readKey);
     }
     let n = this.name;
-    // console.log({ n });
   }
   return next();
 });
@@ -213,17 +210,12 @@ deviceSchema.pre("findByIdAndUpdate", function(next) {
 });
 
 deviceSchema.methods = {
-  _transformDeviceName(name) {
-    let transformedName = tranformDeviceName(name);
-    return transformedName;
-  },
   _encryptKey(key) {
-    let encryptedKey = cryptr.encrypt(key);
+    let encryptedKey = cryptoJS.AES.encrypt(
+      key,
+      constants.KEY_ENCRYPTION_KEY
+    ).toString();
     return encryptedKey;
-  },
-  _decryptKey(encryptedKey) {
-    let decryptedKey = cryptr.decrypt(encryptedKey);
-    return decryptedKey;
   },
   toJSON() {
     return {
@@ -386,10 +378,18 @@ deviceSchema.statics = {
       delete modifiedUpdate.generation_version;
       logObject("modifiedUpdate", modifiedUpdate);
       if (update.writeKey) {
-        modifiedUpdate.writeKey = cryptr.encrypt(update.writeKey);
+        let key = update.writeKey;
+        modifiedUpdate.writeKey = cryptoJS.AES.encrypt(
+          key,
+          constants.KEY_ENCRYPTION_KEY
+        ).toString();
       }
       if (update.readKey) {
-        modifiedUpdate.readKey = cryptr.encrypt(update.readKey);
+        let key = update.readKey;
+        modifiedUpdate.readKey = cryptoJS.AES.encrypt(
+          key,
+          constants.KEY_ENCRYPTION_KEY
+        ).toString();
       }
       let updatedDevice = await this.findOneAndUpdate(
         filter,
@@ -410,6 +410,7 @@ deviceSchema.statics = {
         };
       }
     } catch (error) {
+      logObject("the error", error);
       return {
         success: false,
         message: "Device model server error - modify",
