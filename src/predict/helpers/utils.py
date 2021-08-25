6207c68825e04b50d258ec812d96d81d369b7ca5
lilly-muyama <muyamalillian@gmail.com>
2021-08-25 12:07:04 +0300

    querying by airqloud id

--- src/predict/helpers/utils.py
+++ src/predict/helpers/utils.py
@@ -194,7 +194,22 @@ def get_gp_predictions(airqloud):
 
     db = client[DB_NAME]
     query = {'airqloud':airqloud}
-    projection = {'_id': 0, 'latitude': 1, 'longitude': 1, 'predicted_value': 1, 'variance': 1, 'interval': 1, 'airqloud':1, 'created_at':1}
+    projection = {'_id': 0, 'latitude': 1, 'longitude': 1, 'predicted_value': 1, 'variance': 1, 'interval': 1, 'airqloud':1, 'created_at':1, 'airqloud_id':1}
+    records = list(db.gp_predictions.find(query, projection))
+    return records
+
+def get_gp_predictions_id(aq_id):
+    '''
+    returns pm 2.5 predictions for a particular airqloud
+    '''
+    try:
+        client = MongoClient(MONGO_URI)
+    except pymongo.errors.ConnectionFailure as e:
+        return {'message':'unable to connect to database', 'success':False}, 400
+
+    db = client[DB_NAME]
+    query = {'airqloud_id': aq_id}
+    projection = {'_id': 0, 'latitude': 1, 'longitude': 1, 'predicted_value': 1, 'variance': 1, 'interval': 1, 'airqloud':1, 'created_at':1, 'airqloud_id':1}
     records = list(db.gp_predictions.find(query, projection))
     return records
 
