16ce0a94ba1b3da40dacfc4b2a4cef6a7611b4cd
Noah Nsimbe <nsimbenoah@gmail.com>
2021-08-26 22:25:46 +0300

    querying events by device

--- src/data-mgt/python/cron-jobs/airqo_api.py
+++ src/data-mgt/python/cron-jobs/airqo_api.py
@@ -8,11 +8,13 @@ class AirQoApi:
         self.AIRQO_BASE_URL = os.getenv("AIRQO_BASE_URL")
         self.AIRQO_API_KEY = f"JWT {os.getenv('AIRQO_API_KEY')}"
 
-    def get_events(self, tenant, start_time, end_time):
+    def get_events(self, tenant, start_time, end_time, device):
         headers = {'Authorization': self.AIRQO_API_KEY}
 
         params = {
             "tenant": tenant,
+            "frequency": 'raw',
+            "device": device,
             "startTime": start_time,
             "endTime": end_time
         }
@@ -26,6 +28,29 @@ class AirQoApi:
 
         if api_request.status_code == 200 and "measurements" in api_request.json():
             return api_request.json()["measurements"]
+
+        print(api_request.request.url)
+        print(api_request.request.body)
+        print(api_request.content)
+        return []
+
+    def get_devices(self, tenant):
+        headers = {'Authorization': self.AIRQO_API_KEY}
+
+        params = {
+            "tenant": tenant,
+            "active": 'yes',
+        }
+
+        api_request = requests.get(
+            '%s%s' % (self.AIRQO_BASE_URL, 'devices'),
+            params=params,
+            headers=headers,
+            verify=False,
+        )
+
+        if api_request.status_code == 200 and "devices" in api_request.json():
+            return api_request.json()["devices"]
 
         print(api_request.request.url)
         print(api_request.request.body)
