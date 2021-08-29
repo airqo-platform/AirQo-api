dbb574952c421fbbca04abdc224719f200c99bff
Noah Nsimbe <nsimbenoah@gmail.com>
2021-08-29 12:13:27 +0300

    more exception handling

--- src/data-mgt/python/cron-jobs/airqo_api.py
+++ src/data-mgt/python/cron-jobs/airqo_api.py
@@ -1,4 +1,5 @@
 import os
+import traceback
 
 import requests
 
@@ -6,7 +7,6 @@ import requests
 class AirQoApi:
     def __init__(self):
         self.AIRQO_BASE_URL = os.getenv("AIRQO_BASE_URL")
-        self.timeout = os.getenv("TIMEOUT")
         self.AIRQO_API_KEY = f"JWT {os.getenv('AIRQO_API_KEY')}"
 
     def get_events(self, tenant, start_time, end_time, device):
@@ -20,21 +20,24 @@ class AirQoApi:
             "endTime": end_time
         }
 
-        api_request = requests.get(
-            '%s%s' % (self.AIRQO_BASE_URL, 'devices/events'),
-            params=params,
-            headers=headers,
-            verify=False,
-            timeout=int(self.timeout)
-        )
+        try:
+            api_request = requests.get(
+                '%s%s' % (self.AIRQO_BASE_URL, 'devices/events'),
+                params=params,
+                headers=headers,
+                verify=False
+            )
 
-        if api_request.status_code == 200 and "measurements" in api_request.json():
-            return api_request.json()["measurements"]
+            if api_request.status_code == 200 and "measurements" in api_request.json():
+                return api_request.json()["measurements"]
 
-        print(api_request.request.url)
-        print(api_request.request.body)
-        print(api_request.content)
-        return []
+            print(api_request.request.url)
+            print(api_request.request.body)
+            print(api_request.content)
+            return []
+        except:
+            traceback.print_exc()
+            return []
 
     def get_devices(self, tenant):
         headers = {'Authorization': self.AIRQO_API_KEY}
@@ -44,18 +47,21 @@ class AirQoApi:
             "active": 'yes',
         }
 
-        api_request = requests.get(
-            '%s%s' % (self.AIRQO_BASE_URL, 'devices'),
-            params=params,
-            headers=headers,
-            verify=False,
-            timeout=int(self.timeout)
-        )
+        try:
+            api_request = requests.get(
+                '%s%s' % (self.AIRQO_BASE_URL, 'devices'),
+                params=params,
+                headers=headers,
+                verify=False
+            )
 
-        if api_request.status_code == 200 and "devices" in api_request.json():
-            return api_request.json()["devices"]
+            if api_request.status_code == 200 and "devices" in api_request.json():
+                return api_request.json()["devices"]
 
-        print(api_request.request.url)
-        print(api_request.request.body)
-        print(api_request.content)
-        return []
+            print(api_request.request.url)
+            print(api_request.request.body)
+            print(api_request.content)
+            return []
+        except:
+            traceback.print_exc()
+            return []
