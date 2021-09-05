26483ce57de7aa42c93c7ca78f5229c1da23911e
Martin <bbaalmart@gmail.com>
2021-09-05 12:48:14 +0300

    controller logic for the create organization use case

---
+++ src/auth-service/controllers/create-organization.js
@@ -0,0 +1,233 @@
+const OrganizationSchema = require("../models/Organization");
+const { getModelByTenant } = require("../utils/multitenancy");
+const { logElement, logText, logObject } = require("../utils/log");
+const generateFilter = require("../utils/generate-filter");
+const HTTPStatus = require("http-status");
+const createOrganizationUtil = require("../utils/create-organization");
+const { validationResult } = require("express-validator");
+const manipulateArraysUtil = require("../utils/manipulate-arrays");
+
+const createOrganization = {
+  create: async (req, res) => {
+    try {
+      const hasErrors = !validationResult(req).isEmpty();
+      if (hasErrors) {
+        let nestedErrors = validationResult(req).errors[0].nestedErrors;
+        return badRequest(
+          res,
+          "bad request errors",
+          manipulateArraysUtil.convertErrorArrayToObject(nestedErrors)
+        );
+      }
+      let { body, query, params } = req;
+      let request = {};
+      request["query"] = query;
+      request["body"] = body;
+      request["params"] = params;
+
+      let responseFromCreateOrganization = await createOrganizationUtil.create(
+        request
+      );
+
+      if (responseFromCreateOrganization.success === true) {
+        let status = responseFromCreateOrganization.status
+          ? responseFromCreateOrganization.status
+          : HTTPStatus.OK;
+
+        return res.status(status).json({
+          success: true,
+          message: responseFromCreateOrganization.message,
+          data: responseFromCreateOrganization.data,
+        });
+      }
+
+      if (responseFromCreateOrganization.success === false) {
+        let errors = responseFromCreateOrganization.errors
+          ? responseFromCreateOrganization.errors
+          : "";
+
+        let status = responseFromCreateOrganization.status
+          ? responseFromCreateOrganization.status
+          : HTTPStatus.INTERNAL_SERVER_ERROR;
+
+        return res.status(status).json({
+          success: false,
+          message: responseFromCreateOrganization.message,
+          errors,
+        });
+      }
+    } catch (err) {
+      res.status(HTTPStatus.INTERNAL_SERVER_ERROR).json({
+        success: false,
+        message: "Internal Server Error",
+        errors: err.message,
+      });
+    }
+  },
+  update: async (req, res) => {
+    try {
+      const hasErrors = !validationResult(req).isEmpty();
+      if (hasErrors) {
+        let nestedErrors = validationResult(req).errors[0].nestedErrors;
+        return badRequest(
+          res,
+          "bad request errors",
+          manipulateArraysUtil.convertErrorArrayToObject(nestedErrors)
+        );
+      }
+      let { body, query, params } = req;
+      let request = {};
+      request["body"] = body;
+      request["query"] = query;
+      request["params"] = params;
+
+      let responseFromUpdateOrganization = await createOrganizationUtil.update(
+        request
+      );
+
+      if (responseFromUpdateOrganization.success === true) {
+        let status = responseFromUpdateOrganization.status
+          ? responseFromUpdateOrganization.status
+          : HTTPStatus.OK;
+
+        return res.status(status).json({
+          message: responseFromUpdateOrganization.message,
+          data: responseFromUpdateOrganization.data,
+          success: true,
+        });
+      }
+
+      if (responseFromUpdateOrganization.success === false) {
+        let status = responseFromUpdateOrganization.status
+          ? responseFromUpdateOrganization.status
+          : HTTPStatus.INTERNAL_SERVER_ERROR;
+        let errors = responseFromUpdateOrganization.errors
+          ? responseFromUpdateOrganization.errors
+          : "";
+        return res.status(status).json({
+          success: false,
+          message: responseFromUpdateOrganization.message,
+          errors,
+          status,
+        });
+      }
+    } catch (error) {
+      return res.status(HTTPStatus.INTERNAL_SERVER_ERROR).json({
+        success: false,
+        message: "Internal Server Error",
+        errors: error.message,
+      });
+    }
+  },
+  delete: async (req, res) => {
+    try {
+      const hasErrors = !validationResult(req).isEmpty();
+      if (hasErrors) {
+        let nestedErrors = validationResult(req).errors[0].nestedErrors;
+        return badRequest(
+          res,
+          "bad request errors",
+          manipulateArraysUtil.convertErrorArrayToObject(nestedErrors)
+        );
+      }
+
+      let { body, query } = req;
+      let request = {};
+      request["body"] = body;
+      request["query"] = query;
+
+      let responseFromDeleteOrganization = await createOrganizationUtil.delete(
+        request
+      );
+
+      if (responseFromDeleteOrganization.success === true) {
+        let status = responseFromDeleteOrganization.status
+          ? responseFromDeleteOrganization.status
+          : HTTPStatus.OK;
+
+        return res.status(status).json({
+          message: responseFromDeleteOrganization.message,
+          data: responseFromDeleteOrganization.data,
+          success: true,
+        });
+      }
+
+      if (responseFromDeleteOrganization.success === false) {
+        let status = responseFromDeleteOrganization.status
+          ? responseFromDeleteOrganization.status
+          : HTTPStatus.INTERNAL_SERVER_ERROR;
+        let errors = responseFromDeleteOrganization.errors
+          ? responseFromDeleteOrganization.errors
+          : "";
+
+        return res.status(status).json({
+          message: responseFromDeleteOrganization.message,
+          errors,
+          success: false,
+        });
+      }
+    } catch (error) {
+      return res.status(HTTPStatus.INTERNAL_SERVER_ERROR).json({
+        message: "Internal Server Error",
+        errors: error.message,
+        success: false,
+      });
+    }
+  },
+  list: async (req, res) => {
+    try {
+      const hasErrors = !validationResult(req).isEmpty();
+      if (hasErrors) {
+        let nestedErrors = validationResult(req).errors[0].nestedErrors;
+        return badRequest(
+          res,
+          "bad request errors",
+          manipulateArraysUtil.convertErrorArrayToObject(nestedErrors)
+        );
+      }
+      let { body, query } = req;
+      let request = {};
+      request["body"] = body;
+      request["query"] = query;
+
+      let responseFromListOrganizations = await createOrganizationUtil.list(
+        request
+      );
+
+      if (responseFromListOrganizations.success === true) {
+        let status = responseFromListOrganizations.status
+          ? responseFromListOrganizations.status
+          : HTTPStatus.OK;
+
+        return res.status(status).json({
+          success: true,
+          message: responseFromListOrganizations.message,
+          data: responseFromListOrganizations.data,
+        });
+      }
+
+      if (responseFromListOrganizations.success === false) {
+        let status = responseFromListOrganizations.status
+          ? responseFromListOrganizations.status
+          : HTTPStatus.INTERNAL_SERVER_ERROR;
+        let errors = responseFromListOrganizations.errors
+          ? responseFromListOrganizations.errors
+          : "";
+
+        return res.status(status).json({
+          errors,
+          message: responseFromListOrganizations.message,
+        });
+      }
+    } catch (error) {
+      logElement("internal server error", error.message);
+      return res.status(HTTPStatus.INTERNAL_SERVER_ERROR).json({
+        success: false,
+        message: "Internal Server Error",
+        errors: error.message,
+      });
+    }
+  },
+};
+
+module.exports = createOrganization;
