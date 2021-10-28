42448fe5d2a93146bec3da85806b4e6639fe6477
ja-odur <odurjoseph8@gmail.com>
2021-10-28 21:15:46 +0300

    [PLAT-848] add schema for report attributes

--- src/analytics/api/views/schemas/report.py
+++ src/analytics/api/views/schemas/report.py
@@ -1,7 +1,14 @@
 from marshmallow import Schema, fields
 
 
+class ReportAttributeSchema(Schema):
+    title = fields.String(required=True)
+    type = fields.String(required=True)
+    filters = fields.Dict(keys=fields.String())
+    fields = fields.List(fields.String(), required=True)
+
+
 class ReportSchema(Schema):
     name = fields.String(required=True)
     user_id = fields.String(required=True)
-    body = fields.List(fields.String(), required=True)
+    attributes = fields.List(fields.Nested(ReportAttributeSchema), required=True)
