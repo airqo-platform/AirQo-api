4d65018a030ccd90e780c35961700b5b318903ab
ja-odur <odurjoseph8@gmail.com>
2021-11-03 10:26:46 +0300

    [PLAT-847] rename marshmallow field to avoid name collision

--- src/analytics/api/views/schemas/report.py
+++ src/analytics/api/views/schemas/report.py
@@ -1,16 +1,16 @@
-from marshmallow import Schema, fields
+from marshmallow import Schema, fields as marshmallow_fields
 
 
 class ReportAttributeSchema(Schema):
-    title = fields.String(required=True)
-    type = fields.String(required=True)
-    asset = fields.String(required=True)
-    filters = fields.Dict(keys=fields.String())
-    fields = fields.Dict(keys=fields.String(), required=True)
-    group_by = fields.String()
+    title = marshmallow_fields.String(required=True)
+    type = marshmallow_fields.String(required=True)
+    asset = marshmallow_fields.String(required=True)
+    filters = marshmallow_fields.Dict(keys=marshmallow_fields.String())
+    fields = marshmallow_fields.Dict(keys=marshmallow_fields.String(), required=True)
+    group_by = marshmallow_fields.String()
 
 
 class ReportSchema(Schema):
-    name = fields.String(required=True)
-    user_id = fields.String(required=True)
-    attributes = fields.List(fields.Nested(ReportAttributeSchema), required=True)
+    name = marshmallow_fields.String(required=True)
+    user_id = marshmallow_fields.String(required=True)
+    attributes = marshmallow_fields.List(marshmallow_fields.Nested(ReportAttributeSchema), required=True)
