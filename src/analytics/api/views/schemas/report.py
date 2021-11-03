f4c2a754050d697353dc41939c707189bb9a8a72
ja-odur <odurjoseph8@gmail.com>
2021-11-03 22:10:45 +0300

    [PLAT-847] add report period schema

--- src/analytics/api/views/schemas/report.py
+++ src/analytics/api/views/schemas/report.py
@@ -10,7 +10,17 @@ class ReportAttributeSchema(Schema):
     group_by = marshmallow_fields.String()
 
 
+class ReportPeriodSchema(Schema):
+    is_relative = marshmallow_fields.Boolean(required=True)
+    value = marshmallow_fields.Int()
+    unit = marshmallow_fields.String()
+    start_date = marshmallow_fields.DateTime()
+    end_date = marshmallow_fields.DateTime()
+    str_rep = marshmallow_fields.String()
+
+
 class ReportSchema(Schema):
     name = marshmallow_fields.String(required=True)
     user_id = marshmallow_fields.String(required=True)
+    period = marshmallow_fields.Nested(ReportPeriodSchema, required=True)
     attributes = marshmallow_fields.List(marshmallow_fields.Nested(ReportAttributeSchema), required=True)
