6cbaf396b1576b17701179dfaa6e8def26692a33
ja-odur <odurjoseph8@gmail.com>
2021-11-05 00:13:31 +0300

    [PLAT-854] add filtering by site on daily averages

--- src/analytics/api/views/dashboard.py
+++ src/analytics/api/views/dashboard.py
@@ -163,17 +163,20 @@ class MonitoringSiteResource(Resource):
 class DailyAveragesResource(Resource):
 
     @swag_from('/api/docs/dashboard/device_daily_measurements_get.yml')
-    @validate_request_json('pollutant|required:str', 'startDate|required:datetime', 'endDate|required:datetime')
+    @validate_request_json(
+        'pollutant|required:str', 'startDate|required:datetime',
+        'endDate|required:datetime', 'sites|optional:list')
     def post(self):
         tenant = request.args.get('tenant')
         json_data = request.get_json()
         pollutant = json_data["pollutant"]
         start_date = json_data["startDate"]
         end_date = json_data["endDate"]
+        sites = json_data.get("sites", None)
 
         events_model = EventsModel(tenant)
         site_model = SiteModel(tenant)
-        sites = site_model.get_sites()
+        sites = site_model.get_sites(sites)
         data = events_model.get_averages_by_pollutant(start_date, end_date, pollutant)
 
         values = []
@@ -228,7 +231,7 @@ class ExceedancesResource(Resource):
 
         exc_model = ExceedanceModel(tenant)
 
-        data = exc_model.get_exceedances(start_date, end_date, pollutant, standard)
+        print('data', data)
 
         return create_response(
             "exceedance data successfully fetched",
