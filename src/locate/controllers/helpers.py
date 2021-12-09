b56c5a5bed4e5156c26b94d84c3046471d1fd532
danielogen <danielogen@gmail.com>
2021-12-09 18:23:40 +0300

    update helpers

--- src/locate/controllers/helpers.py
+++ src/locate/controllers/helpers.py
@@ -1,30 +1,22 @@
 from flask import Blueprint, request, jsonify
 import logging
-import datetime as dt
-from helpers.db_helpers import app_configuration
-from bson import json_util, ObjectId
-import json
-from datetime import datetime, timedelta
-from helpers import db_helpers
-from routes import api
-import os
-from dotenv import load_dotenv
-load_dotenv()
+import routes
+from config import environment
 
 
 _logger = logging.getLogger(__name__)
 monitor_bp = Blueprint('monitor_bp', __name__)
 
 
-@monitor_bp.route(api.route['root'], methods=['GET', 'POST'])
+@monitor_bp.route(routes.ROOT, methods=['GET', 'POST'])
 def root():
     if request.method == 'GET':
         _logger.info('root endpoint OK')
         return jsonify({"message": "ok", "status": True}), 200
 
 
-@monitor_bp.route(api.route['health_check'], methods=['GET', 'POST'])
+@monitor_bp.route(routes.HEALTH_CHECK, methods=['GET', 'POST'])
 def health():
     if request.method == 'GET':
         _logger.info('health status OK')
-        return jsonify({"message": f'health check passed. DB_URL: {app_configuration.MONGO_URI}, DB_NAME: {app_configuration.DB_NAME}', "status": True}), 200
+        return jsonify({"message": f'health check passed. ENVIROMNET: {environment}', "status": True}), 200
