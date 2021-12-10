d1cb0aaf56d39c3283f32c64af102d6ed84180a0
danielogen <danielogen@gmail.com>
2021-12-10 16:08:49 +0300

    using f string

--- src/predict/routes/api.py
+++ src/predict/routes/api.py
@@ -3,11 +3,11 @@ base_url_v2 = '/api/v2'
 route = {
     'root': '/',
     'health_check': '/health',
-    'get_coordinates': base_url + '/coordinates',
-    'averages_training':base_url + '/train',
-    'averages_prediction': base_url + '/predict/',
-    'predict_channel_next_24hrs' : base_url+ '/channel/predict/',
-    'next_24hr_predictions': base_url_v2 + '/predict/<int:device_channel_id>/<prediction_start_time>',
-    'predict_for_heatmap' : base_url+'/predict/heatmap'
+    'get_coordinates': f'{base_url}/coordinates',
+    'averages_training': f'{base_url}/train',
+    'averages_prediction': f'{base_url}/predict/',
+    'predict_channel_next_24hrs' : f'{base_url}/channel/predict/',
+    'next_24hr_predictions': f'{base_url_v2}/predict/<int:device_channel_id>/<prediction_start_time>',
+    'predict_for_heatmap' : f'{base_url}/predict/heatmap'
 
 }
