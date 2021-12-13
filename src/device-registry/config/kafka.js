58ecc2690b4f20bda2823416b3b0e735e0bcde31
Martin <bbaalmart@gmail.com>
2021-12-13 10:33:46 +0300

    error handling for the kafka configuration

--- src/device-registry/config/kafka.js
+++ src/device-registry/config/kafka.js
@@ -1,35 +1,39 @@
 const kafka = require("kafka-node");
 const { logObject, logElement, logText } = require("../utils/log");
-
 const constants = require("./constants");
-const KAFKA_BOOTSTRAP_SERVERS = constants.KAFKA_BOOTSTRAP_SERVERS;
+const { KAFKA_BOOTSTRAP_SERVERS } = constants;
+const log4js = require("log4js");
+const logger = log4js.getLogger("kafka-config");
 
 logElement("Kafka Bootstrap Servers", KAFKA_BOOTSTRAP_SERVERS);
 
-const Producer = kafka.Producer;
-const client = new kafka.KafkaClient({
-  kafkaHost: KAFKA_BOOTSTRAP_SERVERS,
-  sessionTimeout: 300,
-  spinDelay: 100,
-  retries: 2,
-});
-const producer = new Producer(client);
-
-const sendMessagesToTopics = (payloads) => {
-  producer.on("ready", () => {
-    producer.send(payloads, (err, data) => {
-      logObject("Kafka producer data", data);
-      logObject("Kafka producer error", err);
-    });
+try {
+  const kafkaClient = new kafka.KafkaClient({
+    kafkaHost: KAFKA_BOOTSTRAP_SERVERS,
+    sessionTimeout: 300,
+    spinDelay: 100,
+    retries: 2,
   });
 
-  producer.on("event", (err) => {
+  kafkaClient.on("error", function(error) {
+    logObject("Kafka connection error", error);
+    logger.error(`kafka connection error -- ${error}`);
+  });
+
+  const Producer = kafka.Producer;
+  const kafkaProducer = new Producer(kafkaClient);
+
+  kafkaProducer.on("ready", () => {
+    logger.info(`kafka producer ready to push topic`);
+  });
+
+  kafkaProducer.on("event", (err) => {
     logObject("kafka producer error", err);
+    logger.error(`kafka producer error -- ${err}`);
   });
-};
 
-client.on("error", function(error) {
-  logObject("Kafka connection error", error);
-});
-
-module.exports = { client, sendMessagesToTopics };
+  module.exports = { kafkaClient, kafkaProducer };
+} catch (error) {
+  logObject("the error on Kafka connection", error.message);
+  logger.error(`kafka config exception, ${error}`);
+}
