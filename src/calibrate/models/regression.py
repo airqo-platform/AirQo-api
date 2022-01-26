bc6c09c51732235967c3ada604cdb485f3d5348b
Priscilla-AL <priscilla@airqo.net>
2022-01-26 10:51:39 +0300

    Update regression.py

--- src/calibrate/models/regression.py
+++ src/calibrate/models/regression.py
@@ -61,52 +61,54 @@ class Regression():
         return calibrated_pm2_5, calibrated_pm10
 
 
-def get_and_clean_ext_data(pm2_5,s2_pm2_5,pm10,s2_pm10,temperature,humidity, datetime, reference_data):
-    datetime = pd.to_datetime(datetime)
-    ext_data = pd.DataFrame([[pm2_5,s2_pm2_5,pm10,s2_pm10,temperature,humidity,datetime,reference_data]],
-                                    columns=['pm2_5','s2_pm2_5','pm10','s2_pm10','temperature','humidity','hour', 'datetime','reference_data'],
-                                    dtype='float',
-                                    index=['input'])
-    # filter outliers
-    ext_data = ext_data[(ext_data['avg_pm2_5'] > 0)&(ext_data['avg_pm2_5'] <= 500.4)]
-    ext_data = ext_data[(ext_data['avg_pm10'] > 0)&(ext_data['avg_pm10'] <= 500.4)]
-    ext_data = ext_data[(ext_data['reference_data'] > 0)&(ext_data['reference_data'] <= 500.4)]
-    ext_data = ext_data[(ext_data['temperature'] >= 0)&(ext_data ['temperature'] <= 30)]
-    ext_data = ext_data[(ext_data['humidity'] >= 0)&(ext_data['humidity'] <= 100)]
-    
-    ext_data.drop_duplicates(subset="datetime", keep='first', inplace=True)
-    ext_data = ext_data.set_index('datetime')
-    ext_data = ext_data.drop(['datetime'], axis=1)
+    def get_and_clean_ext_data(pm2_5,s2_pm2_5,pm10,s2_pm10,temperature,humidity, datetime, reference_data):
+        datetime = pd.to_datetime(datetime)
+        ext_data = pd.DataFrame([[pm2_5,s2_pm2_5,pm10,s2_pm10,temperature,humidity,datetime,reference_data]],
+                                        columns=['pm2_5','s2_pm2_5','pm10','s2_pm10','temperature','humidity','hour', 'datetime','reference_data'],
+                                        dtype='float',
+                                        index=['input'])
+        # filter outliers
+        ext_data = ext_data[(ext_data['avg_pm2_5'] > 0)&(ext_data['avg_pm2_5'] <= 500.4)]
+        ext_data = ext_data[(ext_data['avg_pm10'] > 0)&(ext_data['avg_pm10'] <= 500.4)]
+        ext_data = ext_data[(ext_data['reference_data'] > 0)&(ext_data['reference_data'] <= 500.4)]
+        ext_data = ext_data[(ext_data['temperature'] >= 0)&(ext_data ['temperature'] <= 30)]
+        ext_data = ext_data[(ext_data['humidity'] >= 0)&(ext_data['humidity'] <= 100)]
+        
+        ext_data.drop_duplicates(subset="datetime", keep='first', inplace=True)
+        ext_data = ext_data.set_index('datetime')
+        ext_data = ext_data.drop(['datetime'], axis=1)
 
-    ext_data = ext_data.resample('H').mean().round(2)
+        ext_data = ext_data.resample('H').mean().round(2)
 
-    ext_data['Average_PM2.5'] = ext_data[['pm2_5', 's2_pm2_5']].mean(axis=1).round(2)
-    ext_data['Average_PM10'] = ext_data[['pm10', 's2_pm10']].mean(axis=1).round(2)
-    
-    ext_data=ext_data[(ext_data['Average_PM2.5'].notnull())&(ext_data['Average_PM10'].notnull())&
-                                                  (ext_data['reference_data'].notnull())].reset_index(drop=True)
-                           
-    ext_data.fillna(method='ffill',inplace = True)
-    return  ext_data
+        ext_data['Average_PM2.5'] = ext_data[['pm2_5', 's2_pm2_5']].mean(axis=1).round(2)
+        ext_data['Average_PM10'] = ext_data[['pm10', 's2_pm10']].mean(axis=1).round(2)
+        
+        ext_data=ext_data[(ext_data['Average_PM2.5'].notnull())&(ext_data['Average_PM10'].notnull())&
+                                                    (ext_data['reference_data'].notnull())].reset_index(drop=True)
+                            
+        ext_data.fillna(method='ffill',inplace = True)
+        return  ext_data
 
-def extract_features(ext_data):  
-    # extract hour feature
-    ext_data['hour'] = ext_data['Time'].dt.hour
+    def extract_features(ext_data):  
+        # extract hour feature
+        ext_data['hour'] = ext_data['Time'].dt.hour
 
-    # Features from PM
-    # 1)"error_pm2_5" the absolute value of the difference between the two sensor values for pm2_5.
-    # 2)"error_pm10","check_symbol_pm10" same as 3 and 4 but for pm10.
-    # 3)"pm2.5-pm10" the difference between "Average_PM2.5" and "Average_PM10" columns
-    # 4)"pm2 5-pm10_%" ratio of "pm2.5-pm10" relative to "Average_PM10"
+        # Features from PM
+        # 1)"error_pm2_5" the absolute value of the difference between the two sensor values for pm2_5.
+        # 2)"error_pm10","check_symbol_pm10" same as 3 and 4 but for pm10.
+        # 3)"pm2.5-pm10" the difference between "Average_PM2.5" and "Average_PM10" columns
+        # 4)"pm2 5-pm10_%" ratio of "pm2.5-pm10" relative to "Average_PM10"
 
-    ext_data["s2_pm2_5"]=np.where(ext_data["s2_pm2_5"]==0,ext_data["pm2_5"],ext_data["s2_pm2_5"])
-    ext_data["s2_pm10"]=np.where(ext_data["s2_pm10"]==0,ext_data["pm10"],ext_data["s2_pm10"])
-    ext_data["error_pm10"]=np.abs(ext_data["pm10"]-ext_data["s2_pm10"])
-    ext_data["error_pm2_5"]=np.abs(ext_data["pm2_5"]-ext_data["s2_pm2_5"])
-    ext_data["pm2_5_pm10"]=ext_data["Average_PM2.5"]-ext_data["Average_PM10"]
-    ext_data["pm2_5_pm10_mod"]=ext_data["pm2_5_pm10"]/ext_data["Average_PM10"]
+        ext_data["s2_pm2_5"]=np.where(ext_data["s2_pm2_5"]==0,ext_data["pm2_5"],ext_data["s2_pm2_5"])
+        ext_data["s2_pm10"]=np.where(ext_data["s2_pm10"]==0,ext_data["pm10"],ext_data["s2_pm10"])
+        ext_data["error_pm10"]=np.abs(ext_data["pm10"]-ext_data["s2_pm10"])
+        ext_data["error_pm2_5"]=np.abs(ext_data["pm2_5"]-ext_data["s2_pm2_5"])
+        ext_data["pm2_5_pm10"]=ext_data["Average_PM2.5"]-ext_data["Average_PM10"]
+        ext_data["pm2_5_pm10_mod"]=ext_data["pm2_5_pm10"]/ext_data["Average_PM10"]
 
-    return combined_ext_data
+        combined_ext_data = ext_data
+
+        return combined_ext_data
                
     
 if __name__ == "__main__":
