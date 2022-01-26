e5600c03aa63a85406a2bf07ef0813888da74fff
Priscilla-AL <priscilla@airqo.net>
2022-01-26 10:24:16 +0300

    Update calibrate_tool.py

--- src/calibrate/jobs/calibrate_tool.py
+++ src/calibrate/jobs/calibrate_tool.py
@@ -19,8 +19,8 @@ load_dotenv(dotenv_path)
 CREDENTIALS = os.getenv('GOOGLE_APPLICATION_CREDENTIALS')
 client = bigquery.Client.from_service_account_json(CREDENTIALS)
 
-# RF_REG_MODEL = os.getenv('RF_REG_MODEL', 'jobs/rf_reg_model.pkl')
-# LASSO_MODEL = os.getenv('LASSO_MODEL', 'jobs/lasso_model.pkl')
+RF_REG_MODEL = os.getenv('RF_REG_MODEL', 'jobs/rf_reg_model.pkl')
+LASSO_MODEL = os.getenv('LASSO_MODEL', 'jobs/lasso_model.pkl')
 
 def get_and_clean_ext_data(pm2_5,s2_pm2_5,pm10,s2_pm10,temperature,humidity, datetime, reference_data):
     datetime = pd.to_datetime(datetime)
@@ -50,10 +50,9 @@ def get_and_clean_ext_data(pm2_5,s2_pm2_5,pm10,s2_pm10,temperature,humidity, dat
     ext_data.fillna(method='ffill',inplace = True)
     return  ext_data
 
-
-def get_and_clean_ext_data(ext_data):  
+def extract_features(ext_data):  
     # extract hour feature
-    hourly_combined_dataset['hour'] = hourly_combined_dataset['Time'].dt.hour
+    ext_data['hour'] = ext_data['Time'].dt.hour
 
     # Features from PM
     # 1)"error_pm2_5" the absolute value of the difference between the two sensor values for pm2_5.
@@ -61,25 +60,23 @@ def get_and_clean_ext_data(ext_data):
     # 3)"pm2.5-pm10" the difference between "Average_PM2.5" and "Average_PM10" columns
     # 4)"pm2 5-pm10_%" ratio of "pm2.5-pm10" relative to "Average_PM10"
 
-    hourly_combined_dataset["s2_pm2_5"]=np.where(hourly_combined_dataset["s2_pm2_5"]==0,hourly_combined_dataset["pm2_5"],hourly_combined_dataset["s2_pm2_5"])
-    hourly_combined_dataset["s2_pm10"]=np.where(hourly_combined_dataset["s2_pm10"]==0,hourly_combined_dataset["pm10"],hourly_combined_dataset["s2_pm10"])
-    hourly_combined_dataset["error_pm10"]=np.abs(hourly_combined_dataset["pm10"]-hourly_combined_dataset["s2_pm10"])
-    hourly_combined_dataset["error_pm2_5"]=np.abs(hourly_combined_dataset["pm2_5"]-hourly_combined_dataset["s2_pm2_5"])
-    hourly_combined_dataset["pm2_5_pm10"]=hourly_combined_dataset["avg_pm2_5"]-hourly_combined_dataset["avg_pm10"]
-    hourly_combined_dataset["pm2_5_pm10_mod"]=hourly_combined_dataset["pm2_5_pm10"]/hourly_combined_dataset["avg_pm10"]
+    ext_data["s2_pm2_5"]=np.where(ext_data["s2_pm2_5"]==0,ext_data["pm2_5"],ext_data["s2_pm2_5"])
+    ext_data["s2_pm10"]=np.where(ext_data["s2_pm10"]==0,ext_data["pm10"],ext_data["s2_pm10"])
+    ext_data["error_pm10"]=np.abs(ext_data["pm10"]-ext_data["s2_pm10"])
+    ext_data["error_pm2_5"]=np.abs(ext_data["pm2_5"]-ext_data["s2_pm2_5"])
+    ext_data["pm2_5_pm10"]=ext_data["Average_PM2.5"]-ext_data["Average_PM10"]
+    ext_data["pm2_5_pm10_mod"]=ext_data["pm2_5_pm10"]/ext_data["Average_PM10"]
 
-    # hourly_combined_dataset.to_csv('hourly_combined_dataset.csv')
-    
-    return hourly_combined_dataset
+    return combined_ext_data
 
 # def save_trained_model(trained_model,project_name,bucket_name,source_blob_name):
 #     fs = gcsfs.GCSFileSystem(project=project_name)    
 #     with fs.open(bucket_name + '/' + source_blob_name, 'wb') as handle:
 #         job = joblib.dump(trained_model,handle)
 
-def random_forest(hourly_combined_dataset):
-    X= hourly_combined_dataset[['avg_pm2_5','avg_pm10','temperature','humidity','hour','error_pm2_5','error_pm10','pm2_5_pm10', 'pm2_5_pm10_mod']].values
-    y = hourly_combined_dataset['bam_pm'].values    
+def random_forest(combined_ext_data):
+    X= combined_ext_data[['Average_PM2.5','Average_PM10','temperature','humidity','hour','error_pm2_5','error_pm10','pm2_5_pm10', 'pm2_5_pm10_mod']].values
+    y = combined_ext_data['reference_data'].values    
 
     rf_regressor = RandomForestRegressor(random_state=42, max_features='sqrt', n_estimators= 1000, max_depth=50, bootstrap = True)
     # Fitting the model 
@@ -94,65 +91,9 @@ def random_forest(hourly_combined_dataset):
     
     return rf_regressor
 
-def get_clean_data_PM10():
-    sql = """
-    SELECT 
-        TimeStamp, AQ_G501_PM10,AQ_G501_PM2_5,AQ_G501_Sensor_I__PM10,AQ_G501_Sensor_II__PM10,
-        AQ_G501_Sensor_I__PM2_5,AQ_G501_Sensor_II__PM2_5,MUK_BAM_Y24516__PM10,MUK_BAM_Y24516__AT_C,
-        MUK_BAM_Y24516__RH
-    FROM 
-        `airqo-250220.thingspeak.collocation_data_PM10`
-    GROUP BY 
-        TimeStamp, AQ_G501_PM10,AQ_G501_PM2_5,AQ_G501_Sensor_I__PM10,AQ_G501_Sensor_II__PM10,
-        AQ_G501_Sensor_I__PM2_5,AQ_G501_Sensor_II__PM2_5,MUK_BAM_Y24516__PM10,MUK_BAM_Y24516__AT_C,
-        MUK_BAM_Y24516__RH
-    ORDER BY 
-        TimeStamp
-        """
-    dataset = client.query(sql).to_dataframe()
-
-    # Remove outliers
-    dataset  = dataset[(dataset['AQ_G501_PM10'] >= 0)&(dataset['AQ_G501_PM10'] <= 500.4)]
-    dataset  = dataset[(dataset['AQ_G501_PM2_5'] >= 0)&(dataset['AQ_G501_PM2_5'] <= 500.4)]
-    dataset  = dataset[(dataset['AQ_G501_Sensor_I__PM10'] >= 0)&(dataset['AQ_G501_Sensor_I__PM10'] <= 500.4)]
-    dataset  = dataset[(dataset['AQ_G501_Sensor_II__PM10'] >= 0)&(dataset['AQ_G501_Sensor_II__PM10'] <= 500.4)]
-    dataset  = dataset[(dataset['AQ_G501_Sensor_I__PM2_5'] >= 0)&(dataset['AQ_G501_Sensor_I__PM2_5'] <= 500.4)]
-    dataset  = dataset[(dataset['AQ_G501_Sensor_II__PM2_5'] >= 0)&(dataset['AQ_G501_Sensor_II__PM2_5'] <= 500.4)]
-
-
-    dataset  = dataset[(dataset['MUK_BAM_Y24516__PM10'] >= 0)&(dataset['MUK_BAM_Y24516__PM10'] <= 500.4)]
-    dataset  = dataset[(dataset['MUK_BAM_Y24516__AT_C'] >= 0)&(dataset['MUK_BAM_Y24516__AT_C'] <=45)]
-    dataset  = dataset[(dataset['MUK_BAM_Y24516__RH'] >= 0)&(dataset['MUK_BAM_Y24516__RH'] <= 99)]
-    
-    # BAM 1 hr ahead (SET to ENDING)
-    dataset['MUK_BAM_Y24516__PM10']=dataset['MUK_BAM_Y24516__PM10'].shift(-1) 
-
-    #fill na values
-    dataset.fillna(method='ffill',inplace = True)
-    dataset.fillna(method='bfill',inplace = True) 
-
-    #FEATURES
-    # extract hour
-    dataset['hour'] =  dataset['TimeStamp'].dt.hour
-
-    # 1)"Average_PM2.5" is the average of the value of pm2_5 from both sensors, the second sensor "Sensor2PM2.5_CF_1_ug/m3" values has some and it was removed and replaced with value of "pm2_5" for same datasetpoints.
-    # 2)"Average_PM10" is the same as "Average_PM2.5" but for "pm_10"
-    # 3)"error_pm2_5" the absolute value of the difference between the two sensor values for pm2_5.
-    # 5)"error_pm10","check_symbol_pm10" same as 3 and 4 but for pm10.
-    # 6)"pm2.5-pm10" the difference between "Average_PM2.5" and "Average_PM10" columns
-    # 7)"pm2 5-pm10_%" ratio of "pm2.5-pm10" relative to "Average_PM10"
-
-    dataset["AQ_G501_Sensor_II__PM2_5"]=np.where(dataset["AQ_G501_Sensor_II__PM2_5"]==0,dataset["AQ_G501_Sensor_I__PM2_5"],dataset["AQ_G501_Sensor_II__PM2_5"])
-    dataset["AQ_G501_Sensor_II__PM10"]=np.where(dataset["AQ_G501_Sensor_II__PM10"]==0,dataset["AQ_G501_Sensor_I__PM10"],dataset["AQ_G501_Sensor_II__PM10"])
-    dataset["error_pm10"]=np.abs(dataset["AQ_G501_Sensor_I__PM10"]-dataset["AQ_G501_Sensor_II__PM10"])
-    dataset["error_pm2_5"]=np.abs(dataset["AQ_G501_Sensor_I__PM2_5"]-dataset["AQ_G501_Sensor_II__PM2_5"])
-    dataset["pm2.5-pm10"]=dataset["AQ_G501_PM2_5"]-dataset["AQ_G501_PM10"]
-    dataset["pm2 5-pm10_%"]=dataset["pm2.5-pm10"]/dataset["AQ_G501_PM10"]
-    return  dataset
-
-def lasso_reg(dataset):
-    X = dataset[['AQ_G501_PM2_5','AQ_G501_PM10','MUK_BAM_Y24516__AT_C', 'MUK_BAM_Y24516__RH','hour','error_pm10', 'error_pm2_5', 'pm2.5-pm10', 'pm2 5-pm10_%']].values
-    y = dataset['MUK_BAM_Y24516__PM10'].values  
+def lasso_reg(combined_ext_data):
+    X= combined_ext_data[['Average_PM2.5','Average_PM10','temperature','humidity','hour','error_pm2_5','error_pm10','pm2_5_pm10', 'pm2_5_pm10_mod']].values
+    y = combined_ext_data['reference_data'].values   
 
    # Fitting the model 
     lasso_regressor = Lasso(random_state=0).fit(X, y)
@@ -165,9 +106,7 @@ def lasso_reg(dataset):
     return lasso_regressor
 
 if __name__ == "__main__":
-    lowcost_hourly_mean = get_lowcost_data()
-    bam_hourly_mean = get_bam_data()
-    hourly_combined_dataset = combine_datasets(lowcost_hourly_mean, bam_hourly_mean)
-    rf_regressor = random_forest(hourly_combined_dataset)
-    dataset = get_clean_data_PM10()
-    lasso_regressor = lasso_reg(dataset)
+    ext_data = get_and_clean_ext_data(pm2_5,s2_pm2_5,pm10,s2_pm10,temperature,humidity, datetime, reference_data)
+    combined_ext_data = extract_features(ext_data)
+    rf_regressor = random_forest(combined_ext_data)
+    lasso_regressor = lasso_reg(combined_ext_data)
