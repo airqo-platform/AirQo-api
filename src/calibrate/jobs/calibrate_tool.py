de66c4e5ef0cdc8a6c39b8e1b9a5439779a7d947
Priscilla-AL <priscilla@airqo.net>
2022-01-26 18:08:37 +0300

    Update calibrate_tool.py

--- src/calibrate/jobs/calibrate_tool.py
+++ src/calibrate/jobs/calibrate_tool.py
@@ -7,7 +7,6 @@ from sklearn.linear_model import Lasso
 import pickle
 import gcsfs
 import joblib
-
 import os
 from dotenv import load_dotenv
 from pathlib import Path
@@ -21,46 +20,45 @@ client = bigquery.Client.from_service_account_json(CREDENTIALS)
 
 RF_REG_MODEL = os.getenv('RF_REG_MODEL', 'jobs/rf_reg_model.pkl')
 LASSO_MODEL = os.getenv('LASSO_MODEL', 'jobs/lasso_model.pkl')
-combined_ext_data = 
+
 
 # def save_trained_model(trained_model,project_name,bucket_name,source_blob_name):
 #     fs = gcsfs.GCSFileSystem(project=project_name)    
 #     with fs.open(bucket_name + '/' + source_blob_name, 'wb') as handle:
 #         job = joblib.dump(trained_model,handle)
 
-def random_forest(combined_ext_data):
-    X= combined_ext_data[['Average_PM2.5','Average_PM10','temperature','humidity','hour','error_pm2_5','error_pm10','pm2_5_pm10', 'pm2_5_pm10_mod']].values
-    y = combined_ext_data['reference_data'].values    
+class Regression():
 
-    rf_regressor = RandomForestRegressor(random_state=42, max_features='sqrt', n_estimators= 1000, max_depth=50, bootstrap = True)
-    # Fitting the model 
-    rf_regressor = rf_regressor.fit(X, y) 
-    # save the model to disk
-    filename = RF_REG_MODEL
-    pickle.dump(rf_regressor, open(filename, 'wb'))
+    def random_forest(combined_ext_data):
+        X= combined_ext_data[['Average_PM2.5','Average_PM10','temperature','humidity','hour','error_pm2_5','error_pm10','pm2_5_pm10', 'pm2_5_pm10_mod']].values
+        y = combined_ext_data['reference_data'].values    
 
-    ##dump the model to google cloud storage.
-    #save_trained_model(rf_regressor,'airqo-250220','airqo_prediction_bucket', 'PM2.5_calibrate_model.pkl')
+        rf_regressor = RandomForestRegressor(random_state=42, max_features='sqrt', n_estimators= 1000, max_depth=50, bootstrap = True)
+        # Fitting the model 
+        rf_regressor = rf_regressor.fit(X, y) 
+        # save the model to disk
+        filename = RF_REG_MODEL
+        pickle.dump(rf_regressor, open(filename, 'wb'))
 
-    
-    return rf_regressor
+        ##dump the model to google cloud storage.
+        #save_trained_model(rf_regressor,'airqo-250220','airqo_prediction_bucket', 'PM2.5_calibrate_model.pkl')
 
-def lasso_reg(combined_ext_data):
-    X= combined_ext_data[['Average_PM2.5','Average_PM10','temperature','humidity','hour','error_pm2_5','error_pm10','pm2_5_pm10', 'pm2_5_pm10_mod']].values
-    y = combined_ext_data['reference_data'].values   
+        
+        return rf_regressor
 
-    # Fitting the model 
-    lasso_regressor = Lasso(random_state=0).fit(X, y)
-    # save the model to disk
-    filename = LASSO_MODEL
-    pickle.dump(lasso_regressor, open(filename, 'wb'))
+    def lasso_reg(combined_ext_data):
+        X= combined_ext_data[['Average_PM2.5','Average_PM10','temperature','humidity','hour','error_pm2_5','error_pm10','pm2_5_pm10', 'pm2_5_pm10_mod']].values
+        y = combined_ext_data['reference_data'].values   
 
-    ##dump the model to google cloud storage.
-    #save_trained_model(rf_regressor,'airqo-250220','airqo_prediction_bucket', 'PM2.5_calibrate_model.pkl')
-    return lasso_regressor
+        # Fitting the model 
+        lasso_regressor = Lasso(random_state=0).fit(X, y)
+        # save the model to disk
+        filename = LASSO_MODEL
+        pickle.dump(lasso_regressor, open(filename, 'wb'))
+
+        ##dump the model to google cloud storage.
+        #save_trained_model(rf_regressor,'airqo-250220','airqo_prediction_bucket', 'PM2.5_calibrate_model.pkl')
+        return lasso_regressor
 
 if __name__ == "__main__":
-    # ext_data = get_and_clean_ext_data(pm2_5,s2_pm2_5,pm10,s2_pm10,temperature,humidity, datetime, reference_data)
-    # combined_ext_data = extract_features(ext_data)
-    rf_regressor = random_forest(combined_ext_data)
-    lasso_regressor = lasso_reg(combined_ext_data)
+    calibrateInstance = Regression()
