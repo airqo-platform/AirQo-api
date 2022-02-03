4d1c5d005044d09f36fec91d843472c77c9f30ec
Priscilla-AL <priscilla@airqo.net>
2022-02-03 19:18:53 +0300

    Update api.py

--- src/calibrate/routes/api.py
+++ src/calibrate/routes/api.py
@@ -3,5 +3,5 @@ base_url = '/api/v1'
 route = {
     'root': '/',
     'calibrate': f'{base_url}/calibrate',
-    'calibrate_tool': f'{base_url}/calibrate_tool'
+    'calibrate_tool': f'{base_url}/train_calibrate_tool'
 }
