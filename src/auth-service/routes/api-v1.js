eeca8e132ce384df20c120c3a06ed34947934ec0
Martin <bbaalmart@gmail.com>
2022-02-10 11:44:21 +0300

    review validations for creating defaults and also fix schema with focus on the PERIOD field

--- src/auth-service/routes/api-v1.js
+++ src/auth-service/routes/api-v1.js
@@ -331,25 +331,28 @@ router.post(
   oneOf([
     [
       body("pollutant")
-        .if(body("pollutant").exists())
-        .notEmpty()
+        .exists()
+        .withMessage("pollutant is is missing in your request")
+        .bail()
         .trim()
         .isIn(["no2", "pm2_5", "pm10", "pm1"])
         .withMessage(
           "the pollutant value is not among the expected ones which include: no2, pm2_5, pm10, pm1"
         ),
       body("frequency")
-        .if(body("frequency").exists())
-        .notEmpty()
+        .exists()
+        .withMessage("frequency is is missing in your request")
+        .bail()
         .trim()
         .toLowerCase()
-        .isIn(["daily", "hourly", "monthly"])
+        .isIn(["daily", "hourly", "monthly, diurnal"])
         .withMessage(
           "the frequency value is not among the expected ones which include: daily, hourly and monthly"
         ),
       body("chartType")
-        .if(body("chartType").exists())
-        .notEmpty()
+        .exists()
+        .withMessage("chartType is is missing in your request")
+        .bail()
         .trim()
         .toLowerCase()
         .isIn(["bar", "line", "pie"])
@@ -357,22 +360,25 @@ router.post(
           "the chartType value is not among the expected ones which include: bar, line and pie"
         ),
       body("startDate")
-        .if(body("startDate").exists())
-        .notEmpty()
+        .exists()
+        .withMessage("startDate is is missing in your request")
+        .bail()
         .trim()
         .toDate()
         .isISO8601({ strict: true, strictSeparator: true })
         .withMessage("startDate must be a valid datetime."),
       body("endDate")
-        .if(body("endDate").exists())
-        .notEmpty()
+        .exists()
+        .withMessage("endDate is is missing in your request")
+        .bail()
         .trim()
         .toDate()
         .isISO8601({ strict: true, strictSeparator: true })
         .withMessage("endDate must be a valid datetime."),
       body("user")
-        .if(body("user").exists())
-        .notEmpty()
+        .exists()
+        .withMessage("user is is missing in your request")
+        .bail()
         .trim()
         .isMongoId()
         .withMessage("the user must be an object ID")
@@ -381,8 +387,9 @@ router.post(
           return ObjectId(value);
         }),
       body("airqloud")
-        .if(body("airqloud").exists())
-        .notEmpty()
+        .exists()
+        .withMessage("airqloud is is missing in your request")
+        .bail()
         .trim()
         .isMongoId()
         .withMessage("the airqloud must be an object ID")
@@ -390,30 +397,51 @@ router.post(
         .customSanitizer((value) => {
           return ObjectId(value);
         }),
-      body("chartTitle").if(body("chartTitle").exists()).notEmpty().trim(),
+      body("chartTitle")
+        .exists()
+        .withMessage("chartTitle is is missing in your request")
+        .bail()
+        .trim(),
       body("period")
-        .if(body("period").exists())
-        .notEmpty()
-
+        .exists()
+        .withMessage("period is is missing in your request")
+        .bail()
         .custom((value) => {
           return typeof value === "object";
         })
         .bail()
         .withMessage("the period should be an object"),
+      body("period.unit")
+        .exists()
+        .withMessage("period.unit is is missing in your request"),
+      body("period.unitValue")
+        .exists()
+        .withMessage("period.unitValue is is missing in your request")
+        .isFloat()
+        .withMessage("height must be a number"),
+      body("period.label")
+        .exists()
+        .withMessage("period.label is is missing in your request"),
+      body("period.value")
+        .exists()
+        .withMessage("period.value is is missing in your request"),
       body("chartSubTitle")
-        .if(body("chartSubTitle").exists())
-        .notEmpty()
+        .exists()
+        .withMessage("chartSubTitle is is missing in your request")
+        .bail()
         .trim(),
       body("sites")
-        .if(body("sites").exists())
-        .notEmpty()
+        .exists()
+        .withMessage("sites is is missing in your request")
+        .bail()
         .custom((value) => {
           return Array.isArray(value);
         })
         .withMessage("the sites should be an array"),
       body("sites.*")
-        .if(body("sites.*").exists())
-        .notEmpty()
+        .exists()
+        .withMessage("No Sites included in your request")
+        .bail()
         .trim()
         .isMongoId()
         .withMessage("site must be an object ID"),
