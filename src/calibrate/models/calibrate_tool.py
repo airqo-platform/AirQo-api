a65900a28f42c707c7a9af9d41f95f0dfd00afd5
Priscilla-AL <priscilla@airqo.net>
2022-02-17 13:09:52 +0300

    Update calibrate_tool.py

--- src/calibrate/models/calibrate_tool.py
+++ src/calibrate/models/calibrate_tool.py
@@ -28,25 +28,25 @@ class Regression():
         # features from datetime and PM
         df["datetime"] = pd.to_datetime(df["datetime"])
         # extract hour
-        df['hour'] =  df['Time'].dt.hour
+        df['hour'] =  df['datetime'].dt.hour
     
-        input_variables["avg_pm2_5"] = input_variables[['pm2_5','s2_pm2_5']].mean(axis=1).round(2)
-        input_variables["avg_pm10"] =  input_variables[['pm10','s2_pm10']].mean(axis=1).round(2)
-        input_variables["error_pm10"]=np.abs(input_variables["pm10"]-input_variables["s2_pm10"])
-        input_variables["error_pm2_5"]=np.abs(input_variables["pm2_5"]-input_variables["s2_pm2_5"])
-        input_variables["pm2_5_pm10"]=input_variables["avg_pm2_5"]-input_variables["avg_pm10"]
-        input_variables["pm2_5_pm10_mod"]=input_variables["pm2_5_pm10"]/input_variables["avg_pm10"]
-        input_variables = input_variables.drop(['pm2_5','s2_pm2_5','pm10','s2_pm10'], axis=1)
+        df["avg_pm2_5"] = df[['pm2_5','s2_pm2_5']].mean(axis=1).round(2)
+        df["avg_pm10"] =  df[['pm10','s2_pm10']].mean(axis=1).round(2)
+        df["error_pm10"]=np.abs(df["pm10"]-df["s2_pm10"])
+        df["error_pm2_5"]=np.abs(df["pm2_5"]-df["s2_pm2_5"])
+        df["pm2_5_pm10"]=df["avg_pm2_5"]-df["avg_pm10"]
+        df["pm2_5_pm10_mod"]=df["pm2_5_pm10"]/df["avg_pm10"]
+        df = df.drop(['pm2_5','s2_pm2_5','pm10','s2_pm10'], axis=1)
         
-        input_variables = input_variables[['avg_pm2_5','avg_pm10','temperature','humidity','hour','error_pm2_5','error_pm10','pm2_5_pm10', 'pm2_5_pm10_mod']]
+        df = df[['avg_pm2_5','avg_pm10','temperature','humidity','hour','error_pm2_5','error_pm10','pm2_5_pm10', 'pm2_5_pm10_mod']]
 
         #load model from disk
         rf_regressor = pickle.load(open(RF_REG_MODEL, 'rb'))
         lasso_regressor = pickle.load(open(LASSO_MODEL, 'rb'))
         # # load model from GCP 
         # rf_regressor = self.get_model('airqo-250220','airqo_prediction_bucket', 'PM2.5_calibrate_model.pkl')
-        calibrated_pm2_5 =  rf_regressor.predict(input_variables)[0]
-        calibrated_pm10 =  lasso_regressor.predict(input_variables)[0]  
+        calibrated_pm2_5 =  rf_regressor.predict(df)[0]
+        calibrated_pm10 =  lasso_regressor.predict(df)[0]  
         
         return calibrated_pm2_5, calibrated_pm10
                
