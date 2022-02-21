8942eb6151cbabbdef853d74ec093c691df4a5d1
Priscilla-AL <priscilla@airqo.net>
2022-02-21 17:45:43 +0300

    Update train_calibrate_tool.py

--- src/calibrate/models/train_calibrate_tool.py
+++ src/calibrate/models/train_calibrate_tool.py
@@ -17,10 +17,10 @@ class Train_calibrate_tool():
         The class contains functionality for computing device calibrated values .
     """
 
-    def train_calibration_model(self, pollutant, df):
-        df.rename(columns={'Time':'datetime','Sensor1 PM2.5_CF_1_ug/m3':'pm2_5','Sensor2 PM2.5_CF_1_ug/m3':'s2_pm2_5',
-                                     'Sensor1 PM10_CF_1_ug/m3':'pm10','Sensor2 PM10_CF_1_ug/m3':'s2_pm10',
-                                     'AT(C)':'temperature', 'RH(%)':'humidity', 'ConcHR(ug/m3)':'ref_data'},inplace=True)
+    def train_calibration_model(self, pollutant, map_columns, df):
+       # Map columns from uploaded csv
+        df.rename(columns=map_columns,inplace=True)
+        print("Columns", df.columns)
         
         #Get average PM
         df['Average_PM2.5'] = df[['pm2_5', 's2_pm2_5']].mean(axis=1).round(2)
