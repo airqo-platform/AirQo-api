a1df02095a81e39d6f3472706cef161eb1fa1b9c
baalmart <bbaalmart@gmail.com>
2022-03-07 20:38:18 +0300

    rename event consumer controller

---
+++ src/device-registry/controllers/consume-event.js
@@ -0,0 +1,45 @@
+const { logObject, logElement } = require("../utils/log");
+const { Kafka } = require("kafkajs");
+const { SchemaRegistry } = require("@kafkajs/confluent-schema-registry");
+const constants = require("../config/constants");
+const SCHEMA_REGISTRY = constants.SCHEMA_REGISTRY;
+const BOOTSTRAP_SERVERS = constants.KAFKA_BOOTSTRAP_SERVERS;
+const RAW_MEASUREMENTS_TOPICS = constants.KAFKA_RAW_MEASUREMENTS_TOPICS;
+const KAFKA_CLIENT_ID = constants.KAFKA_CLIENT_ID;
+const KAFKA_CLIENT_GROUP = constants.KAFKA_CLIENT_GROUP;
+
+const kafka = new Kafka({
+  clientId: KAFKA_CLIENT_ID,
+  brokers: [BOOTSTRAP_SERVERS],
+});
+const registry = new SchemaRegistry({ host: SCHEMA_REGISTRY });
+const consumer = kafka.consumer({ groupId: KAFKA_CLIENT_GROUP });
+
+const rawMeasurementsConsumer = async () => {
+  await consumer.connect();
+
+  const topics = RAW_MEASUREMENTS_TOPICS.split(",");
+
+  for (const topic of topics) {
+    await consumer.subscribe({
+      topic: topic.trim().toLowerCase(),
+      fromBeginning: true,
+    });
+  }
+
+  await consumer.run({
+    eachMessage: async ({ topic, partition, message }) => {
+      try {
+        const decodedValue = await registry.decode(message.value);
+        const measurements = decodedValue.measurements;
+        // insertMeasurtements.addValuesArray(measurements);
+      } catch (e) {
+        logObject("Kafka Raw Measurements consumer", e);
+      }
+    },
+  });
+};
+
+rawMeasurementsConsumer().catch(console.error);
+
+module.exports = rawMeasurementsConsumer;
