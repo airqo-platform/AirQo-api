ac830780f61a950a2bd9b9b961304acccea9e270
baalmart <bbaalmart@gmail.com>
2022-03-16 11:10:18 +0300

    handle a runtime error for error handling

--- src/device-registry/app.js
+++ src/device-registry/app.js
@@ -26,13 +26,6 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/api/v1/devices/", apiV1);
 app.use("/api/v2/devices/", apiV2);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error("Not Found");
-  err.status = 404;
-  next(err);
-});
-
 app.use(function(err, req, res, next) {
   logger.error(`${err.message}`);
   if (err.status === 404) {
