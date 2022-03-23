4a0a5d5190cd26f188cb28c4391e82dbb294b34a
danielogen <danielogen@gmail.com>
2022-03-23 17:14:04 +0300

    update configuration file

--- src/predict/jobs/predict/config.py
+++ src/predict/jobs/predict/config.py
@@ -2,6 +2,8 @@ import os
 from pymongo import MongoClient
 from dotenv import load_dotenv
 from pathlib import Path
+from datetime import datetime
+import pandas as pd
 BASE_DIR = Path(__file__).resolve().parent
 dotenv_path = os.path.join(BASE_DIR, '.env')
 load_dotenv(dotenv_path)
@@ -13,6 +15,26 @@ class Config:
     CSRF_ENABLED = True
     CREDENTIALS = os.getenv('GOOGLE_APPLICATION_CREDENTIALS')
     GOOGLE_CLOUD_PROJECT_ID = os.getenv('GOOGLE_CLOUD_PROJECT')
+    # prediction configs
+
+    #load & preprocess test data:
+    METADATA_PATH = 'meta.csv'
+    BOUNDARY_LAYER_PATH = 'boundary_layer.csv'
+
+    #set constants
+    test_start_datetime = datetime.now().strftime('%Y-%m-%d %H')
+    #test_end_datetime = date_to_str(datetime.now() + timedelta(hours=24))
+
+    TEST_DATE_HOUR_START = pd.to_datetime(test_start_datetime)
+
+    ### Prediction will end at this date-hour
+    TEST_DATE_HOUR_END = TEST_DATE_HOUR_START + pd.Timedelta(hours=23)
+    N_HRS_BACK = 24
+    SEQ_LEN = 24
+    ROLLING_SEQ_LEN = 24*90
+    MAX_LAGS = N_HRS_BACK + max(ROLLING_SEQ_LEN, SEQ_LEN) + 48 # Extra 48 or 2 days for safety
+    TEST_LAG_LAST_DATE_HOUR = TEST_DATE_HOUR_START - pd.Timedelta(hours = MAX_LAGS)
+    TARGET_COL = 'pm2_5'
 class ProductionConfig(Config):
     DB_NAME = os.getenv("DB_NAME_PROD")
     MONGO_URI = os.getenv('MONGO_GCE_URI')
@@ -48,3 +70,4 @@ def connect_mongo():
     client = MongoClient(configuration.MONGO_URI)
     db = client[configuration.DB_NAME]
     return db
+
