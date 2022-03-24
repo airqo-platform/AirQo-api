33d8476414519ba732b3d35a76f897597d8d1c65
danielogen <danielogen@gmail.com>
2022-03-24 21:42:07 +0300

    add more configuration details

--- src/predict/jobs/predict/config.py
+++ src/predict/jobs/predict/config.py
@@ -15,6 +15,9 @@ class Config:
     CSRF_ENABLED = True
     CREDENTIALS = os.getenv('GOOGLE_APPLICATION_CREDENTIALS')
     GOOGLE_CLOUD_PROJECT_ID = os.getenv('GOOGLE_CLOUD_PROJECT')
+
+    TENANT = os.getenv('TENANT', 'airqo')
+    MONTHS_OF_DATA = os.getenv('MONTHS_OF_DATA', 2)
     # prediction configs
 
     #load & preprocess test data:
@@ -38,19 +41,30 @@ class Config:
 class ProductionConfig(Config):
     DB_NAME = os.getenv("DB_NAME_PROD")
     MONGO_URI = os.getenv('MONGO_GCE_URI')
+    DB_NAME_DEVICE_REGISTRY = os.getenv("DB_NAME_PROD_DEVICE_REGISTRY")
+    MONGO_URI_DEVICE_REGISTRY = os.getenv('MONGO_GCE_URI_DEVICE_REGISTRY')
     AIRQO_PREDICT_BUCKET = os.getenv('AIRQO_PREDICT_BUCKET_PROD')
+    AIRQO_API_BASE_URL = os.getenv('AIRQO_API_BASE_URL_PROD')
 
 class TestingConfig(Config):
     DEBUG = True
     TESTING = True
     MONGO_URI = os.getenv('MONGO_GCE_URI')
     DB_NAME = os.getenv("DB_NAME_STAGE")
+    DB_NAME_DEVICE_REGISTRY = os.getenv("DB_NAME_STAGE_DEVICE_REGISTRY")
+    MONGO_URI_DEVICE_REGISTRY = os.getenv('MONGO_GCE_URI_DEVICE_REGISTRY')
     AIRQO_PREDICT_BUCKET = os.getenv('AIRQO_PREDICT_BUCKET_STAGE')
+    AIRQO_API_BASE_URL = os.getenv('AIRQO_API_BASE_URL_STAGE')
+
 class DevelopmentConfig(Config):
     DEVELOPMENT = True
     DEBUG = True
     MONGO_URI = os.getenv("MONGO_DEV_URI")
     DB_NAME = os.getenv("DB_NAME_DEV")
+    DB_NAME_DEVICE_REGISTRY = os.getenv("DB_NAME_STAGE_DEVICE_REGISTRY")
+    MONGO_URI_DEVICE_REGISTRY = os.getenv('MONGO_GCE_URI_DEVICE_REGISTRY')
+    AIRQO_PREDICT_BUCKET = os.getenv('AIRQO_PREDICT_BUCKET_DEV')
+    AIRQO_API_BASE_URL = os.getenv('AIRQO_API_BASE_URL_STAGE')
 
 
 app_config = {
@@ -69,5 +83,10 @@ configuration = app_config.get(environment, TestingConfig)
 def connect_mongo():
     client = MongoClient(configuration.MONGO_URI)
     db = client[configuration.DB_NAME]
+    return db
+
+def connect_mongo_device_registry():
+    client = MongoClient(configuration.MONGO_URI_DEVICE_REGISTRY)
+    db = client[f'{configuration.DB_NAME_DEVICE_REGISTRY}_{configuration.TENANT.lower()}']
     return db
 
