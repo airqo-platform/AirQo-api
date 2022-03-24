af2752b1b00af7b214de717ee86970b9022412b5
danielogen <danielogen@gmail.com>
2022-03-24 21:44:12 +0300

    update main file

--- src/predict/jobs/predict/main.py
+++ src/predict/jobs/predict/main.py
@@ -15,20 +15,6 @@ from utils import (
 
 db=connect_mongo()
 
-# def query_prediction_data(test_lag_last_date_hour):
-#     client = bigquery.Client()
-#     sql_query = """SELECT created_at ,channel_id,pm2_5
-#         FROM `airqo-250220.thingspeak.clean_feeds_pms`
-#         WHERE created_at >= CAST({0} AS DATETIME)"""
-#     last_date = "'"+ test_lag_last_date_hour +"'"
-#     sql_query = sql_query.format(last_date)
-#     job_config = bigquery.QueryJobConfig()
-#     job_config.use_legacy_sql = False
-
-#     df = client.query(sql_query,job_config=job_config).to_dataframe()
-#     df['created_at'] =  pd.to_datetime(df['created_at'],format='%Y-%m-%d %H:%M:%S')
-#     return df
-
 def preprocess_forecast_data(metadata_path, boundary_layer_path, test_lag_last_date_hour=""):
 
   # getting metadata
@@ -44,41 +30,17 @@ def preprocess_forecast_data(metadata_path, boundary_layer_path, test_lag_last_d
   forecast_data['date'] = forecast_data['created_at'].dt.date
   forecast_data = forecast_data[forecast_data['device_number'].isin(metadata['device_number'])].reset_index(drop=True)
 
-
-  # forecast_data['created_at'] = pd.to_datetime(forecast_data['created_at'], format='%Y-%m-%d %H:%M:%S')
-  # forecast_data['date'] = forecast_data['created_at'].dt.date
-  # forecast_data = forecast_data[forecast_data['channel_id'].isin(metadata['channel_id'])].reset_index(drop=True)
-  # channel_max_dates = forecast_data.groupby('channel_id').apply(lambda x: x['created_at'].max())
-
-  # ### Set this as a list of chanels to be used. Can be read from a file.
-  # use_channels = channel_max_dates[channel_max_dates > pd.to_datetime('2020-07-12')].index.tolist()
-  # #use_channels =  get_all_static_channels()
-
-  # forecast_data = forecast_data[forecast_data['channel_id'].isin(use_channels)]
-
   return forecast_data, metadata,boundary_layer_mapper
 
 
 def preprocess_metadata(METADATA_PATH):
   metadata = get_csv_file_from_gcs(configuration.GOOGLE_CLOUD_PROJECT_ID, configuration.AIRQO_PREDICT_BUCKET, METADATA_PATH)
-  # fts = [c for c in metadata if c not in ['chan_id']]
-  # cat_fts = metadata[fts].select_dtypes('object').columns.tolist()
-  # metadata[cat_fts] = metadata[cat_fts].apply(lambda x: pd.factorize(x)[0])
-  # metadata = metadata.rename({'chan_id': 'channel_id'}, axis=1)
-  # drop_fts = ['loc_ref', 'chan_ref', 'loc_mob_stat', 'airqo_name', 'district_lookup', 'county_lookup', 'coords', 'loc_start_date', 'loc_end_date', 'gmaps_link',
-  #             'OSM_link', 'nearby_sources', 'geometry', 'geometry_43', 'event_logging_link']
-  # metadata = metadata.drop(drop_fts, axis=1)
-
 
   fts = [c for c in metadata if c not in ['device_number']]
   cat_fts = metadata[fts].select_dtypes('object').columns.tolist()
   metadata[cat_fts] = metadata[cat_fts].apply(lambda x: pd.factorize(x)[0])
-  
-  # this features can be fully eliminated during the refractoring process
-  drop_fts = ['device','location_activities', 'site_name', 'road_intensity', 
-            'land_use', 'traffic_factor', 'road_status', 'region']
-  result = metadata.drop(drop_fts, axis=1)
-  return result
+
+  return metadata
 
 def get_boundary_layer_mapper(BOUNDARY_LAYER_PATH):
   boundary_layer =  get_csv_file_from_gcs(configuration.GOOGLE_CLOUD_PROJECT_ID, configuration.AIRQO_PREDICT_BUCKET, BOUNDARY_LAYER_PATH)
@@ -93,6 +55,7 @@ def preprocess_df(df_tmp, boundary_layer_mapper, metadata, target_column,n_hrs_b
   return df_tmp
 
 def get_lag_features(df_tmp,TARGET_COL,N_HRS_BACK, SEQ_LEN):
+
   df_tmp = df_tmp.sort_values(by='created_at')
   
   ### Shift Features
@@ -121,7 +84,6 @@ def get_lag_features(df_tmp,TARGET_COL,N_HRS_BACK, SEQ_LEN):
   return df_tmp
 
 def get_other_features(df_tmp,boundary_layer_mapper,metadata):
-
   D_COL = 'created_at'
   for attr in ['hour', 'day', 'dayofweek', 'month', 'is_month_start', 'is_month_end', 'week', 'year']:
       df_tmp[f'{D_COL}_{attr}'] = getattr(df_tmp[D_COL].dt, attr)
@@ -153,8 +115,7 @@ def save_next_24hrs_prediction_results(data):
     """
     """
     for i  in data:
-        #db.predictions.insert_one(i)
-        print(i)
+        db.predictions.insert_one(i)
         print('saved')
 
 
@@ -174,26 +135,7 @@ def make_test_forecast_data(forecast_data, test_lag_last_date_hour, test_end_dat
 
 def get_next_24hrs_predictions():
     #load & preprocess test data:
-    # METADATA_PATH = 'meta.csv'
-    # BOUNDARY_LAYER_PATH = 'boundary_layer.csv'
 
-    #set constants
-    # test_start_datetime = datetime.now().strftime('%Y-%m-%d %H')
-    #test_end_datetime = date_to_str(datetime.now() + timedelta(hours=24))
-
-    # TEST_DATE_HOUR_START = pd.to_datetime(test_start_datetime)
-
-    ### Prediction will end at this date-hour
-    # TEST_DATE_HOUR_END = TEST_DATE_HOUR_START + pd.Timedelta(hours=23)
-    # N_HRS_BACK = 24
-    # SEQ_LEN = 24
-    # ROLLING_SEQ_LEN = 24*90
-    # MAX_LAGS = N_HRS_BACK + max(ROLLING_SEQ_LEN, SEQ_LEN) + 48 # Extra 48 or 2 days for safety
-    # TEST_LAG_LAST_DATE_HOUR = TEST_DATE_HOUR_START - pd.Timedelta(hours = MAX_LAGS)
-    # TARGET_COL = 'pm2_5'
-
-    # print(TEST_LAG_LAST_DATE_HOUR)
-    # print(TEST_DATE_HOUR_END)
     print(configuration.TEST_LAG_LAST_DATE_HOUR)
     print(configuration.TEST_DATE_HOUR_END)
 
@@ -212,7 +154,7 @@ def get_next_24hrs_predictions():
 
     test_orig = test[["created_at",  "pm2_5", "device_number"]].copy()
 
-    features = [c for c in test.columns if c not in ["created_at",  "pm2_5"]]
+    features = [c for c in test.columns if c not in ["created_at",  "pm2_5", "Unnamed: 0"]]
     test_preds = clf.predict(test[features])
 
     test_orig['preds'] = test_preds
