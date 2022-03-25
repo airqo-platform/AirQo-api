e01f6696dbd37dcadc199245a2fd0b65eef90377
danielogen <danielogen@gmail.com>
2022-03-25 18:05:36 +0300

    refactor controller

--- src/locate/controllers/locate.py
+++ src/locate/controllers/locate.py
@@ -47,7 +47,7 @@ def place_sensors_map():
             except KeyError as err:
                 return {'message': f'missing parameter: {str(err)}. please refer to API documentation for details', 'success': False}, 400
             except Exception as err:
-                return {'message': f'Some error occurred: {str(err)}', 'success': False}, 400
+                return {'message': f'Some errors occurred: {str(err)}', 'success': False}, 400
             if must_have_coordinates == "":
                 must_have_coordinates = None
                 return helper.recommend_locations(sensor_number, must_have_coordinates, geometry, tenant)
@@ -68,151 +68,231 @@ def place_sensors_map():
         return jsonify({"message": "Invalid request method. Please refer to the API documentation", "success": False}), 400
 
 
-@locate_blueprint.route(routes.SAVE_MAP, methods=['DELETE', 'GET', 'PUT', 'PATCH', 'POST'])
-def save_locate_map():
+@locate_blueprint.route(routes.CREATE_MAP, methods=['DELETE', 'GET', 'PUT', 'PATCH', 'POST'])
+def create_locate_map():
     '''
-    Saves planning space
+    create planning space
     '''
+    errors = {}
+
     if request.method == 'POST':
-        # make sure content type is of type 'json'
-        if request.content_type != 'application/json':
-            return jsonify({"message": "Invalid Content Type", "success": False}),400
+        try:
+            tenant = request.args.get('tenant')
+        except Exception:
+            errors['tenant'] = 'This query param is required.'\
+                               'please specify the organization name.'
 
-        # check that all fields are supplied
-        data = request.json
-        if not all([data.get('user_id'), data.get('space_name'), data.get('plan')]):
-            return jsonify({"message": "missing field/s (user_id, space_name or plan). please provide all required fields", "success": False}), 400
-
-        # make user_id is of type string
-        if type(data.get('user_id')) is not str:
-            return jsonify({"message": "invalid user_id, expects string. please refer to API documentation for details", "success": False}),400
-
-        # if all checks have passed, save planning space
-        user_id = data['user_id']
-        space_name = data['space_name']
-        plan = data['plan']
+        try:
+            id = str(request.json.get('id'))
+        except Exception:
+            errors['id'] = 'This field is required.'\
+                            'please enter a valid str(user id).'
         
-        tenant = request.args.get('tenant')
-        if not tenant:
-            return jsonify({"message": "please specify the organization name. Refer to the API documentation for details.", "success": False}), 400
-        locate_map = Map(tenant)
+        try:
+            name = str(request.json.get('name'))
+        except Exception:
+            errors['name'] = 'This field is required.'\
+                            'please enter a valid str(planning space name).'
+    
+        try:
+            plan = request.json.get('plan')
+        except Exception:
+            errors['plan'] = 'This field is required.'\
+                            'Please check the request body.'
+
+        if request.content_type != 'application/json':
+            errors['content_type'] = "Invalid content type. Please use json"
 
 
         org = f'{configuration.DB_NAME}_{tenant.lower()}'
         if org not in dbs:
-            return jsonify({"message": "organization doesn't exist. Refer to the API documentation for details.", "success": False}), 400
+            errors['org'] = 'organization does not exist.'\
+                            'Refer to the API documentation for details.'
         
-        # check if space name already been taken by the same user. 
-        # avoid duplicated planning space name for the same user
-        if locate_map.plan_space_exist(user_id, space_name) > 0:
-            return jsonify({"message": f'planning space name: {space_name} already exist for user: {user_id}', "success": False}), 400
-
-        locate_map.save_locate_map(user_id, space_name, plan)
-        return jsonify({"message": "Locate Planning Space Saved Successfully", "success": True}), 200
+        
+        if errors:
+            return jsonify({
+                'message': 'Some errors occurred while processing this request',
+                'errors': errors
+            }), 400
+        
+        locate_map = Map(tenant)
+        if locate_map.plan_space_exist(id, name) > 0:
+            return jsonify({'message': f'planning space name: {name} already exist for user: {id}', "success": False}), 400
+        
+        locate_map.create_locate_map(id, name, plan)
+        return jsonify({'message': 'Locate Planning Space Saved Successfully', 'success': True}), 200
     else:
-        return jsonify({"message": "Invalid request method. Please refer to the API documentation", "success": False}), 400
+        return jsonify({'message': 'Invalid request method. Please refer to the API documentation', 'success': False}), 400
 
 
 
 @locate_blueprint.route(routes.GET_MAP, methods=['DELETE', 'GET', 'PUT', 'PATCH', 'POST'])
-def get_locate_map(user_id):
+def get_locate_map():
     '''
-    Get saved planning space for the user
+    Get saved planning space for a specific user
     '''
+    errors = {}
     if request.method == 'GET':
-        tenant = request.args.get('tenant')
-        if not tenant:
-            return jsonify({"message": "please specify the organization name. Refer to the API documentation for details.", "success": False}), 400
-        locate_map = Map(tenant)
-
+        try:
+            tenant = request.args.get('tenant')
+        except Exception:
+            errors['tenant'] = 'This query param is required. '\
+                               'please specify the organization name.'
+        try:
+            id = request.args.get('id')
+        except Exception:
+            errors['id'] = 'This query param is required.'\
+                            'please enter a valid str(user id).'
+                            
         org = f'{configuration.DB_NAME}_{tenant.lower()}'
         if org not in dbs:
-            return jsonify({"message": "organization doesn't exist. Refer to the API documentation for details.", "success": False}), 400
+            errors['org'] = 'organization does not exist.'\
+                            'Refer to the API documentation for details.'
+        
+        if errors:
+            return jsonify({
+                'message': 'Some errors occurred while processing this request',
+                'errors': errors
+            }),400 
 
-        documents = locate_map.get_locate_map(user_id)
+        locate_map = Map(tenant)
+        documents = locate_map.get_locate_map(id)
+       
         response = []
         for document in documents:
             document['_id'] = str(document['_id'])
             response.append(document)
         if len(response) == 0:
-            return jsonify({"message": "no record available. please check the user_id or organization name.", "success": False}), 400
-        return jsonify(response), 200
+            return jsonify({'message': 'No record available. please check the user_id or organization name.', 
+                            'success': False
+                            }), 400
+        else:
+            return jsonify(response), 200
     else:
-        return jsonify({"message": "Invalid request method. please refer to the API documentation", "success": False}), 400
+        return jsonify({'message': 'Invalid request method. please refer to the API documentation', 
+                        'success': False
+                        }), 400
 
 
 @locate_blueprint.route(routes.UPDATE_MAP, methods=['DELETE', 'GET', 'PUT', 'PATCH', 'POST'])
-def update_locate_map(user_id, space_name):
+def update_locate_map():
     '''
     updates a previously saved planning space
     @param: space_name
     @return: message: <MESSAGE> , status: <BOOLEAN>
     '''
+    errors = {}
     if request.method == 'PUT':
         try:
-            # Get the value which needs to be updated
+
             try:
-                json_data = request.get_json()
-                update_plan = json_data.get('plan')
-            except:
-                # Bad request as the request body is not available
-                return jsonify({"message": "bad request! request body required.", "success": False}), 400
+                tenant = request.args.get('tenant')
+            except Exception:
+                errors['tenant'] = 'This query param is required.'\
+                                'please specify the organization name.'
+
+            try:
+                id = request.args.get('id')
+            except Exception:
+                errors['id'] = 'This query param is required.'\
+                                'please enter a valid str(user id).'
             
-            # check for organization name
-            tenant = request.args.get('tenant')
-            if not tenant:
-                return jsonify({"message": "please specify the organization name. Refer to the API documentation for details.", "success": False}), 400
-            
-            locate_map = Map(tenant)
-            
+            try:
+                name = request.args.get('name')
+            except Exception:
+                errors['name'] = 'This query param is required.'\
+                                'please enter a valid str(planning space name).'
+        
+            try:
+                plan = request.json.get('plan')
+            except Exception:
+                errors['plan '] = 'This field is required.'\
+                                'Please check the request body.'
+
             org = f'{configuration.DB_NAME}_{tenant.lower()}'
             if org not in dbs:
-                return jsonify({"message": "organization doesnot exist. Refer to the API documentation for details.", "success": False}), 400
+                errors['org'] = 'organization does not exist.'\
+                                'Refer to the API documentation for details.'
             
-            # Updating the planning space
-            records_updated = locate_map.update_locate_map(user_id, space_name, update_plan)
+            if errors:
+                return jsonify({
+                    'message': 'Some errors occurred while processing this request',
+                    'errors': errors
+                }),400 
 
-            # Check if resource is updated
-            if records_updated.modified_count > 0:
-                # Prepare the response as resource is updated successfully
-                return jsonify({"message": "planning space '" + space_name + "' updated successfully", "success": True}), 200
+            locate_map = Map(tenant)
+            updated = locate_map.update_locate_map(id, name, plan)
+
+            if updated.modified_count > 0:
+                return jsonify({'message': f'planning space {name} updated successfully', 
+                                'success': True
+                                }), 200
             else:
-                # Bad request as the resource is not available to update
-                return jsonify({"message": "planning not updated. please make sure the plan name / request body is correct", "success": False}), 404
+                return jsonify({'message': 'planning space NOT update. please make sure the plan name or request body is correct', 
+                                'success': False
+                                }), 404
         except:
-            # Error while trying to update the resource
-            return jsonify({"message": "error occured while trying to update planning space", "success": False}), 500
+            return jsonify({'message': 'errors occured while trying to update planning space', 
+                            'success': False
+                            }), 500
     else:
-       return jsonify({"message": "Invalid request method. Please refer to the API documentation", "success": False}), 400
+       return jsonify({'message': 'Invalid request method. Please refer to the API documentation',
+                        'success': False
+                        }), 400
  
 
 @locate_blueprint.route(routes.DELETE_MAP, methods=['DELETE', 'GET', 'PUT', 'PATCH', 'POST'])
-def delete_locate_map(user_id, space_name):
+def delete_locate_map():
     '''
-    deletes a previously saved planning space
+    delete a previously saved planning space
     @param: space_name
     @return: null
     '''
+    errors = {}
     if request.method == 'DELETE':
-        tenant = request.args.get('tenant')
-        if not tenant:
-            return jsonify({"message": "please specify the organization name. Refer to the API documentation for details.", "success": False}), 400
-        
-        locate_map = Map(tenant)
+        try:
+            tenant = request.args.get('tenant')
+        except Exception:
+            errors['tenant'] = 'This query param is required.'\
+                            'please specify the organization name.'
 
+        try:
+            id = request.args.get('id')
+        except Exception:
+            errors['id'] = 'This query param is required.'\
+                            'please enter a valid str(user id).'
+        
+        try:
+            name = request.args.get('name')
+        except Exception:
+            errors['name'] = 'This query param is required.'\
+                            'please enter a valid str(planning space name).'
+        
         org = f'{configuration.DB_NAME}_{tenant.lower()}'
         if org not in dbs:
-            return jsonify({"message": "organization doesnot exist. Refer to the API documentation for details.", "success": False}), 400
-        if space_name is not None:
-            db_response = locate_map.delete_locate_map(user_id, space_name)
-            if db_response.deleted_count == 1:
-                response = {
-                    "message": "planning space deleted successfully", "success": True}
-            else:
-                response = {
-                    "message": "planning space name not found of " + tenant + " organization. Please enter a correct planning space or organization name", "Success": False}
+            errors['org'] = 'organization does not exist.'\
+                            'Refer to the API documentation for details.'
+        
+        if errors:
+                return jsonify({
+                    'message': 'Some errors occurred while processing this request',
+                    'errors': errors
+                }),400 
+        locate_map = Map(tenant)
+        result = locate_map.delete_locate_map(id, name)
+        if result.deleted_count == 1:
+            response = {'message': 'planning space deleted successfully', 
+                        'success': True
+                        }
             return jsonify(response), 200
         else:
-            return jsonify({"message": "Bad request parameters!", "success": False}), 400
+            response = {'message': 'Some erorr occurred, please check the user id and planning space name', 
+                        'success': False
+                        }
+            return jsonify(response), 400
+
     else:
-        return jsonify({"message": "Invalid request method", "success": False}), 400
+        return jsonify({"message": "Invalid request method", 
+                        "success": False
+                        }), 400
