423848466f996c077f7cfb6c19c0cc221a2e4ad6
danielogen <danielogen@gmail.com>
2022-03-25 18:05:19 +0300

    update helpers

--- src/locate/controllers/helpers.py
+++ src/locate/controllers/helpers.py
@@ -8,13 +8,6 @@ _logger = logging.getLogger(__name__)
 monitor_bp = Blueprint('monitor_bp', __name__)
 
 
-@monitor_bp.route(routes.ROOT, methods=['GET', 'POST'])
-def root():
-    if request.method == 'GET':
-        _logger.info('root endpoint OK')
-        return jsonify({"message": "ok", "status": True}), 200
-
-
 @monitor_bp.route(routes.HEALTH_CHECK, methods=['GET', 'POST'])
 def health():
     if request.method == 'GET':
