52349e8aa207308b25517a32fdf0c44f5658627e
Noah Nsimbe <37845280+NoahNsimbe@users.noreply.github.com>
2022-03-31 16:14:51 +0300

    fix - forecast delete job

--- src/view/src/main/java/airqo/tasks/InsightsTasks.java
+++ src/view/src/main/java/airqo/tasks/InsightsTasks.java
@@ -28,24 +28,27 @@ public class InsightsTasks {
 
 	@Scheduled(cron = "@hourly")
 	public void forecastInsightsTasks() {
-		List<Insight> oldInsights = measurementService.getInsightsBefore(new Date());
-		List<Insight> insights = new ArrayList<>();
 		log.info("Deleting forecast Insights");
+		List<Insight> oldInsights = measurementService.getForecastInsightsBefore(new Date());
+		List<Insight> insights = new ArrayList<>();
+
 		for (Insight insight : oldInsights) {
 			insight.setForecast(false);
 			insights.add(insight);
 		}
 		measurementService.saveInsights(insights);
+		log.info("Deleted forecast Insights");
 	}
 
 	@Scheduled(cron = "@monthly")
 	public void oldInsightsTasks() {
-		log.info("Running old Insights");
+		log.info("Deleting old Insights");
 		Calendar cal = Calendar.getInstance();
 		cal.setTime(new Date());
 		cal.add(Calendar.DAY_OF_MONTH, -50);
 		Date dateTime = cal.getTime();
 		measurementService.deleteInsightsBefore(dateTime);
+		log.info("Deleted old Insights");
 	}
 
 }
