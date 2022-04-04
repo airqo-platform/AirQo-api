808eea6a8c27cf163661262815601b30b366f824
Noah Nsimbe <37845280+NoahNsimbe@users.noreply.github.com>
2022-04-04 18:41:25 +0300

    caching and package update
    upgraded paakages and enabled caching

--- src/view/src/main/java/airqo/ViewApplication.java
+++ src/view/src/main/java/airqo/ViewApplication.java
@@ -2,6 +2,7 @@ package airqo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 import javax.annotation.PostConstruct;
@@ -10,6 +11,7 @@ import java.util.TimeZone;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableCaching
 public class ViewApplication {
 
 	@PostConstruct
