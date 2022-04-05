9d98dc98ca91965487e7c371398685e73a8a8db8
Priscilla-AL <priscilla@airqo.net>
2022-04-05 20:21:58 +0300

    Update train_calibrate_tool.py

--- src/calibrate/models/train_calibrate_tool.py
+++ src/calibrate/models/train_calibrate_tool.py
@@ -20,7 +20,6 @@ class Train_calibrate_tool():
     def train_calibration_model(self, pollutant, map_columns, df):
        # Map columns from uploaded csv
         df.rename(columns=map_columns,inplace=True)
-        print("Columns", df.columns)
         
         #Get average PM
         df['Average_PM2.5'] = df[['pm2_5', 's2_pm2_5']].mean(axis=1).round(2)
