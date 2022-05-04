e7f01104d46483cc20861e84fbb352f5143816eb
Priscilla-AL <priscilla@airqo.net>
2022-05-04 16:26:02 +0300

    Create api.py

---
+++ src/fault-detection/routes/api.py
@@ -0,0 +1,8 @@
+base_url = '/api/v1'
+
+route = {
+    'root': '/',
+    'calibrate': f'{base_url}/calibrate',
+    'calibrate_tool': f'{base_url}/calibrate_tool',
+    'train_calibrate_tool': f'{base_url}/train_calibrate_tool'
+}
