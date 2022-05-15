84919faff1b6dc72284f114534a20f4600ca8f1e
fajemila <fajemild@gmail.com>
2022-05-15 22:49:25 +0100

    controller

--- src/fault-detection/controllers/calibrate.py
+++ src/fault-detection/controllers/calibrate.py
@@ -15,21 +15,17 @@ def calibrate():
         raw_values = data.get('raw_values')       
         if (not datetime or not raw_values):
             return jsonify({"message": "Please specify the datetime, pm2.5, pm10, temperature and humidity values in the body. Refer to the API documentation for details.", "success": False}), 400     
-        rgModel = rg.Regression()
+        model = rg.Regression()
 
         response = []
         for raw_value in raw_values:
             device_id = raw_value.get('device_id')
-            pm2_5 = raw_value.get('sensor1_pm2.5')
+            s1_pm2_5 = raw_value.get('sensor1_pm2.5')
             s2_pm2_5 = raw_value.get('sensor2_pm2.5')
-            pm10 = raw_value.get('sensor1_pm10')
-            s2_pm10 = raw_value.get('sensor2_pm10')
-            temperature = raw_value.get('temperature')
-            humidity = raw_value.get('humidity')
 
-            if (not device_id or not pm2_5 or not s2_pm2_5  or not pm10 or not s2_pm10 or not temperature or not humidity):
+            if (not device_id or not s1_pm2_5 or not s2_pm2_5):
                 return jsonify({"message": "Please specify the device_id, datetime, sensor1 pm2.5, sensor2 pm2.5, sensor1 pm10, sensor1 pm10, temperature and humidity values in the body. Refer to the API documentation for details.", "success": False}), 400
-            calibrated_pm2_5, calibrated_pm10 = rgModel.compute_calibrated_val(pm2_5,s2_pm2_5,pm10,s2_pm10,temperature,humidity, datetime)           
+            calibrated_pm2_5, calibrated_pm10 = model.compute_calibrated_val(s1_pm2_5,s2_pm2_5)           
             response.append({'device_id': device_id, 'calibrated_PM2.5': calibrated_pm2_5, 'calibrated_PM10': calibrated_pm10 })
         return jsonify(response), 200
 
@@ -42,8 +38,7 @@ def calibrate_tool():
 
     map_columns = request.form
     form_keys = map_columns.keys()
-    if not ("datetime" in form_keys and "pm2_5" in form_keys and "s2_pm2_5" in form_keys and "pm10" in form_keys
-            and map_columns.get("s2_pm10", None) and "humidity" in form_keys and map_columns.get("temperature", None)):
+    if not ("datetime" in form_keys and "pm2_5" in form_keys and "s2_pm2_5"):
         return jsonify({"message": "Please specify the corresponding datetime, sensor1 pm2.5, sensor2 pm2.5, "
                                    "sensor1 pm10, sensor1 pm10, temperature and humidity values. "
                                    "Refer to the API documentation for details.",
@@ -55,15 +50,15 @@ def calibrate_tool():
     if not set(list(map_columns.values())).issubset(set(list(df.columns))):
         return jsonify({"message": "One or more keys are missing in the uploaded file.", "success": False}), 400
 
-    rgModel = calibration_tool.Regression()
+    model = calibration_tool.Regression()
 
     map_columns = {value: key for key, value in map_columns.items()}
-    calibrated_data = rgModel.compute_calibrated_val(map_columns, df)
+    calibrated_data = model.compute_calibrated_val(map_columns, df)
     resp = make_response(calibrated_data.to_csv())
     resp.headers["Content-Disposition"] = "attachment; filename=calibrated_data.csv"
     resp.headers["Content-Type"] = "text/csv"
     return resp
-        
+
 @calibrate_bp.route(api.route['train_calibrate_tool'], methods=['POST', 'GET'])
 def train_calibrate_tool(): 
     if request.method == 'POST': # get headers to check content type eg json or csv
