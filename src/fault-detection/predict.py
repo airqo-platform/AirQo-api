35a92e31508619d6c9ddc95bf6444aa5347793cf
Osinkolu <femiosinkolu@gmail.com>
2022-05-19 14:49:27 +0100

    fault detecton api

--- src/fault-detection/predict.py
+++ src/fault-detection/predict.py
@@ -40,7 +40,7 @@ sample = {
 @author: Professor
 """
 
-from flask import Flask,render_template,request
+from flask import Flask,render_template,request, jsonify
 from models.classification import Classification
 import numpy as np
 import pandas as pd
@@ -50,11 +50,11 @@ app = Flask(__name__)
 def hello_world():
     return("Welcome, please smile more")
 
-@app.route("/predict", methods=["POST","GET"])
+@app.route("/predict", methods=['GET', 'POST'])
 def predict():
-    data = request.form.values()
-    #Sdata = pd.DataFrame(sample["raw_values"])
-    data["datetime"] = sample["datetime"]
+    raw_data = request.get_json(force=True)
+    data = pd.DataFrame(raw_data["raw_values"])
+    data["datetime"] = raw_data["datetime"]
     return Classification.compute_predictions(data)
 if __name__ =="__main__":
     app.run()
