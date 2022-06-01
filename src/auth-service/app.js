6b293d852e69315720cb659e3a8ec4736359b4a0
Mike Mwanje <mwanjemike767@gmail.com>
2022-06-01 13:52:51 +0300

    fix describe error

--- src/auth-service/app.js
+++ src/auth-service/app.js
@@ -14,6 +14,7 @@ mongodb;
 const app = express();
 
 app.use(logger("dev"));
+app.use(bodyParser.json());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
