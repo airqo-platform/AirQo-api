0ae1013560c5218ecf58b294306d3b39cfaa5802
Benjamin Ssempala <benssempal1445@gmail.com>
2022-06-28 18:46:04 +0300

    Backing Up Initial Code

---
+++ src/firebase/backup/index.js
@@ -0,0 +1,29 @@
+const firestore = require('@google-cloud/firestore');
+const client = new firestore.v1.FirestoreAdminClient();
+// Replace BUCKET_NAME
+const bucket = process.env.EXPORT_BUCKET;
+
+exports.scheduledFirestoreExport = (event, context) => {
+  const databaseName = client.databasePath(
+    process.env.GCLOUD_PROJECT,
+    '(default)'
+  );
+
+  return client
+    .exportDocuments({
+      name: databaseName,
+      outputUriPrefix: bucket,
+      // Leave collectionIds empty to export all collections
+      // or define a list of collection IDs:
+      // collectionIds: ['users', 'posts']
+      collectionIds: [],
+    })
+    .then(responses => {
+      const response = responses[0];
+      console.log(`Operation Name: ${response['name']}`);
+      return response;
+    })
+    .catch(err => {
+      console.error(err);
+    });
+};
