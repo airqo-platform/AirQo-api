803d99c8519686d39e9b44d7ad3dde8f660bdda3
Noah Nsimbe <37845280+NoahNsimbe@users.noreply.github.com>
2022-07-15 15:15:54 +0300

    Update deployment-script.sh

--- k8s/kafka-operator/deployment-script.sh
+++ k8s/kafka-operator/deployment-script.sh
@@ -5,5 +5,5 @@ tar -zxvf strimzi-0.29.0.tar.gz
 cd  strimzi-0.29.0/
 sed -i 's/namespace: .*/namespace: kafka-strimzi/' install/cluster-operator/*RoleBinding*.yaml
 kubectl apply -f install/cluster-operator -n kafka-strimzi
-kubectl scale deployment strimzi-cluster-operator-3.2.0 --replicas=2
+kubectl scale deployment strimzi-cluster-operator --replicas=2 -n kafka-strimzi
 exit 0
