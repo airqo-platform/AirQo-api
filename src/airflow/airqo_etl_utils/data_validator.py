6e9982b29a9d43f49954cda042db28b5d4796968
Noah Nsimbe <37845280+NoahNsimbe@users.noreply.github.com>
2022-08-02 19:08:14 +0300

    Fix - airqo data validations
    Fix - app insights
    
    Update airnow_utils.py

--- src/airflow/airqo_etl_utils/data_validator.py
+++ src/airflow/airqo_etl_utils/data_validator.py
@@ -1,87 +1,100 @@
 import pandas as pd
 
-from .commons import get_valid_value
-
 
 class DataValidationUtils:
     @staticmethod
-    def get_valid_values(raw_value: pd.DataFrame) -> pd.DataFrame:
-        valid_data = raw_value.copy()
-        columns = valid_data.columns
+    def get_valid_value(value, name):
 
-        if "timestamp" in columns:
-            valid_data["timestamp"] = valid_data["timestamp"].apply(
+        if (name == "pm2_5" or name == "pm10") and (value < 1 or value > 1000):
+            return None
+        elif name == "latitude" and (value < -90 or value > 90):
+            return None
+        elif name == "longitude" and (value < -180 or value > 180):
+            return None
+        elif name == "battery" and (value < 2.7 or value > 5):
+            return None
+        elif (name == "altitude" or name == "hdop") and value <= 0:
+            return None
+        elif name == "satellites" and (value <= 0 or value > 50):
+            return None
+        elif (name == "temperature") and (value <= 0 or value > 45):
+            return None
+        elif (name == "humidity") and (value <= 0 or value > 99):
+            return None
+        elif name == "pressure" and (value < 30 or value > 110):
+            return None
+        else:
+            pass
+
+        return value
+
+    @staticmethod
+    def get_valid_values(data: pd.DataFrame) -> pd.DataFrame:
+        float_columns = {
+            "battery",
+            "hdop",
+            "altitude",
+            "satellites",
+            "pm2_5",
+            "pm10",
+            "s1_pm2_5",
+            "s2_pm2_5",
+            "s1_pm10",
+            "s2_pm10",
+            "no2",
+            "no2_raw_value",
+            "pm1",
+            "pm1_raw_value",
+            "external_temperature",
+            "external_humidity",
+            "latitude",
+            "longitude",
+            "humidity",
+            "temperature",
+            "device_humidity",
+            "device_temperature",
+            "pressure",
+            "wind_speed",
+            "speed",
+        }
+
+        float_columns = list(float_columns & set(data.columns))
+
+        data[float_columns] = data[float_columns].apply(pd.to_numeric, errors="coerce")
+        if "timestamp" in data.columns:
+            data["timestamp"] = data["timestamp"].apply(
                 lambda x: pd.to_datetime(x, errors="coerce")
             )
 
-        if "battery" in columns:
-            valid_data["battery"] = valid_data["battery"].apply(
-                lambda x: get_valid_value(x, "battery")
+        for col in float_columns:
+            name = col
+            if name in [
+                "pm2_5",
+                "s1_pm2_5",
+                "s2_pm2_5",
+                "pm2_5_raw_value",
+                "pm2_5_calibrated_value",
+            ]:
+                name = "pm2_5"
+            elif name in [
+                "pm10",
+                "s1_pm10",
+                "s2_pm10",
+                "pm10_raw_value",
+                "pm10_calibrated_value",
+            ]:
+                name = "pm10"
+            elif name in ["device_humidity", "humidity", "external_humidity"]:
+                name = "humidity"
+            elif col in ["device_temperature", "temperature", "external_temperature"]:
+                name = "temperature"
+            elif name in ["no2", "no2_raw_value"]:
+                name = "no2"
+            elif name in ["pm1", "pm1_raw_value"]:
+                name = "pm1"
+
+            data[col] = data[col].apply(
+                lambda x: DataValidationUtils.get_valid_value(x, name)
             )
 
-        if "hdop" in columns:
-            valid_data["hdop"] = valid_data["hdop"].apply(
-                lambda x: get_valid_value(x, "hdop")
-            )
-
-        if "altitude" in columns:
-            valid_data["altitude"] = valid_data["altitude"].apply(
-                lambda x: get_valid_value(x, "altitude")
-            )
-
-        if "satellites" in columns:
-            valid_data["satellites"] = valid_data["satellites"].apply(
-                lambda x: get_valid_value(x, "satellites")
-            )
-
-        if "pm2_5" in columns:
-            valid_data["pm2_5"] = valid_data["pm2_5"].apply(
-                lambda x: get_valid_value(x, "pm2_5")
-            )
-
-        if "s1_pm2_5" in columns:
-            valid_data["s1_pm2_5"] = valid_data["s1_pm2_5"].apply(
-                lambda x: get_valid_value(x, "pm2_5")
-            )
-
-        if "pm10" in columns:
-            valid_data["pm10"] = valid_data["pm10"].apply(
-                lambda x: get_valid_value(x, "pm10")
-            )
-
-        if "s1_pm10" in columns:
-            valid_data["s1_pm10"] = valid_data["s1_pm10"].apply(
-                lambda x: get_valid_value(x, "pm10")
-            )
-
-        if "humidity" in columns:
-            valid_data["humidity"] = valid_data["humidity"].apply(
-                lambda x: get_valid_value(x, "humidity")
-            )
-
-        if "humidity" in columns:
-            valid_data["humidity"] = valid_data["humidity"].apply(
-                lambda x: get_valid_value(x, "humidity")
-            )
-
-        if "wind_speed" in columns:
-            valid_data["wind_speed"] = valid_data["wind_speed"].apply(
-                lambda x: get_valid_value(x, "wind_speed")
-            )
-
-        if "pressure" in columns:
-            valid_data["pressure"] = valid_data["pressure"].apply(
-                lambda x: get_valid_value(x, "pressure")
-            )
-
-        if "device_humidity" in columns:
-            valid_data["device_humidity"] = valid_data["device_humidity"].apply(
-                lambda x: get_valid_value(x, "humidity")
-            )
-
-        if "device_temperature" in columns:
-            valid_data["device_temperature"] = valid_data["device_temperature"].apply(
-                lambda x: get_valid_value(x, "temperature")
-            )
-
-        return valid_data
+        return data
