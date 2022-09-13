18f92934b4bfbacb5c24cdb17860a8d349909e6a
Sserunjogi Richard <sserurich@gmail.com>
2022-09-13 17:38:25 +0300

    modified get_gp_predictions method to return all data for all the airqlouds for the last 2 days

--- src/predict/api/helpers/utils.py
+++ src/predict/api/helpers/utils.py
@@ -13,6 +13,8 @@ import requests
 import pymongo
 from pymongo import MongoClient
 from config.constants import connect_mongo, configuration
+from datetime import datetime
+from datetime import timedelta
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -215,7 +217,10 @@ def get_gp_predictions_id(aq_id):
     #     return {'message':'unable to connect to database', 'success':False}, 400
 
     # db = client[DB_NAME]
-    query = {'airqloud_id': aq_id}
+    #query = {'airqloud_id': aq_id}
+    today = datetime.today()
+    query=  {"created_at": {"$gt": today - timedelta(2)}}
+
     projection = {'_id': 0, 'latitude': 1, 'longitude': 1, 'predicted_value': 1, 'variance': 1, 'interval': 1, 'airqloud':1, 'created_at':1, 'airqloud_id':1}
     records = list(db.gp_predictions.find(query, projection))
     return records
