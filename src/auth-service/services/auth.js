770236ded3f570b63340b9f9c00b334af06e1282
baalmart <bbaalmart@gmail.com>
2022-09-14 23:55:49 +0300

    install new modules for the adminjs tool and also remove unused modules and code

--- src/auth-service/services/auth.js
+++ src/auth-service/services/auth.js
@@ -6,14 +6,7 @@ const UserSchema = require("../models/User");
 const constants = require("../config/constants");
 const { logElement, logText, logObject } = require("../utils/log");
 const { Strategy: JwtStrategy, ExtractJwt } = require("passport-jwt");
-const expressJwt = require("express-jwt");
-const privileges = require("../utils/privileges");
-const {
-  axiosError,
-  tryCatchErrors,
-  missingQueryParams,
-  callbackErrors,
-} = require("../utils/errors");
+
 const { getModelByTenant } = require("../utils/multitenancy");
 const UserModel = (tenant) => {
   return getModelByTenant(tenant, "user", UserSchema);
@@ -181,26 +174,6 @@ const setJWTStrategy = (tenant, req, res, next) => {
   passport.use("jwt", useJWTStrategy(tenant, req, res, next));
 };
 
-// passport.serializeUser((user, cb) => {
-//   if (privileges.isUser(user)) {
-//     cb(null, user._id);
-//   } else if (privileges.isCollab(user)) {
-//     cb(null, user._id);
-//   }
-// });
-
-// passport.deserializeUser((id, cb) => {
-//   if (privileges.isUser(id)) {
-//     User.findById(id)
-//       .then((user) => cb(null, user))
-//       .catch((err) => cb(err));
-//   } else if (privileges.isCollab(user)) {
-//     Collaborator.findById(id)
-//       .then((user) => cb(null, user))
-//       .catch((err) => cb(err));
-//   }
-// });
-
 function setLocalAuth(req, res, next) {
   try {
     const hasErrors = !validationResult(req).isEmpty();
@@ -255,34 +228,13 @@ const authLocal = passport.authenticate("user-local", {
   failureFlash: true,
 });
 
-const authColabLocal = passport.authenticate("colab-local", {
-  successFlash: "Welcome!",
-  failureFlash: "Invalid username or password.",
-});
-
 const authJWT = passport.authenticate("jwt", {
   session: false,
 });
 
-const isLoggedIn = function isLoggedIn(req, res, next) {
-  if (req.isAuthenticated()) {
-    return next();
-  } else {
-    return res.redirect("/setLocalAuth");
-  }
-};
-
-const requiresSignIn = expressJwt({
-  secret: process.env.JWT_SECRET,
-  userProperty: "auth",
-});
-
 module.exports = {
   setLocalAuth: setLocalAuth,
+  setJWTAuth: setJWTAuth,
   authLocal: authLocal,
   authJWT: authJWT,
-  setJWTAuth: setJWTAuth,
-  authColabLocal: authColabLocal,
-  isLoggedIn: isLoggedIn,
-  requiresSignIn: requiresSignIn,
 };
