c92e28ea46d29836a68e43eddfc531b0f5e73fd8
Noah Nsimbe <37845280+NoahNsimbe@users.noreply.github.com>
2022-09-18 22:53:51 +0300

    cleanup - Device category enum

--- src/airflow/airqo_etl_utils/airqo_api.py
+++ src/airflow/airqo_etl_utils/airqo_api.py
@@ -129,9 +129,9 @@ class AirQoApi:
             }
             for device in devices
         ]
-        # Temporarily disabling category since endpoint does not return all devices
-        # if category != DeviceCategory.NONE:
-        #     devices = list(filter(lambda y: y["category"] == category, devices))
+
+        if category != DeviceCategory.NONE:
+            devices = list(filter(lambda y: y["category"] == category, devices))
 
         return devices
 
