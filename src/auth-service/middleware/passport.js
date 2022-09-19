d5b01a25697684986a03d52aca0076a9a1a94335
baalmart <bbaalmart@gmail.com>
2022-09-19 22:55:08 +0300

    moving the passport to a new location

---
+++ src/auth-service/middleware/passport.js
@@ -0,0 +1,320 @@
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const HTTPStatus = require("http-status");
+const Validator = require("validator");
+const UserSchema = require("../models/User");
+const AccessTokenSchema = require("../models/AccessToken");
+const constants = require("../config/constants");
+const { logElement, logText, logObject } = require("../utils/log");
+const { Strategy: JwtStrategy, ExtractJwt } = require("passport-jwt");
+const AuthTokenStrategy = require("passport-auth-token");
+
+const { getModelByTenant } = require("../utils/multitenancy");
+const UserModel = (tenant) => {
+  return getModelByTenant(tenant, "user", UserSchema);
+};
+
+const AccessTokenModel = (tenant) => {
+  return getModelByTenant(tenant, "token", AccessTokenSchema);
+};
+
+const { validationResult } = require("express-validator");
+const manipulateArraysUtil = require("../utils/manipulate-arrays");
+const { badRequest } = require("../utils/errors");
+
+const setLocalOptions = (req) => {
+  try {
+    let authenticationFields = {};
+    if (
+      !Validator.isEmpty(req.body.userName) &&
+      Validator.isEmail(req.body.userName)
+    ) {
+      authenticationFields.usernameField = "email";
+      authenticationFields.passwordField = "password";
+    }
+
+    if (
+      !Validator.isEmpty(req.body.userName) &&
+      !Validator.isEmail(req.body.userName)
+    ) {
+      authenticationFields.usernameField = "userName";
+      authenticationFields.passwordField = "password";
+    }
+
+    if (Validator.isEmpty(req.body.userName)) {
+      return {
+        success: false,
+        message: "the userName field is missing",
+      };
+    }
+
+    return {
+      success: true,
+      message: "the auth fields have been set",
+      authenticationFields,
+    };
+  } catch (e) {
+    return {
+      success: false,
+      message: e.message,
+    };
+  }
+};
+
+const authenticateWithEmailOptions = {
+  usernameField: "email",
+  passwordField: "password",
+};
+
+const authenticateWithUsernameOptions = {
+  usernameField: "userName",
+  passwordField: "password",
+};
+
+const jwtOpts = {
+  jwtFromRequest: ExtractJwt.fromAuthHeaderWithScheme("jwt"),
+  secretOrKey: constants.JWT_SECRET,
+};
+
+const useLocalStrategy = (tenant, req, res, next) => {
+  let localOptions = setLocalOptions(req);
+  logObject("the localOptions", localOptions);
+  if (localOptions.success == true) {
+    logText("success state is true");
+    let { usernameField } = localOptions.authenticationFields;
+    logElement("the username field", usernameField);
+    if (usernameField == "email") {
+      req.body.email = req.body.userName;
+      logText("we are using email");
+      return useEmailWithLocalStrategy(tenant, req, res, next);
+    } else if (usernameField == "userName") {
+      logText("we are using username");
+      return useUsernameWithLocalStrategy(tenant, req, res, next);
+    }
+  } else if (localOptions.success == false) {
+    logText("success state is false");
+    return localOptions;
+  }
+};
+const useEmailWithLocalStrategy = (tenant, req, res, next) =>
+  new LocalStrategy(
+    authenticateWithEmailOptions,
+    async (email, password, done) => {
+      try {
+        const user = await UserModel(tenant.toLowerCase())
+          .findOne({ email })
+          .exec();
+        req.auth = {};
+        if (!user) {
+          req.auth.success = false;
+          req.auth.message = `username or password does not exist in this organisation (${tenant})`;
+          next();
+        } else if (!user.authenticateUser(password)) {
+          req.auth.success = false;
+          req.auth.message = "incorrect username or password";
+          next();
+        }
+        req.auth.success = true;
+        req.auth.message = "successful login";
+        return done(null, user);
+      } catch (e) {
+        req.auth.success = false;
+        req.auth.message = "Server Error";
+        req.auth.error = e.message;
+        next();
+      }
+    }
+  );
+const useUsernameWithLocalStrategy = (tenant, req, res, next) =>
+  new LocalStrategy(
+    authenticateWithUsernameOptions,
+    async (userName, password, done) => {
+      try {
+        const user = await UserModel(tenant.toLowerCase())
+          .findOne({ userName })
+          .exec();
+        req.auth = {};
+        if (!user) {
+          req.auth.success = false;
+          req.auth.message = `username or password does not exist in this organisation (${tenant})`;
+          next();
+        } else if (!user.authenticateUser(password)) {
+          req.auth.success = false;
+          req.auth.message = "incorrect username or password";
+          next();
+        }
+        req.auth.success = true;
+        req.auth.message = "successful login";
+        return done(null, user);
+      } catch (e) {
+        req.auth.success = false;
+        req.auth.message = "Server Error";
+        req.auth.error = e.message;
+        next();
+      }
+    }
+  );
+
+const useJWTStrategy = (tenant, req, res, next) =>
+  new JwtStrategy(jwtOpts, async (payload, done) => {
+    try {
+      const user = await UserModel(tenant.toLowerCase())
+        .findOne({ _id: payload._id })
+        .exec();
+      if (!user) {
+        return done(null, false);
+      }
+      return done(null, user);
+    } catch (e) {
+      return done(e, false);
+    }
+  });
+
+const useAuthTokenStrategy = (tenant, req, res, next) =>
+  new AuthTokenStrategy(async function (token, done) {
+    await AccessTokenModel(tenant.toLowerCase()).findOne(
+      {
+        id: token,
+      },
+      function (error, accessToken) {
+        if (error) {
+          return done(error);
+        }
+
+        if (accessToken) {
+          if (!token.isValid(accessToken)) {
+            return done(null, false);
+          }
+
+          UserModel(tenant.toLowerCase()).findOne(
+            {
+              id: accessToken.userId,
+            },
+            function (error, user) {
+              if (error) {
+                return done(error);
+              }
+
+              if (!user) {
+                return done(null, false);
+              }
+
+              return done(null, user);
+            }
+          );
+        } else {
+          return done(null);
+        }
+      }
+    );
+  });
+
+const setLocalStrategy = (tenant, req, res, next) => {
+  passport.use("user-local", useLocalStrategy(tenant, req, res, next));
+};
+
+const setJWTStrategy = (tenant, req, res, next) => {
+  passport.use("jwt", useJWTStrategy(tenant, req, res, next));
+};
+
+const setAuthTokenStrategy = (tenant, req, res, next) => {
+  passport.use("authtoken", useAuthTokenStrategy(tenant, req, res, next));
+};
+
+function setLocalAuth(req, res, next) {
+  try {
+    const hasErrors = !validationResult(req).isEmpty();
+    if (hasErrors) {
+      let nestedErrors = validationResult(req).errors[0].nestedErrors;
+      return badRequest(
+        res,
+        "bad request errors",
+        manipulateArraysUtil.convertErrorArrayToObject(nestedErrors)
+      );
+    }
+    let tenant = "airqo";
+    if (req.query.tenant) {
+      tenant = req.query.tenant;
+    }
+    setLocalStrategy(tenant, req, res, next);
+    next();
+  } catch (e) {
+    console.log("the error in setLocalAuth is: ", e.message);
+    res.json({ success: false, message: e.message });
+  }
+}
+
+function setJWTAuth(req, res, next) {
+  try {
+    const hasErrors = !validationResult(req).isEmpty();
+    if (hasErrors) {
+      let nestedErrors = validationResult(req).errors[0].nestedErrors;
+      return badRequest(
+        res,
+        "bad request errors",
+        manipulateArraysUtil.convertErrorArrayToObject(nestedErrors)
+      );
+    }
+    let tenant = "airqo";
+    if (req.query.tenant) {
+      tenant = req.query.tenant;
+    }
+    logElement("the tenant for the job", tenant);
+    setJWTStrategy(tenant, req, res, next);
+    next();
+  } catch (e) {
+    console.log("the error in setLocalAuth is: ", e.message);
+    res
+      .status(HTTPStatus.BAD_GATEWAY)
+      .json({ success: false, message: e.message });
+  }
+}
+
+function setAuthToken(req, res, next) {
+  try {
+    const hasErrors = !validationResult(req).isEmpty();
+    if (hasErrors) {
+      let nestedErrors = validationResult(req).errors[0].nestedErrors;
+      logObject("nestedErrors", nestedErrors);
+      return badRequest(
+        res,
+        "bad request errors",
+        manipulateArraysUtil.convertErrorArrayToObject(nestedErrors)
+      );
+    }
+    let tenant = "airqo";
+    if (req.query.tenant) {
+      tenant = req.query.tenant;
+    }
+    setAuthTokenStrategy(tenant, req, res, next);
+    next();
+  } catch (e) {
+    console.log("the error in setAuthToken is: ", e.message);
+    res
+      .status(HTTPStatus.INTERNAL_SERVER_ERROR)
+      .json({ success: false, message: e.message });
+  }
+}
+
+const authLocal = passport.authenticate("user-local", {
+  session: false,
+  failureFlash: true,
+});
+
+const authJWT = passport.authenticate("jwt", {
+  session: false,
+});
+
+const authToken = passport.authenticate("authtoken", {
+  session: false,
+  optional: false,
+});
+
+module.exports = {
+  setLocalAuth: setLocalAuth,
+  setJWTAuth: setJWTAuth,
+  setAuthToken: setAuthToken,
+  authLocal: authLocal,
+  authJWT: authJWT,
+  authToken: authToken,
+};
