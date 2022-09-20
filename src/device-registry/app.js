0bcd582354827474b15b443282cc8eed8b945547
baalmart <bbaalmart@gmail.com>
2022-09-20 17:40:34 +0300

    adding environment to the app alert notifs

--- src/device-registry/app.js
+++ src/device-registry/app.js
@@ -50,7 +50,7 @@ app.use(function(req, res, next) {
 });
 
 app.use(function(err, req, res, next) {
-  logger.error(`${err.message}`);
+  logger.error(`${constants.ENVIRONMENT} -- ${err.message}`);
   if (err.status === 404) {
     res.status(err.status).json({
       success: false,
