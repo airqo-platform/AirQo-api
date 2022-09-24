ea74e1cf8cda508f24939afa09287ebba4acbaed
Mike Mwanje <mwanjemike767@gmail.com>
2022-09-24 17:20:54 +0300

    remove pipeline cluster vm's and boot disks

--- infra/terraform/gcp/airqo-250220/resources/_BACKUP_/ComputeAddress/pipeline-k8s-controller.tf
+++
@@ -1,9 +0,0 @@
-resource "google_compute_address" "pipeline_k8s_controller" {
-  address      = "34.140.20.232"
-  address_type = "EXTERNAL"
-  name         = "pipeline-k8s-controller"
-  network_tier = "PREMIUM"
-  project      = "airqo-250220"
-  region       = "europe-west1"
-}
-# terraform import google_compute_address.pipeline_k8s_controller projects/airqo-250220/regions/europe-west1/addresses/pipeline-k8s-controller
--- infra/terraform/gcp/airqo-250220/resources/_BACKUP_/ComputeAddress/pipeline-k8s-worker-1.tf
+++
@@ -1,9 +0,0 @@
-resource "google_compute_address" "pipeline_k8s_worker_1" {
-  address      = "35.189.251.42"
-  address_type = "EXTERNAL"
-  name         = "pipeline-k8s-worker-1"
-  network_tier = "PREMIUM"
-  project      = "airqo-250220"
-  region       = "europe-west1"
-}
-# terraform import google_compute_address.pipeline_k8s_worker_1 projects/airqo-250220/regions/europe-west1/addresses/pipeline-k8s-worker-1
