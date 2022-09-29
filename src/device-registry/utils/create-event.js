73e1783a2ff054d1d5ae210e00b20f86d60c1113
baalmart <bbaalmart@gmail.com>
2022-09-29 18:53:19 +0300

    removing device from SQL queries

--- src/device-registry/utils/create-event.js
+++ src/device-registry/utils/create-event.js
@@ -103,7 +103,7 @@ const createEvent = {
 
       let table = `${constants.DATAWAREHOUSE_AVERAGED_DATA}.hourly_device_measurements`;
       let averaged_fields =
-        "site_id, name, device, device_number, timestamp, " +
+        "site_id, name, device_id, device_number, timestamp, " +
         "pm2_5_raw_value, pm2_5_calibrated_value, pm10_raw_value," +
         "pm10_calibrated_value, no2_raw_value, no2_calibrated_value, pm1_raw_value," +
         "pm1_calibrated_value, external_temperature, external_humidity, wind_speed,";
