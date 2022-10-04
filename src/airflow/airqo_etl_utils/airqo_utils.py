e1739b25f50646e7273c06f2c4faccbbc02647e4
Noah Nsimbe <37845280+NoahNsimbe@users.noreply.github.com>
2022-10-04 14:13:52 +0300

    Added device category to latest measurements

--- src/airflow/airqo_etl_utils/airqo_utils.py
+++ src/airflow/airqo_etl_utils/airqo_utils.py
@@ -536,7 +536,10 @@ class AirQoDataUtils:
 
             data["pm2_5"] = data["pm2_5"].fillna(data["pm2_5_raw_value"])
             data["pm10"] = data["pm10"].fillna(data["pm10_raw_value"])
+
         data.loc[:, "tenant"] = str(Tenant.AIRQO)
+        data.loc[:, "device_category"] = str(device_category)
+
         return data
 
     @staticmethod
