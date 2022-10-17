7faba62f264d8535e69f32f483b07d7c91dd6ce0
Mike Mwanje <mwanjemike767@gmail.com>
2022-10-17 09:46:15 +0300

    add test-mqtt network, subnetwork, and firewall

--- infra/terraform/gcp/main.tf
+++ infra/terraform/gcp/main.tf
@@ -7,9 +7,15 @@ module "ComputeDisk" {
   source = "./modules/ComputeDisk"
 
   project-id = var.project-id
-  zone      = var.zone
-  os        = var.os
-  disk_size = var.disk_size
+  zone       = var.zone
+  os         = var.os
+  disk_size  = var.disk_size
+}
+
+module "ComputeFirewall" {
+  source = "./modules/ComputeFirewall"
+
+  project-id = var.project-id
 }
 
 module "ComputeInstance" {
@@ -29,6 +35,19 @@ module "ComputeInstanceTemplate" {
   project-number = var.project-number
   os             = var.os
   disk_size      = var.disk_size
+}
+
+module "ComputeNetwork" {
+  source = "./modules/ComputeNetwork"
+
+  project-id = var.project-id
+}
+
+module "ComputeSubnetwork" {
+  source = "./modules/ComputeSubnetwork"
+
+  project-id = var.project-id
+  region     = var.region
 }
 
 module "SecretManagerSecret" {
