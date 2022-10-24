2c6c4b65c10fa8a3925ed801f657c8dd1a500c35
baalmart <bbaalmart@gmail.com>
2022-10-24 20:26:06 +0300

    linking the middleware to the can-access middleware

--- src/auth-service/middleware/can-access.js
+++ src/auth-service/middleware/can-access.js
@@ -1,27 +1,29 @@
-const sendResponseUtil = require("../utils/sendResponse");
-// import model from "../models";
+import { logElement } from "../utils/log";
+const HTTPStatus = require("http-status");
 
-// const { Role, Permission } = model;
+const sendResponseUtil = require("../utils/sendResponse");
+const RoleSchema = "../models/Role";
+const PermissionSchema = "../models/Permission";
+const { getModelByTenant } = require("../utils/multitenancy");
 
 export default (permission) => async (req, res, next) => {
-  const access = await Permission.findOne({
-    where: { name: permission },
-    include: [
-      {
-        attributes: ["id", "name"],
-        model: Role,
-        as: "roles",
-        through: { attributes: [] },
-      },
-    ],
+  const { tenant } = req.query;
+  const filter = {
+    name: permission,
+  };
+  const access = await getModelByTenant(
+    tenant,
+    "permission",
+    PermissionSchema
+  ).list({
+    filter,
   });
   if (await req.userData.hasPermissionTo(access)) {
     return next();
   }
-  console.error("You do not have the authorization to access this.");
-  return sendResponseUtil.sendErrorResponse(
-    res,
-    403,
-    "You do not have the authorization to access this"
-  );
+  logElement("You do not have the authorization to access this.");
+  return res.status(403).json({
+    status: HTTPStatus.UNAUTHORIZED,
+    error: "You do not have the authorization to access this",
+  });
 };
