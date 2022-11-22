484145e821c76333c3a9140a8c163aceb609b008
baalmart <bbaalmart@gmail.com>
2022-11-22 13:35:03 +0300

    rename and deletion of some utils

---
+++ src/auth-service/utils/request-access.js
@@ -0,0 +1,450 @@
+const UserSchema = require("../models/User");
+const CandidateSchema = require("../models/Candidate");
+const { getModelByTenant } = require("./multitenancy");
+const { logObject, logElement, logText } = require("./log");
+const mailer = require("./mailer");
+const generateFilter = require("./generate-filter");
+const isEmpty = require("is-empty");
+const httpStatus = require("http-status");
+constants = require("../config/constants");
+const kickbox = require("kickbox")
+  .client(`${constants.KICKBOX_API_KEY}`)
+  .kickbox();
+
+const UserModel = (tenant) => {
+  return getModelByTenant(tenant, "user", UserSchema);
+};
+
+const CandidateModel = (tenant) => {
+  return getModelByTenant(tenant, "candidate", CandidateSchema);
+};
+
+const requestAccess = {
+  create: async (req, callback) => {
+    try {
+      let {
+        firstName,
+        lastName,
+        email,
+        long_organization,
+        jobTitle,
+        website,
+        description,
+        category,
+        tenant,
+      } = req;
+
+      // await validationsUtil.checkEmailExistenceUsingKickbox(email, (value) => {
+      //   if (value.success == false) {
+      //     const errors = value.errors ? value.errors : "";
+      //     logObject("the validation checks results", {
+      //       success: false,
+      //       message: value.message,
+      //       errors,
+      //       status: value.status,
+      //     });
+      //     callback({
+      //       success: false,
+      //       message: value.message,
+      //       errors,
+      //       status: value.status,
+      //     });
+      //   }
+      // });
+
+      let filterRequestBody = {};
+      filterRequestBody["body"] = {};
+      filterRequestBody["query"] = {};
+      filterRequestBody["body"] = req;
+      filterRequestBody["query"]["category"] = category;
+
+      const responseFromGenerateCandidateFilter =
+        generateFilter.candidates(filterRequestBody);
+
+      if (responseFromGenerateCandidateFilter.success === false) {
+        callback({
+          success: false,
+          message: "internal server error",
+          errors: { message: responseFromGenerateCandidateFilter.error },
+        });
+      }
+
+      let filter = responseFromGenerateCandidateFilter.data;
+      const responseFromListCandidates = await requestAccess.list({
+        tenant,
+        filter,
+      });
+
+      if (responseFromListCandidates.success === true) {
+        if (
+          Array.isArray(responseFromListCandidates.data) &&
+          responseFromListCandidates.data.length === 1
+        ) {
+          callback({
+            success: true,
+            message: "candidate already exists or successfully created",
+            status: httpStatus.OK,
+          });
+        } else {
+          const responseFromCreateCandidate = await CandidateModel(
+            tenant
+          ).register(req);
+
+          if (responseFromCreateCandidate.success === true) {
+            let createdCandidate = await responseFromCreateCandidate.data;
+            let responseFromSendEmail = await mailer.candidate(
+              firstName,
+              lastName,
+              email,
+              tenant
+            );
+            if (responseFromSendEmail.success === true) {
+              const status = responseFromSendEmail.status
+                ? responseFromSendEmail.status
+                : "";
+              callback({
+                success: true,
+                message: "candidate already exists or successfully created",
+                data: createdCandidate,
+                status,
+              });
+            } else if (responseFromSendEmail.success === false) {
+              const errors = responseFromSendEmail.error
+                ? responseFromSendEmail.error
+                : "";
+              const status = responseFromSendEmail.status
+                ? responseFromSendEmail.status
+                : "";
+
+              callback({
+                success: false,
+                message: responseFromSendEmail.message,
+                errors,
+                status,
+              });
+            }
+          } else if (responseFromCreateCandidate.success === false) {
+            const errors = responseFromCreateCandidate.errors
+              ? responseFromCreateCandidate.errors
+              : "";
+            const status = responseFromCreateCandidate.status
+              ? responseFromCreateCandidate.status
+              : "";
+            callback({
+              success: false,
+              message: responseFromCreateCandidate.message,
+              errors,
+              status,
+            });
+          }
+        }
+      } else if (responseFromListCandidates.success === false) {
+        const errors = responseFromListCandidates.error
+          ? responseFromListCandidates.error
+          : { message: "Internal Server Error" };
+        const status = responseFromListCandidates.status
+          ? responseFromListCandidates.status
+          : httpStatus.INTERNAL_SERVER_ERROR;
+        const message = responseFromListCandidates.message
+          ? responseFromListCandidates.message
+          : "Internal Server Error";
+        callback({
+          success: false,
+          message,
+          errors,
+          status,
+        });
+      }
+    } catch (e) {
+      callback({
+        success: false,
+        message: "Internal Server Error",
+        errors: { message: e.message },
+        status: httpStatus.INTERNAL_SERVER_ERROR,
+      });
+    }
+  },
+
+  list: async ({ tenant, filter, limit, skip }) => {
+    try {
+      logElement("the tenant", tenant);
+      logObject("the filter", filter);
+      logElement("limit", limit);
+      logElement("the skip", skip);
+
+      let responseFromListCandidate = await CandidateModel(
+        tenant.toLowerCase()
+      ).list({
+        filter,
+        limit,
+        skip,
+      });
+
+      if (responseFromListCandidate.success == true) {
+        return {
+          success: true,
+          message: responseFromListCandidate.message,
+          data: responseFromListCandidate.data,
+        };
+      } else if (responseFromListCandidate.success == false) {
+        if (responseFromListCandidate.error) {
+          return {
+            success: false,
+            message: responseFromListCandidate.message,
+            error: responseFromListCandidate.error,
+          };
+        } else {
+          return {
+            success: false,
+            message: responseFromListCandidate.message,
+          };
+        }
+      }
+    } catch (e) {
+      return {
+        success: false,
+        message: "utils server error",
+        error: e.message,
+      };
+    }
+  },
+
+  update: async (tenant, filter, update) => {
+    try {
+      let responseFromModifyCandidate = await CandidateModel(
+        tenant.toLowerCase()
+      ).modify({
+        filter,
+        update,
+      });
+      logObject("responseFromModifyCandidate", responseFromModifyCandidate);
+      if (responseFromModifyCandidate.success == true) {
+        return {
+          success: true,
+          message: responseFromModifyCandidate.message,
+          data: responseFromModifyCandidate.data,
+        };
+      } else if (responseFromModifyCandidate.success == false) {
+        if (responseFromModifyCandidate.error) {
+          return {
+            success: false,
+            message: responseFromModifyCandidate.message,
+            error: responseFromModifyCandidate.error,
+          };
+        } else {
+          return {
+            success: false,
+            message: responseFromModifyCandidate.message,
+          };
+        }
+      }
+    } catch (e) {
+      return {
+        success: false,
+        message: "util server error",
+        error: e.message,
+      };
+    }
+  },
+
+  confirm: async (req) => {
+    let {
+      tenant,
+      firstName,
+      lastName,
+      email,
+      organization,
+      long_organization,
+      jobTitle,
+      website,
+      category,
+      filter,
+      description,
+    } = req;
+    try {
+      let responseFromListCandidate = await requestAccess.list({
+        tenant,
+        filter,
+      });
+      if (
+        responseFromListCandidate.success === true &&
+        !isEmpty(responseFromListCandidate.data)
+      ) {
+        const password = accessCodeGenerator.generate(
+          constants.RANDOM_PASSWORD_CONFIGURATION(10)
+        );
+
+        let requestBody = {
+          tenant,
+          firstName,
+          lastName,
+          email,
+          organization,
+          long_organization,
+          jobTitle,
+          website,
+          password,
+          description,
+          category,
+          privilege: "user",
+          userName: email,
+        };
+        logObject("requestBody during confirmation", requestBody);
+
+        let responseFromCreateUser = await UserModel(tenant).register(
+          requestBody
+        );
+
+        logObject(
+          "responseFromCreateUser during confirmation",
+          responseFromCreateUser
+        );
+
+        const createdUser = await responseFromCreateUser.data;
+        logObject("createdUser", createdUser);
+        const jsonify = (createdUser) => {
+          let jsonData = JSON.stringify(dataFromDatabase);
+          let parsedMap = JSON.parse(jsonData);
+          return parsedMap;
+        };
+
+        const jsonifyCreatedUser = jsonify(createdUser);
+
+        logObject("jsonifyCreatedUser", jsonifyCreatedUser);
+
+        if (responseFromCreateUser.success === true) {
+          let responseFromSendEmail = await mailer.user(
+            firstName,
+            lastName,
+            email,
+            password,
+            tenant,
+            "confirm"
+          );
+          logObject(
+            "responseFromSendEmail during confirmation",
+            responseFromSendEmail
+          );
+          if (responseFromSendEmail.success === true) {
+            let responseFromDeleteCandidate = await requestAccess.delete(
+              tenant,
+              filter
+            );
+            if (responseFromDeleteCandidate.success === true) {
+              return {
+                success: true,
+                message: "candidate successfully confirmed",
+                data: jsonifyCreatedUser,
+              };
+            } else if (responseFromDeleteCandidate.success === false) {
+              if (responseFromDeleteCandidate.error) {
+                return {
+                  success: false,
+                  message: responseFromDeleteCandidate.message,
+                  data: responseFromDeleteCandidate.data,
+                  error: responseFromDeleteCandidate.error,
+                };
+              } else {
+                return {
+                  success: false,
+                  message: responseFromDeleteCandidate.message,
+                  data: responseFromDeleteCandidate.data,
+                };
+              }
+            }
+          } else if (responseFromSendEmail.success === false) {
+            const error = responseFromSendEmail.error
+              ? responseFromSendEmail.error
+              : {};
+
+            return {
+              success: false,
+              message: responseFromSendEmail.message,
+              error,
+            };
+          }
+        } else if (responseFromCreateUser.success === false) {
+          const error = responseFromCreateUser.error
+            ? responseFromCreateUser.error
+            : {};
+          return {
+            success: false,
+            message: responseFromCreateUser.message,
+            error,
+          };
+        }
+      } else if (
+        responseFromListCandidate.success === true &&
+        isEmpty(responseFromListCandidate.data)
+      ) {
+        return {
+          success: false,
+          message: "the candidate does not exist",
+        };
+      } else if (responseFromListCandidate.success === false) {
+        const error = responseFromListCandidate.error
+          ? responseFromListCandidate.error
+          : {};
+        return {
+          success: false,
+          message: "unable to retrieve candidate",
+          error,
+        };
+      }
+    } catch (e) {
+      if (e.code === 11000) {
+        return {
+          success: false,
+          message: "duplicate entry",
+          error: e.keyValue,
+          status: httpStatus.BAD_REQUEST,
+        };
+      }
+      return {
+        success: false,
+        message: "util server error",
+        error: e.message,
+        status: httpStatus.INTERNAL_SERVER_ERROR,
+      };
+    }
+  },
+
+  delete: async (tenant, filter) => {
+    try {
+      let responseFromRemoveCandidate = await CandidateModel(
+        tenant.toLowerCase()
+      ).remove({
+        filter,
+      });
+
+      if (responseFromRemoveCandidate.success == true) {
+        return {
+          success: true,
+          message: responseFromRemoveCandidate.message,
+          data: responseFromRemoveCandidate.data,
+        };
+      } else if (responseFromRemoveCandidate.success == false) {
+        if (responseFromRemoveCandidate.error) {
+          return {
+            success: false,
+            message: responseFromRemoveCandidate.message,
+            error: responseFromRemoveCandidate.error,
+          };
+        } else {
+          return {
+            success: false,
+            message: responseFromRemoveCandidate.message,
+          };
+        }
+      }
+    } catch (e) {
+      return {
+        success: false,
+        message: "util server error",
+        error: e.message,
+      };
+    }
+  },
+};
+
+module.exports = requestAccess;
