11507a8d4e8a89753c0fe987ba95e8f65cc29fd7
Sserunjogi Richard <sserurich@gmail.com>
2023-02-09 11:55:39 +0300

    refactored forecast train job to ensure authentication header is added while making request to api

--- src/predict/jobs/train/config.py
+++ src/predict/jobs/train/config.py
@@ -4,8 +4,9 @@ import pandas as pd
 from pymongo import MongoClient
 from dotenv import load_dotenv
 from pathlib import Path
+
 BASE_DIR = Path(__file__).resolve().parent
-dotenv_path = os.path.join(BASE_DIR, '.env')
+dotenv_path = os.path.join(BASE_DIR, ".env")
 load_dotenv(dotenv_path)
 
 
@@ -13,59 +14,67 @@ class Config:
     DEBUG = False
     TESTING = False
     CSRF_ENABLED = True
-    CREDENTIALS = os.getenv('GOOGLE_APPLICATION_CREDENTIALS')
-    GOOGLE_CLOUD_PROJECT_ID = os.getenv('GOOGLE_CLOUD_PROJECT_ID')
-    TRAIN_DATE_HOUR_START = pd.to_datetime(os.getenv('TRAIN_START_DATE'))
+    CREDENTIALS = os.getenv("GOOGLE_APPLICATION_CREDENTIALS")
+    GOOGLE_CLOUD_PROJECT_ID = os.getenv("GOOGLE_CLOUD_PROJECT_ID")
+    TRAIN_DATE_HOUR_START = pd.to_datetime(os.getenv("TRAIN_START_DATE"))
     TRAIN_DATE_HOUR_END = pd.to_datetime(
-        os.environ.get('TRAIN_END_DATE', datetime.strftime(datetime.now(), '%Y-%m-%d %H:00:00'))
+        os.environ.get(
+            "TRAIN_END_DATE", datetime.strftime(datetime.now(), "%Y-%m-%d %H:00:00")
+        )
     )
-    TENANT = os.getenv('TENANT', 'airqo')
-    MONTHS_OF_DATA = os.getenv('MONTHS_OF_DATA', 2)
+    TENANT = os.getenv("TENANT", "airqo")
+    MONTHS_OF_DATA = os.getenv("MONTHS_OF_DATA", 2)
+
 
 class ProductionConfig(Config):
-    DB_NAME = os.getenv('DB_NAME_PROD')
-    MONGO_URI = os.getenv('MONGO_GCE_URI')
-    MLFLOW_TRACKING_URI = os.getenv('MLFLOW_TRACKING_URI')
-    MLFLOW_TRACKING_USERNAME = os.getenv('MLFLOW_TRACKING_USERNAME')
-    MLFLOW_TRACKING_PASSWORD = os.getenv('MLFLOW_TRACKING_PASSWORD')
-    AIRQO_PREDICT_BUCKET = os.getenv('AIRQO_PREDICT_BUCKET_PROD')
-    AIRQO_API_BASE_URL = os.getenv('AIRQO_API_BASE_URL_PROD')
+    DB_NAME = os.getenv("DB_NAME_PROD")
+    MONGO_URI = os.getenv("MONGO_GCE_URI")
+    MLFLOW_TRACKING_URI = os.getenv("MLFLOW_TRACKING_URI")
+    MLFLOW_TRACKING_USERNAME = os.getenv("MLFLOW_TRACKING_USERNAME")
+    MLFLOW_TRACKING_PASSWORD = os.getenv("MLFLOW_TRACKING_PASSWORD")
+    AIRQO_PREDICT_BUCKET = os.getenv("AIRQO_PREDICT_BUCKET_PROD")
+    AIRQO_API_BASE_URL = os.getenv("AIRQO_API_BASE_URL_PROD")
+    AIRQO_API_AUTH_TOKEN = os.getenv("AIRQO_API_AUTH_TOKEN_PROD")
+
 
 class TestingConfig(Config):
     DEBUG = True
     TESTING = True
-    MONGO_URI = os.getenv('MONGO_GCE_URI')
-    DB_NAME = os.getenv('DB_NAME_STAGE')
-    MLFLOW_TRACKING_URI = os.getenv('MLFLOW_TRACKING_URI')
-    MLFLOW_TRACKING_USERNAME = os.getenv('MLFLOW_TRACKING_USERNAME')
-    MLFLOW_TRACKING_PASSWORD = os.getenv('MLFLOW_TRACKING_PASSWORD')
-    AIRQO_PREDICT_BUCKET = os.getenv('AIRQO_PREDICT_BUCKET_STAGE')
-    AIRQO_API_BASE_URL = os.getenv('AIRQO_API_BASE_URL_STAGE')
+    MONGO_URI = os.getenv("MONGO_GCE_URI")
+    DB_NAME = os.getenv("DB_NAME_STAGE")
+    MLFLOW_TRACKING_URI = os.getenv("MLFLOW_TRACKING_URI")
+    MLFLOW_TRACKING_USERNAME = os.getenv("MLFLOW_TRACKING_USERNAME")
+    MLFLOW_TRACKING_PASSWORD = os.getenv("MLFLOW_TRACKING_PASSWORD")
+    AIRQO_PREDICT_BUCKET = os.getenv("AIRQO_PREDICT_BUCKET_STAGE")
+    AIRQO_API_BASE_URL = os.getenv("AIRQO_API_BASE_URL_PROD")
+    AIRQO_API_AUTH_TOKEN = os.getenv("AIRQO_API_AUTH_TOKEN_STAGE")
+
 
 class DevelopmentConfig(Config):
     DEVELOPMENT = True
     DEBUG = True
-    MONGO_URI = os.getenv('MONGO_GCE_URI')
-    DB_NAME = os.getenv('DB_NAME_STAGE')
-    MLFLOW_TRACKING_URI = os.getenv('MLFLOW_TRACKING_URI')
-    AIRQO_PREDICT_BUCKET = os.getenv('AIRQO_PREDICT_BUCKET_DEV')
-    AIRQO_API_BASE_URL = os.getenv('AIRQO_API_BASE_URL_STAGE')
+    MONGO_URI = os.getenv("MONGO_GCE_URI")
+    DB_NAME = os.getenv("DB_NAME_STAGE")
+    MLFLOW_TRACKING_URI = os.getenv("MLFLOW_TRACKING_URI")
+    AIRQO_PREDICT_BUCKET = os.getenv("AIRQO_PREDICT_BUCKET_DEV")
+    AIRQO_API_BASE_URL = os.getenv("AIRQO_API_BASE_URL_STAGE")
+    AIRQO_API_AUTH_TOKEN = os.getenv("AIRQO_API_AUTH_TOKEN_STAGE")
 
 
 app_config = {
     "development": DevelopmentConfig,
     "testing": TestingConfig,
     "production": ProductionConfig,
-    "staging": TestingConfig
+    "staging": TestingConfig,
 }
 
 environment = os.getenv("ENV")
-print("ENVIRONMENT", environment or 'staging')
+print("ENVIRONMENT", environment or "staging")
 
 configuration = app_config.get(environment, TestingConfig)
 
 
 def connect_mongo():
     client = MongoClient(configuration.MONGO_URI)
-    db = client[f'{configuration.DB_NAME}_{configuration.TENANT.lower()}']
+    db = client[f"{configuration.DB_NAME}_{configuration.TENANT.lower()}"]
     return db
--- src/predict/jobs/train/models.py
+++ src/predict/jobs/train/models.py
@@ -10,6 +10,7 @@ class BaseModel:
     def __init__(self):
         self.db = connect_mongo()
 
+
 class BoundaryLayer(BaseModel):
     def __init__(self):
         super().__init__()
@@ -23,10 +24,15 @@ class Site(BaseModel):
         super().__init__()
 
     def get_sites():
-        #return self.db.sites.find()
+        # return self.db.sites.find()
         try:
-            api_url = f'{configuration.AIRQO_API_BASE_URL}devices/sites?tenant={configuration.TENANT}'
-            results = requests.get(api_url,verify=False)
+            api_url = f"{configuration.AIRQO_API_BASE_URL}devices/sites?tenant={configuration.TENANT}&network={configuration.TENANT}"
+            print(api_url)
+            authorisation_token = f"'TOK:<{configuration.AIRQO_API_AUTH_TOKEN}>'"
+            results = requests.get(
+                api_url, verify=False, headers={"Authorization": authorisation_token}
+            )
+            print(results)
             sites_data = results.json()["sites"]
         except Exception as ex:
             print("Sites Url returned an error : " + str(ex))
@@ -42,30 +48,35 @@ class Events(BaseModel):
     def get_events_db(self):
         # TODO add two/three months data filtering
         start_date, _ = previous_months_range(int(configuration.MONTHS_OF_DATA))
-        created_at = str_to_date(date_to_str(
-            start_date.replace(microsecond=0, second=0, minute=0) - timedelta(days=4)))
+        created_at = str_to_date(
+            date_to_str(
+                start_date.replace(microsecond=0, second=0, minute=0)
+                - timedelta(days=4)
+            )
+        )
         print("created at", created_at)
-        time_format = '%Y-%m-%dT%H:%M:%S%z'
-        query = {'$match': {'first': {'$gte': created_at}}}
-        projection = {'$project': {
-            '_id': 0,
-            'first': {
-                '$dateToString': {
-                    'format': time_format, 'date': '$first', 'timezone': 'Africa/Kampala'
-                }},
-            'values': {
-                'pm2_5': 1,
-                'device_number': 1,
-                'time': 1
+        time_format = "%Y-%m-%dT%H:%M:%S%z"
+        query = {"$match": {"first": {"$gte": created_at}}}
+        projection = {
+            "$project": {
+                "_id": 0,
+                "first": {
+                    "$dateToString": {
+                        "format": time_format,
+                        "date": "$first",
+                        "timezone": "Africa/Kampala",
+                    }
+                },
+                "values": {"pm2_5": 1, "device_number": 1, "time": 1},
             }
-        }}
+        }
 
         return list(self.db.events.aggregate([query, projection]))
 
     def get_events(self):
         try:
-            api_url = f'{configuration.AIRQO_API_BASE_URL}device/events?tenant={configuration.TENANT}&active=yes&startTime=2021-11-19'
-            results = requests.get(api_url,verify=False)
+            api_url = f"{configuration.AIRQO_API_BASE_URL}device/events?tenant={configuration.TENANT}&active=yes&startTime=2021-11-19"
+            results = requests.get(api_url, verify=False)
             events_data = results.json()["measurements"]
         except Exception as ex:
             print("Events Url returned an error : " + str(ex))
@@ -77,12 +88,15 @@ class Events(BaseModel):
 class Devices(BaseModel):
     def __init__(self):
         super().__init__()
-    
+
     def get_devices():
-  
+
         try:
-            api_url = f'{configuration.AIRQO_API_BASE_URL}devices?tenant={configuration.TENANT}&active=yes'
-            results = requests.get(api_url,verify=False)
+            api_url = f"{configuration.AIRQO_API_BASE_URL}devices?tenant={configuration.TENANT}&active=yes&network={configuration.TENANT}"
+            authorisation_token = f"'TOK:<{configuration.AIRQO_API_AUTH_TOKEN}>'"
+            results = requests.get(
+                api_url, verify=False, headers={"Authorization": authorisation_token}
+            )
             devices_data = results.json()["devices"]
         except Exception as ex:
             print("Devices Url returned an error : " + str(ex))
