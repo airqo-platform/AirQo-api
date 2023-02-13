73cfbe282ec5406a5e3725f3cefbba9f77726c9a
baalmart <bbaalmart@gmail.com>
2023-02-13 13:45:57 +0300

    return 200 for cases where photo does not exist

--- src/device-registry/models/Photo.js
+++ src/device-registry/models/Photo.js
@@ -153,11 +153,10 @@ photoSchema.statics = {
 
       if (!isEmpty(response)) {
         logObject("response", response);
-        let data = response;
         return {
           success: true,
           message: "successfully retrieved the photo(s)",
-          data,
+          data: response,
           status: HTTPStatus.OK,
         };
       } else if (isEmpty(response)) {
@@ -165,7 +164,7 @@ photoSchema.statics = {
           success: true,
           message: "this photo does not exist, please crosscheck",
           status: HTTPStatus.OK,
-          errors: { message: "this photo does not exist, please crosscheck" },
+          data: [],
         };
       }
     } catch (err) {
