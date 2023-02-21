486afb911c2690e9eff7814268cf7fc49fd3d3f1
Mike Mwanje <mwanjemike767@gmail.com>
2023-02-21 18:18:06 +0300

    Use UTC times instead of Africa/Kampala

--- infra/terraform/gcp/modules/ComputeResourcePolicy/monthly-k8s-runners.tf
+++ infra/terraform/gcp/modules/ComputeResourcePolicy/monthly-k8s-runners.tf
@@ -5,12 +5,12 @@ resource "google_compute_resource_policy" "monthly_k8s_runners" {
   description = "Start and stop instances that run the Kubernetes monthly cronjobs"
   instance_schedule_policy {
     vm_start_schedule {
-      schedule = "0 2 1 * *"
+      schedule = "0 3 1 * *"
     }
     vm_stop_schedule {
-      schedule = "0 4 1 * *"
+      schedule = "0 5 1 * *"
     }
-    time_zone = "Africa/Kampala"
+    time_zone = "UTC"
   }
 }
 # terraform import google_compute_resource_policy.monthly_k8s_runners projects/${var.project_id}/regions/${var.region}/resourcePolicies/monthly-k8s-runners
