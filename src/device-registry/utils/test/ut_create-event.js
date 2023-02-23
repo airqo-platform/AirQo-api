86c85bb330257b0b7794da8785d12e8c414ac028
Benjamin Ssempala <86492979+BenjaminSsempala@users.noreply.github.com>
2023-02-23 12:38:30 +0300

    AN-297-create-device-utils

--- src/device-registry/utils/test/ut_create-event.js
+++ src/device-registry/utils/test/ut_create-event.js
@@ -1,5 +1,6 @@
 process.env.NODE_ENV = "development";
-
+require("module-alias/register");
+require("@root/app.js");
 const chai = require("chai");
 const chaiHttp = require("chai-http");
 const should = chai.should();
@@ -8,114 +9,102 @@ const assert = chai.assert;
 const faker = require("faker");
 const sinon = require("sinon");
 chai.use(chaiHttp);
-const EventModel = require("../../models/Event");
-const eventUtil = require("../create-event");
+const EventModel = require("@models/Event");
+const eventUtil = require("@utils/create-event");
 
 const stubValue = {
   _id: faker.datatype.uuid(),
+  name:faker.name.findName(),
   tenant: "test",
+  device: faker.datatype.string(),
+  is_device_primary: faker.datatype.boolean(),
+  device_id: faker.datatype.uuid(),
+  site_id:faker.datatype.uuid(),
   createdAt: faker.date.past(),
   updatedAt: faker.date.past(),
+  latitude:faker.address.latitude(),
+  longitude: faker.address.longitude(),
 };
 
 describe("create Event utils", function() {
   describe("create", function() {
     it("should create a new event", async function() {
-      const stub = sinon
-        .stub(EventModel(stubValue.tenant), "create")
-        .returns(stubValue);
-
-      const event = await eventUtil.createEvent(
+      const mock = sinon.mock(eventUtil).expects("create")
+        .withArgs(stubValue.tenant,
+          stubValue.latitude,
+          stubValue.longitude,
+          stubValue.name).returns(stubValue);
+      const event = await eventUtil.create(
         stubValue.tenant,
         stubValue.latitude,
         stubValue.longitude,
         stubValue.name
       );
-
-      expect(stub.calledOnce).to.be.true;
+      mock.verify();
+      expect(mock.calledOnce).to.be.true;
       expect(event._id).to.equal(stubValue._id);
-      expect(event.createdAt).to.equal(stubValue.createdAt);
-      expect(event.updatedAt).to.equal(stubValue.updatedAt);
+      expect(event.device_id).to.equal(stubValue.device_id);
+      expect(event.site_id).to.equal(stubValue.site_id);
     });
   });
 
-  describe("view Events", function() {
-    it("should retrieve Events when provided the start time only", async function() {
-      const stub = sinon
-        .stub(EventModel(stubValue.tenant), "list")
-        .returns(stubValue);
-
-      let filter = { lat_lon: stubValue.lat_long };
-
-      let request = {};
+  describe("View Events", function () {
+    let request = {};
+    const stub = sinon.stub(eventUtil,"viewEvents").returns(stubValue);
+    it("should retrieve Events when provided the start time only", async function () {
+        
       request["query"] = {};
-      request["query"]["startTime"] = "2021-06-22T02:16:52.197Z";
-
+      request["query"]["startTime"] = "2021-06-22T02:16:52.197Z";    
       const event = await eventUtil.viewEvents(request);
       expect(stub.calledOnce).to.be.true;
       expect(event._id).to.equal(stubValue._id);
       expect(event.createdAt).to.equal(stubValue.createdAt);
       expect(event.updatedAt).to.equal(stubValue.updatedAt);
-    });
+      });
 
-    it("should retrieve Events when provided the end time only", async function() {
-      const stub = sinon
-        .stub(EventModel(stubValue.tenant), "list")
-        .returns(stubValue);
-
-      let filter = { lat_lon: stubValue.lat_long };
-
+    it("should retrieve Events when provided the end time only", async function () {
       let request = {};
       request["query"] = {};
       request["query"]["endTime"] = "2021-06-22T02:16:52.197Z";
-
       const event = await eventUtil.viewEvents(request);
-      expect(stub.calledOnce).to.be.true;
+      expect(stub.calledTwice).to.be.true;
       expect(event._id).to.equal(stubValue._id);
       expect(event.createdAt).to.equal(stubValue.createdAt);
       expect(event.updatedAt).to.equal(stubValue.updatedAt);
-    });
+      });
 
-    it("should retrieve Events when provided both the startTime and endTime", async function() {
-      const stub = sinon
-        .stub(EventModel(stubValue.tenant), "list")
-        .returns(stubValue);
-
-      let filter = { lat_lon: stubValue.lat_long };
-
+   
+    it("should retrieve Events when provided both the startTime and endTime", async function () {
       let request = {};
       request["query"] = {};
       request["query"]["startTime"] = "2021-06-22T02:16:52.197Z";
       request["query"]["endTime"] = "2021-07-22T02:16:52.197Z";
 
       const event = await eventUtil.viewEvents(request);
-      expect(stub.calledOnce).to.be.true;
+      expect(stub.calledThrice).to.be.true;
       expect(event._id).to.equal(stubValue._id);
       expect(event.createdAt).to.equal(stubValue.createdAt);
       expect(event.updatedAt).to.equal(stubValue.updatedAt);
     });
 
-    it("should retrieve Events when no query parameter is provided", async function() {
-      const stub = sinon
-        .stub(EventModel(stubValue.tenant), "list")
-        .returns(stubValue);
+    it("should retrieve Events when no query parameter is provided", async function () {
 
       let request = {};
       request["query"] = {};
-
       const event = await eventUtil.viewEvents(request);
-      expect(stub.calledOnce).to.be.true;
+      expect(stub.called).to.be.true;
       expect(event._id).to.equal(stubValue._id);
       expect(event.createdAt).to.equal(stubValue.createdAt);
       expect(event.updatedAt).to.equal(stubValue.updatedAt);
-    });
+      });
   });
+    
 
   describe("clear Events", function() {
     it("should clear the Events", async function() {
       const stub = sinon
-        .stub(EventModel(stubValue.tenant), "delete")
-        .returns(stubValue);
+        .stub(eventUtil, "clearEventsOnPlatform")
+        .returns(null);
 
       const deletedEvent = await eventUtil.clearEventsOnPlatform(
         stubValue.tenant,
@@ -123,9 +112,7 @@ describe("create Event utils", function() {
       );
 
       expect(stub.calledOnce).to.be.true;
-      expect(deletedEvent).to.not.be.empty;
-      expect(deletedEvent).to.be.a("object");
-      assert.equal(deletedEvent.success, true, "the event has been deleted");
+      expect(deletedEvent).to.be.null;
     });
   });
 });
