07eeeb2a69dfed58faeb2406f117ec0bd97b692d
Noah Nsimbe <37845280+NoahNsimbe@users.noreply.github.com>
2023-03-14 15:12:15 +0300

    Added celery

--- src/device-monitoring/app.py
+++ src/device-monitoring/app.py
@@ -1,13 +1,18 @@
+from datetime import timedelta
 from flask import Flask
+from celery import Celery
+from celery.utils.log import get_task_logger
 import logging
 import os
 from flask_caching import Cache
 from flask_cors import CORS
 from flask_pymongo import PyMongo
 from config import constants
+from config.constants import Config
 from helpers.pre_request import PreRequest
 
 
+celery_logger = get_task_logger(__name__)
 _logger = logging.getLogger(__name__)
 
 # db initialization
@@ -17,15 +22,15 @@ cache = Cache()
 
 def create_app(environment):
     # create a flask app instance
-    app = Flask(__name__)
+    application = Flask(__name__)
 
-    app.config.from_object(constants.app_config[environment])
+    application.config.from_object(constants.app_config[environment])
 
-    mongo.init_app(app)
-    cache.init_app(app)
+    mongo.init_app(application)
+    cache.init_app(application)
 
     # Allow cross-browser resource sharing
-    CORS(app)
+    CORS(application)
 
     # import blueprints
     from controllers.check_health import health_check_bp
@@ -33,14 +38,46 @@ def create_app(environment):
     from controllers.collocation import collocation_bp
 
     # register blueprints
-    app.register_blueprint(health_check_bp)
-    app.register_blueprint(device_status_bp)
-    app.register_blueprint(collocation_bp)
+    application.register_blueprint(health_check_bp)
+    application.register_blueprint(device_status_bp)
+    application.register_blueprint(collocation_bp)
 
-    return app
+    return application
 
 
 app = create_app(os.getenv("FLASK_ENV"))
+
+
+def make_celery(application):
+    application.config['broker_url'] = f'{Config.REDIS_URL}/0'
+    application.config['result_backend'] = f'{Config.REDIS_URL}/0'
+    application.config['beat_schedule'] = {
+        'collocation_periodic_task': {
+            'task': 'collocation_periodic_task',
+            'schedule': timedelta(seconds=15),
+        }
+    }
+
+    celery_app = Celery(application.import_name, broker=application.config['broker_url'])
+    celery_app.conf.update(application.config)
+    task_base = celery_app.Task
+
+    class ContextTask(task_base):
+        abstract = True
+
+        def __call__(self, *args, **kwargs):
+            with application.app_context():
+                return task_base.__call__(self, *args, **kwargs)
+    celery_app.Task = ContextTask
+    return celery_app
+
+
+celery = make_celery(app)
+
+
+@celery.task(name="collocation_periodic_task")
+def collocation_task():
+    celery_logger.info("Collocation periodic task")
 
 
 @app.before_request
