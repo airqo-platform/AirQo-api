4ad1f8293ba327b99943257eb456b2c87fe2faf3
baalmart <bbaalmart@gmail.com>
2023-03-20 16:42:12 +0300

    add new middleware for logging users

--- src/auth-service/config/middleware.js
+++ src/auth-service/config/middleware.js
@@ -20,6 +20,46 @@ module.exports = (app) => {
 
   const options = { mongooseConnection: mongoose.connection };
 
+  const logUserActivity = (req, res, next) => {
+    const userId = req.user ? req.user.id : "anonymous";
+    const message = `User ${userId} performed ${req.method} request to ${req.originalUrl}`;
+    logger.info(message);
+    next();
+  };
+
+  // Use logging middleware for all routes
+  app.use(logUserActivity);
+
+  /***
+   * 
+   * In this example, we first connect to MongoDB 
+   * using Mongoose, and define a schema and 
+   * model for the log collection. 
+   * We then configure the Winston logger to log to the 
+   * console and to a MongoDB collection named "logs", 
+   * using the winston.transports.MongoDB transport.
+
+We define a middleware function called logUserActivity 
+that logs the user's ID (if available) and 
+the HTTP method and URL of the incoming request, 
+using the configured Winston logger. 
+This middleware is added to the Express app using app.use, 
+which applies it to all routes.
+
+Finally, we define a simple route handler that returns 
+a "Hello World!" response, and start the server on port 3000.
+
+With this setup, all incoming requests to the Node.js 
+authentication microservice will be logged to the console 
+and to a MongoDB collection, with the user's ID 
+(if available), HTTP method, and URL. 
+You can customize the logging behavior 
+further by modifying the Winston logger or 
+the middleware function as needed.
+   * 
+   * 
+   */
+
   app.use(
     session({
       secret: process.env.SESSION_SECRET,
