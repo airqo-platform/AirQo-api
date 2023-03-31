9e07975b67babaca2bb5c2a445dd63b6ac8c1684
Mutabazi Noble <mutabazinoble@gmail.com>
2023-03-31 16:26:44 +0300

    update api to daily forecasts

--- src/predict/api/helpers/utils.py
+++ src/predict/api/helpers/utils.py
@@ -2,14 +2,10 @@ import json
 import os
 from datetime import datetime, timedelta
 
-import pandas as pd
-import pytz
 import requests
 from dotenv import load_dotenv
-from geopy import distance
 
 from config.constants import connect_mongo
-from models import datamanagement as dm
 
 load_dotenv()
 
@@ -18,83 +14,6 @@ MET_API_CLIENT_ID = os.getenv('MET_API_CLIENT_ID')
 MET_API_CLIENT_SECRET = os.getenv('MET_API_CLIENT_SECRET')
 
 db = connect_mongo()
-
-
-def get_hourly_met_forecasts():
-    """
-        gets hourly spot wether forecasts from met 
-        office(https://metoffice.apiconnect.ibmcloud.com/metoffice/production/)
-        saves them to big query table.
-    """
-    weather_predictions = []
-    specified_locations = dm.get_all_static_channels()
-    # specified_locationsx = specified_locations[0:2]
-    if specified_locations:
-        for i in range(0, len(specified_locations)):
-            channel_id = specified_locations[i].get('channel_id')
-            latitude = specified_locations[i].get('latitude')
-            longitude = specified_locations[i].get('longitude')
-
-            forecast_results, status_code = get_location_hourly_weather_forecasts(latitude, longitude)
-
-            if status_code == 200:
-                features_geometry = forecast_results['features'][0]['geometry']
-                features_properties = forecast_results['features'][0]['properties']
-                forecast_timeseries = features_properties['timeSeries']
-                location = features_properties['location'].get('name')
-                location_lat = features_geometry.get('coordinates')[1]
-                location_long = features_geometry.get('coordinates')[0]
-                point_elevation = features_geometry.get('coordinates')[2]
-                distance_from_requested_point = features_properties['requestPointDistance']
-                created_at = datetime.now()
-
-                if forecast_timeseries:
-                    for i in range(0, len(forecast_timeseries)):
-                        location_name = location
-                        location_latitude = location_lat
-                        location_longitude = location_long
-                        time = pd.to_datetime(forecast_timeseries[i].get('time'))
-                        screen_temperature = forecast_timeseries[i].get('screenTemperature')
-                        screen_dewpoint_temperature = forecast_timeseries[i].get('screenDewPointTemperature')
-                        feels_like_temperature = forecast_timeseries[i].get('feelsLikeTemperature')
-                        wind_speed_10m = forecast_timeseries[i].get('windSpeed10m')
-                        wind_direction_from_10m = forecast_timeseries[i].get('windDirectionFrom10m')
-                        wind_gust_speed_10m = forecast_timeseries[i].get('windGustSpeed10m')
-                        visibility = forecast_timeseries[i].get('visibility')
-                        screen_relative_humidity = forecast_timeseries[i].get('screenRelativeHumidity')
-                        mean_sea_level_pressure = forecast_timeseries[i].get('mslp')
-                        uvIndex = forecast_timeseries[i].get('uvIndex')
-                        significant_weather_code = forecast_timeseries[i].get('significantWeatherCode')
-                        precipitation_rate = forecast_timeseries[i].get('precipitationRate')
-                        total_precipitation_amount = forecast_timeseries[i].get('totalPrecipAmount')
-                        total_snow_amount = forecast_timeseries[i].get('totalSnowAmount')
-                        prob_of_precipitation = forecast_timeseries[i].get('probOfPrecipitation ')
-                        created_at = created_at
-                        airqo_channel_id = channel_id
-                        elevation = point_elevation
-                        request_distance_point = distance_from_requested_point
-
-                        weather_predictions_tuple = (location_name, location_latitude, location_longitude, time,
-                                                     screen_temperature, screen_dewpoint_temperature,
-                                                     feels_like_temperature, wind_speed_10m,
-                                                     wind_direction_from_10m, wind_gust_speed_10m, visibility,
-                                                     screen_relative_humidity,
-                                                     mean_sea_level_pressure, uvIndex, significant_weather_code,
-                                                     precipitation_rate,
-                                                     total_precipitation_amount, total_snow_amount,
-                                                     prob_of_precipitation, created_at,
-                                                     airqo_channel_id, elevation, request_distance_point)
-
-                        weather_predictions.append(weather_predictions_tuple)
-
-        # print(weather_predictions)
-        # save_json_data('weather_forecast.json', weather_predictions)
-
-        results = dm.save_weather_forecasts(weather_predictions)
-        print(results)
-
-    else:
-        return "Failed to get forecasts for specified coordinates", status_code
 
 
 def get_location_hourly_weather_forecasts(latitude: float, longitude: float):
@@ -133,57 +52,6 @@ def save_json_data(file_name, data_to_save):
     '''
     with open(file_name, 'w') as fp:
         json.dump(data_to_save, fp)
-
-
-def checkKey(dict, key):
-    '''
-        checks wether specified key is available in the specified dictionary.
-    '''
-    if key in dict.keys():
-        return dict[key]
-    else:
-        return "Channel Id Not available"
-
-
-def get_closest_channel(latitude, longitude) -> int:
-    '''
-        gets and returns the channel with the minimum distance 
-        from the location with the specified latitude and longitude
-    '''
-    specified_coordinates = (latitude, longitude)
-    channel_ids_with_distances_from_specified_coordinates = {}
-
-    all_coordinates = dm.get_all_coordinates()
-
-    for i in range(0, len(all_coordinates)):
-        channel_id = all_coordinates[i].get('channel_id')
-        channel_coordinates = (all_coordinates[i].get('latitude'), all_coordinates[i].get('longitude'))
-        distance_between_coordinates = distance.distance(specified_coordinates, channel_coordinates).km
-        channel_ids_with_distances_from_specified_coordinates[channel_id] = distance_between_coordinates
-
-    channel_id_with_min_distance = min(channel_ids_with_distances_from_specified_coordinates.keys(),
-                                       key=(lambda k: channel_ids_with_distances_from_specified_coordinates[k]))
-    minimum_distance = channel_ids_with_distances_from_specified_coordinates[channel_id_with_min_distance]
-    return channel_id_with_min_distance
-
-
-def convert_local_string_date_to_tz_aware_datetime(local_date_string):
-    '''
-        converts a date string into localised datetime object for Africa/Kampala timezone.
-    '''
-    timezone = pytz.timezone('Africa/Kampala')
-    date_time_obj = datetime.strptime(local_date_string, '%Y-%m-%d %H:%M:%S+3:00')
-    timezone_date_time_obj = timezone.localize(date_time_obj)
-    return timezone_date_time_obj
-
-
-def string_to_hourly_datetime(my_list):
-    '''
-    converts a datetime string in a list to a format known by the gp model
-    '''
-    my_list[2] = datetime.strptime(my_list[2], '%Y-%m-%dT%H:%M:%SZ')
-    my_list[2] = my_list[2].timestamp() / 3600
-    return my_list
 
 
 def get_all_gp_predictions():
@@ -260,10 +128,5 @@ def str_to_date(st):
 
 
 if __name__ == '__main__':
-    # get_closest_channel(0.540184, 31.439622)
-    # get_all_coordinates()
-
+    # get_closest_channel(0.540184, 31.)
     print('main')
-
-    get_hourly_met_forecasts()
-    # print(get_gp_predictions('kampala'))
--- src/predict/api/models/predict.py
+++ src/predict/api/models/predict.py
@@ -1,25 +1,18 @@
-import ast
-import datetime
 import logging
-import time
 
 import numpy as np
-import pandas as pd
 from numpy import array
-from statsmodels.tsa.statespace.sarimax import SARIMAX
 
 from config.constants import connect_mongo
-from helpers import utils
-from models import datamanagement, processing
+from models import processing
 
 _logger = logging.getLogger(__name__)
 
 
-def get_next_24hr_forecasts_for_channel(channel_id, prediction_start_time):
+def get_next_24hr_forecasts_for_channel(channel_id, forecast_start_time):
     db = connect_mongo()
-    print(prediction_start_time)
-    print(type(prediction_start_time))
-    prediction_start_time = utils.str_to_date(prediction_start_time)
+    print(forecast_start_time)
+    print(type(forecast_start_time))
     channel_forecasts = list(db.hourly_forecasts.find(
         {'channel_id': channel_id
          }, {'_id': 0}).sort([('$natural', -1)]).limit(1))
@@ -28,18 +21,17 @@ def get_next_24hr_forecasts_for_channel(channel_id, prediction_start_time):
         for i in range(0, len(channel_forecasts[0]['forecasts'])):
             forecast_datetime = channel_forecasts[0]['forecast_time'][i]
             forecast_value = channel_forecasts[0]['forecasts'][i]
-            result = {'forecast_time': forecast_datetime, 'forecast_value': forecast_value,
-                      'lower_ci': 0, 'upper_ci': 0}
+            result = {'forecast_time': forecast_datetime, 'forecast_value': forecast_value}
             results.append(result)
 
     formated_results = {'forecasts': results}
     return formated_results
 
 
-def get_next_1_week_forecasts_for_channel(channel_id, prediction_start_date):
+def get_next_1_week_forecasts_for_channel(channel_id, forecast_start_date):
     db = connect_mongo()
-    print(prediction_start_date)
-    print(type(prediction_start_date))
+    print(forecast_start_date)
+    print(type(forecast_start_date))
 
     channel_forecasts = list(db.daily_forecasts.find(
         {'channel_id': channel_id
@@ -56,289 +48,6 @@ def get_next_1_week_forecasts_for_channel(channel_id, prediction_start_date):
 
     formatted_results = {'forecasts': results}
     return formatted_results
-
-
-def make_prediction_using_averages_for_all_locations(entered_chan, entered_time, entered_latitude, entered_longitude):
-    """
-        Generating predictions based on channel and time entered
-        saves the predictions in database and returns the predictions
-    """
-    channels_with_out_predictions = []
-
-    start_pred_time = (pd.to_datetime(entered_time))
-    current_best_config = datamanagement.get_channel_best_configurations(entered_chan)
-    # to de removed #
-    print(current_best_config)
-
-    start_pred = (pd.to_datetime(start_pred_time))
-    # generating list of hours based on start time
-    fcst_hours, start_hour = processing.forecast_hours(start_pred_time)
-    selected_channel_id = int(entered_chan)
-
-    data = datamanagement.get_channel_data_raw(selected_channel_id)
-
-    hourly_data = datamanagement.calculate_hourly_averages(data)
-    all_channel_data = hourly_data
-    if all_channel_data.empty:
-        results = {'predictions': 0, 'channel_id': selected_channel_id, 'errors': 'no data available for this channel'}
-        channels_with_out_predictions.append(results)
-
-    else:
-        all_channel_data_clean = fill_gaps_and_set_datetime(all_channel_data)
-        all_channel_data_clean.to_csv('all_channel_data_clean' + str(selected_channel_id) + '.csv')
-
-        if not current_best_config:
-            results = {'predictions': 0, 'errors': 'no configurations for this location'}
-            channels_with_out_predictions.append(results)
-
-        else:
-            best_number_of_days_back = int(current_best_config[0].get('number_of_days_to_use'))
-            considered_hours = int(current_best_config[0].get('considered_hours'))
-
-            start_base_time = str(start_pred - datetime.timedelta(best_number_of_days_back)) + '+3:00'
-            start_pred = str(start_pred) + '+3:00'
-            print(start_base_time + '\t\t start pred:' + start_pred)
-
-            # select final range of data to be used in forecast
-            data_to_use = all_channel_data_clean.loc[start_base_time:start_pred].values
-            pd.DataFrame(data_to_use).to_csv("data_to_use" + str(selected_channel_id) + ".csv")
-            print('length of data to use: \n')
-            print(len(data_to_use))
-
-            if len(data_to_use) == 0:
-                results = {'predictions': 0, 'errors': 'no predictions, check status of the device at this location',
-                           'channel_id': selected_channel_id}
-                channels_with_out_predictions.append(results)
-            else:
-                # generating mean, lower ci, upper ci
-                yhat_24, lower_ci, upper_ci = simple_forecast_ci(data_to_use, best_number_of_days_back,
-                                                                 considered_hours)
-
-                model_predictions = []
-                model_name = 'simple_average_prediction'
-                channel_id = selected_channel_id
-                location_name = " "
-                location_latitude = float(entered_latitude)
-                location_longitude = float(entered_longitude)
-                prediction_start_datetime = pd.to_datetime(start_pred)
-                created_at = datetime.datetime.now()
-                result_modified = []
-
-                for i in range(0, len(yhat_24)):
-                    hour_to_add = i + 1
-                    prediction_value = yhat_24[i]
-                    prediction_datetime = pd.to_datetime(
-                        prediction_start_datetime + datetime.timedelta(hours=hour_to_add))
-
-                    lower_confidence_interval_value = lower_ci[i]
-                    upper_confidence_interval_value = upper_ci[i]
-                    resultx = {'prediction_time': prediction_datetime, 'prediction_value': prediction_value,
-                               'lower_ci': lower_confidence_interval_value, 'upper_ci': upper_confidence_interval_value}
-                    result_modified.append(resultx)
-
-                    model_predictions_tuple = (
-                        model_name, channel_id, location_name, location_latitude, location_longitude,
-                        prediction_value, prediction_start_datetime, prediction_datetime,
-                        lower_confidence_interval_value,
-                        upper_confidence_interval_value, created_at)
-                    model_predictions.append(model_predictions_tuple)
-
-                results = {'prediction_start_time': start_pred, 'prediction_hours': fcst_hours,
-                           'predictions': yhat_24, 'prediction_upper_ci': upper_ci, 'prediction_lower_ci': lower_ci}
-
-                formated_results = {'predictions': result_modified}
-                print(formated_results)
-                # datamanagement.save_predictions_all(model_predictions)
-                _logger.info(f'Predictions: {results}')
-                return formated_results
-
-
-def make_24hr_predictions_for_specified_locations(specified_locations, entered_time):
-    """
-        makes the 24 hour predictions for all the locations(channels) and saves them in db.
-    """
-    all_channel_predictions = []
-    # specified_locations = dm.get_all_static_channels()
-    # entered_time =datetime.datetime.now()
-    if specified_locations:
-        for i in range(0, len(specified_locations)):
-            channel_id = specified_locations[i].get('channel_id')
-            latitude = specified_locations[i].get('latitude')
-            longitude = specified_locations[i].get('longitude')
-            # print statement below to be removed
-            print(channel_id, latitude, longitude)
-            results = make_prediction_using_averages_for_all_locations(channel_id, entered_time, latitude, longitude)
-            all_channel_predictions.append(results)
-    return all_channel_predictions
-
-
-def get_channel_with_coordinates(latitude, longitude) -> int:
-    channel_id = 0
-    ### return channel with the specified latitude and longitude
-    hourly_data = datamanagement.get_all_channels_hourly_data()
-    # hourly_data = datamanagement.calculate_hourly_averages(data)
-    channel_id = hourly_data.loc[hourly_data.latitude == latitude and hourly_data.longitude][0]
-    return channel_id
-
-
-def make_prediction_using_averages(entered_chan, entered_time, entered_latitude, entered_longitude):
-    """
-        Generating predictions based on channel and time entered
-        saves the predictions in database and returns the predictions
-    """
-
-    start_pred_time = entered_time
-    current_best_config = datamanagement.get_channel_best_configurations(entered_chan)
-
-    start_pred = (pd.to_datetime(start_pred_time))
-    # generating list of hours based on start time
-    fcst_hours, start_hour = processing.forecast_hours(start_pred_time)
-    selected_channel_id = int(entered_chan)
-    # hourly_data = datamanagement.get_channel_hourly_data(selected_channel_id)
-    # data = datamanagement.get_channel_data(selected_channel_id)
-    data = datamanagement.get_channel_data_raw(selected_channel_id)
-
-    hourly_data = datamanagement.calculate_hourly_averages(data)
-    all_channel_data = hourly_data
-    if all_channel_data.empty:
-        results = {'predictions': 0}
-        return results
-    else:
-        all_channel_data_clean = fill_gaps_and_set_datetime(all_channel_data)
-
-        if not current_best_config:
-            best_number_of_days_back = ast.literal_eval(current_best_config[0])[0]
-            config_to_use = ast.literal_eval(current_best_config[0])
-        else:
-            best_number_of_days_back = int(current_best_config[0].get('number_of_days_to_use'))
-            considered_hours = int(current_best_config[0].get('considered_hours'))
-
-        start_base_time = str(start_pred - datetime.timedelta(best_number_of_days_back)) + '+3:00'
-        start_pred = str(start_pred) + '+3:00'
-
-        # select final range of data to be used in forecast
-        data_to_use = all_channel_data_clean.loc[start_base_time:start_pred].values
-
-        # generating mean, lower ci, upper ci
-        yhat_24, lower_ci, upper_ci = simple_forecast_ci(data_to_use, best_number_of_days_back, considered_hours)
-
-        model_predictions = []
-        model_name = 'simple_average_prediction'
-        channel_id = selected_channel_id
-        location_name = " "
-        location_latitude = float(entered_latitude)
-        location_longitude = float(entered_longitude)
-        prediction_start_datetime = pd.to_datetime(start_pred)
-        created_at = datetime.datetime.now()
-        result_modified = [];
-
-        for i in range(0, len(yhat_24)):
-            hour_to_add = i + 1
-            prediction_value = yhat_24[i]
-            prediction_datetime = pd.to_datetime(prediction_start_datetime + datetime.timedelta(hours=hour_to_add))
-
-            lower_confidence_interval_value = lower_ci[i]
-            upper_confidence_interval_value = upper_ci[i]
-            resultx = {'prediction_time': prediction_datetime, 'prediction_value': prediction_value,
-                       'lower_ci': lower_confidence_interval_value, 'upper_ci': upper_confidence_interval_value}
-            result_modified.append(resultx)
-
-            model_predictions_tuple = (model_name, channel_id, location_name, location_latitude, location_longitude,
-                                       prediction_value, prediction_start_datetime, prediction_datetime,
-                                       lower_confidence_interval_value,
-                                       upper_confidence_interval_value, created_at)
-            model_predictions.append(model_predictions_tuple)
-
-        results = {'prediction_start_time': start_pred, 'prediction_hours': fcst_hours,
-                   'predictions': yhat_24, 'prediction_upper_ci': upper_ci, 'prediction_lower_ci': lower_ci}
-
-        formated_results = {'predictions': result_modified}
-        datamanagement.save_predictions(model_predictions)
-        _logger.info(f'Predictions: {results}')
-        return formated_results
-
-
-def make_prediction(enter_chan, enter_time) -> dict:
-    """Make a prediction using the saved best configurations."""
-    # validated_data = validate_inputs(input_data=data)
-
-    enter_time_split = enter_time.split("/")
-    enter_time_tuple = tuple([int(i) for i in enter_time_split])
-    endings = (0, 0, 0, 0, 0,)
-    start_time = (enter_time_tuple + endings)
-    start_pred_time = datetime.datetime.fromtimestamp(time.mktime(start_time))
-
-    # current_best_config = model_config.BEST_CONFIG_DICT.get(str(enter_chan))
-    current_best_config = datamanagement.get_channel_best_configurations(enter_chan)
-
-    start_pred = (pd.to_datetime(start_pred_time))
-
-    hourly_data = datamanagement.get_all_channels_hourly_data()
-    # hourly_data = datamanagement.calculate_hourly_averages(data)
-    # select all data relating to a particular channel
-    all_channel_data = hourly_data.loc[hourly_data.channel_id == enter_chan]
-
-    # clean the channel data, fill gaps and set datetime as index
-    all_channel_data_clean = fill_gaps_and_set_datetime(all_channel_data)
-
-    # set start time as being 8 weeks before start of forecast
-    # THis will need to be changed to max number of whole days
-
-    start_base_time = str(start_pred - datetime.timedelta(84)) + '+3:00'
-    start_pred = str(start_pred) + '+3:00'
-    # print((start_base_time))
-    # print((start_pred))
-    # select final range of data to be used in forecast
-    data_to_use = all_channel_data_clean.loc[start_base_time:(start_pred)].values
-
-    yhat, yhat_ci = sarima_forecast(data_to_use, current_best_config)
-    results = {'prediction time': start_pred, 'predictions': yhat.tolist(), 'prediction_ci': yhat_ci.tolist()}
-
-    _logger.info(f'Predictions: {results}')
-
-    return results
-
-
-def fill_gaps_and_set_datetime(d):
-    """
-         interpolating, removing nans and dropping columns
-    """
-    # Interpolating gaps within the data
-    # d['time'] = pd.to_datetime(d['time'])
-    d.time = pd.to_datetime(d.time)
-    print('datatypes', d.dtypes)
-    d = d.set_index('time')
-    d = d.drop('channel_id', axis=1)
-    d_cleaned = d.interpolate(method='time');
-    cleaned_data = d_cleaned.dropna();
-    return cleaned_data
-
-
-# sarima forecast
-# Takes the history (train set plus day by day testing) and configuration
-# converts history values to a single long series
-# generates the sarima model based on config parameters
-# fits the sarima model to the series data
-# creates yhat, a prediction of the next 24 hours int he test set
-def sarima_forecast(history, config):
-    order, sorder, trend = config
-    # convert history into a univariate series
-    series = to_series(history)
-    # define model
-    model = SARIMAX(series, order=order, seasonal_order=sorder, trend=trend, enforce_stationarity=False,
-                    enforce_invertibility=False)
-    #     model = SARIMAX(history, order=order, seasonal_order=sorder, trend=trend, enforce_stationarity=False, enforce_invertibility=False)
-    # fit model
-    model_fit = model.fit(disp=False)
-    # make one step forecast using predict method
-    #     yhat = model_fit.predict(len(series), len(series)+23)
-    # This does th same thing but using forecast which has slightly different features
-    fcst = model_fit.get_forecast(24)
-    # Generating list of mean forecasts
-    yhat = fcst.predicted_mean
-    # Generating confidence intervals
-    yhat_ci = fcst.conf_int()
-    return yhat, yhat_ci
 
 
 def to_series(data):
@@ -389,11 +98,3 @@ def simple_forecast_ci(history, configs, considered_hours):
     upper_ci_forecast = np.around(list_of_upper_ci_of_hourly_values[::-1], decimals=2)
 
     return forecast, lower_ci_forecast, upper_ci_forecast
-
-
-if __name__ == '__main__':
-    entered_time = datetime.datetime.now() - datetime.timedelta(hours=1)
-    entered_time = entered_time.strftime('%Y-%m-%d %H:%M')
-    print(entered_time)
-    specified_locations = datamanagement.get_all_static_channels()
-    all_channel_predictions = make_24hr_predictions_for_specified_locations(specified_locations, entered_time)
