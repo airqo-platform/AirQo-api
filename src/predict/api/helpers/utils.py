1b6ad03457e9ff3b73df3f1de51bd3aaaa2f4a17
Mutabazi Noble <mutabazinoble@gmail.com>
2023-04-02 11:24:25 +0300

    remove redudant code

--- src/predict/api/helpers/utils.py
+++ src/predict/api/helpers/utils.py
@@ -1,5 +1,3 @@
-import json
-import os
 from datetime import datetime, timedelta
 
 from dotenv import load_dotenv
@@ -7,30 +5,7 @@ from dotenv import load_dotenv
 from config.constants import connect_mongo
 
 load_dotenv()
-
-MET_API_URL = os.getenv('MET_API_URL')
-MET_API_CLIENT_ID = os.getenv('MET_API_CLIENT_ID')
-MET_API_CLIENT_SECRET = os.getenv('MET_API_CLIENT_SECRET')
-
 db = connect_mongo()
-
-
-def load_json_data(full_file_path):
-    """
-        loads and returns json data from the specified file path.
-    """
-    data = None
-    with open(full_file_path, 'r') as fp:
-        data = json.load(fp)
-    return data
-
-
-def save_json_data(file_name, data_to_save):
-    """
-        saves data to a json file in the path specified by the file_name argument.
-    """
-    with open(file_name, 'w') as fp:
-        json.dump(data_to_save, fp)
 
 
 def get_all_gp_predictions():
@@ -86,13 +61,6 @@ def get_gp_predictions_id(aq_id):
                   'airqloud': 1, 'created_at': 1, 'airqloud_id': 1}
     records = list(db.gp_predictions.find(query, projection))
     return records
-
-
-def str_to_date(st):
-    """
-    Converts a string to datetime
-    """
-    return datetime.strptime(st, '%Y-%m-%d %H:%M:%S')
 
 
 if __name__ == '__main__':
