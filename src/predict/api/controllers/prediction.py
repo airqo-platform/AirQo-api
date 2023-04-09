ecfc9f43866ff9ba14badae2cf419e10409e59f2
Mutabazi Noble <mutabazinoble@gmail.com>
2023-04-09 11:42:01 +0300

    Update endpoint to use query params, site_id

--- src/predict/api/controllers/prediction.py
+++ src/predict/api/controllers/prediction.py
@@ -1,15 +1,14 @@
-import datetime as dt
 import logging
 import os
 
 from dotenv import load_dotenv
 from flask import Blueprint, jsonify, request
+from flask_caching import Cache
 
 from config import constants
 from helpers.utils import get_all_gp_predictions, get_gp_predictions, get_gp_predictions_id
-from models.predict import get_next_1_week_forecasts_for_channel, get_next_24hr_forecasts_for_channel
+from models.predict import get_forecasts
 from routes import api
-from flask_caching import Cache
 
 load_dotenv()
 
@@ -28,30 +27,13 @@ ml_app = Blueprint('ml_app', __name__)
 
 
 @ml_app.route(api.route['next_24hr_forecasts'], methods=['GET'])
-def get_next_24hr_forecasts(device_channel_id, forecast_start_time):
+def get_next_24hr_forecasts():
     """
     Get forecasts for the next 24 hours from specified start time.
     """
     if request.method == 'GET':
-        if type(device_channel_id) is not int:
-            device_channel_id = int(device_channel_id)
-
-        if type(forecast_start_time) is not int:
-            try:
-                prediction_start_time = int(forecast_start_time)
-            except ValueError:
-                error = {
-                    "message": "Invalid prediction start time. expected unix timestamp format like 1500000000",
-                    "success": False}
-                return jsonify(error, 400)
-
-        prediction_start_timestamp = dt.datetime.fromtimestamp(
-            prediction_start_time)
-        prediction_start_datetime = dt.datetime.strftime(
-            prediction_start_timestamp, "%Y-%m-%d %H:00:00")
-        print(prediction_start_datetime)
-        result = get_next_24hr_forecasts_for_channel(
-            device_channel_id, prediction_start_datetime)
+        site_id = request.args.get('site_id')
+        result = get_forecasts(site_id, 'hourly_forecasts')
         if result:
             response = result
         else:
@@ -64,30 +46,13 @@ def get_next_24hr_forecasts(device_channel_id, forecast_start_time):
 
 
 @ml_app.route(api.route['next_1_week_forecasts'], methods=['GET'])
-def get_next_1_week_forecasts(device_channel_id, forecast_start_date):
+def get_next_1_week_forecasts():
     """
     Get forecasts for the next 1 week from specified start day.
     """
     if request.method == 'GET':
-        if type(device_channel_id) is not int:
-            device_channel_id = int(device_channel_id)
-
-        if type(forecast_start_date) is not int:
-            try:
-                forecast_start_date = int(forecast_start_date)
-            except ValueError:
-                error = {
-                    "message": "Invalid prediction start date. expected unix timestamp format like 1500000000",
-                    "success": False}
-                return jsonify(error, 400)
-
-        # change prediction_start_date to datetime format
-        forecast_start_timestamp = dt.datetime.fromtimestamp(
-            forecast_start_date).isoformat()
-
-        print(forecast_start_timestamp)
-        result = get_next_1_week_forecasts_for_channel(
-            device_channel_id, forecast_start_timestamp)
+        site_id = request.args.get('site_id', type=str)
+        result = get_forecasts(site_id, db_name='daily_forecasts')
         if result:
             response = result
         else:
@@ -97,13 +62,6 @@ def get_next_1_week_forecasts(device_channel_id, forecast_start_date):
         return data, 201
     else:
         return jsonify({"message": "Invalid request method", "success": False}), 400
-
-
-@ml_app.route('/health', methods=['GET'])
-def health():
-    if request.method == 'GET':
-        _logger.info('health status OK')
-        return 'ok'
 
 
 @ml_app.route(api.route['predict_for_heatmap'], methods=['GET'])
