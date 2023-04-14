664284da69461d630963cdd494ca942daafadc43
baalmart <bbaalmart@gmail.com>
2023-04-14 05:46:08 +0300

    modifications to paths of new measurements endpoints

--- src/device-registry/routes/v2/measurements.js
+++ src/device-registry/routes/v2/measurements.js
@@ -1512,11 +1512,11 @@ router.get(
         .withMessage("valid values include: YES and NO"),
     ],
   ]),
-  eventController.listLatest
+  eventController.listRecent
 );
 
 router.get(
-  "/site/:siteId",
+  "/sites/:siteId",
   oneOf([
     query("tenant")
       .optional()
@@ -1639,7 +1639,7 @@ router.get(
 );
 
 router.get(
-  "/airqloud/:airqloudId",
+  "/airqlouds/:airqloudId",
   oneOf([
     query("tenant")
       .optional()
@@ -1762,7 +1762,7 @@ router.get(
 );
 
 router.get(
-  "/device/:deviceId",
+  "/devices/:deviceId",
   oneOf([
     query("tenant")
       .optional()
@@ -2005,129 +2005,6 @@ router.get(
     ],
   ]),
   eventController.listByLatLong
-);
-
-router.get(
-  "/time/:startTime/:endTime",
-  oneOf([
-    query("tenant")
-      .optional()
-      .notEmpty()
-      .withMessage("tenant should not be empty if provided")
-      .bail()
-      .trim()
-      .toLowerCase()
-      .isIn(constants.NETWORKS)
-      .withMessage("the tenant value is not among the expected ones"),
-  ]),
-  oneOf([
-    [
-      query("startTime")
-        .optional()
-        .notEmpty()
-        .trim()
-        .isISO8601({ strict: true, strictSeparator: true })
-        .withMessage("startTime must be a valid datetime."),
-      query("endTime")
-        .optional()
-        .notEmpty()
-        .trim()
-        .isISO8601({ strict: true, strictSeparator: true })
-        .withMessage("endTime must be a valid datetime."),
-      query("frequency")
-        .optional()
-        .notEmpty()
-        .withMessage("the frequency cannot be empty if provided")
-        .bail()
-        .trim()
-        .toLowerCase()
-        .isIn(["hourly", "daily", "raw", "minute"])
-        .withMessage(
-          "the frequency value is not among the expected ones which include: hourly, daily, minute and raw"
-        ),
-      query("format")
-        .optional()
-        .notEmpty()
-        .withMessage("the format cannot be empty if provided")
-        .bail()
-        .trim()
-        .toLowerCase()
-        .isIn(["json", "csv"])
-        .withMessage(
-          "the format value is not among the expected ones which include: csv and json"
-        ),
-      query("external")
-        .optional()
-        .notEmpty()
-        .trim()
-        .toLowerCase()
-        .isIn(["yes", "no"])
-        .withMessage(
-          "the external value is not among the expected ones which include: no and yes"
-        ),
-      query("recent")
-        .optional()
-        .notEmpty()
-        .trim()
-        .toLowerCase()
-        .isIn(["yes", "no"])
-        .withMessage(
-          "the recent value is not among the expected ones which include: no and yes"
-        ),
-      query("device")
-        .optional()
-        .notEmpty()
-        .trim(),
-      query("device_id")
-        .optional()
-        .notEmpty()
-        .trim(),
-      query("lat_long")
-        .optional()
-        .notEmpty()
-        .trim(),
-      query("airqloud_id")
-        .optional()
-        .notEmpty()
-        .trim(),
-      query("device_number")
-        .optional()
-        .notEmpty()
-        .trim(),
-      query("site")
-        .optional()
-        .notEmpty()
-        .trim(),
-      query("site_id")
-        .optional()
-        .notEmpty()
-        .trim(),
-      query("primary")
-        .optional()
-        .notEmpty()
-        .trim()
-        .toLowerCase()
-        .isIn(["yes", "no"])
-        .withMessage("valid values include: YES and NO"),
-      query("metadata")
-        .optional()
-        .notEmpty()
-        .trim()
-        .toLowerCase()
-        .isIn(["site", "site_id", "device", "device_id"])
-        .withMessage(
-          "valid values include: site, site_id, device and device_id"
-        ),
-      query("test")
-        .optional()
-        .notEmpty()
-        .trim()
-        .toLowerCase()
-        .isIn(["yes", "no"])
-        .withMessage("valid values include: YES and NO"),
-    ],
-  ]),
-  eventController.list
 );
 
 module.exports = router;
