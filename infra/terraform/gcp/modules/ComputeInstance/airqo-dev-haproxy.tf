5f6366820ef3274008d0cf18b8db65ddb72ab911
Mike Mwanje <mwanjemike767@gmail.com>
2023-05-03 10:56:30 +0300

    auto-delete haproxy disk

--- infra/terraform/gcp/modules/ComputeInstance/airqo-dev-haproxy.tf
+++ infra/terraform/gcp/modules/ComputeInstance/airqo-dev-haproxy.tf
@@ -6,7 +6,7 @@ resource "google_compute_instance" "airqo_dev_haproxy" {
   machine_type = "e2-small"
 
   boot_disk {
-    auto_delete = false
+    auto_delete = true
     source      = "https://www.googleapis.com/compute/v1/projects/${var.project_id}/zones/${var.zone["b"]}/disks/airqo-dev-haproxy"
   }
 
