d5cb023a32c1467f577f8f6845bbb33a89fc15e7
Mike Mwanje <mwanjemike767@gmail.com>
2023-05-08 17:49:26 +0300

    use ubuntu focal in dev-haproxy disk

--- infra/terraform/gcp/modules/ComputeDisk/airqo-dev-haproxy.tf
+++ infra/terraform/gcp/modules/ComputeDisk/airqo-dev-haproxy.tf
@@ -1,5 +1,5 @@
 resource "google_compute_disk" "airqo_dev_haproxy" {
-  image                     = var.os["ubuntu-jammy"]
+  image                     = var.os["ubuntu-focal"]
   name                      = "airqo-dev-haproxy"
   physical_block_size_bytes = 4096
   project                   = var.project_id
