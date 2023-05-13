d642c0c5a54051130ca88f491347c4095d90b2b8
Mike Mwanje <mwanjemike767@gmail.com>
2023-05-13 08:14:14 +0300

    use VPC self links only

--- infra/terraform/gcp/modules/ComputeNetworkPeering/default-to-k8s.tf
+++ infra/terraform/gcp/modules/ComputeNetworkPeering/default-to-k8s.tf
@@ -1,5 +1,5 @@
 resource "google_compute_network_peering" "default_to_k8s" {
   name         = "default-to-k8s"
-  network      = google_compute_network.default.projects/airqo-250220/global/networks/default
-  peer_network = google_compute_network.airqo-k8s-cluster.projects/airqo-250220/global/networks/airqo-k8s-cluster
+  network      = "projects/airqo-250220/global/networks/default"
+  peer_network = "projects/airqo-250220/global/networks/airqo-k8s-cluster"
 }
