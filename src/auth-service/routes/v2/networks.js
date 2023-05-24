a4f7914323ef643f17c975df4b08a24e7c8089bf
baalmart <bbaalmart@gmail.com>
2023-05-24 18:15:51 +0300

    new endpoint for listing roles of a network

--- src/auth-service/routes/v2/networks.js
+++ src/auth-service/routes/v2/networks.js
@@ -488,6 +488,40 @@ router.delete(
 );
 
 router.get(
+  "/:net_id/roles",
+  oneOf([
+    [
+      query("tenant")
+        .optional()
+        .notEmpty()
+        .withMessage("tenant should not be empty if provided")
+        .trim()
+        .toLowerCase()
+        .bail()
+        .isIn(["kcca", "airqo"])
+        .withMessage("the tenant value is not among the expected ones"),
+    ],
+  ]),
+  oneOf([
+    [
+      param("net_id")
+        .exists()
+        .withMessage("the network ID param is missing in the request")
+        .bail()
+        .notEmpty()
+        .withMessage("the network ID param cannot be empty")
+        .bail()
+        .trim()
+        .isMongoId()
+        .withMessage("the network provided must be an object ID"),
+    ],
+  ]),
+  setJWTAuth,
+  authJWT,
+  createNetworkController.listRolesForNetwork
+);
+
+router.get(
   "/:net_id",
   oneOf([
     [
