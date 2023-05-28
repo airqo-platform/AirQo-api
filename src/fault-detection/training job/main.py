f7b8fd78112b5c1066f75445d45b9226a34aa00d
Mutabazi Noble <60974514+Mnoble-19@users.noreply.github.com>
2023-05-29 00:55:38 +0300

    training job fixes

--- src/fault-detection/training job/main.py
+++ src/fault-detection/training job/main.py
@@ -4,11 +4,8 @@ from google.oauth2 import service_account
 from config import configuration, environment
 from keras.models import Sequential
 from keras.layers import LSTM, Dense
-from keras.callbacks import EarlyStopping, ModelCheckpoint
 from sklearn.preprocessing import MinMaxScaler
-from sklearn.model_selection import TimeSeriesSplit
-
-from sklearn.utils import class_weight
+from sklearn.model_selection import train_test_split
 import gcsfs
 from datetime import datetime
 import joblib
@@ -28,132 +25,115 @@ def fetch_bigquery_data():
     print('fetching data from bigquery')
     credentials = service_account.Credentials.from_service_account_file(configuration.CREDENTIALS)
     query = f"""
-  SELECT DISTINCT timestamp, device_number, s1_pm2_5 FROM {configuration.GOOGLE_CLOUD_PROJECT_ID}.raw_data.device_measurements where DATE(timestamp) >= DATE_SUB(CURRENT_DATE(), INTERVAL 1 MONTH) and tenant = 'airqo'
+  SELECT DISTINCT timestamp, device_number, s1_pm2_5, s2_pm2_5 FROM {configuration.GOOGLE_CLOUD_PROJECT_ID}.raw_data.device_measurements where DATE(timestamp) >= DATE_SUB(CURRENT_DATE(), INTERVAL 1 MONTH) and tenant = 'airqo'
     ORDER
     BY
     timestamp
     """
     df = pd.read_gbq(query, project_id=configuration.GOOGLE_CLOUD_PROJECT_ID, credentials=credentials)
-    df.rename(columns={'s1_pm2_5': 'pm2_5'}, inplace=True)
     print('Data fetched from bigquery')
-    return df
-
-
-def sort_and_clean_data(df):
-    """ sorts the data by device_number and hour and removes rows with null values. """
-    print('sorting and cleaning data')
     df['timestamp'] = pd.to_datetime(df['timestamp'])
-    df['device_number'] = df['device_number'].astype(str)
-    df = df.groupby('device_number').resample('1H', on='timestamp').mean()
-    df = df.drop('device_number', axis=1)  # this line drops the column
-    df = df.reset_index()
-    # interpolate missing values
-    df['pm2_5'] = df['pm2_5'].interpolate(method='linear', limit_direction='forward', axis=0)
+    df = df.sort_values(by=['device_number', 'timestamp'])
+    df.reset_index(inplace=True)
+    df.to_csv('data.csv', index=False)
     return df
 
 
-# Option 1: convert the string to an integer using pd.Timedelta
-def add_fault_columns(df, stuckat_threshold, highvar_threshold):
+def add_fault_columns(df, offset_threshold, min_value, max_value, highvar_threshold):
     """
 
-    adds stuck-at and high-variance fault columns to the dataframe.
+    adds offset, out of bounds, data loss and high variance fault columns to the dataframe.
 
     """
     final_df = pd.DataFrame()
     for device in df["device_number"].unique():
         df1 = df[df["device_number"] == device]
-        df1['pm2_5'] = df1.pm2_5.astype(float)
+        df1['s1_pm2_5'] = df1.s1_pm2_5.astype(float)
+        df1['s2_pm2_5'] = df1.s2_pm2_5.astype(float)
         df1 = df1.set_index('timestamp').sort_index()
-        df1['rolling_mean_hr'] = df1['pm2_5'].rolling(1).mean()
-        df1['rolling_mean_hr_diff'] = df1['rolling_mean_hr'].diff()
 
-        df1['stuck_at_fault'] = df1['rolling_mean_hr_diff'].apply(
-            lambda x: 1 if abs(x) <= stuckat_threshold else 0)
+        df1['offset_fault'] = (df1['s1_pm2_5'] - df1['s2_pm2_5']).abs().apply(
+            lambda x: 1 if x >= offset_threshold else 0)
+        df1['out_of_bounds_fault'] = ((df1['s1_pm2_5'] < min_value) | (df1['s1_pm2_5'] > max_value) |
+                                      (df1['s2_pm2_5'] < min_value) | (df1['s2_pm2_5'] > max_value)).astype(int)
+        df1['data_loss_fault'] = (df1['s1_pm2_5'].isnull() | df1['s2_pm2_5'].isnull()).astype(int)
 
-        df1['col_diff'] = (df1.pm2_5.values -
-                           df1.rolling_mean_hr.values)
-        df1['high_var_fault'] = df1['col_diff'].apply(
-            lambda x: 1 if abs(x) <= highvar_threshold else 0)
+        df1['high_var_fault'] = (df1['s1_pm2_5'].rolling(10).std() > highvar_threshold) | (
+                df1['s2_pm2_5'].rolling(10).std() > highvar_threshold).astype(int)
+
         df1.reset_index(inplace=True)
         final_df = pd.concat([final_df, df1], ignore_index=True)
 
-    return final_df
+        return final_df
 
 
-def scale_and_reshape_data_for_lstm(dataframe):
-    print('Scaling and reshaping data for LSTM')
-    features = dataframe[['timestamp', 'device_number', 'pm2_5']].values
-    labels = dataframe[['stuck_at_fault', 'high_var_fault']].values
+def get_other_features(df):
+    attributes = ['year', 'month', 'day', 'dayofweek', 'hour', 'minute', 'second']
+    for a in attributes:
+        df[a] = df['timestamp'].dt.__getattribute__(a)
 
+    df['week'] = df['timestamp'].dt.isocalendar().week
+
+    print("Adding other features")
+    return df
+
+
+def fit_scaler(df):
     scaler = MinMaxScaler()
-    try:
-        features = scaler.fit_transform(features)
-    except ValueError as e:
-        print("Error while scaling the features:", e)
-        return None, None, None
+    num_cols = ['pm2_5_1', 'pm2_5_2', 'year', 'month', 'day', 'hour', 'minute', 'second']
 
-    timesteps = 24
-    try:
-        # group the features by device_number
-        grouped_features = []
-        for device_number, group in dataframe.groupby('device_number'):
-            group_features = group[['timestamp', 'device_number', 'pm2_5']].values
-            group_features = group_features.reshape((group_features.shape[0], timesteps, group_features.shape[1]))
+    # fit and transform the training data
+    scaled_df = scaler.fit_transform(df[num_cols])
 
-            grouped_features.append(group_features)
-        features = np.concatenate(grouped_features, axis=0)
-    except ValueError as e:
-        print("Error while reshaping the features:", e)
-        return None, None, None
-
-    return features, labels, scaler
+    return scaled_df
 
 
-def train_lstm_model(features, labels):
-    with mlflow.start_run():
+def reshape_data(data, timesteps):
+    """
+    reshapes data into a 3D array of shape (samples, timesteps, features)
+    """
+    reshaped_data = []
+    for i in range(len(data) - timesteps):
+        window = data[i:(i + timesteps), :]
+        reshaped_data.append(window)
 
-        test_size = 0.2
-        # use TimeSeriesSplit to split the data into train and validation sets
-        splitter = TimeSeriesSplit(n_splits=5)
-        for train_index, val_index in splitter.split(features):
-            features_train, features_val = features[train_index], features[val_index]
-            labels_train, labels_val = labels[train_index], labels[val_index]
+    reshaped_data = np.array(reshaped_data)
 
-        num_units = 50
-        model = Sequential()
-        model.add(LSTM(num_units, input_shape=(features.shape[1], features.shape[2])))
-        model.add(Dense(4, activation='sigmoid'))
-        model.compile(optimizer="adam", loss='binary_crossentropy', metrics="accuracy")
-        num_epochs = 10
-        batch_size = 128
-        class_weights = class_weight.compute_class_weight('balanced', classes=np.unique(labels_train), y=labels_train)
+    return reshaped_data
 
-        # define callbacks for early stopping and model checkpointing
-        early_stopping = EarlyStopping(monitor='val_loss', patience=3, mode='min')
-        model_checkpoint = ModelCheckpoint('best_model.h5', monitor='val_loss', save_best_only=True, mode='min')
 
-        try:
-            model.fit(features_train,
-                      labels_train,
-                      epochs=num_epochs,
-                      verbose=1,
-                      batch_size=batch_size,
-                      validation_data=(features_val, labels_val),
-                      class_weight=class_weights,
-                      callbacks=[early_stopping, model_checkpoint])
-        except ValueError as e:
-            print("Error while training the model:", e)
-            return None
+def create_model(timesteps, features):
+    """
+    creates an LSTM model with one LSTM layer and one dense layer
+    """
+    model = Sequential()
+    model.add(LSTM(64, input_shape=(timesteps, features)))
+    model.add(Dense(4, activation='sigmoid'))
+    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 
-        mlflow.log_param("test_size", test_size)
-        mlflow.log_param("num_epochs", num_epochs)
-        mlflow.log_param("batch_size", batch_size)
-        mlflow.log_param("num_units", num_units)
+    return model
 
-        # log the model as an artifact in MLFlow
-        mlflow.keras.log_model(model, "model")
 
-        return model
+def train_model(data, timesteps, test_size):
+    """
+    trains an LSTM model on the data and logs the parameters and metrics with mlflow
+    """
+    X = np.array([x for x, y in data])
+    y = np.array([y for x, y in data])
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size)
+    features = X.shape[2]
+    model = create_model(timesteps, features)
+    model.fit(X_train, y_train, epochs=10, batch_size=32)
+    test_loss, test_acc = model.evaluate(X_test, y_test)
+
+    # log the parameters and metrics with mlflow
+    mlflow.log_param('timesteps', timesteps)
+    mlflow.log_param('features', features)
+    mlflow.log_param('test_size', test_size)
+    mlflow.log_metric('test_loss', test_loss)
+    mlflow.log_metric('test_acc', test_acc)
+
+    return model
 
 
 def upload_trained_model_to_gcs(trained_model, scaler, project_name, bucket_name, source_blob_name):
@@ -172,11 +152,24 @@ def upload_trained_model_to_gcs(trained_model, scaler, project_name, bucket_name
 
 
 if __name__ == '__main__':
-    df = fetch_bigquery_data()
-    df = sort_and_clean_data(df)
-    df = add_fault_columns(df, stuckat_threshold=1, highvar_threshold=50)
-    X, y, scaler = scale_and_reshape_data_for_lstm(df)
-    model = train_lstm_model(X, y)
+    training_df = fetch_bigquery_data()
+    training_df = add_fault_columns(training_df, 25, 0, 500, 50)
+    training_df = get_other_features(training_df)
+    training_df = fit_scaler(training_df)
+    groups = training_df.groupby('device_number')
+    timesteps = 336
+    reshaped_data = []
 
-    upload_trained_model_to_gcs(model, scaler, {configuration.GOOGLE_CLOUD_PROJECT_ID},
-                                {configuration.AIRQO_FAULT_DETECTION_BUCKET}, "fault-detection-model")
+    for device, group in groups:
+        group = group.drop('device_number', axis=1)
+
+        X = group.drop(['offset_fault', 'out_of_bounds_fault', 'data_loss_fault', 'high_var_fault'], axis=1).values
+        y = group[['offset_fault', 'out_of_bounds_fault', 'data_loss_fault', 'high_var_fault']].values
+
+        X_reshaped = reshape_data(X, timesteps)
+        y_reshaped = reshape_data(y, timesteps)
+
+        reshaped_data.extend((X_reshaped, y_reshaped))
+
+    trained_model = train_model(reshaped_data, timesteps, 0.2)
+    print(trained_model)
