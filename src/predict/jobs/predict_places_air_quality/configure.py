45e2554b20aba8c1a6e1ebd68dfda1966dda940e
Noah Nsimbe <37845280+NoahNsimbe@users.noreply.github.com>
2023-05-28 17:53:31 +0300

    initial commit

---
+++ src/predict/jobs/predict_places_air_quality/configure.py
@@ -0,0 +1,24 @@
+import os
+from pathlib import Path
+
+from dotenv import load_dotenv
+
+BASE_DIR = Path(__file__).resolve().parent
+dotenv_path = os.path.join(BASE_DIR, ".env")
+load_dotenv(dotenv_path)
+
+
+class Config:
+    BIGQUERY_SITES = os.getenv("BIGQUERY_SITES")
+    BIGQUERY_AIRQLOUDS_SITES = os.getenv("BIGQUERY_AIRQLOUDS_SITES")
+    BIGQUERY_HOURLY_DATA = os.getenv("BIGQUERY_HOURLY_DATA")
+    BIGQUERY_PLACES_PREDICTIONS = os.getenv("BIGQUERY_PLACES_PREDICTIONS")
+
+    MONGO_URI = os.getenv("MONGO_URI")
+    MONGO_DB = os.getenv("MONGO_DB")
+    MONGO_SHAPE_FILES_COLLECTION = os.getenv("MONGO_SHAPE_FILES_COLLECTION")
+
+    AIRQO_API_TOKEN = os.getenv("AIRQO_API_TOKEN")
+    DEVICE_REGISTRY_URL = os.getenv(
+        "DEVICE_REGISTRY_URL", "https://api.airqo.net/api/v1/devices"
+    )
