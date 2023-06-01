313789c41c34177c763c79e63777d3a33786c300
Noah Nsimbe <37845280+NoahNsimbe@users.noreply.github.com>
2023-06-01 12:26:09 +0300

    Updated docker file
    Added v2 to meta data APIs

--- src/meta-data/app.py
+++ src/meta-data/app.py
@@ -1,11 +1,13 @@
 import logging
+import traceback
 
 from dotenv import load_dotenv
 from flasgger import Swagger, LazyJSONEncoder
-from flask import Flask
+from flask import Flask, jsonify
 from flask_cors import CORS
+from werkzeug.exceptions import NotFound
 
-from api.controllers.extract import extract_bp
+from api.controllers.extract import extract_bp_v1, extract_bp_v2
 from config import Config
 
 load_dotenv()
@@ -17,4 +19,18 @@ app.json_encoder = LazyJSONEncoder
 swagger = Swagger(app, template=Config.SWAGGER_TEMPLATE, config=Config.SWAGGER_CONFIG)
 CORS(app)
 
-app.register_blueprint(extract_bp)
+app.register_blueprint(extract_bp_v1)
+app.register_blueprint(extract_bp_v2)
+
+
+@app.errorhandler(Exception)
+def handle_exception(error):
+    traceback.print_exc()
+    print(error)
+    return jsonify({"message": "Error occurred. Contact support"}), 500
+
+
+@app.errorhandler(NotFound)
+def handle_404_exception(error):
+    print(error)
+    return jsonify({"message": "Requested Url not found on this service"}), 404
