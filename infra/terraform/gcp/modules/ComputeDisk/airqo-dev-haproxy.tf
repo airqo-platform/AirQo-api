8140ede872bc87f364fc48f25d6fefbd14768cfe
Mike Mwanje <mwanjemike767@gmail.com>
2023-06-12 10:33:01 +0300

    use tiny disk size for airqo_dev_haproxy disk

--- infra/terraform/gcp/modules/ComputeDisk/airqo-dev-haproxy.tf
+++ infra/terraform/gcp/modules/ComputeDisk/airqo-dev-haproxy.tf
@@ -1,9 +1,9 @@
 resource "google_compute_disk" "airqo_dev_haproxy" {
-  image                     = var.os["ubuntu-focal"]
+  snapshot                  = "https://www.googleapis.com/compute/v1/projects/${var.project_id}/global/snapshots/airqo-haproxy"
   name                      = "airqo-dev-haproxy"
   physical_block_size_bytes = 4096
   project                   = var.project_id
-  size                      = var.disk_size["medium"]
+  size                      = var.disk_size["tiny"]
   type                      = "pd-standard"
   zone                      = var.zone["b"]
   description               = "Disk for the airqo-dev-haproxy instance"
