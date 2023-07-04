97944e5e29ec13a0e0c8228a579cf3559c7892b3
Mutabazi Noble <60974514+Mnoble-19@users.noreply.github.com>
2023-07-04 18:27:29 +0300

    code cleanup - training job

--- src/fault-detection/training job/main.py
+++ src/fault-detection/training job/main.py
@@ -4,7 +4,8 @@ from google.oauth2 import service_account
 from config import configuration
 from sklearn.preprocessing import MinMaxScaler
 from keras.models import Sequential
-from keras.layers import Bidirectional, LSTM, Dense
+import keras.backend as K
+from keras.layers import LSTM, Dense, Dropout
 # import gcsfs
 from datetime import datetime
 import joblib
@@ -29,7 +30,6 @@ def fetch_bigquery_data():
     df['device_number'] = df['device_number'].astype(int)
     print('data fetched from bigquery')
     return df
-
 
 def add_fault_columns(df, offset_threshold, min_value, max_value, highvar_threshold):
     """
@@ -76,20 +76,19 @@ def scale_data(df):
     """
     scales data using MinMaxScaler
     """
-    inputs = df.iloc[:, : -4].values
+    inputs = df.iloc[:, : 3].values
+    additional_features = df.iloc[:, 3:-4].values
     targets = df.iloc[:, -4:].values
     input_scaler = MinMaxScaler()
-    target_scaler = MinMaxScaler()
 
     # fit the scalers on the training data
     input_scaler.fit(inputs)
-    target_scaler.fit(targets)
 
     # transform the inputs and targets
     scaled_inputs = input_scaler.transform(inputs)
-    scaled_targets = target_scaler.transform(targets)
+    scaled_inputs = np.concatenate((scaled_inputs, additional_features), axis=1)
     print("Data scaled")
-    return scaled_inputs, scaled_targets, input_scaler, target_scaler
+    return scaled_inputs, targets, input_scaler
 
 
 def create_dataset(X, y, time_steps=1):
@@ -126,20 +125,23 @@ def split_data_by_date(df):
 
 
 def preprocess_data(train_data, test_data):
-    train_data = get_other_features(train_data)
-    test_data = get_other_features(test_data)
-    fault_train_data = add_fault_columns(train_data, 25, 0, 350, 50)
-    fault_test_data = add_fault_columns(test_data, 25, 0, 350, 50)
+    train_data_1 = get_other_features(train_data)
+    test_data_1 = get_other_features(test_data)
+    fault_train_data = add_fault_columns(train_data_1, 25, 0, 350, 50)
+    fault_test_data = add_fault_columns(test_data_1, 25, 0, 350, 50)
+    fault_train_data.drop('index', axis=1, inplace=True)
+    fault_test_data.drop('index', axis=1, inplace=True)
     return fault_train_data, fault_test_data
 
 
-def create_lstm_model(X_train, y_train):
+def create_lstm_model(X_train, y_train, X_test, y_test):
     timesteps = X_train.shape[1]
     features = X_train.shape[2]
 
     # create the model
     model = Sequential()
     model.add(LSTM(16, input_shape=(timesteps, features)))
+    model.add(Dropout(0.2))
     model.add(Dense(4, activation='sigmoid'))
 
     # compile the model
@@ -147,20 +149,20 @@ def create_lstm_model(X_train, y_train):
 
     # train the model
     history = model.fit(X_train, y_train, epochs=10, batch_size=32, validation_split=0.2)
-    loss, accuracy = model.evaluate(X_test, y_test)
-    return model, history, loss, accuracy
+    loss, f1_score = model.evaluate(X_test, y_test)
+    return model, history, loss, f1_score
 
 
 if __name__ == '__main__':
     device_data = fetch_bigquery_data()
     train_data, test_data = split_data_by_date(device_data)
     fault_train_data, fault_test_data = preprocess_data(train_data, test_data)
-    X_train, y_train, input_scaler, target_scaler = scale_data(fault_train_data)
-    X_test, y_test, _, _ = scale_data(fault_test_data)
-    X_train, y_train = create_dataset(X_train, y_train, 10)
-    X_test, y_test = create_dataset(X_test, y_test, 10)
+    X_train, y_train, input_scaler = scale_data(fault_train_data)
+    X_test, y_test, _ = scale_data(fault_test_data)
+    X_train, y_train = create_dataset(X_train, y_train,7)
+    X_test, y_test = create_dataset(X_test, y_test, 7)
 
-    model, history, loss, accuracy = create_lstm_model(X_train, y_train)
+    model, history, loss, f1_score = create_lstm_model(X_train.astype('float32'), y_train.astype('float32'), X_test.astype('float32'), y_test.astype('float32'))
 
     print(model)
     # print()
