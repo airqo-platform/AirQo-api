8b1f0302a64d1781f39998a99983e4e64ba1b063
Mutabazi Noble <60974514+Mnoble-19@users.noreply.github.com>
2023-07-05 22:30:44 +0300

    add Db connection

--- src/fault-detection/fault_detection_job/config.py
+++ src/fault-detection/fault_detection_job/config.py
@@ -1,6 +1,10 @@
 import os
+from datetime import datetime
 from pathlib import Path
+
+import pandas as pd
 from dotenv import load_dotenv
+from pymongo import MongoClient
 
 BASE_DIR = Path(__file__).resolve().parent
 dotenv_path = os.path.join(BASE_DIR, '.env')
@@ -12,29 +16,31 @@ class Config:
     TESTING = False
     CSRF_ENABLED = True
     CREDENTIALS = os.getenv('GOOGLE_APPLICATION_CREDENTIALS')
-    GOOGLE_CLOUD_PROJECT_ID = os.getenv('GOOGLE_CLOUD_PROJECT_ID')
-    MLFLOW_TRACKING_URI = os.getenv('MLFLOW_TRACKING_URI')
-    MLFLOW_TRACKING_USERNAME = os.getenv('MLFLOW_TRACKING_USERNAME')
-    MLFLOW_TRACKING_PASSWORD = os.getenv('MLFLOW_TRACKING_PASSWORD')
+    GOOGLE_CLOUD_PROJECT_ID = os.getenv('GOOGLE_CLOUD_PROJECT')
+    NUMBER_OF_HOURS = os.getenv('NUMBER_OF_HOURS')
 
 
 class ProductionConfig(Config):
-    AIRQO_FAULT_DETECTION_BUCKET = os.getenv('AIRQO_FAULT_DETECTION_BUCKET_PROD')
-    AIRQO_API_BASE_URL = os.getenv('AIRQO_API_BASE_URL_PROD')
+    DB_NAME = os.getenv("DB_NAME_PROD")
+    MONGO_URI = os.getenv('MONGO_GCE_URI')
+    AIRQO_PREDICT_BUCKET = os.getenv('AIRQO_PREDICT_BUCKET_PROD')
+    GOOGLE_CLOUD_PROJECT_ID = os.getenv('GOOGLE_CLOUD_PROJECT')
 
 
 class TestingConfig(Config):
     DEBUG = True
     TESTING = True
-    AIRQO_FAULT_DETECTION_BUCKET = os.getenv('AIRQO_FAULT_DETECTION_BUCKET_STAGE')
-    AIRQO_API_BASE_URL = os.getenv('AIRQO_API_BASE_URL_STAGE')
+    MONGO_URI = os.getenv('MONGO_GCE_URI')
+    DB_NAME = os.getenv("DB_NAME_STAGE")
+    AIRQO_PREDICT_BUCKET = os.getenv('AIRQO_PREDICT_BUCKET_STAGE')
 
 
 class DevelopmentConfig(Config):
     DEVELOPMENT = True
     DEBUG = True
-    AIRQO_FAULT_DETECTION_BUCKET = os.getenv('AIRQO_FAULT_DETECTION_BUCKET_STAGE')
-    AIRQO_API_BASE_URL = os.getenv('AIRQO_API_BASE_URL_STAGE')
+    MONGO_URI = os.getenv("MONGO_DEV_URI")
+    DB_NAME = os.getenv("DB_NAME_DEV")
+    BUCKET_NAME = os.getenv('AIRQO_FAULT_DETECTION_BUCKET_STAGE')
 
 
 app_config = {
@@ -44,7 +50,13 @@ app_config = {
     "staging": TestingConfig
 }
 
-environment = os.getenv("ENVIRONMENT")
+environment = os.getenv("ENV")
 print("ENVIRONMENT", environment or 'staging')
 
 configuration = app_config.get(environment, TestingConfig)
+
+
+def connect_mongo():
+    client = MongoClient(configuration.MONGO_URI)
+    db = client[configuration.DB_NAME]
+    return db
--- src/fault-detection/fault_detection_job/main.py
+++ src/fault-detection/fault_detection_job/main.py
@@ -1,14 +1,16 @@
+import pandas as pd
+import joblib
 from google.oauth2 import service_account
 from config import configuration
 import gcsfs
-
+import numpy as np
 
 def fetch_bigquery_data():
     """gets data from the bigquery table"""
     # print('fetching data from bigquery')
     credentials = service_account.Credentials.from_service_account_file(configuration.CREDENTIALS)
     query = f"""
-    SELECT DISTINCT timestamp, device_number, s1_pm2_5, s2_pm2_5 FROM {configuration.GOOGLE_CLOUD_PROJECT_ID}.raw_data.device_measurements where DATE(timestamp) >= DATE_SUB(CURRENT_DATE(), INTERVAL 1 HOUR) and tenant = 'airqo'
+    SELECT DISTINCT timestamp, device_number, s1_pm2_5, s2_pm2_5 FROM {configuration.GOOGLE_CLOUD_PROJECT_ID}.raw_data.device_measurements where DATE(timestamp) >= DATE_SUB(CURRENT_DATE(), INTERVAL 10 DAY) and tenant = 'airqo'
       ORDER
       BY
       timestamp
@@ -29,12 +31,9 @@ def scale_data(df, scaler):
     scales data using MinMaxScaler
     """
     inputs = df.iloc[:, : 3].values
-    additional_features = df.iloc[:, 3:].values
+    additional_features = df.iloc[:, 3:].values.astype('float32')
 
-    # transform the inputs using the scaler
     scaled_inputs = scaler.transform(inputs)
-
-    # concatenate the scaled inputs and additional features
     scaled_inputs = np.concatenate((scaled_inputs, additional_features), axis=1)
 
     print("Data scaled")
@@ -44,7 +43,6 @@ def scale_data(df, scaler):
 
 def preprocess_data(df):
     df1 = get_other_features(df)
-    df1.drop('index', axis=1, inplace=True)
     return df1
 
 
@@ -69,7 +67,7 @@ def load_trained_model_and_scaler_from_gcs(project_name, bucket_name, source_blo
 
     # load the model from GCS bucket
     with fs.open(bucket_name + '/' + source_blob_name, 'rb') as model_handle:
-        model = load_model(model_handle)
+        model = joblib.load(model_handle)
 
         print("Model loaded from GCS bucket")
 
@@ -82,6 +80,13 @@ def load_trained_model_and_scaler_from_gcs(project_name, bucket_name, source_blo
     return model, scaler
 
 
+# def get_trained_model_from_gcs(project_name, bucket_name, source_blob_name):
+#     fs = gcsfs.GCSFileSystem(project=project_name)
+#     fs.ls(bucket_name)
+#     with fs.open(bucket_name + '/' + source_blob_name, 'rb') as handle:
+#         job = joblib.load(handle)
+#     return job
+
 def create_dataset(X, time_steps=1):
     Xs = []
     for i in range(len(X) - time_steps):
@@ -90,13 +95,34 @@ def create_dataset(X, time_steps=1):
     print("Dataset created")
     return np.array(Xs)
 
+def get_faulty_devices(df, fault_df):
+    faulty_devices = []
+    for device in df['device'].unique():
+        device_df = df[df['device'] == device]
+        if (fault_df[fault_df['device'] == device].iloc[:, 1:] == 1).any().any():
+            faulty_devices.append(device_df)
+    return pd.concat(faulty_devices)
+
 
 if __name__ == '__main__':
-    model = get_trained_model_from_gcs(configuration.GOOGLE_CLOUD_PROJECT_ID, configuration.BUCKET_NAME,
-                                       'lstm_model.h5')
     device_data = fetch_bigquery_data()
     original_data = device_data.copy()
     new_data = preprocess_data(original_data)
     model, scaler = load_trained_model_and_scaler_from_gcs(configuration.GOOGLE_CLOUD_PROJECT_ID,
-                                                              configuration.BUCKET_NAME, 'lstm_model.h5',)
+                                                           configuration.BUCKET_NAME, 'lstm_model.h5', 'scaler.pkl')
+    scaled_data = scale_data(new_data, scaler)
+    X_new = create_dataset(scaled_data, 7)
+    y_pred = model.predict(X_new)
+    y_pred = np.round(y_pred)
+    pred_df = pd.DataFrame(y_pred,
+                           columns=['offset_fault', 'out_of_bounds_fault', 'data_loss_fault', 'high_variance_fault'])
 
+    pred_df['device_number'] = original_data['device_number'].iloc[7:].values
+
+    final_df = pd.merge(original_data, pred_df, on='device_number')
+
+    # return rows where atleast one fault is detected(1)
+    final_df = final_df[(final_df.iloc[:, 3:] == 1).any(axis=1)]
+    #save df to mongodb
+
+    print(final_df.head())
