3c60d078a0119ab2611b1c649f442b7573b2b863
Mutabazi Noble <60974514+Mnoble-19@users.noreply.github.com>
2023-07-08 19:24:32 +0300

    Refactor config - predict api

--- src/predict/api/config/constants.py
+++ src/predict/api/config/constants.py
@@ -12,37 +12,31 @@ load_dotenv(dotenv_path)
 
 
 class Config:
-    DEBUG = False
-    TESTING = False
     CSRF_ENABLED = True
     SECRET_KEY = os.getenv("SECRET_KEY")
     CREDENTIALS = os.getenv("GOOGLE_APPLICATION_CREDENTIALS")
     BIGQUERY_MEASUREMENTS_PREDICTIONS = os.getenv("BIGQUERY_MEASUREMENTS_PREDICTIONS")
     AIRQO_BASE_URL = os.getenv("AIRQO_BASE_URL", "https://api.airqo.net")
     AIRQO_API_AUTH_TOKEN = os.getenv("AIRQO_API_AUTH_TOKEN")
+    DB_NAME = os.getenv("DB_NAME")
+    MONGO_URI = os.getenv("MONGO_GCE_URI")
+    REDIS_SERVER = os.getenv("REDIS_SERVER")
     CACHE_TIMEOUT = os.getenv("CACHE_TIMEOUT", 3600)
 
 
 class ProductionConfig(Config):
-    DB_NAME = os.getenv("DB_NAME_PROD")
-    MONGO_URI = os.getenv("MONGO_GCE_URI")
-    REDIS_SERVER = os.getenv("REDIS_SERVER_PROD")
+    DEBUG = False
+    TESTING = False
 
 
 class DevelopmentConfig(Config):
     DEVELOPMENT = True
     DEBUG = True
-    MONGO_URI = os.getenv("MONGO_DEV_URI")
-    DB_NAME = os.getenv("DB_NAME_DEV")
-    REDIS_SERVER = os.getenv("REDIS_SERVER_DEV")
 
 
 class TestingConfig(Config):
     DEBUG = True
     TESTING = True
-    MONGO_URI = os.getenv("MONGO_GCE_URI")
-    DB_NAME = os.getenv("DB_NAME_STAGE")
-    REDIS_SERVER = os.getenv("REDIS_SERVER_PROD")
 
 
 app_config = {
