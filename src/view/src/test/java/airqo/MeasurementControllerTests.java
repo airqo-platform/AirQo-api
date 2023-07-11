a4831dbf54ca002cdfc43e3170bb4195cb6e30cf
Noah Nsimbe <37845280+NoahNsimbe@users.noreply.github.com>
2023-07-11 17:08:01 +0300

    Removed old test cases

--- src/view/src/test/java/airqo/MeasurementControllerTests.java
+++ src/view/src/test/java/airqo/MeasurementControllerTests.java
@@ -21,25 +21,19 @@ import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.MediaType;
-import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.restdocs.operation.preprocess.Preprocessors;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultActions;
 
 import java.text.SimpleDateFormat;
 import java.util.*;
 import java.util.stream.Collectors;
 
 import static airqo.config.Constants.dateTimeFormat;
-import static org.hamcrest.Matchers.hasSize;
-import static org.hamcrest.Matchers.is;
-import static org.mockito.Mockito.*;
-import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
-import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
-import static org.springframework.restdocs.request.RequestDocumentation.requestParameters;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @Slf4j
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -57,7 +51,6 @@ public class MeasurementControllerTests {
 	String endDateTimeStr = "";
 	Date startDateTime;
 	Date endDateTime;
-	String v1UrlTemplate = "";
 	String v2UrlTemplate = "";
 	String siteIds = "";
 	List<String> siteIdsList = new ArrayList<>();
@@ -73,7 +66,6 @@ public class MeasurementControllerTests {
 		endDateTime = queryDates.get("endDateTime");
 		startDateTimeStr = simpleDateFormat.format(startDateTime);
 		endDateTimeStr = simpleDateFormat.format(endDateTime);
-		v1UrlTemplate = "/measurements/app/insights";
 		v2UrlTemplate = "/measurements/mobile-app/insights";
 		siteIds = "site-01,site-02";
 		siteIdsList = Arrays.stream(siteIds.split(",")).toList();
@@ -121,59 +113,6 @@ public class MeasurementControllerTests {
 	public void dateQueryParameters() {
 		Assertions.assertEquals(new DateTime(startDateTime).getDayOfWeek(), 1);
 		Assertions.assertEquals(new DateTime(endDateTime).getDayOfWeek(), 7);
-	}
-
-	@Test
-	@DisplayName("Testing app insights V1 API query parameters")
-	public void testAppInsightsV1QueryParameters() throws Exception {
-
-		// Testing start date parameter
-		this.mockMvc.perform(get(v1UrlTemplate)
-				.param("endDateTime", endDateTimeStr)
-				.param("siteId", siteIds))
-			.andExpect(status().isBadRequest());
-
-		// Testing end date parameter
-		this.mockMvc.perform(get(v1UrlTemplate)
-				.param("startDateTime", startDateTimeStr)
-				.param("siteId", siteIds))
-			.andExpect(status().isBadRequest());
-
-		// Testing site id parameter
-		this.mockMvc.perform(get(v1UrlTemplate)
-				.param("startDateTime", startDateTimeStr)
-				.param("endDateTime", endDateTimeStr))
-			.andExpect(status().isBadRequest());
-
-		List<Insight> insights = siteIdsList.stream().map(site -> {
-			Insight insight = new Insight();
-			insight.setSiteId(site);
-			insight.setId(site);
-			insight.setEmpty(false);
-			insight.setForecast(false);
-			insight.setFrequency(Frequency.HOURLY);
-			insight.setPm2_5(24);
-			insight.setPm10(50);
-			insight.setTime(new Date());
-			return insight;
-		}).collect(Collectors.toList());
-
-		when(insightsService.getInsights(startDateTime, endDateTime, siteIdsList)).thenReturn(insights);
-
-		// Testing all parameters
-		ResultActions resultActions = this.mockMvc.perform(get(v1UrlTemplate)
-				.param("siteId", siteIds)
-				.param("startDateTime", startDateTimeStr)
-				.param("endDateTime", endDateTimeStr))
-			.andExpect(content().contentType(MediaType.APPLICATION_JSON))
-			.andExpect(jsonPath("$.message", is("Operation Successful")))
-			.andExpect(jsonPath("$.data").isArray())
-			.andExpect(jsonPath("$.data", hasSize(2)));
-		verify(this.insightsService, times(1)).getInsights(startDateTime, endDateTime, siteIdsList);
-
-		MockHttpServletResponse response = resultActions.andReturn().getResponse();
-		Assertions.assertEquals(response.getStatus(), 200);
-
 	}
 
 	@Test
@@ -231,14 +170,7 @@ public class MeasurementControllerTests {
 				.param("startDateTime", startDateTimeStr)
 				.param("endDateTime", endDateTimeStr))
 			.andDo(print())
-			.andExpect(status().isOk())
-			.andDo(document("app-insights",
-				requestParameters(
-					parameterWithName("siteId").description("Site id(s). Separate multiple site ids using commas").optional(),
-					parameterWithName("startDateTime").description("Start date time. Format `yyyy-MM-ddTHH:mm:ssZ` . Timezone is UTC").optional(),
-					parameterWithName("endDateTime").description("End date time. Format `yyyy-MM-ddTHH:mm:ssZ` . Timezone is UTC").optional()
-				)
-			));
+			.andExpect(status().isOk());
 	}
 
 	@TestConfiguration
