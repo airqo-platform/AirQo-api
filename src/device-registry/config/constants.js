cfbe9a51fcec1bd7319e4a07730c421d027bfbdb
baalmart <bbaalmart@gmail.com>
2023-08-02 00:42:26 +0300

    adding accessibility field to both Cohort and Grid

--- src/device-registry/config/constants.js
+++ src/device-registry/config/constants.js
@@ -1033,6 +1033,7 @@ const defaultConfig = {
     long_name: 1,
     description: 1,
     grid_tags: 1,
+    visibility: 1,
     admin_level: 1,
     grid_codes: 1,
     centers: 1,
@@ -1099,6 +1100,7 @@ const defaultConfig = {
     name: 1,
     description: 1,
     cohort_tags: 1,
+    visibility: 1,
     cohort_codes: 1,
     devices: "$devices",
     numberOfDevices: {
