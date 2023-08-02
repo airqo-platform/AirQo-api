83b111ceeecc3101fec70dbeed3f0cfd13069c56
baalmart <bbaalmart@gmail.com>
2023-08-02 14:24:31 +0300

    adding the config folder unit tests

--- src/device-registry/config/test/ut_cloudinary.js
+++ src/device-registry/config/test/ut_cloudinary.js
@@ -1,1 +1,45 @@
 require("module-alias/register");
+const { expect } = require("chai");
+const sinon = require("sinon");
+const cloudinary = require("@config/cloudinary");
+
+describe("Cloudinary Configuration", () => {
+  // Create stubs for environment variables
+  const env = {
+    CLOUD_NAME: "sample_cloud_name",
+    CLOUDINARY_API_KEY: "sample_api_key",
+    CLOUDINARY_API_SECRET: "sample_api_secret",
+  };
+
+  before(() => {
+    // Stub the process.env object with our custom environment variables
+    sinon.stub(process, "env").value(env);
+  });
+
+  after(() => {
+    // Restore the original process.env object after the tests
+    sinon.restore();
+  });
+
+  describe("Cloudinary Configuration Initialization", () => {
+    it("should initialize Cloudinary with the correct configuration", () => {
+      const cloudinaryConfigSpy = sinon.spy(cloudinary, "config");
+
+      // Require the module again to trigger the configuration
+      // This should happen inside the cloudinaryConfig module
+      require("./cloudinaryConfig");
+
+      // Assert that cloudinary.config was called with the correct values
+      expect(cloudinaryConfigSpy.calledOnce).to.be.true;
+      expect(cloudinaryConfigSpy.firstCall.args[0]).to.deep.equal({
+        cloud_name: env.CLOUD_NAME,
+        api_key: env.CLOUDINARY_API_KEY,
+        api_secret: env.CLOUDINARY_API_SECRET,
+      });
+
+      cloudinaryConfigSpy.restore();
+    });
+  });
+
+  // Add more describe blocks for other configuration tests if necessary
+});
--- src/device-registry/config/test/ut_redis.js
+++ src/device-registry/config/test/ut_redis.js
@@ -1,1 +1,57 @@
 require("module-alias/register");
+const { expect } = require("chai");
+const sinon = require("sinon");
+const proxyquire = require("proxyquire");
+
+// Stub the required modules
+const constantsStub = {
+  REDIS_SERVER: "localhost",
+  REDIS_PORT: 6379,
+};
+
+const redisClientStub = {
+  on: sinon.stub(),
+};
+
+const redisModule = proxyquire("@config/redis", {
+  "@config/constants": constantsStub,
+  redis: {
+    createClient: sinon.stub().returns(redisClientStub),
+  },
+  "@utils/log": {
+    logElement: sinon.stub(),
+  },
+});
+
+describe("Redis Client Configuration", () => {
+  afterEach(() => {
+    sinon.restore();
+  });
+
+  it("should create a Redis client with the correct host and port", () => {
+    expect(redisModule.host).to.equal(constantsStub.REDIS_SERVER);
+    expect(redisModule.port).to.equal(constantsStub.REDIS_PORT);
+
+    sinon.assert.calledOnceWithExactly(redis.createClient, {
+      host: constantsStub.REDIS_SERVER,
+      port: constantsStub.REDIS_PORT,
+      retry_strategy: sinon.match.func,
+    });
+  });
+
+  it("should set up error event listener", () => {
+    const errorListener = redisClientStub.on.withArgs("error").args[0][1];
+    expect(errorListener).to.be.a("function");
+
+    // Test the error event handling here if needed
+  });
+
+  it("should set up ready event listener", () => {
+    const readyListener = redisClientStub.on.withArgs("ready").args[0][1];
+    expect(readyListener).to.be.a("function");
+
+    // Test the ready event handling here if needed
+  });
+
+  // Add more tests for other properties or behavior if necessary
+});
