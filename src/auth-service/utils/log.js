9e70fb072af9145f41c8f54f70d248f41400e5ab
baalmart <bbaalmart@gmail.com>
2023-08-09 00:21:11 +0300

    restoring staging logging

--- src/auth-service/utils/log.js
+++ src/auth-service/utils/log.js
@@ -1,19 +1,19 @@
 const logText = (message) => {
-  if (process.env.NODE_ENV === "development") {
+  if (process.env.NODE_ENV !== "production") {
     console.log(message);
   }
   return "log deactivated in prod and stage";
 };
 
 const logElement = (message, body) => {
-  if (process.env.NODE_ENV === "development") {
+  if (process.env.NODE_ENV !== "production") {
     console.log(message + ": " + body);
   }
   return "log deactivated in prod and stage";
 };
 
 const logObject = (message, object) => {
-  if (process.env.NODE_ENV === "development") {
+  if (process.env.NODE_ENV !== "production") {
     console.log(message + ": ");
     console.dir(object);
   }
@@ -21,7 +21,7 @@ const logObject = (message, object) => {
 };
 
 const logError = (error) => {
-  if (process.env.NODE_ENV === "development") {
+  if (process.env.NODE_ENV !== "production") {
     console.log("an unhandled promise rejection" + ": ");
     console.error(e);
   }
