b0608262e380cd6e88bafa47397f94a91d694a16
Benjamin Ssempala <86492979+BenjaminSsempala@users.noreply.github.com>
2023-09-06 18:57:05 +0300

    Adding translate API

--- src/device-registry/utils/create-health-tips.js
+++ src/device-registry/utils/create-health-tips.js
@@ -8,6 +8,7 @@ const log4js = require("log4js");
 const logger = log4js.getLogger(
   `${constants.ENVIRONMENT} -- create-health-tip-util`
 );
+const translateUtil = require("./translate");
 
 const { Kafka } = require("kafkajs");
 const kafka = new Kafka({
@@ -24,12 +25,17 @@ const createHealthTips = {
       const limit = parseInt(request.query.limit, 0);
       const skip = parseInt(request.query.skip, 0);
       const filter = generateFilter.tips(request);
+      const language = request.query.language;
 
       const responseFromListHealthTips = await HealthTipModel(tenant).list({
         filter,
         limit,
         skip,
       });
+      if (language != null) {
+        responseFromListHealthTips.data = await translateUtil.translate(responseFromListHealthTips.data, language);
+      }
+
       logObject("responseFromListHealthTips", responseFromListHealthTips);
       return responseFromListHealthTips;
     } catch (error) {
