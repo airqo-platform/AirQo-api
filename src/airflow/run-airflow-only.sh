be20f67790c8e3c71c3c7db5131249de47572963
wabinyai <rajacastro@gmail.com>
2023-10-10 18:24:24 +0300

    Run-airflow read me for window

--- src/airflow/run-airflow-only.sh
+++ src/airflow/run-airflow-only.sh
@@ -2,3 +2,7 @@
 
 export DOCKER_BUILDKIT=1
 docker-compose -f docker-compose.yaml up --build db db-setup webserver scheduler
+
+#WINDOW
+set DOCKER_BUILDKIT=1
+docker-compose -f docker-compose.yaml up --build db db-setup webserver scheduler
