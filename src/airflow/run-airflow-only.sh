c1e853705c5f3dfe0611268ba48acad4822e4623
Mutabazi Noble <60974514+Mnoble-19@users.noreply.github.com>
2023-10-13 10:12:44 +0300

    Update README & bash scripts

--- src/airflow/run-airflow-only.sh
+++ src/airflow/run-airflow-only.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
 export DOCKER_BUILDKIT=1
-docker-compose -f docker-compose.yaml up --build db db-setup webserver scheduler
-
-##For windows users
-#steps to follow
-set DOCKER_BUILDKIT=1
+# For Windows, uncomment the line below and comment the line above
+# set DOCKER_BUILDKIT=1
 docker-compose -f docker-compose.yaml up --build db db-setup webserver scheduler
