2865583758f90581bbec490a58e82ea96b90c36b
baalmart <bbaalmart@gmail.com>
2023-10-18 14:53:07 +0300

    updating the url in the email

--- src/auth-service/utils/email.templates.js
+++ src/auth-service/utils/email.templates.js
@@ -101,7 +101,7 @@ module.exports = {
     targetId,
     inviterEmail,
   } = {}) => {
-    const url = `${constants.ANALYTICS_BASE_URL}/account/creation/step2/${email}/${targetId}`;
+    const url = `${constants.ANALYTICS_BASE_URL}/account/creation/step2?userEmail=${email}&target_id=${targetId}`;
     const content = `<tr>
                                 <td
                                     style="color: #344054; font-size: 16px; font-family: Inter; font-weight: 400; line-height: 24px; word-wrap: break-word;">
