dd96c2e6b2570e183147e1527d98fd34dd9f29ef
baalmart <bbaalmart@gmail.com>
2023-10-31 17:55:10 +0300

    optimisations to the isActive cron job

--- src/auth-service/bin/cronJob.js
+++ src/auth-service/bin/cronJob.js
@@ -10,23 +10,42 @@ const logger = log4js.getLogger(
 // Everyday at midnight
 cron.schedule("0 0 * * *", async () => {
   try {
-    await UserModel("airqo").updateMany(
-      {
-        $or: [
-          {
-            lastLogin: {
-              $lt: new Date(Date.now() - inactiveThreshold),
+    const batchSize = 100; // Process 100 users at a time
+    let skip = 0;
+
+    while (true) {
+      const users = await UserModel("airqo")
+        .find({
+          $or: [
+            {
+              lastLogin: {
+                $lt: new Date(Date.now() - inactiveThreshold),
+              },
             },
-          },
-          {
-            lastLogin: null,
-          },
-        ],
-      },
-      {
-        isActive: false,
+            {
+              lastLogin: null,
+            },
+          ],
+        })
+        .limit(batchSize)
+        .skip(skip)
+        .select("_id")
+        .lean();
+
+      if (users.length === 0) {
+        break;
       }
-    );
+
+      const userIds = users.map((user) => user._id);
+
+      // Update users to set isActive: false
+      await UserModel("airqo").updateMany(
+        { _id: { $in: userIds } },
+        { isActive: false }
+      );
+
+      skip += batchSize;
+    }
   } catch (error) {
     logger.error(
       `An error occurred in the cron job --- ${JSON.stringify(error)}`
