23ac672125a1e008890d3d7cbb42b679541f0b4b
Mutabazi Noble <60974514+Mnoble-19@users.noreply.github.com>
2023-11-13 18:16:37 +0300

    update dag for tweeting workflow

--- src/airflow/dags/airqo_automated_tweets.py
+++ src/airflow/dags/airqo_automated_tweets.py
@@ -1,6 +1,8 @@
-from airflow.decorators import dag
+from airflow.decorators import dag, task
 
 from airqo_etl_utils.airflow_custom_utils import AirflowUtils
+from airqo_etl_utils.airqo_tweets_utils import AirQoTweetsUtils
+from airqo_etl_utils.config import configuration
 
 
 @dag(
@@ -9,8 +11,29 @@ from airqo_etl_utils.airflow_custom_utils import AirflowUtils
     default_args=AirflowUtils.dag_default_configs(),
     tags=["airqo", "automated-tweets"],
 )
-
 def create_forecast_tweets():
     @task()
-    def select_locations():
-        from airqo_etl_utils.air
+    def retrieve_sites():
+        from airqo_etl_utils.airqo_api import AirQoApi
+
+        return AirQoApi().get_sites(tenant=configuration.TENANT)
+
+    @task()
+    def select_forecast_sites(sites):
+        return AirQoTweetsUtils.select_sites_for_forecast(sites)
+
+    @task()
+    def get_site_forecast(sites):
+        return AirQoTweetsUtils.fetch_site_forecasts(sites)
+
+    @task()
+    def send_tweet(site_forecasts):
+        AirQoTweetsUtils.create_tweet(site_forecasts)
+
+    sites = retrieve_sites()
+    selected_sites = select_forecast_sites(sites)
+    site_forecasts = get_site_forecast(selected_sites)
+    send_tweet(site_forecasts)
+
+
+create_forecast_tweets()
