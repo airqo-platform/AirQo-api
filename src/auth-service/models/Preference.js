344b70216a0d86099c5b8bb2291b7e3f5be9537f
baalmart <bbaalmart@gmail.com>
2023-11-14 12:59:17 +0300

    few enhancements for Preferences

--- src/auth-service/models/Preference.js
+++ src/auth-service/models/Preference.js
@@ -42,6 +42,7 @@ const siteSchema = new mongoose.Schema(
     region: { type: String },
     search_name: { type: String },
     sub_county: { type: String },
+    grid_id: { type: ObjectId },
   },
   { _id: false }
 );
@@ -89,25 +90,55 @@ const PreferenceSchema = new mongoose.Schema(
       ref: "airqloud",
       default: mongoose.Types.ObjectId(constants.DEFAULT_AIRQLOUD),
     },
+    airqloud_ids: [
+      {
+        type: ObjectId,
+        ref: "airqloud",
+      },
+    ],
     grid_id: {
       type: ObjectId,
       ref: "grid",
       default: mongoose.Types.ObjectId(constants.DEFAULT_GRID),
     },
+    grid_ids: [
+      {
+        type: ObjectId,
+        ref: "grid",
+      },
+    ],
     cohort_id: {
       type: ObjectId,
       ref: "cohort",
     },
+    cohort_ids: [
+      {
+        type: ObjectId,
+        ref: "cohort",
+      },
+    ],
     network_id: {
       type: ObjectId,
       ref: "network",
       default: mongoose.Types.ObjectId(constants.DEFAULT_NETWORK),
     },
+    network_ids: [
+      {
+        type: ObjectId,
+        ref: "network",
+      },
+    ],
     group_id: {
       type: ObjectId,
       ref: "group",
       default: mongoose.Types.ObjectId(constants.DEFAULT_GROUP),
     },
+    group_ids: [
+      {
+        type: ObjectId,
+        ref: "group",
+      },
+    ],
     user_id: {
       type: ObjectId,
       required: [true, "user_id is required"],
@@ -138,6 +169,42 @@ PreferenceSchema.plugin(uniqueValidator, {
   message: `{VALUE} should be unique!`,
 });
 
+PreferenceSchema.pre("save", function (next) {
+  this.selected_sites = Array.from(
+    new Set(this.selected_sites.map((id) => id.toString()))
+  ).map((id) => mongoose.Types.ObjectId(id));
+
+  this.airqloud_ids = Array.from(
+    new Set(this.airqloud_ids.map((id) => id.toString()))
+  ).map((id) => mongoose.Types.ObjectId(id));
+
+  this.device_ids = Array.from(
+    new Set(this.device_ids.map((id) => id.toString()))
+  ).map((id) => mongoose.Types.ObjectId(id));
+
+  this.site_ids = Array.from(
+    new Set(this.site_ids.map((id) => id.toString()))
+  ).map((id) => mongoose.Types.ObjectId(id));
+
+  this.cohort_ids = Array.from(
+    new Set(this.cohort_ids.map((id) => id.toString()))
+  ).map((id) => mongoose.Types.ObjectId(id));
+
+  this.grid_ids = Array.from(
+    new Set(this.grid_ids.map((id) => id.toString()))
+  ).map((id) => mongoose.Types.ObjectId(id));
+
+  this.network_ids = Array.from(
+    new Set(this.network_ids.map((id) => id.toString()))
+  ).map((id) => mongoose.Types.ObjectId(id));
+
+  this.group_ids = Array.from(
+    new Set(this.group_ids.map((id) => id.toString()))
+  ).map((id) => mongoose.Types.ObjectId(id));
+
+  return next();
+});
+
 PreferenceSchema.methods = {
   toJSON() {
     return {
@@ -148,6 +215,9 @@ PreferenceSchema.methods = {
       airqloud_id: this.airqloud_id,
       cohort_id: this.cohort_id,
       grid_id: this.grid_id,
+      airqloud_ids: this.airqloud_ids,
+      cohort_ids: this.cohort_ids,
+      grid_ids: this.grid_ids,
       startDate: this.startDate,
       endDate: this.endDate,
       chartType: this.chartType,
@@ -156,6 +226,9 @@ PreferenceSchema.methods = {
       site_ids: this.site_ids,
       device_ids: this.device_ids,
       network_id: this.network_id,
+      group_id: this.group_id,
+      network_ids: this.network_ids,
+      group_ids: this.group_ids,
       period: this.period,
       createdAt: this.createdAt,
       selected_sites: this.selected_sites,
@@ -167,15 +240,11 @@ PreferenceSchema.statics = {
   async register(args) {
     try {
       let createBody = args;
+      logObject("args", args);
       if (createBody._id) {
         delete createBody._id;
       }
-      if (createBody.selected_sites) {
-        createBody["$addToSet"] = {
-          selected_sites: { $each: createBody.selected_sites },
-        };
-        delete createBody.selected_sites;
-      }
+
       if (isEmpty(createBody.period)) {
         createBody.period = {
           value: "Last 7 days",
@@ -184,6 +253,8 @@ PreferenceSchema.statics = {
           unit: "day",
         };
       }
+
+      logObject("createBody", createBody);
       let data = await this.create({
         ...createBody,
       });
@@ -269,6 +340,7 @@ PreferenceSchema.statics = {
     try {
       const options = { new: true };
       let updateBody = update;
+
       if (updateBody._id) {
         delete updateBody._id;
       }
@@ -279,6 +351,56 @@ PreferenceSchema.statics = {
         };
         delete updateBody.selected_sites;
       }
+
+      if (updateBody.airqloud_ids) {
+        updateBody["$addToSet"] = {
+          airqloud_ids: { $each: updateBody.airqloud_ids },
+        };
+        delete updateBody.airqloud_ids;
+      }
+
+      if (updateBody.device_ids) {
+        updateBody["$addToSet"] = {
+          device_ids: { $each: updateBody.device_ids },
+        };
+        delete updateBody.device_ids;
+      }
+
+      if (updateBody.cohort_ids) {
+        updateBody["$addToSet"] = {
+          cohort_ids: { $each: updateBody.cohort_ids },
+        };
+        delete updateBody.cohort_ids;
+      }
+
+      if (updateBody.grid_ids) {
+        updateBody["$addToSet"] = {
+          grid_ids: { $each: updateBody.grid_ids },
+        };
+        delete updateBody.grid_ids;
+      }
+
+      if (updateBody.site_ids) {
+        updateBody["$addToSet"] = {
+          site_ids: { $each: updateBody.site_ids },
+        };
+        delete updateBody.site_ids;
+      }
+
+      if (updateBody.network_ids) {
+        updateBody["$addToSet"] = {
+          network_ids: { $each: updateBody.network_ids },
+        };
+        delete updateBody.network_ids;
+      }
+
+      if (updateBody.group_ids) {
+        updateBody["$addToSet"] = {
+          group_ids: { $each: updateBody.group_ids },
+        };
+        delete updateBody.group_ids;
+      }
+
       const updatedPreference = await this.findOneAndUpdate(
         filter,
         updateBody,
