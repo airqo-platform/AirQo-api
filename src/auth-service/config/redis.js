edb6f9bc8907bfaee11c35a153f4d72dff2f1e9e
baalmart <bbaalmart@gmail.com>
2023-12-07 00:13:47 +0300

    separating the ioredis and redis configurations

--- src/auth-service/config/redis.js
+++ src/auth-service/config/redis.js
@@ -1,19 +1,10 @@
 const credis = require("redis");
 const constants = require("./constants");
 const { logElement } = require("@utils/log");
-const cioredis = require("ioredis");
 const REDIS_SERVER = constants.REDIS_SERVER;
 const REDIS_PORT = constants.REDIS_PORT;
 logElement("redis URL", REDIS_SERVER && REDIS_SERVER.concat(":", REDIS_PORT));
 
-/*************** using ioredis ********************** */
-const ioredis = new cioredis({
-  port: REDIS_PORT,
-  host: REDIS_SERVER,
-  showFriendlyErrorStack: true,
-});
-
-/*************** using redis ********************** */
 const redis = credis.createClient({
   host: REDIS_SERVER,
   port: REDIS_PORT,
@@ -28,4 +19,4 @@ redis.on("ready", () => {
   console.log("Redis connection esablished");
 });
 
-module.exports = { ioredis, redis };
+module.exports = redis;
