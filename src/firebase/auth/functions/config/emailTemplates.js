8a4ea8a9964b15094681fcd32575c151f1333fb2
Benjamin Ssempala <86492979+BenjaminSsempala@users.noreply.github.com>
2023-12-06 18:27:16 +0300

    Finalising forecast email

--- src/firebase/auth/functions/config/emailTemplates.js
+++ src/firebase/auth/functions/config/emailTemplates.js
@@ -222,8 +222,8 @@ module.exports = {
 
     email_notification: (userFavorites, userID) => {
         const { userEmail } = userFavorites[0];
-        const unSubsciptionUrl = `https://us-central1-airqo-250220.cloudfunctions.net/emailNotifsUnsubscribe?email=${userEmail}&userId=${userID}`;
-        const footerTemplate = module.exports.email_footer_template(userEmail, unSubsciptionUrl);
+        const unSubscriptionUrl = `https://us-central1-airqo-250220.cloudfunctions.net/emailNotifsUnsubscribe?email=${userEmail}&userId=${userID}`;
+        const footerTemplate = module.exports.email_footer_template(userEmail, unSubscriptionUrl);
         function generateFavoriteHTML(name, location) {
             return `
             <tr style="border: 1px #EBF1FF solid;">
@@ -339,147 +339,36 @@ module.exports = {
 
     favorite_forecast_email: (userFavorites, userID) => {
         const { userEmail } = userFavorites[0];
-        const unSubsciptionUrl = `https://us-central1-airqo-250220.cloudfunctions.net/emailNotifsUnsubscribe?email=${userEmail}&userId=${userID}`;
-        const footerTemplate = module.exports.email_footer_template(userEmail, unSubsciptionUrl);
+        const unSubscriptionUrl = `https://us-central1-airqo-250220.cloudfunctions.net/emailNotifsUnsubscribe?email=${userEmail}&userId=${userID}`;
+        const footerTemplate = module.exports.email_footer_template(userEmail, unSubscriptionUrl);
         let favoritesContent = ``;
 
+        for (const favorite of userFavorites) {
+            const airQualityLevels = favorite.airQualityLevels;
+            const days = ['M', 'T', 'W', 'T', 'F', 'S', 'S'];
 
-        for (let i = 0; i < userFavorites.length; i++) {
-            let airQualityLevels = [];
-            let calendarContent = ``;
-            let locationContent = ``;
-            airQualityLevels = userFavorites[i].airQualityLevels;
-
-            calendarContent = `    
-             <!-- Insights Calendar -->
-              <div style=" border: 2px solid #EBF1FF; width: 80%; margin-left: 50px; display:flex; place-content: center;">
-            
-             <table style="width: 60%;padding: 30px; display: revert;padding-left: 40px;">
-                                        <tr>
-                                            <td>
-                                                <div
-                                                    style="width:fit-content; padding: 4.59px; background: #145DFF; border-radius: 10px; flex-direction: column; justify-content: flex-start; align-items: center; gap: 1.32px; display: flex">
-                                                    <div
-                                                        style="color: white; font-size: 8.31px; font-family: Inter; font-weight: 400; line-height: 12.48px; word-wrap: break-word">
-                                                        M</div>
-                                                </div>
-                                            </td>
-                                            <td>
-                                                <div
-                                                    style="padding: 4.59px; flex-direction: column; justify-content: flex-start; align-items: center; gap: 1.32px; display: flex">
-                                                    <div
-                                                        style="color: rgba(18, 23, 35, 0.80); font-size: 8.31px; font-family: Inter; font-weight: 400; line-height: 12.48px; word-wrap: break-word">
-                                                        T</div>
-                                                </div>
-                                            </td>
-                                            <td>
-                                                <div
-                                                    style="padding: 4.59px; flex-direction: column; justify-content: flex-start; align-items: center; gap: 1.32px; display: flex">
-                                                    <div
-                                                        style="color: rgba(18, 23, 35, 0.80); font-size: 8.31px; font-family: Inter; font-weight: 400; line-height: 12.48px; word-wrap: break-word">
-                                                        W</div>
-                                                </div>
-                                            </td>
-                                            <td>
-                                                <div
-                                                    style="padding: 4.59px; flex-direction: column; justify-content: flex-start; align-items: center; gap: 1.32px; display: flex">
-                                                    <div
-                                                        style="color: rgba(18, 23, 35, 0.80); font-size: 8.31px; font-family: Inter; font-weight: 400; line-height: 12.48px; word-wrap: break-word">
-                                                        T</div>
-                                                </div>
-                                            </td>
-                                            <td>
-                                                <div
-                                                    style="padding: 4.59px; flex-direction: column; justify-content: flex-start; align-items: center; gap: 1.32px; display: flex">
-                                                    <div
-                                                        style="color: rgba(18, 23, 35, 0.80); font-size: 8.31px; font-family: Inter; font-weight: 400; line-height: 12.48px; word-wrap: break-word">
-                                                        F</div>
-                                                </div>
-                                            </td>
-                                            <td>
-                                                <div
-                                                    style="padding: 4.59px; flex-direction: column; justify-content: flex-start; align-items: center; gap: 1.32px; display: flex">
-                                                    <div
-                                                        style="color: rgba(18, 23, 35, 0.80); font-size: 8.31px; font-family: Inter; font-weight: 400; line-height: 12.48px; word-wrap: break-word">
-                                                        S</div>
-                                                </div>
-                                            </td>
-                                            <td>
-                                                <div
-                                                    style="padding: 4.59px; flex-direction: column; justify-content: flex-start; align-items: center; gap: 1.32px; display: flex">
-                                                    <div
-                                                        style="color: rgba(18, 23, 35, 0.80); font-size: 8.31px; font-family: Inter; font-weight: 400; line-height: 12.48px; word-wrap: break-word">
-                                                        S</div>
-                                                </div>
-                                            </td>
-                  
-                                        </tr>
-                                        <tr>
-                                            <td>
-                                                <div style=" height: 22.30px; place-content: center; display: flex;  padding-right: 5px; ">
-                                                    <img src="cid:${airQualityLevels[0]}Emoji" style="height: 24px; width: 24px;">
-                                                </div>
-                                            </td>
-                                            <td>
-                                                <div style=" height: 22.30px; position: relative; place-content: center; display: flex;padding-right: 5px;">
-                                                    <img src="cid:${airQualityLevels[1]}Emoji" style="height: 24px; width: 24px;">
-                                                </div>
-                                            </td>
-                                            <td>
-                                                <div style=" height: 22.30px; position: relative; place-content: center; display: flex;padding-right: 5px;">
-                                                    <img src="cid:${airQualityLevels[2]}Emoji" style="height: 24px; width: 24px;">
-                                                </div>
-                                            </td>
-                                            <td>
-                                                <div style=" height: 22.30px; position: relative; place-content: center; display: flex;padding-right: 5px;">
-                                                    <img src="cid:${airQualityLevels[3]}Emoji" style="height: 24px; width: 24px;">
-                                                </div>
-                                            </td>
-                                            <td>
-                                                <div style=" height: 22.30px; position: relative; place-content: center; display: flex;padding-right: 5px;">
-                                                    <img src="cid:${airQualityLevels[4]}Emoji" style="height: 24px; width: 24px;">
-                                                </div>
-                                            </td>
-                                            <td>
-                                                <div style=" height: 22.30px; position: relative; place-content: center; display: flex;padding-right: 5px;">
-                                                    <img src="cid:${airQualityLevels[5]}Emoji" style="height: 24px; width: 24px;">
-                                                </div>
-                                            </td>
-                                            <td>
-                                                <div style=" height: 22.30px; position: relative; place-content: center; display: flex;padding-right: 5px;">
-                                                    <img src="cid:${airQualityLevels[6]}Emoji" style="height: 24px; width: 24px;">
-                                                </div>
-                                            </td>
-                                        
-                                        </tr>
-                                        </table>
-                                    </div>
-                                    `;
-            locationContent = `
-            <table style="width: 100%; text-align: center; background-color: #fff;">
-                                        <tr style="border: 1px #EBF1FF solid;">
-                                            <td style="text-align: center; padding: 10px;">
-                                                <img src="cid:FavoriteIcon" style="height: 24px; width: 24px;">
-                                            </td>
-                                            <td>
-                                                <div
-                                                    style="width: 100px; color: black; font-size: 16px; font-family: Inter; font-weight: 700; line-height: 20px; word-wrap: break-word">
-                                                    ${userFavorites[i].name}</div>
-                                                <div
-                                                    style="width: 150px; font-size: 14px; font-family: Inter; font-weight: 500; line-height: 20px; color: #8D8D8D;">
-                                                    ${userFavorites[i].location}
-                                                </div>
-                                            </td>
-                                            <td
-                                                style="text-align: center; padding: 10px; display: grid; place-items: center;">
-                                                <div
-                                                    style="width: 80px;  background: #145DFF; border-radius: 6.09px; display: grid; align-content: center; justify-content: center; color: white; font-size: 14px; padding-top: 9.13px; padding-bottom: 9.13px;">
-                                                    View now
-                                                </div>
-                                            </td>
-                                        </tr>
-                                    </table> <br/>`;
-            favoritesContent += locationContent + calendarContent;
+            favoritesContent += `
+              <!-- Location Content -->
+        <table style="width: 100%; text-align: center; background-color: #fff;">
+            <tr style="border: 1px #EBF1FF solid;">
+                <td style="text-align: center; padding: 10px;"><img src="cid:FavoriteIcon" style="height: 24px; width: 24px;"></td>
+                <td>
+                    <div style="width: 100px; color: black; font-size: 16px; font-family: Inter; font-weight: 700; line-height: 20px; word-wrap: break-word">${favorite.name}</div>
+                    <div style="width: 150px; font-size: 14px; font-family: Inter; font-weight: 500; line-height: 20px; color: #8D8D8D;">${favorite.location}</div>
+                </td>
+                <td style="text-align: center; padding: 10px; display: grid; place-items: center;">
+                    <div style="width: 80px; background: #145DFF; border-radius: 6.09px; display: grid; align-content: center; justify-content: center; color: white; font-size: 14px; padding-top: 9.13px; padding-bottom: 9.13px;">View now</div>
+                </td>
+            </tr>
+        </table><br/>
+        <!-- Insights Calendar -->
+        <div style="border: 2px solid #EBF1FF; width: 80%; margin-left: 50px; display:flex; place-content: center;">
+            <table style="width: 60%; padding: 30px; display: revert; padding-left: 40px;">
+                <tr>${days.map((day) => `<td>${day}</td>`).join('')}</tr>
+                <tr>${airQualityLevels.map((level) => `<td><div style="height: 22.30px; position: relative; place-content: center; display: flex; padding-right: 5px;"><img src="cid:${level}Emoji" style="height: 24px; width: 24px;"></div></td>`).join('')}</tr>
+            </table>
+        </div>
+      `;
         }
 
         return `<!DOCTYPE html>
