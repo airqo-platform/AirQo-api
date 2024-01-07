d32e3e8fa7bfd6c93699af8261aaaf95ec0a5e4c
Mutabazi Noble <60974514+Mnoble-19@users.noreply.github.com>
2024-01-08 00:13:55 +0300

    Update xcom-setup.sh

--- src/workflows/xcom-setup.sh
+++ src/workflows/xcom-setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-mkdir -p "$AIRFLOW__CORE__XCOM_BACKEND_FOLDER" && cp -r /usr/local/bin/workflows_xcom/* "$AIRFLOW__CORE__XCOM_BACKEND_FOLDER"
+mkdir -p "$AIRFLOW__CORE__XCOM_BACKEND_FOLDER" && cp -r /usr/local/bin/airflow_xcom/* "$AIRFLOW__CORE__XCOM_BACKEND_FOLDER"
 echo "Copied Xcom Content to folder : $AIRFLOW__CORE__XCOM_BACKEND_FOLDER"
 ls "$AIRFLOW__CORE__XCOM_BACKEND_FOLDER"
 exit 0
