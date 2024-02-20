458b14f5ea4a142e5b4e492e805f435cb34190d9
wabinyai <rajacastro@gmail.com>
2024-02-20 14:18:45 +0300

    Local moran

--- src/spatial/job/local_moran/configure.py
+++ src/spatial/job/local_moran/configure.py
@@ -7,5 +7,37 @@ BASE_DIR = Path(__file__).resolve().parent
 dotenv_path = os.path.join(BASE_DIR, ".env")
 load_dotenv(dotenv_path)
 
+
 class Config:
     AIRQO_API_TOKEN = os.getenv("AIRQO_API_TOKEN")
+    GRID_URL = os.getenv("GRID_URL_ID")
+    BIGQUERY_HOURLY_CONSOLIDATED = os.getenv("BIGQUERY_HOURLY_CONSOLIDATED") 
+    CREDENTIALS = os.getenv("GOOGLE_APPLICATION_CREDENTIALS")
+
+
+class ProductionConfig(Config):
+    DEBUG = False
+    TESTING = False
+
+
+class DevelopmentConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+
+class TestingConfig(Config):
+    DEBUG = True
+    TESTING = True
+
+
+app_config = {
+    "development": DevelopmentConfig,
+    "testing": TestingConfig,
+    "production": ProductionConfig,
+    "staging": TestingConfig,
+}
+
+environment = os.getenv("FLASK_ENV", "staging")
+print("ENVIRONMENT", environment or "staging")
+
+configuration = app_config.get(environment, "staging")
--- src/spatial/job/local_moran/utils.py
+++ src/spatial/job/local_moran/utils.py
@@ -25,7 +25,7 @@ def fetch_air_quality_data(grid_id, start_time, end_time) -> list:
         "recent": "no"
     }
 
-    grid_url = f"https://platform.airqo.net/api/v2/devices/measurements/grids/{grid_id}"
+    grid_url = f"{Config.GRID_URL}{grid_id}"
 
     try:
         grid_response = requests.get(grid_url, params=grid_params)
@@ -49,7 +49,7 @@ def query_bigquery(site_ids, start_time, end_time):
     query = f"""
         SELECT site_id, timestamp, site_name, site_latitude, site_longitude, pm2_5, pm2_5_raw_value,
         pm2_5_calibrated_value, pm10, pm10_raw_value, pm10_calibrated_value, country, region, city, county
-        FROM `airqo-250220.consolidated_data.hourly_device_measurements`
+        FROM {Config.BIGQUERY_HOURLY_CONSOLIDATED}
         WHERE site_id IN UNNEST({site_ids})
         AND timestamp BETWEEN TIMESTAMP('{start_time.isoformat()}')
         AND TIMESTAMP('{end_time.isoformat()}')
@@ -70,20 +70,20 @@ def query_bigquery(site_ids, start_time, end_time):
         return None
     
 def results_to_dataframe(results):
-    # Convert 'timestamp' to datetime format
-    df = pd.DataFrame(results)
-    df['timestamp'] = pd.to_datetime(df['timestamp'])
-
-    # Create additional columns using dt accessor
-    df['dates'] = df['timestamp'].dt.date.astype(str)
-    df['date'] = pd.to_datetime(df['dates'])
-    df['day'] = df['timestamp'].dt.day_name()
-    df['hour'] = df['timestamp'].dt.hour
-    df['year'] = df['timestamp'].dt.year
-    df['month'] = df['timestamp'].dt.month
-    df['month_name'] = df['timestamp'].dt.month_name()   
-    df=df.dropna(subset='site_latitude')
-
+    df = (
+        pd.DataFrame(results)
+        .assign(timestamp=lambda x: pd.to_datetime(x['timestamp']))
+        .assign(
+            dates=lambda x: x['timestamp'].dt.date.astype(str),
+            date=lambda x: pd.to_datetime(x['dates']),
+            day=lambda x: x['timestamp'].dt.day_name(),
+            hour=lambda x: x['timestamp'].dt.hour,
+            year=lambda x: x['timestamp'].dt.year,
+            month=lambda x: x['timestamp'].dt.month,
+            month_name=lambda x: x['timestamp'].dt.month_name()
+        )
+        .dropna(subset=['site_latitude', 'site_longitude'])
+    )
     return df
 
 def get_data_for_moran(df):
