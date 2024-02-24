5e6c7b8342cdef2f18598b34baa0bde717ebe3fd
wabinyai <rajacastro@gmail.com>
2024-02-25 02:18:34 +0300

    Getis-Ord Gi* statistic

--- src/spatial/job/getis_ord/controller/controllers.py
+++ src/spatial/job/getis_ord/controller/controllers.py
@@ -3,6 +3,6 @@ from flask import Blueprint, request, jsonify
 from services.getis_services import SpatialDataHandler
 controller_bp = Blueprint('controller', __name__)
 
-@controller_bp.route('/getis', methods=['POST'])
+@controller_bp.route('/getisord', methods=['POST'])
 def get_air_quality_data():
     return SpatialDataHandler.get_air_quality_data()
