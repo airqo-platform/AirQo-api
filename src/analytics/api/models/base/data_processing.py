5932616c458c3300bcce795fe5ccb842a3a42c53
wabinyai <rajacastro@gmail.com>
2024-02-29 09:20:34 +0300

    diurnal report

--- src/analytics/api/models/base/data_processing.py
+++ src/analytics/api/models/base/data_processing.py
@@ -1,9 +1,12 @@
 from flask import Flask, request, jsonify
 from datetime import datetime
 import logging
-from api.utils.pollutants.report import (fetch_air_quality_data, query_bigquery, 
-                                         results_to_dataframe, PManalysis)
-
+from api.utils.pollutants.report import (
+    fetch_air_quality_data,
+    query_bigquery,
+    results_to_dataframe,
+    PManalysis
+)
 # Configure logging
 logging.basicConfig(filename='report_log.log', level=logging.INFO, filemode='w')
 
