bf6a063dc9bfe341a1edd164c122df59a946967f
wabinyai <rajacastro@gmail.com>
2024-03-17 20:45:01 +0300

    cors

--- src/spatial/app.py
+++ src/spatial/app.py
@@ -1,9 +1,10 @@
 # app.py
 from flask import Flask
+from flask_cors import CORS
 from controllers.controllers import controller_bp
 
 app = Flask(__name__)
-
+CORS(app)
 app.register_blueprint(controller_bp, url_prefix='/api/v2/spatial')
 
 if __name__ == '__main__':
