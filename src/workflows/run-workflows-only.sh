de674189c6daa1345611d86d91a9219b4a13b20d
Mutabazi Noble <60974514+Mnoble-19@users.noreply.github.com>
2024-04-27 02:57:16 +0300

    Update README

--- src/workflows/run-workflows-only.sh
+++ src/workflows/run-workflows-only.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 export DOCKER_BUILDKIT=1
+
 # For Windows, uncomment the line below and comment the line above
 # set DOCKER_BUILDKIT=1
+
 docker-compose -f docker-compose.yaml up --build db db-setup webserver scheduler
