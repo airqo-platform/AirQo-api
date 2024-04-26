9be66f2423b04fd40f49a9cde26fa04844e318c8
Mutabazi Noble <60974514+Mnoble-19@users.noreply.github.com>
2024-04-27 02:54:59 +0300

    update airflow & db setup - (setup init container)

--- src/workflows/db-setup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-airflow db init
-#airflow db upgrade
-airflow users create --role Admin --username "$ADMIN_USERNAME" --email airflow@airflow.org --firstname airflow --lastname airflow --password "$ADMIN_USERNAME"
-exit 0
