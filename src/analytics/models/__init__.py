7d41c0ea7404e95f019dde1f78a2aa78e421b02c
Mutabazi Noble <60974514+Mnoble-19@users.noreply.github.com>
2024-05-16 14:03:42 +0300

    remove old unused code

--- src/analytics/models/__init__.py
+++ src/analytics/models/__init__.py
@@ -1,8 +1,4 @@
 from .site import SiteModel
 from .events import EventsModel
 from .exceedance import ExceedanceModel
-from .data_export import (
-    DataExportModel,
-    DataExportStatus,
-    DataExportRequest,
-)
+
