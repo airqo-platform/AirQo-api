5bcdfb0994afea5e310ccb6a97ddbd937fdd8bcc
balmart <balmart@balmart-2.local>
2024-05-25 23:45:40 +0300

    use East African timezone as point of trigger

--- src/auth-service/bin/token-expiration-job.js
+++ src/auth-service/bin/token-expiration-job.js
@@ -9,34 +9,41 @@ const logger = log4js.getLogger(
 );
 const moment = require("moment-timezone");
 
-cron.schedule("0 0 1 * *", async () => {
-  const timeZone = moment.tz.guess();
-  // Calculate the current date and time in the user's timezone
-  const now = moment().tz(timeZone);
-  // Calculate two months from now in the user's timezone
-  const twoMonthsFromNow = now.clone().add(2, "months");
-  const filter = {
-    expires: { $gte: now.toDate(), $lt: twoMonthsFromNow.toDate() },
-  };
-  try {
-    const tokens = await AccessTokenModel.list({ filter });
-    tokens.forEach(async (token) => {
-      const email = token.email;
-      const firstName = token.firstName;
-      const lastName = token.lastName;
-      const emailResponse = await mailer.expiringToken({
-        email,
-        firstName,
-        lastName,
-      });
+cron.schedule(
+  "0 0 1 * *",
+  async () => {
+    const timeZone = moment.tz.guess();
+    // Calculate the current date and time in the user's timezone
+    const now = moment().tz(timeZone);
+    // Calculate two months from now in the user's timezone
+    const twoMonthsFromNow = now.clone().add(2, "months");
+    const filter = {
+      expires: { $gte: now.toDate(), $lt: twoMonthsFromNow.toDate() },
+    };
+    try {
+      const tokens = await AccessTokenModel.list({ filter });
+      tokens.forEach(async (token) => {
+        const email = token.email;
+        const firstName = token.firstName;
+        const lastName = token.lastName;
+        const emailResponse = await mailer.expiringToken({
+          email,
+          firstName,
+          lastName,
+        });
 
-      if (emailResponse && emailResponse.success === false) {
-        logger.error(
-          `🐛🐛 Internal Server Error -- ${stringify(emailResponse)}`
-        );
-      }
-    });
-  } catch (error) {
-    logger.error(`🐛🐛 Internal Server Error -- ${stringify(error)}`);
+        if (emailResponse && emailResponse.success === false) {
+          logger.error(
+            `🐛🐛 Internal Server Error -- ${stringify(emailResponse)}`
+          );
+        }
+      });
+    } catch (error) {
+      logger.error(`🐛🐛 Internal Server Error -- ${stringify(error)}`);
+    }
+  },
+  {
+    scheduled: true,
+    timezone: "Africa/Nairobi",
   }
-});
+);
