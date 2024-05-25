5bdb7864db6c18087567d5915bfe85ded7c685f1
balmart <balmart@balmart-2.local>
2024-05-25 23:29:55 +0300

    handling potential timezone errors

--- src/auth-service/bin/token-expiration-job.js
+++ src/auth-service/bin/token-expiration-job.js
@@ -5,17 +5,19 @@ const mailer = require("@utils/mailer");
 const stringify = require("@utils/stringify");
 const log4js = require("log4js");
 const logger = log4js.getLogger(
-  `${constants.ENVIRONMENT} -- bin/token-expiration`
+  `${constants.ENVIRONMENT} -- bin/token-expiration-job`
 );
+const moment = require("moment-timezone");
 
 cron.schedule("0 0 1 * *", async () => {
-  // Find tokens expiring within the next 2 months
-  const now = new Date();
-  const twoMonthsFromNow = new Date(now.setMonth(now.getMonth() + 2));
+  const timeZone = moment.tz.guess();
+  // Calculate the current date and time in the user's timezone
+  const now = moment().tz(timeZone);
+  // Calculate two months from now in the user's timezone
+  const twoMonthsFromNow = now.clone().add(2, "months");
   const filter = {
-    expires: { $gte: now, $lt: twoMonthsFromNow },
+    expires: { $gte: now.toDate(), $lt: twoMonthsFromNow.toDate() },
   };
-
   try {
     const tokens = await AccessTokenModel.list({ filter });
     tokens.forEach(async (token) => {
