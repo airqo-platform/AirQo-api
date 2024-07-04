ef9a9d0317eeb30ac05fe7ec934421eb4ee00b48
MartinKalema <kalemaaaaaaaa@gmail.com>
2024-07-04 20:48:08 +0300

    Ran black formatter

--- src/workflows/airqo_etl_utils/openmeteo_api.py
+++ src/workflows/airqo_etl_utils/openmeteo_api.py
@@ -2,6 +2,7 @@ import requests
 from concurrent.futures import ThreadPoolExecutor, as_completed
 from airqo_etl_utils.config import configuration
 
+
 class OpenMeteoWeatherAPI:
     def __init__(self):
         self.BASE_URL = configuration.OPEN_METEO_BASE_URL
@@ -15,7 +16,7 @@ class OpenMeteoWeatherAPI:
 
         try:
             response = requests.get(self.BASE_URL, params=params)
-            response.raise_for_status() 
+            response.raise_for_status()
             data = response.json()
             return data
         except requests.exceptions.RequestException as e:
@@ -36,4 +37,3 @@ class OpenMeteoWeatherAPI:
                     print(f"Weather data: {result}")
 
         return results
-
