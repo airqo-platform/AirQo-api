84d62f922bfc576d7ff7f3636c00cd6c32ec3de6
Benjamin Ssempala <86492979+BenjaminSsempala@users.noreply.github.com>
2024-08-21 01:08:59 +0300

    Revert and Upgrade Kafka cluster

--- k8s/kafka/operator/deployment-script.sh
+++ k8s/kafka/operator/deployment-script.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-
-kubectl apply -f 'https://strimzi.io/install/latest?namespace=message-broker' -n message-broker
+wget https://github.com/strimzi/strimzi-kafka-operator/releases/download/0.42.0/strimzi-0.42.0.tar.gz
+tar -zxvf strimzi-0.42.0.tar.gz
+cd  strimzi-0.42.0/
+sed -i 's/namespace: .*/namespace: message-broker/' install/cluster-operator/*RoleBinding*.yaml
+kubectl apply -f install/cluster-operator -n message-broker
 kubectl scale deployment strimzi-cluster-operator --replicas=2 -n message-broker
-
 exit 0
