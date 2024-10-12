a45b5b7151c7209b30b6a6048c5cd2b08f7a5c83
baalmart <bbaalmart@gmail.com>
2024-10-12 15:02:06 +0300

    just updating the import for the unit test

--- src/device-registry/bin/jobs/test/ut_v2-store-readings-job.js
+++ src/device-registry/bin/jobs/test/ut_v2-store-readings-job.js
@@ -14,7 +14,7 @@ const {
   isEntityActive,
   updateEntityStatus,
   processDocument,
-} = require("@bin/jobs/new-store-readings-job");
+} = require("@bin/jobs/v2-store-readings-job");
 
 chai.use(sinonChai);
 chai.use(chaiAsPromised);
