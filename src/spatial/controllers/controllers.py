d5dd706505b22c2b388b86f2ccc6bc3b996aaea5
Mutabazi Noble <60974514+Mnoble-19@users.noreply.github.com>
2024-10-12 07:49:48 +0300

    run black formatter

--- src/spatial/controllers/controllers.py
+++ src/spatial/controllers/controllers.py
@@ -3,51 +3,66 @@ from flask import Blueprint, request, jsonify
 from views.getis_services import SpatialDataHandler
 from views.getis_confidence_services import SpatialDataHandler_confidence
 from views.localmoran_services import SpatialDataHandler_moran
-from views.derived_pm2_5 import PM25View, PM25_aod_Model_daily, Sentinel5PView, Satellite_data
+from views.derived_pm2_5 import (
+    PM25View,
+    PM25_aod_Model_daily,
+    Sentinel5PView,
+    Satellite_data,
+)
 from views.satellite_predictions import SatellitePredictionView
 from views.site_category_view import SiteCategorizationView
 from views.site_selection_views import SiteSelectionView
 
 
+controller_bp = Blueprint("controller", __name__)
 
-controller_bp = Blueprint('controller', __name__)
-  # 95% confidence level
-@controller_bp.route('/getisord', methods=['POST'])
+
+# 95% confidence level
+@controller_bp.route("/getisord", methods=["POST"])
 def get_air_quality_data():
     return SpatialDataHandler.get_air_quality_data()
 
-@controller_bp.route('/getisord_confidence', methods=['POST'])
+
+@controller_bp.route("/getisord_confidence", methods=["POST"])
 def get_air_quality_data_confi():
     return SpatialDataHandler_confidence.get_air_quality_data_getis()
 
-@controller_bp.route('/localmoran', methods=['POST'])
+
+@controller_bp.route("/localmoran", methods=["POST"])
 def get_air_quality_data_moran():
     return SpatialDataHandler_moran.get_air_quality_data_moran()
 
-@controller_bp.route('/derived_pm2_5', methods=['GET'])
+
+@controller_bp.route("/derived_pm2_5", methods=["GET"])
 def get_derived_pm2_5():
     return PM25View.get_pm25()
 
-@controller_bp.route('/derived_pm2_5_daily', methods=['GET'])
+
+@controller_bp.route("/derived_pm2_5_daily", methods=["GET"])
 def get_derived_pm2_5_daily():
     return PM25_aod_Model_daily.get_aod_for_dates()
 
-@controller_bp.route('/sentinel5p', methods=['GET'])
+
+@controller_bp.route("/sentinel5p", methods=["GET"])
 def get_sentinel5p():
     return Sentinel5PView.get_pollutants_data()
 
-@controller_bp.route('/satellite_data', methods=['GET'])
+
+@controller_bp.route("/satellite_data", methods=["GET"])
 def get_satellite_data():
     return Satellite_data.get_pollutants_data()
 
-@controller_bp.route('/categorize_site', methods=['GET'])
+
+@controller_bp.route("/categorize_site", methods=["GET"])
 def categorize_site():
     return SiteCategorizationView.get_site_categorization()
 
-@controller_bp.route('/site_location', methods=['GET'])
+
+@controller_bp.route("/site_location", methods=["GET"])
 def site_selection():
     return SiteSelectionView.site_selection()
 
-@controller_bp.route('/satellite_prediction', methods=['POST'])
+
+@controller_bp.route("/satellite_prediction", methods=["POST"])
 def get_satellite_prediction():
     return SatellitePredictionView.make_predictions()
--- src/spatial/models/site_selection_model.py
+++ src/spatial/models/site_selection_model.py
@@ -8,49 +8,52 @@ import multiprocessing
 
 random.seed(42)
 
+
 class SiteCategoryModel:
     def __init__(self):
         self.geolocator = Nominatim(user_agent="sensor_deployment")
 
     def categorize_site_osm(self, lat, lon):
         try:
-            location = self.geolocator.reverse((lat, lon), language='en')
-            address = location.raw.get('address', {})
+            location = self.geolocator.reverse((lat, lon), language="en")
+            address = location.raw.get("address", {})
         except Exception as e:
             address = {}
 
         category = self._determine_category(address)
-        highway = address.get('road', None) if category in ["Urban", "Commercial", "Residential"] else None
-        landuse = address.get('landuse', None) if category in ["Residential", "Commercial", "Industrial"] else None
-        natural = address.get('natural', None) if category in ["Natural", "Rural"] else None
-
-        place_name = address.get('suburb', address.get('city', 'Unknown'))
-
-        return (
-            category,
-            None,
-            place_name,
-            landuse,
-            natural,
-            None,
-            highway,
-            None
+        highway = (
+            address.get("road", None)
+            if category in ["Urban", "Commercial", "Residential"]
+            else None
+        )
+        landuse = (
+            address.get("landuse", None)
+            if category in ["Residential", "Commercial", "Industrial"]
+            else None
+        )
+        natural = (
+            address.get("natural", None) if category in ["Natural", "Rural"] else None
         )
 
+        place_name = address.get("suburb", address.get("city", "Unknown"))
+
+        return (category, None, place_name, landuse, natural, None, highway, None)
+
     def _determine_category(self, address):
-        if 'landuse' in address:
-            landuse = address['landuse']
-            if landuse in ['residential']:
-                return 'Residential'
-            elif landuse in ['commercial']:
-                return 'Commercial'
-            elif landuse in ['industrial']:
-                return 'Industrial'
-        if 'natural' in address:
-            return 'Natural'
-        if 'city' in address or 'suburb' in address:
-            return 'Urban'
-        return 'Unknown'
+        if "landuse" in address:
+            landuse = address["landuse"]
+            if landuse in ["residential"]:
+                return "Residential"
+            elif landuse in ["commercial"]:
+                return "Commercial"
+            elif landuse in ["industrial"]:
+                return "Industrial"
+        if "natural" in address:
+            return "Natural"
+        if "city" in address or "suburb" in address:
+            return "Urban"
+        return "Unknown"
+
 
 class SensorDeployment:
     def __init__(self, polygon, must_have_locations=None, min_distance_km=0.5):
@@ -61,8 +64,10 @@ class SensorDeployment:
 
     def is_far_enough(self, point):
         for site in self.sites:
-            existing_point = Point(site['longitude'], site['latitude'])
-            distance = great_circle((point.y, point.x), (existing_point.y, existing_point.x)).kilometers
+            existing_point = Point(site["longitude"], site["latitude"])
+            distance = great_circle(
+                (point.y, point.x), (existing_point.y, existing_point.x)
+            ).kilometers
             if distance < self.min_distance_km:
                 return False
         return True
@@ -81,18 +86,22 @@ class SensorDeployment:
         for loc in self.must_have_locations:
             lat, lon = loc
             if self.polygon.contains(Point(lon, lat)):
-                self.sites.append({
-                    'latitude': round(lat, 6),
-                    'longitude': round(lon, 6),
-                    'category': "Unknown",
-                    'area_name': "Unknown",
-                    'highway': "Unknown",
-                    'landuse': "Unknown",
-                    'natural': "Unknown"
-                })
+                self.sites.append(
+                    {
+                        "latitude": round(lat, 6),
+                        "longitude": round(lon, 6),
+                        "category": "Unknown",
+                        "area_name": "Unknown",
+                        "highway": "Unknown",
+                        "landuse": "Unknown",
+                        "natural": "Unknown",
+                    }
+                )
 
         # Generate random points within the polygon
-        random_points = self.generate_random_points(num_sensors * 100)  # Generate more points than needed
+        random_points = self.generate_random_points(
+            num_sensors * 100
+        )  # Generate more points than needed
         random_coords = [(point.y, point.x) for point in random_points]
 
         # Apply KMeans clustering to find optimal sensor locations
@@ -102,37 +111,58 @@ class SensorDeployment:
 
         for center in cluster_centers:
             lat, lon = center
-            self.sites.append({
-                'latitude': round(lat, 6),
-                'longitude': round(lon, 6),
-                'category': "Unknown",
-                'area_name': "Unknown",
-                'highway': "Unknown",
-                'landuse': "Unknown",
-                'natural': "Unknown"
-            })
+            self.sites.append(
+                {
+                    "latitude": round(lat, 6),
+                    "longitude": round(lon, 6),
+                    "category": "Unknown",
+                    "area_name": "Unknown",
+                    "highway": "Unknown",
+                    "landuse": "Unknown",
+                    "natural": "Unknown",
+                }
+            )
 
     def categorize_sites(self, max_workers=None):
         model = SiteCategoryModel()
-        
+
         def process_site(site):
             try:
-                lat, lon = site['latitude'], site['longitude']
-                category, _, area_name, landuse, natural, _, highway, _ = model.categorize_site_osm(lat, lon)
+                lat, lon = site["latitude"], site["longitude"]
+                (
+                    category,
+                    _,
+                    area_name,
+                    landuse,
+                    natural,
+                    _,
+                    highway,
+                    _,
+                ) = model.categorize_site_osm(lat, lon)
 
-                if natural in ["water", "wetland", "lake", "river", "stream", "glacier", "beach"]:
+                if natural in [
+                    "water",
+                    "wetland",
+                    "lake",
+                    "river",
+                    "stream",
+                    "glacier",
+                    "beach",
+                ]:
                     return None  # Skip sites with natural features that are not desired
 
-                site.update({
-                    'category': category,
-                    'area_name': area_name,
-                    'highway': highway,
-                    'landuse': landuse,
-                    'natural': natural
-                })
+                site.update(
+                    {
+                        "category": category,
+                        "area_name": area_name,
+                        "highway": highway,
+                        "landuse": landuse,
+                        "natural": natural,
+                    }
+                )
                 return site
             except Exception as e:
-             #   print(f"Error processing site at ({site['latitude']}, {site['longitude']}): {e}")
+                #   print(f"Error processing site at ({site['latitude']}, {site['longitude']}): {e}")
                 return None
 
         # Determine the number of workers, defaulting to the number of CPU cores if not provided
@@ -140,18 +170,20 @@ class SensorDeployment:
             max_workers = multiprocessing.cpu_count()
 
         with ThreadPoolExecutor(max_workers=max_workers) as executor:
-            future_to_site = {executor.submit(process_site, site): site for site in self.sites}
+            future_to_site = {
+                executor.submit(process_site, site): site for site in self.sites
+            }
             results = []
             for future in as_completed(future_to_site):
                 result = future.result()
                 if result is not None:
                     results.append(result)
-        
+
         self.sites = results
 
     def get_category_counts(self):
         counts = {}
         for site in self.sites:
-            category = site.get('category', 'Unknown')
+            category = site.get("category", "Unknown")
             counts[category] = counts.get(category, 0) + 1
         return counts
