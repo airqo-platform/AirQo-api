00e77b2d7435cbf1faf4cb0a3b424e66a17f839e
wabinyai <rajacastro@gmail.com>
2024-11-14 10:14:55 +0300

    resolve

--- src/spatial/configure.py
+++ src/spatial/configure.py
@@ -28,7 +28,6 @@ class Config:
     GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
     OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
-
 class ProductionConfig(Config):
     DEBUG = False
     TESTING = False
