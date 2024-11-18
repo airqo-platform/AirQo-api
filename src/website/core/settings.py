acaea99dd0d571079f999ea4bcc6e552a818c9d3
Ochieng Paul <ochiengpaul442@gmail.com>
2024-11-18 16:27:53 +0300

    updated settings file

--- src/website/core/settings.py
+++ src/website/core/settings.py
@@ -20,7 +20,8 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
+# ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
+ALLOWED_HOSTS = '*'
 
 # Application definition
 INSTALLED_APPS = [
