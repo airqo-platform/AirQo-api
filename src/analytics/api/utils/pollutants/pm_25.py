c52e05885defcaf78a3d6de984553f4eceb9b841
NicholasTurner23 <mugtrade@gmail.com>
2024-11-19 19:44:47 +0300

    Update pollutant mapping for more dynamic queries

--- src/analytics/api/utils/pollutants/pm_25.py
+++ src/analytics/api/utils/pollutants/pm_25.py
@@ -54,28 +54,26 @@ POLLUTANT_BIGQUERY_MAPPER = {
     "no2": ["no2_calibrated_value", "no2_raw_value"],
 }
 
+COMMON_POLLUTANT_MAPPING = {
+    "pm2_5_calibrated": ["pm2_5_calibrated_value"],
+    "pm2_5_raw": ["pm2_5_raw_value"],
+    "pm10_calibrated": ["pm10_calibrated_value"],
+    "pm10_raw": ["pm10_raw_value"],
+    "no2_calibrated": ["no2_calibrated_value"],
+    "no2_raw": ["no2_raw_value"],
+}
+
 BIGQUERY_FREQUENCY_MAPPER = {
     "raw": {
         "pm2_5": ["pm2_5", "s1_pm2_5", "s2_pm2_5"],
         "pm10": ["pm10", "s1_pm10", "s2_pm10"],
         "no2": ["no2"],
     },
-    "daily": {
-        "pm2_5_calibrated": ["pm2_5_calibrated_value"],
-        "pm2_5_raw": ["pm2_5_raw_value"],
-        "pm10_calibrated": ["pm10_calibrated_value"],
-        "pm10_raw": ["pm10_raw_value"],
-        "no2_calibrated": ["no2_calibrated_value"],
-        "no2_raw": ["no2_raw_value"],
-    },
-    "hourly": {
-        "pm2_5_calibrated": ["pm2_5_calibrated_value"],
-        "pm2_5_raw": ["pm2_5_raw_value"],
-        "pm10_calibrated": ["pm10_calibrated_value"],
-        "pm10_raw": ["pm10_raw_value"],
-        "no2_calibrated": ["no2_calibrated_value"],
-        "no2_raw": ["no2_raw_value"],
-    },
+    "daily": COMMON_POLLUTANT_MAPPING,
+    "hourly": COMMON_POLLUTANT_MAPPING,
+    "weekly": COMMON_POLLUTANT_MAPPING,
+    "monthly": COMMON_POLLUTANT_MAPPING,
+    "yearly": COMMON_POLLUTANT_MAPPING,
 }
 
 PM_COLOR_CATEGORY = {
@@ -119,9 +117,9 @@ NO2_CATEGORY = {
 }
 
 WEATHER_FIELDS_MAPPER = {
-    "temperature": "device_temperature",
-    "humidity": "device_humidity",
-    "wind_speed": "wind_speed",
+    "temperature": ["device_temperature"],
+    "humidity": ["device_humidity"],
+    "wind_speed": ["wind_speed"],
 }
 
 
