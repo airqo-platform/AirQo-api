760d296ef03a0c00295a88e7f8029be07e4dcaeb
NicholasTurner23 <mugtrade@gmail.com>
2024-12-02 15:03:05 +0300

    Clean up

--- src/workflows/airqo_etl_utils/weather_data_utils.py
+++ src/workflows/airqo_etl_utils/weather_data_utils.py
@@ -134,6 +134,8 @@ class WeatherDataUtils:
 
         data["value"] = pd.to_numeric(data["value"], errors="coerce", downcast="float")
         data["time"] = pd.to_datetime(data["time"], errors="coerce")
+        # TODO Clean this up.
+        data["network"] = "airqo"
         parameter_mappings = {
             "te": "temperature",
             "rh": "humidity",
