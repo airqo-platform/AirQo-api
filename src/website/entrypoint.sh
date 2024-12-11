8b99e73284ea1df302a2eb9b4b495becd444847e
Ochieng Paul <ochiengpaul442@gmail.com>
2024-12-11 20:05:31 +0300

    updated deploypreview workflow and docker files accordingly

--- src/website/entrypoint.sh
+++ src/website/entrypoint.sh
@@ -1,16 +1,13 @@
 #!/bin/sh
 
-# Exit immediately if a command exits with a non-zero status
+# Exit on error
 set -e
 
-# Run Django migrations
 echo "Running migrations..."
 python manage.py migrate --noinput
 
-# Collect static files (ensure the static files directory exists)
 echo "Collecting static files..."
 python manage.py collectstatic --noinput
 
-# Start Gunicorn server to serve the Django application
-echo "Starting Gunicorn server..."
+echo "Starting Gunicorn..."
 exec gunicorn core.wsgi:application --bind 0.0.0.0:8000 --timeout 600 --workers 3 --log-level info
