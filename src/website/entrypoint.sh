743f2e13250cbfe696ed3a8590733ac0997f669d
Ochieng Paul <ochiengpaul442@gmail.com>
2024-12-11 20:45:41 +0300

    UP

--- src/website/entrypoint.sh
+++ src/website/entrypoint.sh
@@ -9,5 +9,9 @@ python manage.py migrate --noinput
 echo "Collecting static files..."
 python manage.py collectstatic --noinput
 
+# Move collected static files to the Nginx static directory
+echo "Moving static files to Nginx directory..."
+cp -r /app/staticfiles/* /usr/share/nginx/html/static/
+
 echo "Starting Supervisor (which runs Nginx and Gunicorn)..."
 exec /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
