c05f399df58805ad0d2bd1bed67eec4096dbf87a
Ochieng Paul <ochiengpaul442@gmail.com>
2024-12-11 20:58:18 +0300

    UPDATE

--- src/website/entrypoint.sh
+++ src/website/entrypoint.sh
@@ -13,5 +13,9 @@ python manage.py collectstatic --noinput
 echo "Moving static files to Nginx directory..."
 cp -r /app/staticfiles/* /usr/share/nginx/html/static/
 
+# Substitute environment variables in nginx.conf.template and generate nginx.conf
+echo "Configuring Nginx to listen on PORT=${PORT}..."
+envsubst '${PORT}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
+
 echo "Starting Supervisor (which runs Nginx and Gunicorn)..."
 exec /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
