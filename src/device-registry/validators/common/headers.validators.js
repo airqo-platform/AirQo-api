99b34e16d0cb8058cd6c75e856b3a5bc3b3f720a
baalmart <bbaalmart@gmail.com>
2025-01-26 15:39:36 +0300

    revert to previous headers config to minimise prod environment issues

--- src/device-registry/validators/common/headers.validators.js
+++ src/device-registry/validators/common/headers.validators.js
@@ -2,16 +2,16 @@ const constants = require("@config/constants");
 const allowedOrigins = constants.ALLOWED_ORIGINS?.split(",") || ["*"];
 
 const headers = (req, res, next) => {
-  const origin = req.get("Origin");
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    allowedOrigins.includes("*")
-      ? "*"
-      : allowedOrigins.includes(origin)
-      ? origin
-      : ""
-  );
-  // res.setHeader("Access-Control-Allow-Origin", "*");
+  // const origin = req.get("Origin");
+  // res.setHeader(
+  //   "Access-Control-Allow-Origin",
+  //   allowedOrigins.includes("*")
+  //     ? "*"
+  //     : allowedOrigins.includes(origin)
+  //     ? origin
+  //     : ""
+  // );
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
