17bb9832ec641855d578d26717788b93b1844764
NicholasTurner23 <mugtrade@gmail.com>
2025-03-18 13:33:02 +0300

    Drop nas after conversion

--- src/workflows/airqo_etl_utils/datautils.py
+++ src/workflows/airqo_etl_utils/datautils.py
@@ -421,8 +421,8 @@ class DataUtils:
             Exception: For unexpected errors during column retrieval or data processing.
         """
         bigquery = BigQueryApi()
-        data.dropna(subset=["timestamp"], inplace=True)
         data["timestamp"] = pd.to_datetime(data["timestamp"], errors="coerce")
+        data.dropna(subset=["timestamp"], inplace=True)
 
         try:
             datasource = Config.DataSource
