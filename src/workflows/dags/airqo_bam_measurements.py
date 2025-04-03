83ef2e2c2ec2fbc7879a8bbb0f0aab0fe0e6a7b1
NicholasTurner23 <mugtrade@gmail.com>
2025-04-03 16:24:52 +0300

    Add cleanup workflow for airqo bam data

--- src/workflows/dags/airqo_bam_measurements.py
+++ src/workflows/dags/airqo_bam_measurements.py
@@ -1,11 +1,11 @@
 from airflow.decorators import dag, task
 
-from airqo_etl_utils.config import configuration
+from airqo_etl_utils.config import configuration as Config
 from airqo_etl_utils.workflows_custom_utils import AirflowUtils
 
 from airflow.utils.dates import days_ago
 import pandas as pd
-from airqo_etl_utils.airqo_utils import AirQoDataUtils
+from airqo_etl_utils.data_validator import DataValidationUtils
 from airqo_etl_utils.datautils import DataUtils
 from airqo_etl_utils.date import DateUtils
 from airqo_etl_utils.bigquery_api import BigQueryApi
@@ -149,7 +149,7 @@ def airqo_bam_realtime_measurements():
 
         broker = MessageBrokerUtils()
         broker.publish_to_topic(
-            topic=configuration.AVERAGED_HOURLY_MEASUREMENTS_TOPIC, data=data
+            topic=Config.AVERAGED_HOURLY_MEASUREMENTS_TOPIC, data=data
         )
 
     unclean_data = extract_bam_data()
@@ -159,5 +159,100 @@ def airqo_bam_realtime_measurements():
     update_latest_data_topic(measurements)
 
 
+@dag(
+    "AirQo-BAM-Measurements-Cleanup",
+    schedule="*/45 * * * *",
+    catchup=False,
+    tags=["airqo", "bam" "raw", "hourly", "cleanup"],
+    default_args=AirflowUtils.dag_default_configs(),
+)
+def airqo_bam_measurements_cleanup():
+    @task(provide_context=True, retries=3, retry_delay=timedelta(minutes=5))
+    def extract_raw_data(**kwargs) -> pd.DataFrame:
+        start_date_time, end_date_time = DateUtils.get_dag_date_time_values(
+            days=10, **kwargs
+        )
+        data = DataUtils.extract_data_from_bigquery(
+            DataType.RAW,
+            start_date_time=start_date_time,
+            end_date_time=end_date_time,
+            frequency=Frequency.RAW,
+            device_category=DeviceCategory.BAM,
+        )
+        return DataValidationUtils.remove_outliers_fix_types(
+            data, remove_outliers=False
+        )
+
+    @task(provide_context=True, retries=3, retry_delay=timedelta(minutes=5))
+    def extract_hourly_data(**kwargs) -> pd.DataFrame:
+        start_date_time, end_date_time = DateUtils.get_dag_date_time_values(
+            days=15, **kwargs
+        )
+        data = DataUtils.extract_data_from_bigquery(
+            DataType.AVERAGED,
+            start_date_time=start_date_time,
+            end_date_time=end_date_time,
+            frequency=Frequency.HOURLY,
+            device_category=DeviceCategory.BAM,
+        )
+        return DataValidationUtils.remove_outliers_fix_types(data)
+
+    @task()
+    def remove_duplicated_raw_data(data: pd.DataFrame) -> pd.DataFrame:
+        exclude_cols = [
+            data.device_number.name,
+            data.latitude.name,
+            data.longitude.name,
+            data.network.name,
+        ]
+        return DataUtils.remove_duplicates(
+            data=data,
+            timestamp_col=data.timestamp.name,
+            id_col=data.device_id.name,
+            group_col=data.site_id.name,
+            exclude_cols=exclude_cols,
+        )
+
+    @task()
+    def remove_duplicated_hourly_data(data: pd.DataFrame) -> pd.DataFrame:
+        exclude_cols = [
+            data.device_number.name,
+            data.latitude.name,
+            data.longitude.name,
+            data.network.name,
+        ]
+        return DataUtils.remove_duplicates(
+            data=data,
+            timestamp_col=data.timestamp.name,
+            id_col=data.device_id.name,
+            group_col=data.site_id.name,
+            exclude_cols=exclude_cols,
+        )
+
+    @task(provide_context=True, retries=3, retry_delay=timedelta(minutes=5))
+    def load_raw_data(data: pd.DataFrame):
+
+        big_query_api = BigQueryApi()
+        big_query_api.reload_data(
+            dataframe=data, table=big_query_api.raw_bam_measurements_table
+        )
+
+    @task(provide_context=True, retries=3, retry_delay=timedelta(minutes=5))
+    def load_hourly_data(data: pd.DataFrame):
+
+        big_query_api = BigQueryApi()
+        big_query_api.reload_data(
+            dataframe=data, table=big_query_api.bam_hourly_measurements_table
+        )
+
+    raw_data = extract_raw_data()
+    hourly_data = extract_hourly_data()
+    clean_raw_data = remove_duplicated_raw_data(raw_data)
+    clean_hourly_data = remove_duplicated_hourly_data(hourly_data)
+    load_raw_data(data=clean_raw_data)
+    load_hourly_data(data=clean_hourly_data)
+
+
 airqo_bam_realtime_measurements()
 airqo_bam_historical_measurements()
+airqo_bam_measurements_cleanup()
