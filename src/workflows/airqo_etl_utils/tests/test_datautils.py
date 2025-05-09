5e1b7fa3c9557db4d7084fe75e2a43a94f00b93f
NicholasTurner23 <mugtrade@gmail.com>
2025-05-09 21:45:49 +0300

    Test datautils-data extraction from bigquery

--- src/workflows/airqo_etl_utils/tests/test_datautils.py
+++ src/workflows/airqo_etl_utils/tests/test_datautils.py
@@ -1,16 +1,34 @@
-import unittest.mock
+from unittest.mock import MagicMock, patch
 import pandas as pd
 import pytest
 from airqo_etl_utils.datautils import DataUtils
-from .conftest import DevicesFixtures
+from .conftest import (
+    DevicesFixtures,
+    LC_RAW_DATA,
+    LC_AVERAGED_DATA,
+    BAM_RAW_DATA,
+    BAM_AVERAGED_DATA,
+    CONSOLIDATE_DATA,
+    WEATHER_DATA,
+    mock_bigquery_api,
+    mock_data_validation_utils,
+    mock_data_utils,
+)
+from airqo_etl_utils.constants import (
+    DataType,
+    Frequency,
+    DeviceCategory,
+    DeviceNetwork,
+)
+from airqo_etl_utils.config import configuration as Config
 
 
 class TestsDevices(DevicesFixtures):
     def test_successful_load_from_cache(self, cached_device_df):
-        with unittest.mock.patch(
+        with patch(
             "airqo_etl_utils.datautils.DataUtils.load_cached_data",
             return_value=cached_device_df,
-        ), unittest.mock.patch(
+        ), patch(
             "airqo_etl_utils.datautils.DataUtils.fetch_devices_from_api"
         ) as mock_fetch_api:
             devices, _ = DataUtils.get_devices()
@@ -22,10 +40,10 @@ class TestsDevices(DevicesFixtures):
             mock_fetch_api.assert_not_called()
 
     def test_successful_fetch_from_api(self, api_device_df, airqo_device_keys):
-        with unittest.mock.patch(
+        with patch(
             "airqo_etl_utils.datautils.DataUtils.load_cached_data",
             return_value=pd.DataFrame(),
-        ), unittest.mock.patch(
+        ), patch(
             "airqo_etl_utils.datautils.DataUtils.fetch_devices_from_api",
             return_value=(api_device_df.copy(), airqo_device_keys),
         ):
@@ -37,17 +55,43 @@ class TestsDevices(DevicesFixtures):
             pd.testing.assert_frame_equal(devices, expected_df)
             assert keys == airqo_device_keys
 
-
-def test_failure_to_retrieve_from_api():
-    with unittest.mock.patch(
-        "airqo_etl_utils.datautils.DataUtils.load_cached_data",
-        return_value=pd.DataFrame(),
-    ), unittest.mock.patch(
-        "airqo_etl_utils.datautils.DataUtils.fetch_devices_from_api",
-        return_value=(pd.DataFrame(), {}),
-    ):
-        with pytest.raises(
-            RuntimeError,
-            match="Failed to retrieve devices data from both cache and API.",
+    def test_failure_to_retrieve_from_api(self):
+        with patch(
+            "airqo_etl_utils.datautils.DataUtils.load_cached_data",
+            return_value=pd.DataFrame(),
+        ), patch(
+            "airqo_etl_utils.datautils.DataUtils.fetch_devices_from_api",
+            return_value=(pd.DataFrame(), {}),
         ):
-            DataUtils.get_devices()
+            with pytest.raises(
+                RuntimeError,
+                match="Failed to retrieve devices data from both cache and API.",
+            ):
+                DataUtils.get_devices()
+
+
+class Test_BigQuery:
+    def test_extract_data_from_bigquery_success(
+        self,
+        mock_bigquery_api,
+        mock_data_utils,
+        mock_data_validation_utils,
+    ):
+        """Test successful data extraction from BigQuery."""
+
+        mock_bigquery_api.query_data.return_value = LC_RAW_DATA
+        mock_data_validation_utils.remove_outliers_fix_types.return_value = LC_RAW_DATA
+        mock_data_utils._get_table.return_value = ("test_table", None)
+
+        result = DataUtils.extract_data_from_bigquery(
+            datatype=DataType.RAW,
+            start_date_time="2025-05-01T00:00:00Z",
+            end_date_time="2025-05-02T00:00:00Z",
+            frequency=Frequency.RAW,
+            device_category=DeviceCategory.GENERAL,
+        )
+        assert result.equals(LC_RAW_DATA)
+        mock_bigquery_api.query_data.assert_called_once()
+        mock_data_validation_utils.remove_outliers_fix_types.assert_called_once_with(
+            LC_RAW_DATA
+        )
