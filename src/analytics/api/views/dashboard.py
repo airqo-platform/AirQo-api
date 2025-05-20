38e1fb65d51aa855cf5aba29ff151a33a3c2617f
NicholasTurner23 <mugtrade@gmail.com>
2025-05-20 22:37:18 +0300

    Clean up charts endpoints

--- src/analytics/api/views/dashboard.py
+++ src/analytics/api/views/dashboard.py
@@ -12,7 +12,17 @@ from api.models import (
     SiteModel,
     ExceedanceModel,
 )
-from api.utils.data_formatters import filter_non_private_sites_devices
+from constants import (
+    Frequency,
+    DataType,
+    DeviceCategory,
+    DeviceNetwork,
+)
+from api.utils.datautils import DataUtils
+from api.utils.data_formatters import (
+    filter_non_private_sites_devices,
+    get_validated_filter,
+)
 
 # Middlewares
 from api.utils.http import AirQoRequests
@@ -22,6 +32,10 @@ from api.utils.pollutants import (
     PM_COLOR_CATEGORY,
     set_pm25_category_background,
 )
+from api.models.dashboard.dashboard import dashboard_chart_data_model
+from api.models.dashboard.dashboarddatautils import DashboardDataUtils
+from schemas.dashboardchartdata import DashboardChartDataSchema
+from marshmallow import ValidationError
 from api.utils.request_validators import validate_request_json
 from main import rest_api_v2
 
@@ -33,21 +47,18 @@ logger = logging.getLogger(__name__)
 @rest_api_v2.route("/dashboard/chart/data")
 class ChartDataResource(Resource):
     @swag_from("/api/docs/dashboard/customised_chart_post.yml")
-    @validate_request_json(
-        "sites|required:list",
-        "startDate|required:datetime",
-        "endDate|required:datetime",
-        "frequency|required:str",
-        "pollutant|required:str",
-        "chartType|required:str",
-    )
+    @rest_api_v2.expect(dashboard_chart_data_model)
     def post(self):
-        network = request.args.get("network", "airqo")
+        try:
+            json_data = DashboardChartDataSchema().load(request.json)
+        except ValidationError as err:
+            return {"errors": err.messages}, 400
 
         json_data = request.get_json()
         sites = filter_non_private_sites_devices(
             filter_type="sites", filter_value=json_data.get("sites", {})
         ).get("data", [])
+        network = json_data.get("network", "airqo")
         start_date = json_data["startDate"]
         end_date = json_data["endDate"]
         frequency = json_data["frequency"]
@@ -138,102 +149,76 @@ class ChartDataResource(Resource):
 @rest_api_v2.route("/dashboard/chart/d3/data")
 class D3ChartDataResource(Resource):
     @swag_from("/api/docs/dashboard/d3_chart_data_post.yml")
-    @validate_request_json(
-        "sites|required:list",
-        "startDate|required:datetime",
-        "endDate|required:datetime",
-        "frequency|required:str",
-        "pollutant|required:str",
-        "chartType|required:str",
-    )
-    def _get_validated_filter(self, json_data):
-        """
-        Validates that exactly one of 'airqlouds', 'sites', or 'devices' is provided in the request,
-        and applies filtering if necessary.
-
-        Args:
-            json_data (dict): JSON payload from the request.
-
-        Returns:
-            tuple: The name of the filter ("sites", "devices", or "airqlouds") and its validated value if valid.
-
-        Raises:
-            ValueError: If more than one or none of the filters are provided.
-        """
-        error_message: str = ""
-        validated_data: List[str] = None
-
-        # TODO Lias with device registry to cleanup this makeshift implementation
-        devices = ["devices", "device_ids", "device_names"]
-        sites = ["sites", "site_names", "site_ids"]
-
-        valid_filters = [
-            "sites",
-            "site_names",
-            "site_ids",
-            "devices",
-            "device_ids",
-            "airqlouds",
-            "device_names",
-        ]
-        provided_filters = [key for key in valid_filters if json_data.get(key)]
-        if len(provided_filters) != 1:
-            raise ValueError(
-                "Specify exactly one of 'airqlouds', 'sites', 'device_names', or 'devices' in the request body."
-            )
-        filter_type = provided_filters[0]
-        filter_value = json_data.get(filter_type)
-
-        if filter_type in sites:
-            validated_value = filter_non_private_sites_devices(
-                filter_type, filter_value
-            )
-        elif filter_type in devices:
-            validated_value = filter_non_private_sites_devices(
-                filter_type, filter_value
-            )
-        else:
-            return filter_type, filter_value, None
-
-        if validated_value and validated_value.get("status") == "success":
-            validated_data = validated_value.get("data", [])
-        else:
-            error_message = validated_value.get("message", "Validation failed")
-
-        return filter_type, validated_data, error_message
-
+    @rest_api_v2.expect(dashboard_chart_data_model)
     def post(self):
-
+        try:
+            json_data = DashboardChartDataSchema().load(request.json)
+        except ValidationError as err:
+            return {"errors": err.messages}, 400
         json_data = request.get_json()
 
         try:
-            _, filter_value, error_message = self._get_validated_filter(json_data)
+            filter_type, filter_value, error_message = get_validated_filter(json_data)
             if error_message:
-                return error_message, AirQoRequests.Status.HTTP_400_BAD_REQUEST
+                return (
+                    AirQoRequests.create_response(error_message, success=False),
+                    AirQoRequests.Status.HTTP_400_BAD_REQUEST,
+                )
         except Exception as e:
             logger.exception(f"An error has occured; {e}")
 
+        network = json_data.get("network", "airqo")
         start_date = json_data["startDate"]
         end_date = json_data["endDate"]
         frequency = json_data["frequency"]
         pollutant = json_data["pollutant"]
         chart_type = json_data["chartType"]
+        device_category = json_data.get("device_category", None)
+        data_filter = {filter_type: filter_value}
+        frequency = Frequency[frequency.upper()]
+        try:
+            device_category = (
+                (DeviceCategory[device_category.upper()])
+                if device_category
+                else DeviceCategory.LOWCOST
+            )
+            datatype = DataType.CALIBRATED
+            data = DataUtils.extract_data_from_bigquery(
+                datatype,
+                start_date,
+                end_date,
+                frequency=frequency,
+                dynamic_query=True,
+                device_category=device_category,
+                columns=[
+                    pollutant,
+                ],
+                data_filter=data_filter,
+                use_cache=True,
+            )
 
-        events_model = EventsModel("airqo")
+            if not data.empty:
+                dashboardata = DashboardDataUtils()
+                data = dashboardata.processd3data(data)
 
-        data = events_model.get_d3_chart_events_v2(
-            filter_value, start_date, end_date, pollutant, frequency
-        )
+            if chart_type.lower() == "pie":
+                data = dashboardata.d3_generate_pie_chart_data(data, pollutant)
 
-        if chart_type.lower() == "pie":
-            data = d3_generate_pie_chart_data(data, pollutant)
-
-        return (
-            AirQoRequests.create_response(
-                "successfully retrieved d3 chart data", data=data
-            ),
-            AirQoRequests.Status.HTTP_200_OK,
-        )
+            return (
+                AirQoRequests.create_response(
+                    "successfully retrieved d3 chart data", data=data
+                ),
+                AirQoRequests.Status.HTTP_200_OK,
+            )
+        except Exception as e:
+            logger.exception(f"An error occurred: {e}")
+            return (
+                AirQoRequests.create_response(
+                    f"An Error occurred while processing your request. Please contact support.",
+                    success=False,
+                ),
+                AirQoRequests.Status.HTTP_500_INTERNAL_SERVER_ERROR,
+            )
 
 
 @rest_api_v2.route("/dashboard/sites")
