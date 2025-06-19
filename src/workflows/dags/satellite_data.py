13f62c45b1b229008e820b2feb13e0590c7c0fcb
NicholasTurner23 <mugtrade@gmail.com>
2025-06-19 22:55:19 +0300

    Add Copernicus and Nomads satelite pipelines

--- src/workflows/dags/satellite_data.py
+++ src/workflows/dags/satellite_data.py
@@ -5,6 +5,7 @@ import concurrent.futures
 from airqo_etl_utils.workflows_custom_utils import AirflowUtils
 from airqo_etl_utils.satellite_utils import SatelliteUtils
 from airqo_etl_utils.datautils import DataUtils
+from airqo_etl_utils.data_sources import DataSourcesApis
 from airqo_etl_utils.bigquery_api import BigQueryApi
 from airqo_etl_utils.constants import DataType, DeviceCategory, Frequency
 from airqo_etl_utils.config import configuration as Config
@@ -79,8 +80,8 @@ def copernicus_hourly_measurements():
         for variable, destination in data_to_read.items():
             dfs.append(SatelliteUtils.process_netcdf(destination, variable))
             files_to_delete.append(destination)
-        dfs1, dfs2 = dfs
-        merged_df = pd.merge(dfs1, dfs2, on=["time", "latitude", "longitude"])
+
+        merged_df = SatelliteUtils.clean_netcdf_data(dfs)
         if not merged_df.empty:
             delete_old_files(files_to_delete)
         return merged_df
@@ -102,5 +103,54 @@ def copernicus_hourly_measurements():
     store_data(cleaned)
 
 
+@dag(
+    "NOMADS-measurements",
+    schedule="0 0,12 * * *",
+    catchup=False,
+    tags=["daily", "raw", "satellite", "NOMADS"],
+    default_args=AirflowUtils.dag_default_configs(),
+)
+def NOMADS_daily_measurements():
+    import pandas as pd
+
+    @task(
+        provide_context=True,
+        retries=2,
+        retry_delay=timedelta(minutes=5),
+    )
+    def extract_data(**kwargs) -> pd.DataFrame:
+        data_source = DataSourcesApis()
+        file = data_source.nomads()
+        return file
+
+    @task(
+        provide_context=True,
+        retries=1,
+        retry_delay=timedelta(minutes=5),
+    )
+    def clean_data(file, **kwargs) -> pd.DataFrame:
+        clean_data = SatelliteUtils.process_nomads_data_files(file)
+        if not clean_data.empty:
+            delete_old_files([file])
+        return clean_data
+
+    @task(
+        provide_context=True,
+        retries=1,
+        retry_delay=timedelta(minutes=5),
+    )
+    def store_data(data, **kwargs) -> pd.DataFrame:
+        formated_data, table = DataUtils.format_data_for_bigquery(
+            data, DataType.RAW, DeviceCategory.SATELLITE, Frequency.RAW
+        )
+        big_query_api = BigQueryApi()
+        big_query_api.load_data(formated_data, table=table)
+
+    file = extract_data()
+    cleaned = clean_data(file)
+    store_data(cleaned)
+
+
 copernicus_hourly_measurements()
+NOMADS_daily_measurements()
 retrieve_satellite_data()
