956171fa6f90ee3bb00e82fc63f7e39f6efcb5be
NicholasTurner23 <mugtrade@gmail.com>
2025-07-16 12:16:50 +0300

    Clean up

--- src/analytics/manage.py
+++ src/analytics/manage.py
@@ -6,19 +6,17 @@ from flask import jsonify
 from decouple import config as env_config
 
 # Config
-from main import create_app, rest_api, rest_api_v2
+from main import create_app, rest_api
 from config import config
 
 # utils
 from api.utils.pre_request import PreRequest
 
-from api.models.base.data_processing import air_quality_data
-from api.models.base.diurnal_data_processing import air_quality_data_diurnal
+from api.models.base.data_processing import air_quality_data, air_quality_data_diurnal
 
 config_name = env_config("FLASK_ENV", "production")
 
 app = create_app(rest_api, config=config[config_name])
-
 
 # @app.before_request
 def check_tenant_param():
