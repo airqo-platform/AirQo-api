c16d4c249b0534adaacc19e4cbdd593d75c2b480
NicholasTurner23 <mr.nnturner@gmail.com>
2025-08-07 23:02:54 +0300

    Externalize view logic

--- src/analytics/api/views/v3/data.py
+++ src/analytics/api/views/v3/data.py
@@ -2,7 +2,8 @@ import flask_excel as excel
 from flasgger import swag_from
 from flask import request
 from flask_restx import Resource
-
+from typing import Dict, Tuple, List, Dict, Any, Union
+import pandas as pd
 
 from constants import Frequency, DataType, DeviceCategory, DeviceNetwork
 from api.utils.datautils import DataUtils
@@ -14,6 +15,8 @@ from api.utils.data_formatters import (
     get_validated_filter,
     format_to_aqcsv,
 )
+from api.views.common.responses import ResponseBuilder
+from api.views.common.data_ops import DownloadService
 from api.utils.exceptions import ExportRequestNotFound
 from api.utils.http import AirQoRequests
 from main import rest_api_v3
@@ -51,102 +54,36 @@ class DataExportResource(Resource):
             - JSON response with air quality data if downloadType is 'json'.
             - CSV file download response if downloadType is 'csv'.
         """
+        """
+        Handles the POST request to download custom air quality data.
+
+        Returns:
+            Tuple containing the response dictionary and HTTP status code.
+        """
         try:
             json_data = DataDownloadSchema().load(request.json)
         except ValidationError as err:
-            return {"errors": err.messages}, 400
+            return ResponseBuilder.error(err.messages, 400)
 
         try:
             filter_type, filter_value, error_message = get_validated_filter(json_data)
             if error_message:
-                return (
-                    AirQoRequests.create_response(error_message, success=False),
-                    AirQoRequests.Status.HTTP_400_BAD_REQUEST,
-                )
-        except Exception as e:
-            logger.exception(f"An error has occured; {e}")
+                return ResponseBuilder.error(error_message, 400)
 
-        startDateTime = json_data["startDateTime"]
-        endDateTime = json_data["endDateTime"]
-        download_type = (json_data.get("downloadType"),)
-        output_format = (json_data.get("outputFormat"),)
-        data_type = json_data.get("datatype")
-        device_category = json_data.get("device_category")
-        frequency = json_data.get("frequency")
-        pollutants = json_data.get("pollutants")
-        metadata_fields = json_data.get("metaDataFields", [])
-        weather_fields = json_data.get("weatherFields", [])
-        minimum_output = json_data.get("minimum", True)
-        postfix = "-" if output_format == "airqo-standard" else "-aqcsv-"
-        data_filter = {filter_type: filter_value}
-        datatype = DataType[data_type.upper()]
-        frequency = Frequency[frequency.upper()]
-        extra_columns = [*metadata_fields, *weather_fields]
-        try:
-            device_category = (
-                DeviceCategory[device_category.upper()]
-                if device_category
-                else DeviceCategory.LOWCOST
+            json_data.update({"dynamic": True})
+            data_frame = DownloadService.fetch_data(
+                json_data, filter_type, filter_value
             )
-            if data_filter:
-                data_frame = DataUtils.extract_data_from_bigquery(
-                    datatype=datatype,
-                    start_date_time=startDateTime,
-                    end_date_time=endDateTime,
-                    frequency=frequency,
-                    dynamic_query=True,
-                    device_category=device_category,
-                    main_columns=pollutants,
-                    data_filter=data_filter,
-                    extra_columns=extra_columns,
-                    use_cache=True,
-                )
-            else:
-                return (
-                    AirQoRequests.create_response("No data filter provided.", data=[]),
-                    AirQoRequests.Status.HTTP_400_BAD_REQUEST,
-                )
 
             if data_frame.empty:
-                return (
-                    AirQoRequests.create_response("No data found", data=[]),
-                    AirQoRequests.Status.HTTP_404_NOT_FOUND,
-                )
+                return ResponseBuilder.error("No data found", 400)
 
-            if minimum_output:
-                # Drop unnecessary columns
-                columns_to_drop = ["site_id"]
-                columns_to_drop.append("timestamp") if frequency.value in [
-                    "hourly",
-                    "daily",
-                ] else columns_to_drop
-                data_frame.drop(columns=columns_to_drop, inplace=True, errors="ignore")
+            return DownloadService.format_and_respond(json_data, data_frame)
 
-            records = data_frame.to_dict("records")
-
-            if output_format == "aqcsv":
-                records = format_to_aqcsv(
-                    data=records, frequency=frequency, pollutants=pollutants
-                )
-
-            if download_type == "json":
-                return (
-                    AirQoRequests.create_response(
-                        "air-quality data download successful", data=records
-                    ),
-                    AirQoRequests.Status.HTTP_200_OK,
-                )
-
-            return excel.make_response_from_records(
-                records, "csv", file_name=f"{frequency}-air-quality{postfix}data"
-            )
         except Exception as e:
-            logger.exception(f"An error occurred: {e}")
-            return (
-                AirQoRequests.create_response(
-                    f"An Error occurred while processing your request. Please contact support.",
-                    success=False,
-                ),
+            logger.exception("Unexpected error occurred during custom data download.")
+            return ResponseBuilder.error(
+                "An error occurred while processing your request. Please contact support.",
                 AirQoRequests.Status.HTTP_500_INTERNAL_SERVER_ERROR,
             )
 
@@ -159,49 +96,25 @@ class RawDataExportResource(Resource):
         try:
             json_data = RawDataSchema().load(request.json)
         except ValidationError as err:
-            return {"errors": err.messages}, 400
+            return ResponseBuilder.error(err.messages, 400)
 
-        network = json_data.get("network")
-        device_names = json_data.get("device_names")
-        device_category = json_data.get("device_category")
-        startDateTime = json_data.get("startDateTime")
-        endDateTime = json_data.get("endDateTime")
-        frequency = json_data.get("frequency")
-        frequency = Frequency[frequency.upper()]
-        device_category = DeviceCategory[device_category.upper()]
-        network = DeviceNetwork[network.upper()]
-        data_filter = {"device_id": device_names}
         try:
-            data_frame = DataUtils.extract_data_from_bigquery(
-                datatype=DataType.RAW,
-                start_date_time=startDateTime,
-                end_date_time=endDateTime,
-                frequency=frequency,
-                device_category=device_category,
-                device_network=network,
-                data_filter=data_filter,
-                use_cache=True,
-            )
+            filter_type, filter_value, error_message = get_validated_filter(json_data)
+            if error_message:
+                return ResponseBuilder.error(error_message, 400)
 
+            json_data.update({"data_type": "raw"})
+            data_frame = DownloadService.fetch_data(
+                json_data, filter_type, filter_value
+            )
             if data_frame.empty:
-                return (
-                    AirQoRequests.create_response("No data found", data=[]),
-                    AirQoRequests.Status.HTTP_404_NOT_FOUND,
-                )
+                return ResponseBuilder.error("No data found", 400)
 
-            records = data_frame.to_dict("records")
+            return DownloadService.format_and_respond(json_data, data_frame)
 
-            return excel.make_response_from_records(
-                records,
-                "csv",
-                file_name=f"{frequency.value}-air-quality-data-{startDateTime}",
-            )
-        except Exception as ex:
-            logger.exception(f"An error occurred: {ex}")
-            return (
-                AirQoRequests.create_response(
-                    f"An Error occurred while processing your request. Please contact support. {ex}",
-                    success=False,
-                ),
+        except Exception as e:
+            logger.exception("Unexpected error occurred during custom data download.")
+            return ResponseBuilder.error(
+                "An error occurred while processing your request. Please contact support.",
                 AirQoRequests.Status.HTTP_500_INTERNAL_SERVER_ERROR,
             )
