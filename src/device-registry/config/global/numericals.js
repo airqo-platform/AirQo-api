c9dcd318d2b80bbbe99f78d2a2462d6da7bfa71d
baalmart <bbaalmart@gmail.com>
2025-10-18 02:24:19 +0300

    prevent circular dependency

--- src/device-registry/config/global/numericals.js
+++ src/device-registry/config/global/numericals.js
@@ -5,8 +5,6 @@ const isEmpty = require("is-empty");
 const { logObject, logText } = require("@utils/shared");
 const logger = log4js.getLogger(`${this.ENVIRONMENT} -- constants-config`);
 
-const MAX_EVENT_AGE_HOURS = 6;
-
 const numericals = {
   DEFAULT_NEAREST_SITE_RADIUS: 15,
   MQTT_BRIDGE_PORT: 8883,
@@ -15,9 +13,10 @@ const numericals = {
   MINIMUM_BACKOFF_TIME: 1,
   MAXIMUM_BACKOFF_TIME: 32,
   CACHE_TIMEOUT_PERIOD: 10000,
-  MAX_EVENT_AGE_HOURS: MAX_EVENT_AGE_HOURS, // Only accept events from last 6 hours
-  MAX_EVENT_AGE_MS: MAX_EVENT_AGE_HOURS * 60 * 60 * 1000, // Calculated from constant
+  MAX_EVENT_AGE_HOURS: 6, // Only accept events from last 6 hours
   MAX_REJECTED_LOGS: 10,
 };
+
+numericals.MAX_EVENT_AGE_MS = numericals.MAX_EVENT_AGE_HOURS * 60 * 60 * 1000;
 
 module.exports = numericals;
